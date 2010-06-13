e819419dce42fcc23188f6d1bf2bbf90414e78e7
Lex <blank>
2010-06-13 11:07:55 +0200

    Introduced 2 new string functions: string.findPattern and string.genNocasePattern. Fixed @see references.

--- src/mudlet-lua/lua/StringUtils.lua
+++ src/mudlet-lua/lua/StringUtils.lua
@@ -22,7 +22,7 @@ end
 
 --- Test if string is ending with specified suffix.
 ---
---- @see starts
+--- @see string.starts
 function string.ends(String, Suffix)
 	return Suffix=='' or string.sub(String,-string.len(Suffix))==Suffix
 end
@@ -63,7 +63,7 @@ end
 
 --- Test if string is starting with specified prefix.
 ---
---- @see ends
+--- @see string.ends
 function string.starts(String, Prefix)
 	return string.sub(String,1,string.len(Prefix))==Prefix
 end
@@ -84,3 +84,54 @@ function string:title()
 	self = self:gsub("^%l", string.upper, 1)
 	return self
 end
+
+
+--- Generate case insensitive search pattern from string.
+--- 
+--- @release post Mudlet 1.1.1 (TODO update before release)
+---
+--- @return case insensitive pattern string 
+---
+--- @usage Following example will generate and print "123[aA][bB][cC]" string.
+---   <pre>
+---   echo(string.genNocasePattern("123abc"))
+---   </pre>
+function string.genNocasePattern(s)
+	s = string.gsub(s, "%a", 
+		function (c)
+			return string.format("[%s%s]", string.lower(c), string.upper(c))
+		end)
+	return s
+end
+
+
+--- Return first matching substring or nil.
+--- 
+--- @release post Mudlet 1.1.1 (TODO update before release)
+---
+--- @return nil or first matching substring
+---
+--- @usage Following example will print: "I did find: Troll" string.
+---   <pre>
+---   local match = string.findPattern("Troll is here!", "Troll")
+---   if match then
+---      echo("I did find: " .. match)
+---   end
+---   </pre>
+--- @usage This example will find substring regardless of case.
+---   <pre>
+---   local match = string.findPattern("Troll is here!", string.genNocasePattern("troll"))
+---   if match then
+---      echo("I did find: " .. match)
+---   end
+---   </pre>
+---
+--- @see string.genNocasePattern
+function string.findPattern(text, pattern)
+	if string.find(text, pattern, 1) then
+		return string.sub(text, string.find(text, pattern, 1))
+	else
+		return nil
+	end
+end
+
