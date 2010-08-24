7b20d9644bf34b720d7480052686c8582259d785
Benjamin Smith <sodalane.ristu@gmail.com>
2010-08-24 19:00:06 -0400

    Loading change

--- lua/LuaGlobal.lua
+++ lua/LuaGlobal.lua
@@ -8,32 +8,29 @@ if package.loaded["lpeg"] then lpeg = require "lpeg" end
 -- TODO this is required by DB.lua, so we might load it all at one place
 --if package.loaded["luasql.sqlite3"] then require "luasql.sqlite3" end
 
-local PATH_SEP = string.char(getMudletHomeDir():byte()) == "/" and "/" or "\\"
-local LUA_DIR = string.format(getMudletHomeDir().."%s..%s..%smudlet-lua%slua%s", PATH_SEP, PATH_SEP, PATH_SEP, PATH_SEP, PATH_SEP)
-
 local packages = {
 	"StringUtils.lua",
 	"TableUtils.lua",
 	"Logging.lua",
 	"DebugTools.lua",
 	"DB.lua",
-	"geyser"..PATH_SEP.."Geyser.lua",
-	"geyser"..PATH_SEP.."GeyserGeyser.lua",
-	"geyser"..PATH_SEP.."GeyserUtil.lua",
-	"geyser"..PATH_SEP.."GeyserColor.lua",
-	"geyser"..PATH_SEP.."GeyserSetConstraints.lua",
-	"geyser"..PATH_SEP.."GeyserContainer.lua",
-	"geyser"..PATH_SEP.."GeyserWindow.lua",
-	"geyser"..PATH_SEP.."GeyserLabel.lua",
-	"geyser"..PATH_SEP.."GeyserGauge.lua",
-	"geyser"..PATH_SEP.."GeyserMiniConsole.lua",
-	"geyser"..PATH_SEP.."GeyserReposition.lua",
+	"geyser/Geyser.lua",
+	"geyser/GeyserGeyser.lua",
+	"geyser/GeyserUtil.lua",
+	"geyser/GeyserColor.lua",
+	"geyser/GeyserSetConstraints.lua",
+	"geyser/GeyserContainer.lua",
+	"geyser/GeyserWindow.lua",
+	"geyser/GeyserLabel.lua",
+	"geyser/GeyserGauge.lua",
+	"geyser/GeyserMiniConsole.lua",
+	"geyser/GeyserReposition.lua",
 	-- TODO probably don't need to load this file
-	"geyser"..PATH_SEP.."GeyserTests.lua",
+	"geyser/GeyserTests.lua",
 	"GUIUtils.lua",
 	"Other.lua"
 	}
 
 for _, package in ipairs(packages) do
-	local result = pcall(dofile, LUA_DIR .. package) or echo("Error attempting to load file: " .. package .. "\n")
+	local result = pcall(dofile, "./mudlet-lua/lua/" .. package) or echo("Error attempting to load file: " .. package .. "\n")
 end
