fe7efd7983c9d9a3f9292b9276ee043a1f87766a
Karsten Bock <KarstenBock@gmx.net>
2011-04-29 15:54:20 +0200

    Various fixes to/for HBox and VBox

--- lua/geyser/GeyserHBox.lua
+++ lua/geyser/GeyserHBox.lua
@@ -2,49 +2,42 @@ Geyser.HBox = Geyser.Container:new({
       name = "HBoxClass"
    })
 
+function Geyser.HBox:begin_update()
+   self.defer_updates = true
+end
+   
 function Geyser.HBox:add (window, cons)
    Geyser.add(self, window, cons)
+   if not self.defer_updates then
+      self:reposition()
+   end
+end
+   
+function Geyser.HBox:end_update()
+   self.defer_updates = false
    self:reposition()
 end
-   
-function Geyser.HBox:calculate_dynamic_window_width()
-   local total_count = #self.windows
-   local fixed_count = 0
-   local fixed_width_sum  = 0
-   if total_count <= 1 then
-      --If there is only one window it can have all the width, if there are none then it doesn't matter
-      return self:get_width()
-   end
-   for _, window_name in ipairs(self.windows) do
-      window = self.windowList[window_name]
-      if window.horizontal_policy == Geyser.Fixed then
-         fixed_count = fixed_count + 1
-         fixed_width_sum = fixed_width_sum + window:get_width()
-      end
-   end
-   return (self:get_width() - fixed_width_sum) / (total_count - fixed_count)
-end
-   
+  
 --- Responsible for placing/moving/resizing this window to the correct place/size.
 -- Called on window resize events.
 function Geyser.HBox:reposition()
-   local window_width = self:calculate_dynamic_window_width()
-   start_x = 0
+   self.parent:reposition()
+   local window_width = self:calculate_dynamic_window_size().width
+   local start_x = 0
    for _, window_name in ipairs(self.windows) do
-      window = self.windowList[window_name]
-      local width = nil
-      local height = nil
+      local window = self.windowList[window_name]
+      local width = window.get_width()
+      local height = window.get_height()
       window:move(start_x, nil)
-      if window.horizontal_policy == Geyser.Dynamic then
-         width = window_width
+      if window.h_policy == Geyser.Dynamic then
+         width = window_width * window.h_stretch_factor
       end
-      if window.vertical_policy == Geyser.Dynamic then
-         height = self.get_height()
+      if window.v_policy == Geyser.Dynamic then
+         height = self:get_height()
       end
       window:resize(width, height)
       start_x = start_x + window:get_width()
    end
-   self.parent:reposition()
 end
 
 Geyser.HBox.parent = Geyser.Container
@@ -57,7 +50,7 @@ function Geyser.HBox:new(cons, container)
 -- Call parent's constructor
    local me = self.parent:new(cons, container)
    setmetatable(me, self)
-   self.__index = self
-   
+   self.__index = self   
+   me:reposition()
    return me
 end
