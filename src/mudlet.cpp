db376867d7c14ac3e10a6ce0f82c8a90354ee9ff
Heiko Koehn <koehnheiko@googlemail.com>
2012-12-31 04:04:35 +0100

    misc

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -98,13 +98,13 @@ mudlet::mudlet()
 , mpMusicBox3(Phonon::createPlayer(Phonon::MusicCategory) )
 , mpMusicBox4(Phonon::createPlayer(Phonon::MusicCategory) )
 #ifdef Q_OS_LINUX
-    , version( "Mudlet 2.0 12-30-2012" )
+    , version( "Mudlet 2.0" )
 #endif
 #ifdef Q_OS_MAC
-    , version( "Mudlet 2.0 12-30-2012" )
+    , version( "Mudlet 2.0" )
 #endif
 #ifdef Q_OS_WIN
-    , version( "Mudlet 2.0 12-30-2012" )
+    , version( "Mudlet 2.0" )
 #endif
 {
     setupUi(this);
