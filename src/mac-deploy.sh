61ae050daa418e39beb3b104234198a78e3f10df
Vadim Peretokin <vperetokin@gmail.com>
2013-12-08 07:52:35 +1000

    Don't generate a .dmg with macdeploy
    Since node-appdmg now does this for us, + makes the dmg look way nicer.

--- src/mac-deploy.sh
+++ src/mac-deploy.sh
@@ -10,5 +10,6 @@ export PATH=$PATH:$HOME/Qt/5.1.1/clang_64/bin
 echo Using: $PATH to find macdeployqt
 
 # Do not use ./mudlet.app here, but just muldlet.app - OSX will mount is wrong otherwise
-sudo macdeployqt Mudlet.app -dmg
+# don't generate the .dmg, just the app - node-appdmg handles nice icons and the dmg
+sudo macdeployqt Mudlet.app
 echo Done!
