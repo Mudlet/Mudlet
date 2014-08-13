1ba9870561c806a5f66d78e95c469d7d28b7bd7b
Ahmed Charles <ahmedcharles@gmail.com>
2014-08-13 00:23:01 -0700

    Update travis scripts.
    Makes their output better.
    Installs the right things on OSX.

--- CI/travis.linux.install.sh
+++ CI/travis.linux.install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-sudo apt-get install build-essential qtbase5-dev qt5-default qtmultimedia5-dev qttools5-dev libhunspell-dev lua5.1 liblua5.1-0-dev libpcre3-dev libboost-dev zlib1g-dbg zlib1g-dev libzip-dev libpulse-dev cmake
-cd yajl
+set -ev
+sudo apt-get install \
+  build-essential \
+  qt5-default qtmultimedia5-dev qttools5-dev \
+  libhunspell-dev \
+  lua5.1 liblua5.1-0-dev \
+  libpcre3-dev \
+  libboost-dev \
+  zlib1g-dbg zlib1g-dev \
+  libzip-dev \
+  libpulse-dev \
+  cmake
+pushd $HOME/yajl
 ./configure
 sudo make install
-cd ..
-
+popd
--- CI/travis.osx.install.sh
+++ CI/travis.osx.install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cd yajl
-./configure
-sudo make install
-cd ..
-
+set -ev
+BREWS="boost cmake hunspell libzip libzzip lua pcre pkg-config qt5 yajl"
+for i in $BREWS; do
+  brew outdated | grep -q $i && brew upgrade $i
+done
+for i in $BREWS; do
+  brew list | grep -q $i || brew install $i
+done
