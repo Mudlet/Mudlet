71f9bc47d2210a51c89fa5897b982de9c31a7db9
Vadim Peretokin <vperetokin@gmail.com>
2014-10-07 06:51:18 +1000

    Update Travis to use GCC 4.7 Per http://www.swiftsoftwaregroup.com/upgrade-gcc-4-7-ubuntu-12-04/

--- CI/travis.linux.before_install.sh
+++ CI/travis.linux.before_install.sh
@@ -2,7 +2,13 @@
 set -ev
 sudo apt-add-repository ppa:ubuntu-sdk-team/ppa -y
 sudo apt-add-repository ppa:kalakris/cmake -y
+# newer GCC version, 4.7
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
+sudo apt-get install gcc-4.7 g++-4.7
+sudo update-alternatives --remove gcc /usr/bin/gcc-4.6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
 pushd $HOME
 git clone https://github.com/lloyd/yajl.git
 popd
