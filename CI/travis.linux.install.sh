e4dd3690bba707c6a311519faa46cf766bce522c
Ahmed Charles <acharles@outlook.com>
2014-10-06 18:50:16 -0700

    Fix gcc 4.7 and add a newer boost.

--- CI/travis.linux.install.sh
+++ CI/travis.linux.install.sh
@@ -6,11 +6,16 @@ sudo apt-get install \
   libhunspell-dev \
   lua5.1 liblua5.1-0-dev \
   libpcre3-dev \
-  libboost-dev \
+  libboost1.55-dev\
   zlib1g-dbg zlib1g-dev \
   libzip-dev \
   libpulse-dev \
-  cmake
+  cmake \
+  gcc-4.7 \
+  g++-4.7
+sudo update-alternatives --remove gcc /usr/bin/gcc-4.6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
 pushd $HOME/yajl
 ./configure
 sudo make install
