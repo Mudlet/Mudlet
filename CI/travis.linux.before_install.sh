7955397280a2a883a95b9dc9163cb018296bf4fe
Stephen Lyons <slysven@virginmedia.com>
2016-06-24 01:22:09 +0100

    BrokenCIFixAttempt: for Travis C.I. testing platform
    * The Ubuntu team have moved the very early Qt 5.x (5.0.2) that is used
      for Linux platform builds out of the ppa:repository that we use - add in
      the extra repository in the those already manually added in the Linux
      before install step...
    
    * use package config within CMake to locate Hunspell libraries as
      they are not being found any longer using our FindHUNSPELL,cmake helper.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- CI/travis.linux.before_install.sh
+++ CI/travis.linux.before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ev
 sudo apt-add-repository ppa:ubuntu-sdk-team/ppa -y
+# The ubuntu team have relocated the old qt 5.0.2 (and other things) from the above to:
+sudo apt-add-repository ppa:canonical-qt5-edgers/ubuntu1204-qt5 -y
 sudo apt-add-repository ppa:kalakris/cmake -y
 # newer GCC version, 4.7
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
