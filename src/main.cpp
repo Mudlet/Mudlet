fa7d1722afcf540014fe6d5becfca32bb2760f84
Vadim Peretokin <vperetokin@gmail.com>
2016-09-16 11:11:11 +1000

    Updates for Qt 5.6.1 on Windows
    Qt changed things in regards to OpenGL and GLUT, see http://doc.qt.io/
    qt-5/windows-requirements.html#graphics-drivers. This commit makes it
    build.

--- src/main.cpp
+++ src/main.cpp
@@ -109,8 +109,11 @@ QCoreApplication * createApplication(int &argc, char *argv[], unsigned int &acti
 
     if( (action) & ( 1 | 2) )
         return new QCoreApplication(argc, argv);  // Ah, we're gonna bail out early, just need a command-line application
-    else
+    else {
+        // Force OpenGL use as we use some functions that aren't provided by Qt's OpenGL layer on Windows (QOpenGLFunctions)
+        QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
         return new QApplication(argc, argv); // Normal course of events - (GUI), so: game on!
+    }
 }
 
 int main(int argc, char *argv[])
