30d9f31de8374bb18fd45ba0c0eb53af9f06d916
dicene <ieadkins@gmail.com>
2016-12-30 01:17:51 -0500

    Copyright line pt 2

--- src/TTextEdit.h
+++ src/TTextEdit.h
@@ -4,6 +4,7 @@
 /***************************************************************************
  *   Copyright (C) 2008-2011 by Heiko Koehn - KoehnHeiko@googlemail.com    *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
+ *   Copyright (C) 2016 by Ian Adkins - ieadkins@gmail.com                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
