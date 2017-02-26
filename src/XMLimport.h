0127cab784932ba75f22f7d8f696d6078b282381
Stephen Lyons <slysven@virginmedia.com>
2017-02-26 23:54:03 +0000

    Enhance: apply clang-formatting to XMLimport class
    Following inter-developer discussion it was decided to apply the same
    clang-format code formatting to the XMLimport class in this branch as
    for the development branch - as this will increase the complexity of the
    changes I felt there was little need to limit the scope of changes in this
    branch to the minimum to provide the functional changes so have increased
    the change-set to include some redundant code elimination steps already
    done in the other branch commits to reduce the overall differences between
    the changed files in the two branches.
    
    I have also added the .clang-format file to the QMake project file so that
    it shows up in the Qt Creator IDE - however the file is in the base
    directory of the project in this branch's code whereas it is in the ./src/
    sub-directory in the development branch at some point one branch should
    be revised to use the same location for the file as the other one.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/XMLimport.h
+++ src/XMLimport.h
@@ -4,7 +4,7 @@
 /***************************************************************************
  *   Copyright (C) 2008-2012 by Heiko Koehn - KoehnHeiko@googlemail.com    *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
- *   Copyright (C) 2016 by Stephen Lyons - slysven@virginmedia.com         *
+ *   Copyright (C) 2016-2017 by Stephen Lyons - slysven@virginmedia.com    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -22,13 +22,16 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-
+// clang-format: off
 #include "pre_guard.h"
+// clang-format: on
 #include <QApplication>
 #include <QMap>
 #include <QMultiHash>
 #include <QXmlStreamReader>
+// clang-format: off
 #include "post_guard.h"
+// clang-format: on
 
 class Host;
 class TAction;
@@ -45,78 +48,72 @@ class XMLimport : public QXmlStreamReader
     Q_DECLARE_TR_FUNCTIONS(XMLimport) // Needed so we can use tr() even though XMLimport is NOT derived from QObject
 
 public:
-              XMLimport( Host * );
+    XMLimport(Host*);
 
-    bool      importPackage( QIODevice *device, QString packageName = QString(), int moduleFlag = 0 );
+    bool importPackage(QIODevice* device, QString packageName = QString(), int moduleFlag = 0);
 
 private:
+    void readPackage();
+    void readUnknownPackage();
 
-    void      readPackage();
-    void      readUnknownPackage();
+    void readHostPackage();
+    void readTriggerPackage();
+    void readTimerPackage();
+    void readAliasPackage();
+    void readActionPackage();
+    void readScriptPackage();
+    void readKeyPackage();
+    void readVariablePackage();
+    void readUnknownMapElement();
+    void readMap();
+    void readRoom(QMultiHash<int, int>&, unsigned int*);
+    void readRooms(QMultiHash<int, int>&);
+    void readEnvColor();
+    void readEnvColors();
+    void readArea();
+    void readAreas();
+    void readHelpPackage();
 
-    void      readHostPackage();
-    void      readTriggerPackage();
-    void      readTimerPackage();
-    void      readAliasPackage();
-    void      readActionPackage();
-    void      readScriptPackage();
-    void      readKeyPackage();
-    void      readVariablePackage();
-    void      readUnknownMapElement();
-    void      readMap();
-    void      readRoom( QMultiHash<int, int> &, unsigned int * );
-    void      readRooms( QMultiHash<int, int> & );
-    void      readEnvColor();
-    void      readEnvColors();
-    void      readArea();
-    void      readAreas();
-    void      readHelpPackage();
+    void readUnknownHostElement();
+    void readUnknownTriggerElement();
+    void readUnknownTimerElement();
+    void readUnknownAliasElement();
+    void readUnknownActionElement();
+    void readUnknownScriptElement();
+    void readUnknownKeyElement();
 
-    void      readUnknownHostElement();
+    void readHostPackage(Host*);
+    void readTriggerGroup(TTrigger*);
+    void readTimerGroup(TTimer*);
+    void readAliasGroup(TAlias*);
+    void readActionGroup(TAction*);
+    void readScriptGroup(TScript*);
+    void readKeyGroup(TKey*);
+    void readVariableGroup(TVar*);
+    void readHiddenVariables();
 
+    void readStringList(QStringList&);
+    void readIntegerList(QList<int>&, const QString&);
+    void readModulesDetailsMap(QMap<QString, QStringList>&);
 
-    void      readUnknownTriggerElement();
-    void      readUnknownTimerElement();
-    void      readUnknownAliasElement();
-    void      readUnknownActionElement();
-    void      readUnknownScriptElement();
-    void      readUnknownKeyElement();
-
-    void      readHostPackage( Host * );
-    void      readTriggerGroup( TTrigger * pParent );
-    void      readTimerGroup( TTimer * pParent );
-    void      readAliasGroup( TAlias * pParent );
-    void      readActionGroup( TAction * pParent );
-    void      readScriptGroup( TScript * pParent );
-    void      readKeyGroup( TKey * pParent );
-    void      readVariableGroup( TVar * pParent );
-    void      readHiddenVariables();
-
-
-    void      readStringList( QStringList & );
-    void      readIntegerList( QList<int> &, const QString &  );
-    void      readModulesDetailsMap( QMap<QString, QStringList> & );
-
-    Host *    mpHost;
-    QString   mPackageName;
-
-    TTrigger * mpTrigger;
-    TTimer * mpTimer;
-    TAlias * mpAlias;
-    TKey *   mpKey;
-    TAction * mpAction;
-    TScript * mpScript;
-    TVar *    mpVar;
-
+    Host* mpHost;
+    QString mPackageName;
+    TTrigger* mpTrigger;
+    TTimer* mpTimer;
+    TAlias* mpAlias;
+    TKey* mpKey;
+    TAction* mpAction;
+    TScript* mpScript;
+    TVar* mpVar;
     bool gotTrigger;
     bool gotTimer;
     bool gotAlias;
     bool gotKey;
     bool gotAction;
     bool gotScript;
-    int  module;
-    int         mMaxRoomId;
-    int         mMaxAreaId; // Could be useful when iterating through map data
+    int module;
+    int mMaxRoomId;
+    int mMaxAreaId; // Could be useful when iterating through map data
 };
 
 #endif // MUDLET_XMLEXPORT_H
