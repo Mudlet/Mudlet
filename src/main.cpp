ebb3adac2f651dd6c6041cbf27184e5bbf1da2b7
Vadim Peretokin <vperetokin@gmail.com>
2017-03-01 19:18:04 +0100

    (development) Updated general about info & credits (#387)
    * Updated credits in the About dialog.
    
    Also taken Heiko off the copyright until 2017, which he has been the original author he hasn't been involved with the project for several years now.

--- src/main.cpp
+++ src/main.cpp
@@ -194,7 +194,7 @@ int main(int argc, char *argv[])
             // Do "version" action - wording and format is quite tightly specified by the coding standards
             std::cout << APP_TARGET << " " << APP_VERSION << APP_BUILD << std::endl;
             std::cout << "Qt libraries " << QT_VERSION_STR << "(compilation) " << qVersion() << "(runtime)" << std::endl;
-            std::cout << "Copyright (C) 2008-" << std::string(__DATE__).substr(7, 4) << " Heiko Koehn." << std::endl;
+            std::cout << "Copyright (C) 2008-" << std::string(__DATE__).substr(7, 4) << " Mudlet devs." << std::endl;
             std::cout << "Licence GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>" << std::endl;
             std::cout << "This is free software: you are free to change and redistribute it." << std::endl;
             std::cout << "There is NO WARRANTY, to the extent permitted by law." << std::endl;
@@ -260,8 +260,8 @@ int main(int argc, char *argv[])
             std::cout << "                       and will make the application wait until a debugger"  << std::endl;
             std::cout << "                       connects to it." << std::endl;
             std::cout << std::endl;
-            std::cout << "Report bugs to: <http://launchpad.mudlet.org/>" << std::endl;
-            std::cout << "pkg home page: <http://mudlet.sourceforge.net/>, also see <http://www.mudlet.org/>"<< std::endl;
+            std::cout << "Report bugs to: <https://bugs.launchpad.net/mudlet/+filebug>" << std::endl;
+            std::cout << "pkg home page: <http://www.mudlet.org/>" << std::endl;
         }
         return 0;
     }
@@ -318,7 +318,7 @@ int main(int argc, char *argv[])
         }
 
         // Repeat for other text, but we know it will fit at given size
-        QString sourceCopyrightText = QChar( 169 ) % QString( " Heiko K" ) % QChar( 246 ) % QString( "hn 2008-" ) % QString(__DATE__).mid(7);
+        QString sourceCopyrightText = QChar( 169 ) % QString( " Mudlet makers 2008-" ) % QString(__DATE__).mid(7);
         QFont font( "DejaVu Serif", 16, QFont::Bold|QFont::Serif|QFont::PreferMatch|QFont::PreferAntialias );
         QTextLayout copyrightTextLayout( sourceCopyrightText, font, painter.device() );
         copyrightTextLayout.beginLayout();
