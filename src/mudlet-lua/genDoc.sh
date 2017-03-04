237b9d14b9a33b0e2de625f523e14727b7e73684
Vadim Peretokin <vperetokin@gmail.com>
2017-03-04 13:28:37 +0100

    Updated generation script

--- src/mudlet-lua/genDoc.sh
+++ src/mudlet-lua/genDoc.sh
@@ -5,11 +5,11 @@ find . -name "*.html" -type f
 
 # generate documentation
 cd ../lua/
-ldoc --style `pwd` --project Geyser --not_luadoc --dir ../mudlet-lua-doc geyser/
+ldoc --style `pwd` --project Geyser --not_luadoc --dir ../mudlet-lua-doc/files geyser/
 
 # maintain compatibility with old luadoc-generated links. 
 # luadoc created geyser/, ldoc makes modules/
-cd ../mudlet-lua-doc
+cd ../mudlet-lua-doc/files
 find . -type f -exec sed -i 's@modules/@geyser/@g' {} +
+rm -rf geyser/
 mv modules geyser
-
