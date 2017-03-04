bdf3f7c16263d67d280d8c8e54b8d9d0400f268f
Vadim Peretokin <vperetokin@gmail.com>
2017-03-04 13:36:17 +0100

    Update to latest LuaGlobal from release_30 branch

--- src/mudlet-lua/lua/LuaGlobal.lua
+++ src/mudlet-lua/lua/LuaGlobal.lua
@@ -118,20 +118,39 @@ local packages = {
 	"GMCP.lua",
 }
 
-
 -- on windows LuaGlobal gets loaded with the current directory set to mudlet.exe's location
--- on Mac, it's set to LuaGlobals location. So work out where to load Lua files from
-local prefix = "./mudlet-lua/lua/"
-if not lfs.attributes(prefix) then
-        prefix = "../Resources/mudlet-lua/lua/"
+-- on Mac, it's set to LuaGlobals location - or the Applications folder, or something else...
+-- So work out where to load Lua files from using some heuristics
+-- Addition of "../src/" to front of first path allows things to work when "Shadow Building"
+-- option of Qt Creator is used (separates object code from source code in directories
+-- beside the latter, allowing parallel builds against different Qt Library versions
+-- or {Release|Debug} types).
+-- TODO: extend to support common Lua code being placed in system shared directory
+-- tree as ought to happen for *nix install builds.
+local prefixes = {"../src/mudlet-lua/lua/", "../Resources/mudlet-lua/lua/",
+    "mudlet.app/Contents/Resources/mudlet-lua/lua/", "mudlet-lua/lua"}
+
+local prefix
+for i = 1, #prefixes do
+    if lfs.attributes(prefixes[i]) then
+        prefix = prefixes[i]
+        break
+    end
 end
-if not lfs.attributes(prefix) then
-  echo("Error locating lua files from LuaGlobal - we're looking from "..lfs.currentdir()..".\n")
-  return
+
+-- For some reason on windows, mudlet-lua/lua/ does not register as a directory with the above strategy.
+-- Thus, if chosen we need to append a slash.
+if prefix == "mudlet-lua/lua" then
+	prefix = prefix .. "/"
+end
+
+if not prefix then
+        echo("Error locating Lua files from LuaGlobal - we're looking from '"..lfs.currentdir().."'.\n")
+	return
 end
 
 for _, package in ipairs(packages) do
-	local result, msg = pcall(dofile, prefix .. package)
+        local result, msg = pcall(dofile, prefix .. package)
 	if not result then echo("Error attempting to load file: " .. package .. ": "..msg.."\n") end
 end
 
