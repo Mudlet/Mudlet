35e56c6f5ef62dd68c5522d52cd7fd54651a5642
Vadim Peretokin <vperetokin@gmail.com>
2017-03-04 12:48:50 +0100

    Improved docs generation to work and align with old luadoc style

--- src/mudlet-lua/genDoc.sh
+++ src/mudlet-lua/genDoc.sh
@@ -1,4 +1,15 @@
 
-cd lua
-ldoc --dir ../mudlet-lua-doc --package geyser/ geyser/
+# wipe all old html files
+cd mudlet-lua-doc/
+find . -name "*.html" -type f
+
+# generate documentation
+cd ../lua/
+ldoc --style `pwd` --project Geyser --not_luadoc --dir ../mudlet-lua-doc geyser/
+
+# maintain compatibility with old luadoc-generated links. 
+# luadoc created geyser/, ldoc makes modules/
+cd ../mudlet-lua-doc
+find . -type f -exec sed -i 's@modules/@geyser/@g' {} +
+mv modules geyser
 
