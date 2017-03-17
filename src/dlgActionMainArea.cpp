aafd654aaec6a7e69d60dc1837f8e56f3ac54dd2
Stephen Lyons <slysven@virginmedia.com>
2017-03-17 05:22:08 +0000

    BugFix: add Qt version conditional code for feature not supported in Qt<5.2
    The current Qt Designer plugin in the Qt IDE is happy to enable features
    that are not present in old Qt 5.x versions and if then asked to switch
    them off does not remove them from the XML format .ui files it generates.
    
    This commit fixes this problem that breaks compilation on Qt 5.x less than
    5.2 for the clear "⌫" icon that can be used on QLineEdits to clear the
    contents in one click in later versions. I had enabled this on the
    "Command"/"Command (Down)" and "Command (Up)" entries on the Editor dialog
    that handles Button/Menu features (some types of instances of the TAction
    class).
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/dlgActionMainArea.cpp
+++ src/dlgActionMainArea.cpp
@@ -1,6 +1,7 @@
 /***************************************************************************
  *   Copyright (C) 2008-2009 by Heiko Koehn - KoehnHeiko@googlemail.com    *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
+ *   Copyright (C) 2017 by Stephen Lyons - slysven@virginmedia.com         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -26,4 +27,20 @@ dlgActionMainArea::dlgActionMainArea(QWidget * pF) : QWidget(pF)
 {
     // init generated dialog
     setupUi(this);
+
+    // I had previously enabled these in the actions_main_area.ui file but when
+    // that file is written out by a recent QtDesigner (or the one built into
+    // QtCreator) it inserts an XML entry for each that is not understood by
+    // older versions of the Qt Libraries and causes compilation to fail even
+    // if the option is THEN switched off in the same QtDesigner/QtCreator. This
+    // is because it does NOT then remove the entry from the file and just
+    // leaves it as "false" which cannot be understood by the older versions.
+    // To correct it one has to edit the .ui file with a text editor to take the
+    // entry out manually.
+    // The only safe way to use the feature is to do a conditional compile of
+    // the relevant item:
+#if QT_VERSION >= 0x050200
+    lineEdit_action_command_up->setClearButtonEnabled( true );
+    lineEdit_action_command_down->setClearButtonEnabled( true );
+#endif
 }
