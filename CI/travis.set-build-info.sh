a7fff03112a3b5e072fbc6674dae9d10c60819c6
keneanung <keneanung@googlemail.com>
2017-03-19 21:24:33 +0100

    Show built version at the end of the build script

--- CI/travis.set-build-info.sh
+++ CI/travis.set-build-info.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# fail on error
-set -e
-
 BUILD=""
 
 if [ -z "${TRAVIS_TAG}" ]; then
@@ -16,8 +13,15 @@ if [ -z "${TRAVIS_TAG}" ]; then
   fi
 fi
 
+VERSION=""
+
 if [ "${Q_OR_C_MAKE}" = "cmake" ]; then
+  VERSION=$(perl -lne 'print $1 if /^SET\(APP_VERSION (.+)\)/' < ${TRAVIS_BUILD_DIR}/CMakeLists.txt)
   perl -pi -e "s/SET\(APP_BUILD \"-dev\"\)/SET(APP_BUILD \"${BUILD}\")/" ${TRAVIS_BUILD_DIR}/CMakeLists.txt
 elif [ "${Q_OR_C_MAKE}" = "qmake" ]; then
+  VERSION=$(perl -lne 'print $1 if /^VERSION = (.+)/' < ${TRAVIS_BUILD_DIR}/src/src.pro)
   perl -pi -e "s/BUILD = -dev/BUILD = ${BUILD}/" ${TRAVIS_BUILD_DIR}/src/src.pro
 fi
+
+export VERSION
+export BUILD
