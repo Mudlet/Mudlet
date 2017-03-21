975e8b000636750c2c7f1777453725a2a2096bde
keneanung <keneanung@googlemail.com>
2017-03-21 21:57:11 +0100

    Some fixes to installer creation process

--- CI/travis.osx.after_success.sh
+++ CI/travis.osx.after_success.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-git clone https://github.com/Mudlet/installers.git "${TRAVIS_BUILD_DIR}/../installers"
+# we deploy only qmake and clang combination for macOS
+if [ "${Q_OR_C_MAKE}" = "qmake"  -a "${CC}" = "clang" ]; then
+  git clone https://github.com/Mudlet/installers.git "${TRAVIS_BUILD_DIR}/../installers"
 
-cd "${TRAVIS_BUILD_DIR}/../installers/osx"
+  cd "${TRAVIS_BUILD_DIR}/../installers/osx"
 
-ln -s "${TRAVIS_BUILD_DIR}" source
+  ln -s "${TRAVIS_BUILD_DIR}" source
 
-bash make-installer.sh
+  bash make-installer.sh
+fi
