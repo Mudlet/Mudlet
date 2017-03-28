93e15b57d127fb6780937bbd3f46597bff76017d
keneanung <keneanung@googlemail.com>
2017-03-28 12:33:58 +0200

    Resolved all conflicts in travis.linux.install.sh
    [skip ci]

--- CI/travis.linux.install.sh
+++ CI/travis.linux.install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -ev
-<<<<<<< HEAD
 # remove cmake and cmake-data that comes with precise by default on travis, as we'd like to upgrade it to cmake from ppa (which does not use a corresponding cmake-data)
 sudo apt-get remove \
   qt4-qmake libqt4-designer libqt4-dev libboost-dev cmake cmake-data
-=======
-sudo apt-get remove \
-  qt4-qmake libqt4-designer libqt4-dev libboost-dev
->>>>>>> SlySven/release_30
 sudo apt-get autoremove
 # libboost seems messed up in Precise 12.04 lts it mixes 1.46 and 1.48 so try
 # and force a uniform 1.55 installation
@@ -21,11 +16,7 @@ sudo apt-get install \
   zlib1g-dbg zlib1g-dev \
   libzip-dev \
   libpulse-dev \
-<<<<<<< HEAD
   cmake cmake-data \
-=======
-  cmake \
->>>>>>> SlySven/release_30
   gcc-4.7 \
   g++-4.7
 sudo update-alternatives --remove gcc /usr/bin/gcc-4.6
