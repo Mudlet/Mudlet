45d8915212affafd30deb2b61ed75815002f6c93
keneanung <keneanung@googlemail.com>
2017-04-03 21:49:37 +0200

    Fix shellcheck warnings

--- CI/travis.osx.after_success.sh
+++ CI/travis.osx.after_success.sh
@@ -3,7 +3,7 @@
 set -e
 
 # we deploy only qmake and clang combination for macOS
-if [ "${Q_OR_C_MAKE}" = "qmake"  -a "${CC}" = "clang" ]; then
+if [ "${Q_OR_C_MAKE}" = "qmake" ] && [ "${CC}" = "clang" ]; then
   git clone https://github.com/Mudlet/installers.git "${TRAVIS_BUILD_DIR}/../installers"
 
   cd "${TRAVIS_BUILD_DIR}/../installers/osx"
@@ -16,11 +16,13 @@ if [ "${Q_OR_C_MAKE}" = "qmake"  -a "${CC}" = "clang" ]; then
 
     bash make-installer.sh "${appBaseName}.app"
 
-    export DEPLOY_URL=$(wget --method PUT --body-file="${HOME}/Desktop/${appBaseName}.dmg"  https://transfer.sh/${appBaseName}.dmg -O - -q)
+    DEPLOY_URL=$(wget --method PUT --body-file="${HOME}/Desktop/${appBaseName}.dmg"  "https://transfer.sh/${appBaseName}.dmg" -O - -q)
   else
 
     # add ssh-key to ssh-agent for deployment
-    openssl aes-256-cbc -K $encrypted_70dbe4c5e427_key -iv $encrypted_70dbe4c5e427_iv -in "${TRAVIS_BUILD_DIR}/CI/mudlet-deploy-key.enc" -out /tmp/mudlet-deploy-key -d
+    # shellcheck disable=2154
+    # the two "undefined" variables are defined by travis
+    openssl aes-256-cbc -K "${encrypted_70dbe4c5e427_key}" -iv "${encrypted_70dbe4c5e427_iv}" -in "${TRAVIS_BUILD_DIR}/CI/mudlet-deploy-key.enc" -out /tmp/mudlet-deploy-key -d
     eval "$(ssh-agent -s)"
     chmod 600 /tmp/mudlet-deploy-key
     ssh-add /tmp/mudlet-deploy-key
@@ -29,7 +31,8 @@ if [ "${Q_OR_C_MAKE}" = "qmake"  -a "${CC}" = "clang" ]; then
 
     mv "${HOME}/Desktop/Mudlet.dmg" "${HOME}/Desktop/Mudlet-${VERSION}.dmg"
 
-    export DEPLOY_URL=$(scp -i /tmp/mudlet-deploy-key "${HOME}/Desktop/Mudlet-${VERSION}.dmg" "keneanung@mudlet.org:${DEPLOY_PATH}")
+    DEPLOY_URL=$(scp -i /tmp/mudlet-deploy-key "${HOME}/Desktop/Mudlet-${VERSION}.dmg" "keneanung@mudlet.org:${DEPLOY_PATH}")
   fi
+  export DEPLOY_URL
 fi
 
