46e817568117edee630a513c6e051ec730dad897
Stephen Lyons <slysven@virginmedia.com>
2017-05-12 20:57:23 +0100

    Revise: further changes arising from review discussions
    Run-time error string on supplying a bad encoding to setServerEncoding(...)
    is now:
    "Encoding \"XXXX\" does not exist;
     use one of the following:
     \"ASCII\",
     <list of other encodings (with UTF-8 at the top)>."
    
    Profile Preferences title for encoding is now "Server data encoding" - it
    has lost the extra capitalisation I originally used. 8-|
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/ctelnet.cpp
+++ src/ctelnet.cpp
@@ -210,11 +210,11 @@ QPair<bool, QString> cTelnet::setEncoding(const QString & newEncoding, const boo
     } else if (!mAcceptableEncodings.contains(newEncoding)) {
         // Not in list - so reject it
         return qMakePair(false,
-                         QLatin1String("encoding \"")
+                         QLatin1String("Encoding \"")
                          % newEncoding
                          % QLatin1String("\" does not exist;\nuse one of the following:\n\"ASCII\", \"")
                          % mAcceptableEncodings.join(QLatin1String("\", \""))
-                         % QLatin1String("\""));
+                         % QLatin1String("\"."));
     } else if (mEncoding != newEncoding) {
         encodingChanged(newEncoding);
         isChanged = true;
