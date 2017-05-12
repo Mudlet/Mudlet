845c08107af469f3d7047fa2f7b36a73a2e03ca1
kbasten <kbasten@gmail.com>
2017-05-12 10:59:50 +0200

    Flash taskbar on bell character

--- src/ctelnet.cpp
+++ src/ctelnet.cpp
@@ -1914,7 +1914,12 @@ void cTelnet::handle_socket_signal_readyRead()
         }
         else
         {
-            if( ch != '\r' && ch != 0 ) cleandata += ch;
+            if( ch == TN_BELL )
+            {
+                // flash taskbar for 3 seconds
+                QApplication::alert( mudlet::self(), 3000 );   
+            }
+            else if( ch != '\r' && ch != 0 ) cleandata += ch;
         }
 MAIN_LOOP_END: ;
         if( recvdGA )
