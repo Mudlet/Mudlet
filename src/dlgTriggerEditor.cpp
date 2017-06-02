dd261a137bb10372c2c1ce8f4ba61745a3e13da3
Vadim Peretokin <vperetokin@gmail.com>
2017-06-02 06:53:43 +0200

    Removed duplicate clear event handlers call

--- src/dlgTriggerEditor.cpp
+++ src/dlgTriggerEditor.cpp
@@ -4770,8 +4770,6 @@ void dlgTriggerEditor::slot_scripts_selected(QTreeWidgetItem *pItem)
     mpSourceEditor->setPlainText( "" );
     mpScriptsMainArea->lineEdit_scripts_name->clear();
     mpScriptsMainArea->listWidget_registered_event_handlers->clear();
-
-    mpScriptsMainArea->listWidget_registered_event_handlers->clear();
     mpScriptsMainArea->lineEdit_scripts_name->setText(pItem->text(0));
     int ID = pItem->data(0,Qt::UserRole).toInt();
     TScript * pT = mpHost->getScriptUnit()->getScript(ID);
