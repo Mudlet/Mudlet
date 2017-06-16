adffc3bba544ead968f9a960eea456ebef75b1c6
Vadim Peretokin <vperetokin@gmail.com>
2017-06-16 05:06:04 +0200

    Moved variable setting to be before the gcc version echo

--- CI/travis.install.sh
+++ CI/travis.install.sh
@@ -3,6 +3,13 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   echo Install on OSX.
   ./CI/travis.osx.install.sh;
 fi
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+  # add GCC 5 to path
+  mkdir -p latest-gcc-symlinks
+  ln -s /usr/bin/g++-5 latest-gcc-symlinks/g++
+  ln -s /usr/bin/gcc-5 latest-gcc-symlinks/gcc
+  export PATH=$PWD/latest-gcc-symlinks:$PATH
+fi
 if [ ! -z "${CXX}" ]; then
   echo "Testing (possibly updated) compiler version:"
   ${CXX} --version;
