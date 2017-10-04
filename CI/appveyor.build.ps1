2fc6b984afc82bc62607314f6249f6a50795ecd1
keneanung <keneanung@googlemail.com>
2017-10-04 21:52:34 +0200

    Use mingw32-make isntead of make

--- CI/appveyor.build.ps1
+++ CI/appveyor.build.ps1
@@ -7,4 +7,4 @@ cd "$Env:APPVEYOR_BUILD_FOLDER\src"
 $Env:PATH="C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;$Env:QT_BASE_DIR\bin;$Env:MINGW_BASE_DIR\bin;" + (($Env:PATH.Split(';') | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';')
 qmake CONFIG+=release LIBPATH+=$Env:MINGW_BASE_DIR_BASH/bin INCLUDEPATH+=/c/Libraries/boost_1_63_0 INCLUDEPATH+=$Env:MINGW_BASE_DIR_BASH/include INCLUDEPATH+=$Env:MINGW_BASE_DIR_BASH/lib/include mudlet.pro
 
-C:\MinGW\msys\1.0\bin\make.exe -j 2
+mingw32-make -j 2
