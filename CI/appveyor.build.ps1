a3db1b62785678e81ea3fc375d5296fdf965941f
keneanung <keneanung@googlemail.com>
2017-10-04 20:28:24 +0200

    Fix working directories of build command

--- CI/appveyor.build.ps1
+++ CI/appveyor.build.ps1
@@ -1,6 +1,8 @@
-cd "$Env:APPVEYOR_BUILD_FOLDER\src"
+cd "$Env:APPVEYOR_BUILD_FOLDER"
 
-. ..\CI\appveyor.set-build-info.ps1
+. CI\appveyor.set-build-info.ps1
+
+cd "$Env:APPVEYOR_BUILD_FOLDER\src"
 
 $Env:PATH="C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;$Env:QT_BASE_DIR\bin;$Env:MINGW_BASE_DIR\bin;C:\MinGW\msys\1.0\bin;$Env:PATH"
 qmake CONFIG+=release LIBPATH+=$Env:MINGW_BASE_DIR_BASH/bin INCLUDEPATH+=/c/Libraries/boost_1_63_0 INCLUDEPATH+=$Env:MINGW_BASE_DIR_BASH/include INCLUDEPATH+=$Env:MINGW_BASE_DIR_BASH/lib/include mudlet.pro
