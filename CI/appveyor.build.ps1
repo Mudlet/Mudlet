8cab0b55707356b2994f2c7ee66219ec1f5cd802
keneanung <keneanung@googlemail.com>
2017-10-04 20:43:58 +0200

    Remove sh.exe from PATH for building

--- CI/appveyor.build.ps1
+++ CI/appveyor.build.ps1
@@ -4,7 +4,7 @@ cd "$Env:APPVEYOR_BUILD_FOLDER"
 
 cd "$Env:APPVEYOR_BUILD_FOLDER\src"
 
-$Env:PATH="C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;$Env:QT_BASE_DIR\bin;$Env:MINGW_BASE_DIR\bin;C:\MinGW\msys\1.0\bin;$Env:PATH"
+$Env:PATH="C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;$Env:QT_BASE_DIR\bin;$Env:MINGW_BASE_DIR\bin;" + (($Env:PATH.Split(';') | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';')
 qmake CONFIG+=release LIBPATH+=$Env:MINGW_BASE_DIR_BASH/bin INCLUDEPATH+=/c/Libraries/boost_1_63_0 INCLUDEPATH+=$Env:MINGW_BASE_DIR_BASH/include INCLUDEPATH+=$Env:MINGW_BASE_DIR_BASH/lib/include mudlet.pro
 
 make -j 2
