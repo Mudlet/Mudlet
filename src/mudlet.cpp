22ed1b2e216df0258fc9418ef40d39f975281a02
Vadim Peretokin <vperetokin@gmail.com>
2017-10-10 09:34:23 +0200

    Got a naive Linux implementation working

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -66,6 +66,8 @@
 #include <QTableWidget>
 #include <QTextCharFormat>
 #include <QToolBar>
+#include <QtConcurrent>
+#include <QFileInfo>
 #include "post_guard.h"
 
 #include "edbee/edbee.h"
@@ -481,10 +483,50 @@ void mudlet::initUpdater()
     });
 
 
-    QObject::connect(feed, &dblsqd::Feed::downloadFinished, [=]() { qDebug() << "fuck yeah! shit is downloaded!";
-    auto file = feed->getDownloadFile();
-    qDebug() << file->fileName();
+    QObject::connect(feed, &dblsqd::Feed::downloadFinished, [=]() {
+        auto file = feed->getDownloadFile();
 
+        QFuture<void> future = QtConcurrent::run(
+                [=](const QString& fileName) {
+                    QProcess tar;
+                    tar.setProcessChannelMode(QProcess::MergedChannels);
+                    // we can assume tar to be present on a Linux system. If it's not, it'd be rather broken.
+                    tar.start("tar",
+                              QStringList() << "-xvf" << fileName << "-C"
+                                            << "/tmp/");
+                    if (!tar.waitForFinished()) {
+                        qDebug() << "Untarring" << fileName << "failed:" << tar.errorString();
+                    }
+                },
+                file->fileName());
+
+
+        // replace current binary with the unzipped one
+        auto watcher = new QFutureWatcher<void>;
+        QObject::connect(watcher, &QFutureWatcher<void>::finished, [=]() {
+            // FIXME don't hardcode name in case we want to change it
+            QFileInfo unzippedBinary("/tmp/Mudlet.AppImage");
+            QString currentBinaryPath(QCoreApplication::applicationFilePath());
+
+            auto executablePermissions = unzippedBinary.permissions();
+            executablePermissions |= QFileDevice::ExeOwner | QFileDevice::ExeUser;
+
+            QDir dir;
+            if (!(dir.remove(currentBinaryPath) && QDir().rename(unzippedBinary.filePath(), currentBinaryPath))) {
+                qDebug() << "updating" << currentBinaryPath << "with new version from" << unzippedBinary.filePath() << "failed";
+                return;
+            }
+
+            QFile updatedBinary(QCoreApplication::applicationFilePath());
+            if (!updatedBinary.setPermissions(executablePermissions)) {
+                qDebug() << "couldn't executable permissions on updated Mudlet binary at" << QCoreApplication::applicationFilePath();
+                return;
+            }
+
+            qDebug() << "Successfully updated Mudlet to" << feed->getUpdates().first().getVersion();
+
+        });
+        watcher->setFuture(future);
     });
 }
 
