e3eb69497b17f1cd295c77f52b3eed89b53563fe
Vadim Peretokin <vperetokin@gmail.com>
2017-10-14 06:05:40 +0200

    WIP

--- src/updater.cpp
+++ src/updater.cpp
@@ -58,7 +58,8 @@ void Updater::untarOnLinux(const QString& fileName) const
     QProcess tar;
     tar.setProcessChannelMode(QProcess::MergedChannels);
     // we can assume tar to be present on a Linux system. If it's not, it'd be rather broken.
-    tar.start("tar", QStringList() << "-xvf" << fileName << "-C" << QStandardPaths::writableLocation(QStandardPaths::TempLocation) << "/");
+    // tar output folder has to end with a slash
+    tar.start("tar", QStringList() << "-xvf" << fileName << "-C" << QStandardPaths::writableLocation(QStandardPaths::TempLocation) + QStringLiteral("/"));
     if (!tar.waitForFinished()) {
         qDebug() << "Untarring" << fileName << "failed:" << tar.errorString();
     } else {
@@ -75,6 +76,7 @@ void Updater::updateBinaryOnLinux() const
     executablePermissions |= QFileDevice::ExeOwner | QFileDevice::ExeUser;
 
     QDir dir;
+    // dir.rename actually moves a file
     if (!(dir.remove(installedBinaryPath) && dir.rename(unzippedBinary.filePath(), installedBinaryPath))) {
         qDebug() << "updating" << installedBinaryPath << "with new version from" << unzippedBinary.filePath() << "failed";
         return;
@@ -89,7 +91,9 @@ void Updater::updateBinaryOnLinux() const
     qDebug() << "Successfully updated Mudlet to" << feed->getUpdates().first().getVersion();
 }
 
-void TUpdateDialog::startUpdate() const
-{
-
+// this really gets called when the update is downloaded
+void TUpdateDialog::startUpdate()
+{    
+    done(QDialog::Accepted);
+    QApplication::quit();
 }
