87611049629d4607cf31668a4dc3b9b3177e253e
Jonathan Macbeth <macbethjonathan@gmail.com>
2017-10-30 23:06:14 +0100

    Conform indentation to soft-tabs / 2 spaces

--- src/mudlet-lua/lua/geyser/GeyserGeyser.lua
+++ src/mudlet-lua/lua/geyser/GeyserGeyser.lua
@@ -13,17 +13,25 @@
 -- of default limits for new windows, in fact, it is the root container
 -- window, and is the metatable for Geyser.Container. It has the 
 -- minimum number of functions needed to behave as a Container.
-Geyser = Geyser or {i = 0, x = 0, y = 0}
+Geyser = Geyser or { i = 0, x = 0, y = 0 }
 
 --Layout policies. Fixed = Do not stretch/shrink, Dynamic = Do stretch/shrink
 Geyser.Fixed, Geyser.Dynamic = 0, 1
 
 Geyser.width, Geyser.height = getMainWindowSize()
 
-Geyser.get_x = function () return 0 end
-Geyser.get_y = function () return 0 end
-Geyser.get_width = function () return getMainWindowSize() end
-Geyser.get_height = function () local w,h = getMainWindowSize() return h end
+Geyser.get_x = function()
+  return 0
+end
+Geyser.get_y = function()
+  return 0
+end
+Geyser.get_width = function()
+  return getMainWindowSize()
+end
+Geyser.get_height = function()
+  local w, h = getMainWindowSize() return h
+end
 Geyser.name = "Geyser Root Window"
 Geyser.__index = Geyser
 
@@ -33,46 +41,46 @@ Geyser.windows = Geyser.windows or {}
 Geyser.defer_updates = false
 
 function Geyser:begin_update()
-   self.defer_updates = true
+  self.defer_updates = true
 end
 
 function Geyser:end_update()
-   self.defer_updates = false
-   self:reposition()
+  self.defer_updates = false
+  self:reposition()
 end
 
 function Geyser:reposition()
-   if not self.defer_updates then
-      GeyserReposition()
-   end
+  if not self.defer_updates then
+    GeyserReposition()
+  end
 end
 
 --- Add a window to the list that this container manages.
 -- @param window The window to add this container
 function Geyser:add (window, cons)
-   cons = cons or window -- 'cons' is optional
-   
-   -- Stop other container from controlling this window
-	if window.container then
-	   window.container:remove(window)
-	end
+  cons = cons or window -- 'cons' is optional
 
-   -- Assume control of this window
-   window.container = self
-   self.windowList[window.name] = window
-   table.insert(self.windows, window.name)
-   Geyser.set_constraints(window, cons, self)
-   if not self.defer_updates then
-      window:reposition()
-   end
-   window:show()
+  -- Stop other container from controlling this window
+  if window.container then
+    window.container:remove(window)
+  end
+
+  -- Assume control of this window
+  window.container = self
+  self.windowList[window.name] = window
+  table.insert(self.windows, window.name)
+  Geyser.set_constraints(window, cons, self)
+  if not self.defer_updates then
+    window:reposition()
+  end
+  window:show()
 end
 
 --- Removes a window from the list that it manages
 -- @param window The window to remove from this container's
 -- windowList
 function Geyser:remove (window)
-   self.windowList[window.name] = nil
-   index = table.index_of(self.windows, window.name) or 0
-   table.remove(self.windows, index)
+  self.windowList[window.name] = nil
+  index = table.index_of(self.windows, window.name) or 0
+  table.remove(self.windows, index)
 end
--- src/mudlet-lua/lua/geyser/GeyserWindow.lua
+++ src/mudlet-lua/lua/geyser/GeyserWindow.lua
@@ -15,49 +15,55 @@ local cparse = Geyser.Color.parse
 -- @field fgColor Text foreground color, default "black"
 -- @field color Window background color, default "#202020"
 Geyser.Window = Geyser.Container:new({
-      name = "WindowClass",
-      message = "",
+  name = "WindowClass",
+  message = "",
 
-      -- default colors.  The are boring
-      fgColor = "white",
-      bgColor = "black",
-      color = "#202020",
-   })
+  -- default colors.  The are boring
+  fgColor = "white",
+  bgColor = "black",
+  color = "#202020",
+})
 
 -- Add regular window methods, these are mostly just OO-ized wrappers
 
 --- Prints a message to the window
 -- @param message The message to print. Can contain html formatting.
 function Geyser.Window:echo(message)
-   self.message = message
-   echo(self.name, self.message)
+  self.message = message
+  echo(self.name, self.message)
 end
 
 --- Prints a message to the window.
 -- @param message The message to print. Uses color formatting information -
 -- a message of "<red>Hi" would make 'Hi' red.
-function Geyser.Window:cecho(message) self.message = message or self.message cecho(self.name, self.message) end
+function Geyser.Window:cecho(message)
+  self.message = message or self.message cecho(self.name, self.message)
+end
 
 --- Prints a message to the window.
 -- @param message The message to print. Uses color formatting information -
 -- a message of "<255,0,0>Hi" would make 'Hi' red.
-function Geyser.Window:decho(message) self.message = message or self.message decho(self.name, self.message) end
+function Geyser.Window:decho(message)
+  self.message = message or self.message decho(self.name, self.message)
+end
 
 --- Prints a message to the window.
 -- @param message The message to print. Uses color formatting information -
 -- a message of "|cff0000Hi" would make 'Hi' red.
-function Geyser.Window:hecho(message) self.message = message or self.message hecho(self.name, self.message) end
+function Geyser.Window:hecho(message)
+  self.message = message or self.message hecho(self.name, self.message)
+end
 
 --- Get the window's foreground color.
 -- @return The foreground color of this window primitive as a Geyser.Color object.
 function Geyser.Window:getFgColor ()
-   return getFgColor(self.name)
+  return getFgColor(self.name)
 end
 
 ---  Get the window's background color
 -- @return The background color of this window primitive as a Geyser.Color object.
 function Geyser.Window:getBgColor ()
-   return getBgColor(self.name)
+  return getBgColor(self.name)
 end
 
 --- Sets the background color of this window primitive.  If this primitive
@@ -65,8 +71,8 @@ end
 -- @param r The red value, or a quoted color name, like "green".
 -- @param g The green value, or nil if using a name.
 -- @param b The blue value, or nil if using a name.
-function Geyser.Window:setBgColor (r,g,b)
-   setBgColor(self.name, cparse(r,g,b))
+function Geyser.Window:setBgColor (r, g, b)
+  setBgColor(self.name, cparse(r, g, b))
 end
 
 --- Sets the foreground color of this window primitive.  If this primitive
@@ -74,8 +80,8 @@ end
 -- @param r The red value, or a quoted color name, like "green".
 -- @param g The green value, or nil if using a name.
 -- @param b The blue value, or nil if using a name.
-function Geyser.Window:setFgColor (r,g,b)
-   setFgColor(self.name, cparse(r,g,b))
+function Geyser.Window:setFgColor (r, g, b)
+  setFgColor(self.name, cparse(r, g, b))
 end
 
 --- Sets the background color and alpha.  If this primitive
@@ -84,13 +90,13 @@ end
 -- @param g The green component, or nil if using named colors.
 -- @param b The blue component, or nil if using named colors.
 -- @param a The alpha component. If nil, uses current alpha value.
-function Geyser.Window:setColor (r,g,b,a)
-   setBackgroundColor(self.name, cparse(r,g,b,a))
+function Geyser.Window:setColor (r, g, b, a)
+  setBackgroundColor(self.name, cparse(r, g, b, a))
 end
 
 --- Pastes text from the clipboard into this window primitive.
 function Geyser.Window:paste ()
-   paste(self.name)
+  paste(self.name)
 end
 
 --- Sets the text format for this window. Note that the *echo()
@@ -105,42 +111,42 @@ end
 -- @param underline The underlined status. 1 is underlined, 0 is normal.
 -- @param italics The italicized status. 1 is italicized, 0 is normal.
 function Geyser.Window:setTextFormat(r1, g1, b1, r1, g2, b2, bold, underline, italics)
-   setTextFormat(self.name, r1, g1, b1, r1, g2, b2, bold, underline, italics)
+  setTextFormat(self.name, r1, g1, b1, r1, g2, b2, bold, underline, italics)
 end
 
 --- Sets bolded text.
 -- @param bool True for bold.
 function Geyser.Window:setBold(bool)
-   setBold(self.name, bool)
+  setBold(self.name, bool)
 end
 
 --- Sets underlined text.
 -- @param bool True for underlined.
 function Geyser.Window:setUnderline(bool)
-   setUnderline(self.name, bool)
+  setUnderline(self.name, bool)
 end
 
 --- Sets italicized text.
 -- @param bool True for italicized.
 function Geyser.Window:setItalics(bool)
-   setItalics(self.name, bool)
+  setItalics(self.name, bool)
 end
 
 -- Save a reference to our parent's constructor
 Geyser.Window.parent = Geyser.Container
 
 function Geyser.Window:new (cons, container)
-   -- Initiate and set Window specific things
-   cons = cons or {}
-   cons.type = cons.type or "window"
+  -- Initiate and set Window specific things
+  cons = cons or {}
+  cons.type = cons.type or "window"
 
-   -- Call parent's constructor
-   local me = self.parent:new(cons, container)
+  -- Call parent's constructor
+  local me = self.parent:new(cons, container)
 
-   -- Set the metatable.
-   setmetatable(me, self)
-   self.__index = self
+  -- Set the metatable.
+  setmetatable(me, self)
+  self.__index = self
 
-   --print(" New in " .. self.name .. " : " .. me.name)
-   return me
+  --print(" New in " .. self.name .. " : " .. me.name)
+  return me
 end
