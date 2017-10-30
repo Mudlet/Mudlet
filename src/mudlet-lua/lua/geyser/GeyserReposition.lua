6d8794c14d57bc6e78614a4b1a97d15e66cc8551
Jonathan Macbeth <macbethjonathan@gmail.com>
2017-10-30 22:49:06 +0100

    Conform indentation to soft tabs / 2 spaces

--- src/mudlet-lua/lua/geyser/GeyserReposition.lua
+++ src/mudlet-lua/lua/geyser/GeyserReposition.lua
@@ -7,9 +7,9 @@
 --- Responds to sysWindowResizeEvent and causes all windows managed
 -- by Geyser to update their sizes and positions.
 function GeyserReposition()
-	for _,window in pairs(Geyser.windowList) do
-		window:reposition()
-	end
+  for _, window in pairs(Geyser.windowList) do
+    window:reposition()
+  end
 end
 
 registerAnonymousEventHandler("sysWindowResizeEvent", "GeyserReposition")
