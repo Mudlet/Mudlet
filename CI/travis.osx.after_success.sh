82c3b9d8407c2e9cef230a33cca9a7d1046ce6f6
Vadim Peretokin <vperetokin@gmail.com>
2017-11-02 21:10:13 +0100

    Added signing steps

--- CI/travis.osx.after_success.sh
+++ CI/travis.osx.after_success.sh
@@ -8,13 +8,26 @@ if [ "${Q_OR_C_MAKE}" = "qmake" ] && [ "${CC}" = "clang" ]; then
 
   cd "${TRAVIS_BUILD_DIR}/../installers/osx"
 
+  # setup macOS keychain for code signing
+  KEYCHAIN=build.keychain
+  security create-keychain -p travis $KEYCHAIN
+  security default-keychain -s $KEYCHAIN
+  security unlock-keychain -p travis $KEYCHAIN
+  security set-keychain-settings -t 3600 -u $KEYCHAIN
+  security import Certificates.p12 -k $KEYCHAIN -P "$CERT_PW" -T /usr/bin/codesign
+  IDENTITY="Developer ID Application: Vadim Peretokin (65HQ5H22TH)"
+
   ln -s "${TRAVIS_BUILD_DIR}" source
 
   if [ -z "${TRAVIS_TAG}" ]; then
     appBaseName="Mudlet-${VERSION}${MUDLET_VERSION_BUILD}"
     mv "source/build/Mudlet.app" "source/build/${appBaseName}.app"
 
+    # codesign will recursively sign everything but the embedded .App
+    codesign --deep -s "$IDENTITY" "source/build/${appBaseName}.app/Contents/Frameworks/Sparkle.framework/Resources/Autoupdate.app/"
+    codesign --deep -s "$IDENTITY" "source/build/${appBaseName}.app"
     bash make-installer.sh "${appBaseName}.app"
+    codesign --deep -s "$IDENTITY" "${HOME}/Desktop/${appBaseName}.dmg"
 
     DEPLOY_URL=$(wget --method PUT --body-file="${HOME}/Desktop/${appBaseName}.dmg"  "https://transfer.sh/${appBaseName}.dmg" -O - -q)
   else
@@ -27,13 +40,19 @@ if [ "${Q_OR_C_MAKE}" = "qmake" ] && [ "${CC}" = "clang" ]; then
     chmod 600 /tmp/mudlet-deploy-key
     ssh-add /tmp/mudlet-deploy-key
 
+    codesign --deep -s "$IDENTITY" "source/build/Mudlet.app/Contents/Frameworks/Sparkle.framework/Resources/Autoupdate.app/"
+    codesign --deep -s "$IDENTITY" "source/build/Mudlet.app"
     bash make-installer.sh -r "${VERSION}" source/build/Mudlet.app
+    codesign --deep -s "$IDENTITY" "${HOME}/Desktop/Mudlet.dmg"
 
     mv "${HOME}/Desktop/Mudlet.dmg" "${HOME}/Desktop/Mudlet-${VERSION}.dmg"
 
     scp -i /tmp/mudlet-deploy-key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${HOME}/Desktop/Mudlet-${VERSION}.dmg" "keneanung@mudlet.org:${DEPLOY_PATH}"
     DEPLOY_URL="http://www.mudlet.org/wp-content/files/Mudlet-${VERSION}.dmg"
   fi
+
+  # delete keychain just in case
+  security delete-keychain $KEYCHAIN
   export DEPLOY_URL
 fi
 
