d018d6e503e7c1cd07f623b5b445f8239002cb14
Vadim Peretokin <vperetokin@gmail.com>
2017-11-05 09:30:10 +0100

    More syslog tweaks

--- src/updater.cpp
+++ src/updater.cpp
@@ -106,7 +106,16 @@ void Updater::setupOnMacOS()
     // don't need to explicitly check for updates - sparkle will do so on its own
 
     QProcess().start("/usr/bin/logger", QStringList() << "0 launched from update?");
-    QProcess().start("syslog", QStringList() << "-s" << "-l" << "notice" << "This message should show up in with a Facility of syslog.");
+    
+    QProcess syslog;
+    syslog.setProcessChannelMode(QProcess::MergedChannels);
+    syslog.start("syslog", QStringList() << "-s" << "-l" << "notice" << "This message should show up in with a Facility of syslog.");
+    if (!syslog.waitForFinished()) {
+        qWarning() << "syslog failed:" << tar.errorString();
+    } else {
+        qDebug() << "syslog worked";
+    }
+
     SYSLOG("IT WORKED");
     qDebug() << "prints should have happened by now";
 
