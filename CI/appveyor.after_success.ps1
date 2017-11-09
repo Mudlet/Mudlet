a1580d7e383d2adf725bd511d475794bdb1dee41
Vadim Peretokin <vperetokin@gmail.com>
2017-11-09 07:41:03 +0100

    Fixed the temp branch it should checkout

--- CI/appveyor.after_success.ps1
+++ CI/appveyor.after_success.ps1
@@ -32,7 +32,7 @@ if ("$Env:QT_BASE_DIR" -eq "C:\Qt\5.6\mingw49_32") {
 #      $DEPLOY_URL = "https://ci.appveyor.com/api/buildjobs/$Env:APPVEYOR_JOB_ID/artifacts/src%2Fmudlet.zip"
 #  } else {
     # C:\src\installbuilder-qt-installer.exe --mode unattended --unattendedmodeui none
-    git clone https://github.com/Mudlet/installers.git -b add-squirrel-install C:\projects\installers
+    git clone https://github.com/Mudlet/installers.git -b squirrel-windows C:\projects\installers
     cd C:\projects\installers\windows
   echo "3 in this path:"
   echo (Get-Item -Path ".\" -Verbose).FullName
