4afbabd3d50da7953126f11fe1e9ec49c838a5c6
Vadim Peretokin <vperetokin@gmail.com>
2017-11-12 20:12:10 +0100

    Remove commented out code

--- src/dlgProfilePreferences.cpp
+++ src/dlgProfilePreferences.cpp
@@ -449,8 +449,7 @@ void dlgProfilePreferences::loadSpecialSettingsTab()
 #if !defined(INCLUDE_UPDATER)
     groupBox_updates->hide();
 #else
-//    if (mudlet::self()->onDevelopmentVersion()) {
-    if (false) {
+    if (mudlet::self()->onDevelopmentVersion()) {
         // tick the box and make it be untickable as automatic updates are disabled in dev builds
         checkbox_noAutomaticUpdates->setChecked(true);
         checkbox_noAutomaticUpdates->setDisabled(true);
