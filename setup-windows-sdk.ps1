45df9eca4096010de15d7eb64b11c76d1e301545
keneanung <keneanung@googlemail.com>
2017-11-29 22:01:20 +0100

    Make the setup script find the install one

--- setup-windows-sdk.ps1
+++ setup-windows-sdk.ps1
@@ -2,7 +2,8 @@ $sourceDir = $pwd.Path
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
     $arguments = "& 'CI\appveyor.install.ps1'"
-    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    $arguments = "cd $sourceDir\CI ; & '.\appveyor.install.ps1'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments -Wait
 }
 else {
     CI\appveyor.install.ps1
