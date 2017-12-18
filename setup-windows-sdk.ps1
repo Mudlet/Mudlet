4ccfff32da6db947be4e8d0fae91a570aefb159e
keneanung <keneanung@googlemail.com>
2017-12-18 21:06:17 +0100

    Minor tweaks

--- setup-windows-sdk.ps1
+++ setup-windows-sdk.ps1
@@ -2,7 +2,7 @@ $sourceDir = $pwd.Path
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
-    $arguments = ("-ExecutionPolicy", "Bypass", "-NoExit", "cd $sourceDir\CI ; & '.\appveyor.install.ps1'")
+    $arguments = ("-ExecutionPolicy", "Bypass", "cd $sourceDir\CI ; & '.\appveyor.install.ps1'")
     Start-Process powershell -Verb runAs -ArgumentList $arguments -Wait
 }
 else {
@@ -15,16 +15,19 @@ cd "$sourceDir"
 SetQtBaseDir "C:\src\verbose_output.log"
 SetMingwBaseDir "C:\src\verbose_output.log"
 
+Write-Output "Updating git submodules"
 git submodule update --init --recursive
 
 cd "$sourceDir\src"
 
+Write-Output "Running qmake"
 $Env:PATH="C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;$Env:QT_BASE_DIR\bin;$Env:MINGW_BASE_DIR\bin;" + (($Env:PATH.Split(';') | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';')
 qmake CONFIG+=debug LIBPATH+=$Env:MINGW_BASE_DIR\bin INCLUDEPATH+=C:\Libraries\boost_1_60_0 INCLUDEPATH+=$Env:MINGW_BASE_DIR\include INCLUDEPATH+=$Env:MINGW_BASE_DIR\lib\include mudlet.pro
 if("$LastExitCode" -ne "0"){
   exit 1
 }
 
+Write-Output "Running make"
 mingw32-make -j $(Get-WmiObject win32_processor | Select -ExpandProperty "NumberOfLogicalProcessors")
 if("$LastExitCode" -ne "0"){
   exit 1
