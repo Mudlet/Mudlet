4b6fe133b239eb24a3b9ca69de6660f40b831be8
keneanung <keneanung@googlemail.com>
2017-12-18 22:28:28 +0100

    Put copying of non qt windows dependencies in own file

--- setup-windows-sdk.ps1
+++ setup-windows-sdk.ps1
@@ -36,19 +36,7 @@ if("$LastExitCode" -ne "0"){
 cd "$sourceDir\src\debug"
 
 windeployqt.exe mudlet.exe
-COPY $Env:MINGW_BASE_DIR\lib\libyajl.dll .
-COPY C:\src\lua-5.1.5\lua-5.1.5\src\lua51.dll .
-COPY C:\src\openssl-1.0.2l\libeay32.dll .
-COPY C:\src\openssl-1.0.2l\ssleay32.dll .
-COPY $Env:MINGW_BASE_DIR\bin\libzip-5.dll .
-COPY $Env:MINGW_BASE_DIR\bin\libhunspell-1.4-0.dll .
-COPY $Env:MINGW_BASE_DIR\bin\libpcre-1.dll .
-COPY $Env:MINGW_BASE_DIR\bin\libsqlite3-0.dll .
-COPY $Env:MINGW_BASE_DIR\bin\zlib1.dll .
-XCOPY /S /I /Q /Y ..\mudlet-lua mudlet-lua
-COPY ..\*.dic .
-COPY C:\src\luazip\luazip-master\zip.dll .
-XCOPY /S /I /Q /Y $Env:MINGW_BASE_DIR\lib\lua\5.1 .
+. "$sourceDir\CI\copy-non-qt-win-dependencies.ps1"
 
 Start-Sleep 10
 Start-Process mudlet.exe
