fa1fcab76cfb1c624a349008c2ee67b6dfdeb360
Stephen Lyons <slysven@virginmedia.com>
2018-01-21 05:35:10 +0000

    BugFix: fix-up a Qt reported issue on QGuiApplication start-up {FreeBSD}
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/main.cpp
+++ src/main.cpp
@@ -126,12 +126,18 @@ QCoreApplication* createApplication(int& argc, char* argv[], unsigned int& actio
 #endif
     } else {
 #if defined(Q_OS_MACOS)
-        // Workaround for horrible mac rendering issues once the mapper widget is open
-        // see https://bugreports.qt.io/browse/QTBUG-41257
+        // Workaround for horrible mac rendering issues once the mapper widget
+        // is open - see https://bugreports.qt.io/browse/QTBUG-41257
         QApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
-#endif
-#if defined(Q_OS_WIN32)
-        // Force OpenGL use as we use some functions that aren't provided by Qt's OpenGL layer on Windows (QOpenGLFunctions)
+#elif defined(Q_OS_FREEBSD)
+        // Cure for diagnostic:
+        // "Qt WebEngine seems to be initialized from a plugin. Please set
+        // Qt::AA_ShareOpenGLContexts using QCoreApplication::setAttribute
+        // before constructing QGuiApplication."
+        QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+#elif defined(Q_OS_WIN32)
+        // Force OpenGL use as we use some functions that aren't provided by
+        // Qt's OpenGL layer on Windows (QOpenGLFunctions)
         QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
 #endif
         return new QApplication(argc, argv); // Normal course of events - (GUI), so: game on!
