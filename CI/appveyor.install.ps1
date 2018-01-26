765fcd8a98893b2961746f001a4bc9af18f6ef77
keneanung <keneanung@googlemail.com>
2018-01-26 04:00:18 +0100

    Automate further Windows dependency installation (#1317)
    This is a squashed and merge commit of 63 commits including:
    * Check for and install 7z
    * Bugfix for 7z installation
    * Remember: PSH doesn't use comma for param separator
    * Add installation of cmake
    * Install 32 bit cmake: this version is installed on appveyor.
    * Add installation of MSYS
    * Add installation of boost
    * Add Qt installer script for windows
    * Add installation of Qt
    * Add item type to New-Item calls
    * More fixes:  Log certain actions and force wait of external commands
    * Make windows SDK installer 32-bit aware
    * Switch to Qt 5.6.3
    * Make script fail on error
    * Experiment: don't use "&" for functions in script blocks
    * Allow hunspell configure to "fail"
    * Fix syntax
    * Don't fail instantly on external commands printing to stderr
    * Remove now unneeded mayFail parameters
    * Use as many logical processors as possible for make calls
    * Check for correct libsqlite file
    * Fix external command calls once more
    * Add message about big downloads
    * Move submodule init to the build step
    * Avoid warning about MINGW_BASE_DIR: The script tries to (naively) find an
      existing QT installation or sets a default path, if it's not found. From this value
      it also deducts the MinGW base directory.
    * Remove depended-on environment variables to make build matrix easier
    * Set MINGW_BASE_DIR after Qt is installed: We can't use Resolve-Path
       if it's not installed as paths don't exist yet.
    * Add debug output
    * Set PATH variable correctly
    * Remove debug output
    * Don't use global variables for log file: Use parameters instead
    * Remove deletion of already extracted folders
    * Use CMD variable syntax
    * Add first version of setup-windows-sdk.ps1
    * Fix QT_BASE_DIR for existing path
    * Remove unwanted setting of build-info
    * Use ErrorAction argument to trigger try-catch
    * Change includes to boost versions we actually install
    * Make the setup script find the install one
    * Another test to get around execution policy
    * Update appveyor.install.ps1: Try avoiding opening blank windows
    * More execution policy handling magic
    * Adapt qmake project file to CI and SDK paths
    * Add some default path (based on SDK) as MINGW_BASE_DIR
    * Put copying of non qt windows dependencies in own file
    * Move creation of working directory up
    * Let project file do the update of submodules
    * Reset lcf submodule to the correct commit
    * Changes according to review feedback

--- CI/appveyor.install.ps1
+++ CI/appveyor.install.ps1
@@ -1,10 +1,54 @@
+# Exit the script whenever an error in a cmdlet occurs
+$global:ErrorActionPreference = "Stop"
+
 # Some global variables / settings
 $workingBaseDir = "C:\src\"
 $logFile = "$workingBaseDir\verbose_output.log"
-$ShPath = "$Env:MINGW_BASE_DIR\bin;C:\MinGW\msys\1.0\bin;C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;$Env:PATH"
-$NoShPath = ($ShPath.Split(';') | Where-Object { $_ -ne 'C:\MinGW\msys\1.0\bin' } | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';'
+
+if (-not $(Test-Path "$workingBaseDir")) {
+    New-Item "$workingBaseDir" -ItemType "directory"
+}
+
+$64Bit = (Get-WmiObject Win32_OperatingSystem).OSArchitecture -eq "64-bit"
+if($64Bit){
+  $CMakePath = "C:\Program Files (x86)\CMake\bin"
+} else {
+  $CMakePath = "C:\Program Files\CMake\bin"
+}
+
+. .\appveyor.set-environment.ps1
+SetQtBaseDir "$logFile"
+
+$Env:PATH = "$CMakePath;C:\MinGW\msys\1.0\bin;C:\Program Files\7-Zip;$Env:PATH"
 
 # Helper functions
+# see http://patrick.lioi.net/2011/08/18/powershell-and-calling-external-executables/
+function script:exec {
+    [CmdletBinding()]
+
+  param(
+    [Parameter(Position=0,Mandatory=1)][string]$cmd,
+    [Parameter(Position=1,Mandatory=0)][string[]]$parameter = @(),
+    [Parameter(Position=2,Mandatory=0)][string]$errorMessage = ("Error executing command: {0}" -f $cmd)
+  )
+  # ignore standard error for external programs
+  $global:ErrorActionPreference = "Continue"
+  $outLog = "$workingBaseDir\stdout.log"
+  $errLog = "$workingBaseDir\stderr.log"
+  if($parameter.Length -eq 0){
+    $exitCode = (Start-Process -FilePath $cmd -Wait -PassThru -RedirectStandardOutput "$outLog" -RedirectStandardError "$errLog" -NoNewWindow).ExitCode
+  } else {
+    $exitCode = (Start-Process -FilePath $cmd -ArgumentList $parameter -Wait -PassThru -RedirectStandardOutput "$outLog" -RedirectStandardError "$errLog" -NoNewWindow).ExitCode
+  }
+  Get-Content $outLog, $errLog | Out-File $logFile -Append
+  if ($exitCode -ne 0)
+  {
+    throw $errorMessage
+  }
+  # restore exit behavior
+  $global:ErrorActionPreference = "Stop"
+}
+
 function PartSkeleton([string] $content) {
   Write-Output "==== $content ====" | Tee-Object -File "$logFile" -Append
 }
@@ -21,36 +65,41 @@ function Step([string] $stepName) {
   Write-Output "---- $stepName ----" | Tee-Object -File "$logFile" -Append
 }
 
-function DownloadFile([string] $url, [string] $outputFile) {
-  Step "Downloading"
-  Invoke-WebRequest "$url" -OutFile "$outputFile" -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome >> "$logFile" 2>&1
+function DownloadFile([string] $url, [string] $outputFile, [bool] $bigDownload = $false) {
+  $stepText = "Downloading"
+  if ($bigDownload)
+  {
+    $stepText = "$stepText, this is a huge download and may take a while"
+  }
+  Step $stepText
+  (New-Object System.Net.WebClient).DownloadFile($url, "$workingBaseDir\$outputFile") >> "$logFile" 2>&1
 }
 
 function ExtractTar([string] $tarFile, [string] $outputPath) {
   Step "Extracting source distribution"
   $file = Get-ChildItem $tarFile
-  7z x "$($file.FullName)" -y >> "$logFile" 2>&1
-  7z -o"$outputPath" x "$($file.Directory)\$($file.BaseName)" -y >> "$logFile" 2>&1
+  exec "7z" @("x", "$($file.FullName)", "-y")
+  exec "7z" @("-o$outputPath", "x", "$($file.Directory)\$($file.BaseName)", "-y")
 }
 
 function ExtractZip([string] $zipFile, [string] $outputPath) {
   Step "Extracting source distribution"
-  7z -o"$outputPath" x "$zipFile" -y >> "$logFile" 2>&1
+  exec "7z" @("-o$outputPath", "x", "$zipFile", "-y")
 }
 
 function RunConfigure([string] $configureArguments = "--prefix=$Env:MINGW_BASE_DIR_BASH") {
   Step "Running configure"
-  bash -c "./configure $configureArguments" >> "$logFile" 2>&1
+  exec "bash" @("-c", "`"./configure $configureArguments`"")
 }
 
 function RunMake([string] $makefile = "Makefile"){
   Step "Running make"
-  mingw32-make -f "$makefile" -j 2 >> "$logFile" 2>&1
+  exec "mingw32-make" @("-f", "$makefile", "-j", $(Get-WmiObject win32_processor | Select -ExpandProperty "NumberOfLogicalProcessors"))
 }
 
 function RunMakeInstall([string] $makefile = "Makefile"){
   Step "Running make install"
-  mingw32-make install -f "$makefile" >> "$logFile" 2>&1
+  exec "mingw32-make" @("install", "-f", "$makefile")
 }
 
 function CheckAndInstall([string] $dependencyName, [string] $signalFile, [scriptblock] $installationFunction){
@@ -65,6 +114,61 @@ function CheckAndInstall([string] $dependencyName, [string] $signalFile, [script
 }
 
 # installation functions
+function InstallSevenZ() {
+  if($64Bit){
+    $downloadUrl = "http://www.7-zip.org/a/7z1604-x64.exe"
+  } else {
+    $downloadUrl = "http://www.7-zip.org/a/7z1604.exe"
+  }
+  DownloadFile "$downloadUrl" "7z-installer.exe"
+  Step "installing 7z"
+  exec ".\7z-installer.exe" ("/S", "/D=`"C:\Program Files\7-Zip`"")
+}
+
+function InstallCmake() {
+  DownloadFile "https://cmake.org/files/v3.9/cmake-3.9.6-win32-x86.msi" "cmake-installer.msi"
+  Step "installing cmake"
+  exec "msiexec.exe" @("/q", "/li", "$workingBaseDir\cmake-installer.log", "/i", "cmake-installer.msi")
+  if(Test-Path -Path "$workingBaseDir\cmake-installer.log" -PathType Leaf){
+    Get-Content "$workingBaseDir\cmake-installer.log" | Out-File $logFile -Append
+  }
+}
+
+function InstallMsys() {
+  DownloadFile "https://sourceforge.net/projects/mingwbuilds/files/external-binary-packages/msys%2B7za%2Bwget%2Bsvn%2Bgit%2Bmercurial%2Bcvs-rev13.7z/download" "msys.7z" $true
+  if (!(Test-Path -Path "C:\MinGW\msys\1.0" -PathType Container)) {
+    Step "Creating MinGW path"
+    New-Item -Path "C:\MinGW\msys\1.0" -ItemType "directory" >> "$logFile" 2>&1
+  }
+  ExtractZip "msys.7z" "."
+  Step "Copying folder"
+  Move-Item "msys\*" "C:\MinGW\msys\1.0" >> "$logFile" 2>&1
+}
+
+function InstallBoost() {
+  DownloadFile "https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download" "boost.tar.gz" $true
+  if (!(Test-Path -Path "C:\Libraries\" -PathType Container)) {
+    Step "Creating Boost path"
+    New-Item -Path "C:\Libraries\" -ItemType "directory" >> "$logFile" 2>&1
+  }
+  ExtractTar "boost.tar.gz" "."
+  Step "Copying folder"
+  Move-Item "boost_1_60_0" "C:\Libraries\" >> "$logFile" 2>&1
+}
+
+function InstallQt() {
+  DownloadFile "http://download.qt.io/official_releases/qt/5.6/5.6.3/qt-opensource-windows-x86-mingw492-5.6.3.exe" "qt-installer.exe" $true
+  exec ".\qt-installer.exe" @("--script=`"$(split-path -parent $script:MyInvocation.MyCommand.Path)\qt-silent-install.qs`"")
+}
+
+function InstallPython() {
+  DownloadFile "https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi" "python-installer.msi" $true
+  exec "msiexec.exe" @("/q", "/li", "$workingBaseDir\python-installer.log", "/i", "python-installer.msi")
+  if(Test-Path -Path "$workingBaseDir\python-installer.log" -PathType Leaf){
+    Get-Content "$workingBaseDir\python-installer.log" | Out-File $logFile -Append
+  }
+}
+
 function InstallOpenssl() {
   DownloadFile "https://indy.fulgan.com/SSL/openssl-1.0.2l-i386-win32.zip" "openssl-1.0.2l-i386-win32.zip"
   ExtractZip "openssl-1.0.2l-i386-win32.zip" "openssl-1.0.2l"
@@ -86,14 +190,17 @@ function InstallYajl() {
   Set-Location "yajl-2.0.1\lloyd-yajl-f4b2b1a"
   Step "changing CMakeLists.txt"
   (Get-Content CMakeLists.txt -Raw) -replace '\/W4' -replace '(?<=SET\(linkFlags)[^\)]+' -replace '\/wd4996 \/wd4255 \/wd4130 \/wd4100 \/wd4711' -replace '(?<=SET\(CMAKE_C_FLAGS_DEBUG .)\/D \DEBUG \/Od \/Z7', '-g' -replace '(?<=SET\(CMAKE_C_FLAGS_RELEASE .)\/D NDEBUG \/O2', '-O2' | Out-File -encoding ASCII CMakeLists.txt >> "$logFile" 2>&1
-  New-Item build -ItemType Directory >> "$logFile" 2>&1
+  if (!(Test-Path -Path "build" -PathType Container)) {
+    Step "Creating yajl build path"
+    New-Item build -ItemType Directory >> "$logFile" 2>&1
+  }
   Set-Location build
   Step "running cmake"
-  cmake -G "MinGW Makefiles" ..  >> "$logFile" 2>&1
+  exec "cmake" @("-G", "`"MinGW Makefiles`"", "..")
   RunMake
   Step "installing"
-  COPY yajl-2.0.1\lib\* $Env:MINGW_BASE_DIR\lib >> "$logFile" 2>&1
-  XCOPY /S /I /Q yajl-2.0.1\include $Env:MINGW_BASE_DIR\include >> "$logFile" 2>&1
+  Copy-Item "yajl-2.0.1\lib\*" "$Env:MINGW_BASE_DIR\lib"
+  exec "XCOPY" @("/S", "/I", "/Q", "yajl-2.0.1\include", "$Env:MINGW_BASE_DIR\include")
   $Env:Path = $ShPath
 }
 
@@ -103,12 +210,12 @@ function InstallLua() {
   DownloadFile "https://github.com/Tieske/luawinmake/archive/master.zip" "luawinmake.zip"
   ExtractZip "luawinmake.zip" "luawinmake"
   Step "copying luawinmake files"
-  XCOPY /S /I /Q "$workingBaseDir\luawinmake\luawinmake-master\etc" "$workingBaseDir\lua-5.1.5\lua-5.1.5\etc" >> "$logFile" 2>&1
+  exec "XCOPY" @("/Y", "/S", "/I", "/Q", "$workingBaseDir\luawinmake\luawinmake-master\etc", "$workingBaseDir\lua-5.1.5\lua-5.1.5\etc")
   Set-Location lua-5.1.5\lua-5.1.5
   Step "compiling lua"
-  .\etc\winmake >> "$logFile" 2>&1
+  exec "etc\winmake"
   Step "installing lua"
-  .\etc\winmake install $Env:MINGW_BASE_DIR >> "$logFile" 2>&1
+  exec "etc\winmake" @("install", "$Env:MINGW_BASE_DIR")
 }
 
 function InstallPcre() {
@@ -138,8 +245,8 @@ function InstallZlib() {
   $Env:LIBRARY_PATH = "$Env:MINGW_BASE_DIR\lib"
   $Env:BINARY_PATH = "$Env:MINGW_BASE_DIR\bin"
   RunMakeInstall "win32/Makefile.gcc"
-  COPY zlib1.dll $Env:MINGW_BASE_DIR\bin >> "$logFile" 2>&1
-  COPY libz.dll.a $Env:MINGW_BASE_DIR\lib >> "$logFile" 2>&1
+  Copy-Item "zlib1.dll" "$Env:MINGW_BASE_DIR\bin"
+  Copy-Item "libz.dll.a" "$Env:MINGW_BASE_DIR\lib"
 }
 
 function InstallLibzip() {
@@ -149,13 +256,13 @@ function InstallLibzip() {
   RunConfigure
   RunMake
   RunMakeInstall
-  COPY lib\zipconf.h $Env:MINGW_BASE_DIR\include >> "$logFile" 2>&1
+  Copy-Item "lib\zipconf.h" "$Env:MINGW_BASE_DIR\include"
 }
 
 function InstallZziplib() {
   DownloadFile "https://sourceforge.net/projects/zziplib/files/zziplib13/0.13.62/zziplib-0.13.62.tar.bz2/download" "zziplib-0.13.62.tar.bz2"
   ExtractTar "zziplib-0.13.62.tar.bz2" "zziplib"
-  cd zziplib\zziplib-0.13.62
+  Set-Location zziplib\zziplib-0.13.62
   Step "changing configure script"
   (Get-Content configure -Raw) -replace 'uname -msr', 'uname -ms' | Out-File -encoding ASCII configure >> "$logFile" 2>&1
   RunConfigure "--disable-mmap --prefix=$Env:MINGW_BASE_DIR_BASH"
@@ -168,7 +275,7 @@ function InstallLuarocks() {
   ExtractZip "luarocks-2.4.0-win32.zip" "luarocks"
   Set-Location luarocks\luarocks-2.4.0-win32
   Step "installing luarocks"
-  .\install.bat /P C:\LuaRocks /MW /Q >> "$logFile" 2>&1
+  exec ".\install.bat" @("/P", "C:\LuaRocks", "/MW", "/Q")
   Set-Location \LuaRocks\lua\luarocks
   Step "changing luarocks config"
   (Get-Content cfg.lua) -replace 'mingw32-gcc', 'gcc' | Out-File -encoding ASCII cfg.lua >> "$logFile" 2>&1
@@ -178,13 +285,13 @@ function InstallLuaModules(){
   StartPart "Installing lua modules"
   Set-Location \LuaRocks
   Step "installing lfs"
-  .\luarocks install LuaFileSystem >> "$logFile" 2>&1
+  exec ".\luarocks" @("install", "LuaFileSystem")
   Step "installing luasql.sqlite3"
-  .\luarocks install LuaSQL-SQLite3 SQLITE_INCDIR="$Env:MINGW_BASE_DIR\include" SQLITE_LIBDIR="$Env:MINGW_BASE_DIR\lib" >> "$logFile" 2>&1
+  exec ".\luarocks" @("install", "LuaSQL-SQLite3", "SQLITE_INCDIR=`"$Env:MINGW_BASE_DIR\include`"", "SQLITE_LIBDIR=`"$Env:MINGW_BASE_DIR\lib`"")
   Step "installing rex.pcre"
-  .\luarocks install lrexlib-pcre PCRE_LIBDIR="$Env:MINGW_BASE_DIR\lib" PCRE_INCDIR="$Env:MINGW_BASE_DIR\include" >> "$logFile" 2>&1
+  exec ".\luarocks" @("install", "lrexlib-pcre", "PCRE_LIBDIR=`"$Env:MINGW_BASE_DIR\lib`"", "PCRE_INCDIR=`"$Env:MINGW_BASE_DIR\include`"")
   Step "installing lua-utf8"
-  .\luarocks install luautf8 >> "$logFile" 2>&1
+  exec ".\luarocks" @("install", "luautf8")
 
   Step "installing luazip"
   Set-Location "$workingBaseDir"
@@ -192,30 +299,34 @@ function InstallLuaModules(){
   ExtractZip "luazip.zip" "luazip"
   Set-Location luazip\luazip-master
   Step "installing luazip"
-  gcc -O2 -c -o src/luazip.o -I"$Env:MINGW_BASE_DIR/include" src/luazip.c >> "$logFile" 2>&1
-  gcc -shared -o zip.dll src/luazip.o -L"$Env:MINGW_BASE_DIR/lib" -lzzip -lz "$Env:MINGW_BASE_DIR/bin/lua51.dll" -lm >> "$logFile" 2>&1
+  exec "gcc" @("-O2", "-c", "-o", "src/luazip.o", "-I`"$Env:MINGW_BASE_DIR/include`"", "src/luazip.c")
+  exec "gcc" @("-shared", "-o", "zip.dll", "src/luazip.o", "-L`"$Env:MINGW_BASE_DIR/lib`"", "-lzzip", "-lz", "`"$Env:MINGW_BASE_DIR/bin/lua51.dll`"", "-lm")
   FinishPart "Installing lua modules"
-}
-
-# This is no longer needed - we selectively pull in the wanted submodules as required:
-# git submodule update --init --recursive
-
-if (-not $(Test-Path "$workingBaseDir")) {
-    New-Item "$workingBaseDir" -ItemType "directory"
 }
 
 # install dependencies
 
-$Env:PATH=$ShPath
+CheckAndInstall "7z" "C:\Program Files\7-Zip\7z.exe" { InstallSevenZ }
+CheckAndInstall "cmake" "$CMakePath\cmake.exe" { InstallCmake }
+CheckAndInstall "MSYS" "C:\MinGW\msys\1.0\bin\bash.exe" { InstallMsys }
+CheckAndInstall "Boost" "C:\Libraries\boost_1_60_0\bootstrap.bat" { InstallBoost }
+CheckAndInstall "Qt" "$Env:QT_BASE_DIR\bin\qmake.exe" { InstallQt }
+CheckAndInstall "Python" "C:\Python27\python.exe" { InstallPython }
 
-CheckAndInstall "openssl" "$workingBaseDir\openssl-1.0.2l\ssleay32.dll" { & InstallOpenssl }
-CheckAndInstall "hunspell" "$Env:MINGW_BASE_DIR\bin\libhunspell-1.4-0.dll" { & InstallHunspell }
-CheckAndInstall "yajl" "$Env:MINGW_BASE_DIR\lib\libyajl.dll" { & InstallYajl }
-CheckAndInstall "lua" "$Env:MINGW_BASE_DIR\bin\lua51.dll" { & InstallLua }
-CheckAndInstall "pcre" "$Env:MINGW_BASE_DIR\bin\libpcre-1.dll" { & InstallPcre }
-CheckAndInstall "sqlite" "$Env:MINGW_BASE_DIR\lib\libsqlite3-0.dll" { & InstallSqlite }
-CheckAndInstall "zlib" "$Env:MINGW_BASE_DIR\bin\zlib1.dll" { & InstallZlib }
-CheckAndInstall "libzip" "$Env:MINGW_BASE_DIR\include\zipconf.h" { & InstallLibzip }
-CheckAndInstall "zziplib" "$Env:MINGW_BASE_DIR\lib\libzzip.la" { & InstallZziplib }
-CheckAndInstall "luarocks" "C:\LuaRocks\luarocks.bat" { & InstallLuarocks }
+# Adapt the PATH variable again as we may have installed some dependencies just now and can determine their location.
+SetMingwBaseDir "$logFile"
+$ShPath = "$Env:MINGW_BASE_DIR\bin;C:\Python27;$Env:PATH"
+$NoShPath = ($ShPath.Split(';') | Where-Object { $_ -ne 'C:\MinGW\msys\1.0\bin' } | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';'
+$Env:PATH = $ShPath
+
+CheckAndInstall "openssl" "$workingBaseDir\openssl-1.0.2l\ssleay32.dll" { InstallOpenssl }
+CheckAndInstall "hunspell" "$Env:MINGW_BASE_DIR\bin\libhunspell-1.4-0.dll" { InstallHunspell }
+CheckAndInstall "yajl" "$Env:MINGW_BASE_DIR\lib\libyajl.dll" { InstallYajl }
+CheckAndInstall "lua" "$Env:MINGW_BASE_DIR\bin\lua51.dll" { InstallLua }
+CheckAndInstall "pcre" "$Env:MINGW_BASE_DIR\bin\libpcre-1.dll" { InstallPcre }
+CheckAndInstall "sqlite" "$Env:MINGW_BASE_DIR\bin\libsqlite3-0.dll" { InstallSqlite }
+CheckAndInstall "zlib" "$Env:MINGW_BASE_DIR\bin\zlib1.dll" { InstallZlib }
+CheckAndInstall "libzip" "$Env:MINGW_BASE_DIR\include\zipconf.h" { InstallLibzip }
+CheckAndInstall "zziplib" "$Env:MINGW_BASE_DIR\lib\libzzip.la" { InstallZziplib }
+CheckAndInstall "luarocks" "C:\LuaRocks\luarocks.bat" { InstallLuarocks }
 InstallLuaModules
---
+++ CI/appveyor.set-environment.ps1
@@ -0,0 +1,37 @@
+function SetQtBaseDir([string] $logFile) {
+  if(!(Test-Path Env:QT_BASE_DIR)){
+    try
+    {
+      $Env:QT_BASE_DIR = Get-Command "qmake.exe" -ErrorAction Stop | Select-Object -ExpandProperty definition | Split-Path -Parent | Split-Path -Parent
+    }
+    catch
+    {
+      if(Test-Path "C:\Qt\5.6.3\mingw49_32\bin\qmake.exe"){
+        $Env:QT_BASE_DIR = "C:\Qt\5.6.3\mingw49_32"
+      }
+      else
+      {
+        $Env:QT_BASE_DIR = "C:\Qt\5.6.3\mingw49_32"
+      }
+    }
+  }
+  Write-Output "Using $Env:QT_BASE_DIR as QT base directory." | Tee-Object -File "$logFile" -Append
+}
+
+function SetMingwBaseDir([string] $logFile) {
+  if(!(Test-Path Env:MINGW_BASE_DIR)){
+    $tmp = $Env:QT_BASE_DIR.Split("\\")
+    $tmp[-2] = "Tools"
+    $tmp[-1] = $tmp[-1] -replace "_32", "*"
+    $tmp = $tmp -join "\" | Resolve-Path
+    if($tmp -is [array]){
+      $tmp = $tmp[-1]
+    }
+    $Env:MINGW_BASE_DIR = $tmp
+  }
+  Write-Output "Using $Env:MINGW_BASE_DIR as MinGW base directory." | Tee-Object -File "$logFile" -Append
+
+  if(!(Test-Path Env:MINGW_BASE_DIR_BASH)){
+    $Env:MINGW_BASE_DIR_BASH = $Env:MINGW_BASE_DIR -replace "\\", "/" -replace "C:", "/c"
+  }
+}
