a353772166024312e54e393c5450c32fcc8d8a63
Xabre <githzerai06@gmail.com>
2018-04-13 16:24:17 +0200

    Fix #1612

--- src/TRoomDB.cpp
+++ src/TRoomDB.cpp
@@ -19,7 +19,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-
+#include <QRegularExpression>
 #include "TRoomDB.h"
 
 #include "Host.h"
