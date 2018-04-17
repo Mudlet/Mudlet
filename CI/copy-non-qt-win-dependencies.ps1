b66ab4a753ea1784cc9359999a83cc0d9735ce97
keneanung <keneanung@googlemail.com>
2018-04-17 16:32:58 +0200

    Copy updated libhunspell

--- CI/copy-non-qt-win-dependencies.ps1
+++ CI/copy-non-qt-win-dependencies.ps1
@@ -3,7 +3,7 @@ COPY C:\src\lua-5.1.5\lua-5.1.5\src\lua51.dll .
 COPY $Env:MINGW_BASE_DIR\bin\libeay32.dll .
 COPY $Env:MINGW_BASE_DIR\bin\ssleay32.dll .
 COPY $Env:MINGW_BASE_DIR\bin\libzip-5.dll .
-COPY $Env:MINGW_BASE_DIR\bin\libhunspell-1.4-0.dll .
+COPY $Env:MINGW_BASE_DIR\bin\libhunspell-1.6-2.dll .
 COPY $Env:MINGW_BASE_DIR\bin\libpcre-1.dll .
 COPY $Env:MINGW_BASE_DIR\bin\libsqlite3-0.dll .
 COPY $Env:MINGW_BASE_DIR\bin\zlib1.dll .
