339e9692d4a8decc9b14923054af55c9a355064e
Vadim Peretokin <vperetokin@gmail.com>
2018-05-03 17:05:15 +0200

    Rename & change initialisation

--- src/updater.cpp
+++ src/updater.cpp
@@ -38,13 +38,12 @@
 //   and promptly quits. Installer updates Mudlet and launches Mudlet when its done
 // mac: handled completely outside of Mudlet by Sparkle
 
-Updater::Updater(QObject* parent, QSettings* settings) : QObject(parent), mUpdateInstalled(false)
+Updater::Updater(QObject* parent, QSettings* settings) : QObject(parent), mUpdateInstalled(false), mpInstallOrRestart(new QPushButton(tr("Update")))
 {
     Q_ASSERT_X(settings, "updater", "QSettings object is required for the updater to work");
     this->settings = settings;
 
     feed = new dblsqd::Feed(QStringLiteral("https://feeds.dblsqd.com/MKMMR7HNSP65PquQQbiDIw"), QStringLiteral("release"));
-    installOrRestartButton = new QPushButton(tr("Update"));
 }
 
 // start the update process and figure out what needs to be done
@@ -150,8 +149,8 @@ void Updater::setupOnWindows()
 
     // finally, create the dblsqd objects. Constructing the UpdateDialog triggers the update check
     updateDialog = new dblsqd::UpdateDialog(feed, updateAutomatically() ? dblsqd::UpdateDialog::Manual : dblsqd::UpdateDialog::OnLastWindowClosed, nullptr, settings);
-    installOrRestartButton->setText(tr("Update"));
-    updateDialog->addInstallButton(installOrRestartButton);
+    mpInstallOrRestart->setText(tr("Update"));
+    updateDialog->addInstallButton(mpInstallOrRestart);
     connect(updateDialog, &dblsqd::UpdateDialog::installButtonClicked, this, &Updater::installOrRestartClicked);
 }
 
@@ -212,8 +211,8 @@ void Updater::setupOnLinux()
 
     // finally, create the dblsqd objects. Constructing the UpdateDialog triggers the update check
     updateDialog = new dblsqd::UpdateDialog(feed, updateAutomatically() ? dblsqd::UpdateDialog::Manual : dblsqd::UpdateDialog::OnLastWindowClosed, nullptr, settings);
-    installOrRestartButton->setText(tr("Update"));
-    updateDialog->addInstallButton(installOrRestartButton);
+    mpInstallOrRestart->setText(tr("Update"));
+    updateDialog->addInstallButton(mpInstallOrRestart);
     connect(updateDialog, &dblsqd::UpdateDialog::installButtonClicked, this, &Updater::installOrRestartClicked);
 }
 
@@ -303,8 +302,8 @@ void Updater::installOrRestartClicked(QAbstractButton* button, QString filePath)
 #elif defined(Q_OS_WIN32)
         finishSetup();
 #endif
-        installOrRestartButton->setText(tr("Restart to apply update"));
-        installOrRestartButton->setEnabled(true);
+        mpInstallOrRestart->setText(tr("Restart to apply update"));
+        mpInstallOrRestart->setEnabled(true);
     });
     watcher->setFuture(future);
 #endif // !Q_OS_MACOS
