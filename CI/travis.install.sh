5c47b6724b65fef61a3958cea972f44bf0caa26b
Keneanung <keneanung@googlemail.com>
2018-05-08 12:44:59 +0200

    Fix broken path

--- CI/travis.install.sh
+++ CI/travis.install.sh
@@ -2,11 +2,11 @@
 
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   echo Install on OSX.
-  source /CI/travis.osx.install.sh;
+  source CI/travis.osx.install.sh;
 fi
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
   echo Install on linux.
-  source /CI/travis.linux.install.sh;
+  source CI/travis.linux.install.sh;
 fi
 if [ ! -z "${CXX}" ]; then
   echo "Testing (possibly updated) compiler version:"
