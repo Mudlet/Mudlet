a462f6473de1f49da2d606d91ddaf6bad53e3d66
Keneanung <keneanung@googlemail.com>
2018-05-08 13:12:08 +0200

    Temporary: build all the things

--- CI/travis.compile.sh
+++ CI/travis.compile.sh
@@ -2,10 +2,12 @@
 compile_line=()
 if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
   if [ "${CC}" = "clang" ] || [ "${Q_OR_C_MAKE}" = "cmake" ] || [ "${QT_VERSION}" = "56" ]; then
-    echo Job not executed under cron run
-    exit 0
+#    echo Job not executed under cron run
+#    exit 0
+    echo "Do nothing"
+  else
+    compile_line+=(cov-build --dir cov-int)
   fi
-  compile_line+=(cov-build --dir cov-int)
 fi
 
 cd build
