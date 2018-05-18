c600bf8102ff0940f21ca88dd58aa8a4fcec9af2
Vadim Peretokin <vperetokin@gmail.com>
2018-05-18 20:49:36 +0200

    New translations mudlet_en.ts (English, United States)

--- translations/mudlet_en.ts
+++ translations/mudlet_en.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en-GB" sourcelanguage="en">
+<TS version="2.1" language="en-US" sourcelanguage="en">
   <context>
     <name>Host</name>
     <message>
