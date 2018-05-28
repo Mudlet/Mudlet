83234a72f450280d6e8aec0146d2304f939a6a61
Vadim Peretokin <vperetokin@gmail.com>
2018-05-28 12:14:06 +0200

    Added in handlers

--- src/discord.cpp
+++ src/discord.cpp
@@ -1,9 +1,66 @@
 #include "discord.h"
 
+#include "pre_guard.h"
 #include "../3rdparty/discord/discord-rpc-linux/discord-rpc/linux-dynamic/include/discord_register.h"
 #include "../3rdparty/discord/discord-rpc-linux/discord-rpc/linux-dynamic/include/discord_rpc.h"
+#include "post_guard.h"
+
+static const char* APPLICATION_ID = "450571881909583884";
 
 Discord::Discord(QObject *parent) : QObject(parent)
 {
-    qDebug() << "aww yeah";
+    DiscordEventHandlers handlers;
+    memset(&handlers, 0, sizeof(handlers));
+    handlers.ready = handleDiscordReady;
+    handlers.errored = handleDiscordError;
+    handlers.disconnected = handleDiscordDisconnected;
+    handlers.joinGame = handleDiscordJoinGame;
+    handlers.spectateGame = handleDiscordSpectateGame;
+    handlers.joinRequest = handleDiscordJoinRequest;
+
+    // Discord_Initialize(const char* applicationId, DiscordEventHandlers* handlers, int autoRegister, const char* optionalSteamId)
+    Discord_Initialize(APPLICATION_ID, &handlers, 1, "1234");
+    qDebug() << "Discord_Initialize called okay";
+}
+
+
+Discord::~Discord()
+{
+    Discord_Shutdown();
+}
+
+void Discord::handleDiscordReady(const DiscordUser* request)
+{
+    qDebug() << "Discord handleDiscordReady!";
+}
+
+
+void Discord::handleDiscordDisconnected(int errorCode, const char* message)
+{
+
+    qDebug() << "Discord handleDiscordDisconnected!";
+}
+
+void Discord::handleDiscordError(int errorCode, const char* message)
+{
+
+    qDebug() << "Discord handleDiscordError!";
+}
+
+void Discord::handleDiscordJoinGame(const char* joinSecret)
+{
+
+    qDebug() << "Discord handleDiscordJoinGame!";
+}
+
+void Discord::handleDiscordSpectateGame(const char* spectateSecret)
+{
+
+    qDebug() << "Discord handleDiscordSpectateGame!";
+}
+
+void Discord::handleDiscordJoinRequest(const DiscordUser* request)
+{
+
+    qDebug() << "Discord handleDiscordJoinRequest!";
 }
--- src/discord.h
+++ src/discord.h
@@ -4,13 +4,25 @@
 #include "pre_guard.h"
 #include <QObject>
 #include <QDebug>
+#include "../3rdparty/discord/discord-rpc-linux/discord-rpc/linux-dynamic/include/discord_register.h"
+#include "../3rdparty/discord/discord-rpc-linux/discord-rpc/linux-dynamic/include/discord_rpc.h"
 #include "post_guard.h"
 
 class Discord : public QObject
 {
     Q_OBJECT
+
 public:
-    explicit Discord(QObject *parent = nullptr);
+    explicit Discord(QObject *parent = nullptr);    
+    ~Discord() override;
+
+private:
+    static void handleDiscordReady(const DiscordUser* request);
+    static void handleDiscordDisconnected(int errorCode, const char* message);
+    static void handleDiscordError(int errorCode, const char* message);
+    static void handleDiscordJoinGame(const char* joinSecret);
+    static void handleDiscordSpectateGame(const char* spectateSecret);
+    static void handleDiscordJoinRequest(const DiscordUser* request);
 
 signals:
 
