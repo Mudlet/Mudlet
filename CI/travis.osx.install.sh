569da19a48ca83bd28bd74c67759086dee71aed1
keneanung <keneanung@googlemail.com>
2018-05-29 22:27:10 +0200

    Output what we are actually trying to install/upgrade

--- CI/travis.osx.install.sh
+++ CI/travis.osx.install.sh
@@ -8,6 +8,7 @@ set +e
 BREWS="boost cmake hunspell libzip libzzip lua51 pcre pkg-config qt5 yajl ccache pugixml"
 for i in $BREWS; do
   for RETRIES in $(seq 1 3); do
+    echo "Upgrading ${i}"
     brew outdated | grep -q $i && brew upgrade $i
     STATUS="$?"
     if [ "${STATUS}" -eq 0 ]; then
@@ -24,6 +25,7 @@ for i in $BREWS; do
 done
 for i in $BREWS; do
   for RETRIES in $(seq 1 3); do
+    echo "Installing ${i}"
     brew list | grep -q $i || brew install $i
     STATUS="$?"
     if [ "${STATUS}" -eq 0 ]; then
