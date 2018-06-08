3c878d97635d8a255c31357dae57d831ff827f05
Vadim Peretokin <vperetokin@gmail.com>
2018-06-08 06:42:33 +0200

    Copy Discord DLL into zip

--- CI/copy-non-qt-win-dependencies.ps1
+++ CI/copy-non-qt-win-dependencies.ps1
@@ -10,3 +10,4 @@ XCOPY /S /I /Q /Y ..\mudlet-lua mudlet-lua
 COPY ..\*.dic .
 XCOPY /S /I /Q /Y $Env:MINGW_BASE_DIR\lib\lua\5.1 .
 XCOPY /S /I /Q /Y ..\..\3rdparty\lcf lcf
+XCOPY /S /I /Q /Y ..\..\3rdparty\discord\discord-rpc-win\discord-rpc\win32-dynamic\lib\discord-rpc.dll discord-rpc.dll
