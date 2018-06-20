71cc07b7fe6e08a322a579ad8b49eb9e5c4ff725
keneanung <keneanung@googlemail.com>
2018-06-20 14:19:44 +0200

    Temp: test code signing on windows

--- CI/appveyor.after_success.ps1
+++ CI/appveyor.after_success.ps1
@@ -8,9 +8,9 @@ windeployqt.exe --release mudlet.exe
 
 Remove-Item * -include *.cpp, *.o
 
-if ("$Env:APPVEYOR_REPO_TAG" -eq "false") {
-  $DEPLOY_URL = "https://ci.appveyor.com/api/buildjobs/$Env:APPVEYOR_JOB_ID/artifacts/src%2Fmudlet.zip"
-} else {
+#if ("$Env:APPVEYOR_REPO_TAG" -eq "false") {
+#  $DEPLOY_URL = "https://ci.appveyor.com/api/buildjobs/$Env:APPVEYOR_JOB_ID/artifacts/src%2Fmudlet.zip"
+#} else {
   git clone https://github.com/Mudlet/installers.git C:\projects\installers
   cd C:\projects\installers\windows
   nuget install secure-file -ExcludeVersion
@@ -52,7 +52,7 @@ if ("$Env:APPVEYOR_REPO_TAG" -eq "false") {
   DEPLOY_URL="http://www.mudlet.org/wp-content/files/Mudlet-${VERSION}-linux-x64.AppImage.tar"
   fi #>
 
-}
+#}
 
 if (Test-Path Env:APPVEYOR_PULL_REQUEST_NUMBER) {
   $prId = " ,#$Env:APPVEYOR_PULL_REQUEST_NUMBER"
