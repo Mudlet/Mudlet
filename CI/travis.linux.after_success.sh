9cd496f621213e5d3f23576194627c42c3cbd367
keneanung <keneanung@googlemail.com>
2018-07-27 12:11:45 +0200

    Create and upload source tarballs on release

--- CI/travis.linux.after_success.sh
+++ CI/travis.linux.after_success.sh
@@ -70,6 +70,17 @@ if [ "${Q_OR_C_MAKE}" = "qmake" ] && [ "${CC}" = "gcc" ]; then
 
     scp -i /tmp/mudlet-deploy-key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "Mudlet-${VERSION}-linux-x64.AppImage.tar" "keneanung@mudlet.org:${DEPLOY_PATH}"
     DEPLOY_URL="http://www.mudlet.org/wp-content/files/Mudlet-${VERSION}-linux-x64.AppImage.tar"
+    
+    # generate and deploy source tarball
+    cd "${HOME}"
+    # get the archive script
+    wget https://raw.githubusercontent.com/meitar/git-archive-all.sh/master/git-archive-all.sh
+    
+    cd "${TRAVIS_BUILD_DIR}"
+    # generate and upload the tarball
+    bash ../git-archive-all.sh "Mudlet-${VERSION}.tar"
+    xz "Mudlet-${VERSION}.tar"
+    scp -i /tmp/mudlet-deploy-key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "Mudlet-${VERSION}.tar.xz" "keneanung@mudlet.org:${DEPLOY_PATH}"
   fi
   export DEPLOY_URL
 fi
