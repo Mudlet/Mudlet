33eb0b639176f98c02668e92d68f7be338f19af9
Vadim Peretokin <vperetokin@gmail.com>
2018-08-29 21:24:43 +0200

    Fix path to copy the DLL from

--- CI/copy-non-qt-win-dependencies.ps1
+++ CI/copy-non-qt-win-dependencies.ps1
@@ -10,4 +10,4 @@ XCOPY /S /I /Q /Y ..\mudlet-lua mudlet-lua
 COPY ..\*.dic .
 XCOPY /S /I /Q /Y $Env:MINGW_BASE_DIR\lib\lua\5.1 .
 XCOPY /S /I /Q /Y ..\..\3rdparty\lcf lcf
-COPY ..\..\3rdparty\discord\discord-rpc-win\discord-rpc\win32-dynamic\lib\discord-rpc.dll discord-rpc.dll
+COPY ..\..\3rdparty\discord\rpc\lib\discord-rpc.dll discord-rpc.dll
