15c86a6800ec1a5ee57ac6f0f0fd2691f7c8c81a
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2018-10-22 11:31:46 +0200

    New translations mudlet.ts (Chinese Simplified)

--- translations/translated/mudlet_zh_CN.ts
+++ translations/translated/mudlet_zh_CN.ts
@@ -2211,7 +2211,17 @@ suffix where each &quot;###&quot; is an increasing number; you may wish to
 change these, perhaps by adding more meaningful area names but it is
 entirely up to you what is used, other then you will not be able to
 set one area&apos;s name to that of another that exists at the time.</source>
-      <translation type="unfinished"/>
+      <translation>[信息] - 过去, 由于没有考虑某些情况，Mudlet允许
+地图中存在未命名区域.
+这会让事情变得混乱, 现在已经不允许这么做了.
+  为了解决这个问题, 如果一个区域没有名字(或者等以后
+再命名)系统将自动分配名称 &quot;%1&quot;.
+  如果这样的未命名区域有很多个, 那么除了第一个被处理的区域之外所有的
+区域名称都以 &quot;_###&quot; 式的后缀结尾, 其中 &quot;###&quot; 是一个递增
+数字; 或许你想改变这些后缀, 试试把后缀改成
+一 &quot;(子区域名称)&quot; 至于具体怎么做, 完全取决于你,
+只是之后你就不能把一个区域的名称设置成
+与另一个已存在的区域相同的名称了.</translation>
     </message>
     <message>
       <location filename="../src/TRoomDB.cpp" line="1241"/>
