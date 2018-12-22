bd1c60fbb35673f64d567988d4d4fadaf6d2425c
keneanung <keneanung@googlemail.com>
2018-12-22 21:35:49 +0100

    Modernize FindHUNSPELL
    It now exports a target so we don't need to manually add variables to
    the Mudlet binary and uses pkg-config enhanced search functionality
    if the tool is available.

--- cmake/FindHUNSPELL.cmake
+++ cmake/FindHUNSPELL.cmake
@@ -1,55 +1,89 @@
 # Locate HUNSPELL library
+# This module exports the following targets
+#
+# HUNSPELL::HUNSPELL
+#
 # This module defines
-#  HUNSPELL_FOUND, if false, do not try to link to HUNSPELL 
+#  HUNSPELL_FOUND, if false, do not try to link to HUNSPELL
 #  HUNSPELL_LIBRARIES
-#  HUNSPELL_INCLUDE_DIR, where to find hunspell/*.h 
+#  HUNSPELL_INCLUDE_DIR, where to find hunspell/*.h
 
+FIND_PACKAGE(PkgConfig)
+
+PKG_SEARCH_MODULE(PC_HUNSPELL hunspell libhunspell)
 
 FIND_PATH(HUNSPELL_INCLUDE_DIR hunspell/hunspell.h
   HINTS
-  ${HUNSPELL_DIR} $ENV{HUNSPELL_DIR}
-  PATH_SUFFIXES include
+    ${HUNSPELL_DIR}
+    $ENV{HUNSPELL_DIR}
+    ${PC_HUNSPELL_INCLUDE_DIRS}
+  PATH_SUFFIXES
+    include
   PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw # Fink
-  /opt/local # DarwinPorts
-  /opt/csw # Blastwave
-  /opt
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /sw # Fink
+    /opt/local # DarwinPorts
+    /opt/csw # Blastwave
+    /opt
 )
 
-FIND_LIBRARY(HUNSPELL_LIBRARY_RELEASE 
-  NAMES hunspell libhunspell hunspell-1.6 hunspell-1.5 hunspell-1.4 hunspell-1.3
+FIND_LIBRARY(HUNSPELL_LIBRARY_RELEASE
+  NAMES
+    hunspell
+    libhunspell
+    hunspell-1.7
+    hunspell-1.6
+    hunspell-1.5
+    hunspell-1.4
+    hunspell-1.3
   HINTS
-  ${HUNSPELL_DIR} $ENV{HUNSPELL_DIR}
-  PATH_SUFFIXES lib64 lib
+    ${HUNSPELL_DIR}
+    $ENV{HUNSPELL_DIR}
+    ${PC_HUNSPELL_LIBRARY_DIRS}
+    ${PC_HUNSPELL_LIBRARY_DIR}
+  PATH_SUFFIXES
+    lib64
+    lib
   PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw
-  /opt/local
-  /opt/csw
-  /opt
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /sw
+    /opt/local
+    /opt/csw
+    /opt
 )
 
-FIND_LIBRARY(HUNSPELL_LIBRARY_DEBUG 
-  NAMES hunspelld libhunspelld hunspelld-1.6 hunspelld-1.5 hunspelld-1.4 hunspelld-1.3
+FIND_LIBRARY(HUNSPELL_LIBRARY_DEBUG
+  NAMES
+    hunspelld
+    libhunspelld
+    hunspelld-1.7
+    hunspelld-1.6
+    hunspelld-1.5
+    hunspelld-1.4
+    hunspelld-1.3
   HINTS
-  ${HUNSPELL_DIR} $ENV{HUNSPELL_DIR}
-  PATH_SUFFIXES lib64 lib
+    ${HUNSPELL_DIR}
+    $ENV{HUNSPELL_DIR}
+    ${PC_HUNSPELL_LIBRARY_DIRS}
+    ${PC_HUNSPELL_LIBRARY_DIR}
+  PATH_SUFFIXES
+    lib64
+    lib
   PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw
-  /opt/local
-  /opt/csw
-  /opt
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /sw
+    /opt/local
+    /opt/csw
+    /opt
 )
 
 IF(HUNSPELL_LIBRARY_DEBUG AND HUNSPELL_LIBRARY_RELEASE)
@@ -60,17 +94,47 @@ ELSEIF(HUNSPELL_LIBRARY_DEBUG)
   SET(HUNSPELL_LIBRARY ${HUNSPELL_LIBRARY_DEBUG} )
 ENDIF()
 
-SET( HUNSPELL_STATIC OFF CACHE BOOL "Use HUNSPELL static libraries.")
+IF(PC_HUNSPELL_hunspell_FOUND)
+  SET(HUNSPELL_VERSION ${PC_HUNSPELL_hunspell_VERSION})
+ELSEIF(PC_HUNSPELL_libhunspell_FOUND)
+  SET(HUNSPELL_VERSION ${PC_HUNSPELL_libhunspell_VERSION})
+ELSE()
+  SET(HUNSPELL_VERSION ${PC_HUNSPELL_VERSION})
+ENDIF()
 
-IF(HUNSPELL_STATIC)
-  ADD_DEFINITIONS(-DHUNSPELL_STATIC)
-ENDIF(HUNSPELL_STATIC)
+
+SET( HUNSPELL_STATIC OFF CACHE BOOL "Use HUNSPELL static libraries.")
 
 SET( HUNSPELL_LIBRARIES "${HUNSPELL_LIBRARY}" CACHE STRING "HUNSPELL Libraries")
 
 INCLUDE(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set HUNSPELL_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set HUNSPELL_FOUND to TRUE if
 # all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(HUNSPELL  DEFAULT_MSG  HUNSPELL_LIBRARIES HUNSPELL_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(HUNSPELL
+  REQUIRED_VARS
+    HUNSPELL_LIBRARIES
+    HUNSPELL_INCLUDE_DIR
+  VERSION_VAR
+    HUNSPELL_VERSION
+)
 
 MARK_AS_ADVANCED(HUNSPELL_INCLUDE_DIR HUNSPELL_LIBRARIES HUNSPELL_LIBRARY HUNSPELL_LIBRARY_RELEASE HUNSPELL_LIBRARY_DEBUG)
+
+IF(HUNSPELL_FOUND AND NOT TARGET HUNSPELL::HUNSPELL)
+  ADD_LIBRARY(HUNSPELL::STATIC STATIC IMPORTED)
+  SET_TARGET_PROPERTIES(HUNSPELL::STATIC PROPERTIES
+    INTERFACE_COMPILE_DEFINITIONS
+      -DHUNSPELL_STATIC
+    IMPORTED_LOCATION
+      "${HUNSPELL_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES
+      "${HUNSPELL_INCLUDE_DIR}"
+  )
+  ADD_LIBRARY(HUNSPELL::SHARED SHARED IMPORTED)
+  SET_TARGET_PROPERTIES(HUNSPELL::SHARED PROPERTIES
+    IMPORTED_LOCATION
+      "${HUNSPELL_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES
+      "${HUNSPELL_INCLUDE_DIR}"
+  )
+ENDIF()
