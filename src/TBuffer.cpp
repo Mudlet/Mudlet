b429704e7f86e6411890c7d61ac7d249a2dc4b77
Richard Moffitt <richard.g.moffitt@gmail.com>
2019-01-13 23:41:29 -0600

    Tbuffer#2130-patch
    erronoeous fixed string in MXP handler fixed

--- src/TBuffer.cpp
+++ src/TBuffer.cpp
@@ -1791,7 +1791,7 @@ void TBuffer::translateToPlainText(std::string& incoming, const bool isFromServe
                     mIgnoreTag = false;
                     mSkip.clear();
                     ch = '&';
-                } else if (mSkip == "&quot;" && ch == ';') {
+                } else if (mSkip == "&quot;" ) {
                     mIgnoreTag = false;
                     mSkip.clear();
                     ch = '"';
