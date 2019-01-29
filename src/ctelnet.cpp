ff3f2add184c343567494362132a4f63e961b6bf
Kebap <leckerkebap@gmx.li>
2019-01-30 00:23:04 +0100

    Align lines 1331 and 1610

--- src/ctelnet.cpp
+++ src/ctelnet.cpp
@@ -1328,7 +1328,7 @@ void cTelnet::processTelnetCommand(const string& command)
                 packageName.remove(QLatin1Char('\\'));
                 packageName.remove(QLatin1Char('.'));
 
-                postMessage(tr("[ INFO ]  - Server offers downloadable GUI (url='%1') (package='%2')...").arg(url, packageName));
+                postMessage(tr("[ INFO ]  - Server offers downloadable GUI (url='%1') (package='%2').").arg(url, packageName));
                 if (mpHost->mInstalledPackages.contains(packageName)) {
                     postMessage(tr("[  OK  ]  - Package is already installed."));
                     return;
