fac6b77af970064323f12f898c8e1a3dae8c70b5
Richard Moffitt <richard.g.moffitt@gmail.com>
2019-02-09 19:37:04 -0600

    more improvement to lambda function for easy readability.

--- src/dlgTriggerEditor.cpp
+++ src/dlgTriggerEditor.cpp
@@ -6957,28 +6957,28 @@ void dlgTriggerEditor::slot_script_main_area_delete_handler()
 
 void dlgTriggerEditor::slot_script_main_area_add_handler()
 {
-    auto createItemList = [](auto * mpScriptsMainArea) {
+    auto createItemList = [=] () {
         auto pItem = new QListWidgetItem;
         pItem->setText(mpScriptsMainArea->lineEdit_script_event_handler_entry->text());
         mpScriptsMainArea->listWidget_script_registered_event_handlers->addItem(pItem);
-        return pItem;
+        //return pItem;
     };
 
     mpScriptsMainArea->trimEventHandlerName();
     if (mIsScriptsMainAreaEditHandler) {
         if (!mpScriptsMainAreaEditHandlerItem) {
             mIsScriptsMainAreaEditHandler = false;
-            auto pItem = createItemList(mpScriptsMainArea);
+            createItemList();
             return;
         }
         QListWidgetItem* pItem = mpScriptsMainArea->listWidget_script_registered_event_handlers->currentItem();
         if (!pItem) {
-            auto pItem = createItemList(mpScriptsMainArea);
+          createItemList();
         }
         mIsScriptsMainAreaEditHandler = false;
         mpScriptsMainAreaEditHandlerItem = nullptr;
     } else {
-        auto pItem = createItemList(mpScriptsMainArea);
+        createItemList();
     }
     mpScriptsMainArea->lineEdit_script_event_handler_entry->clear();
 }
