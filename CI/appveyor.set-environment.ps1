b3c7b4397f3c6e23ab0338814aac40a5f022cd8b
Vadim Peretokin <vperetokin@gmail.com>
2019-02-12 07:34:54 +0100

    Update hardcoded location in C:\Qt\5.12.1\mingw73_64 as well

--- CI/appveyor.set-environment.ps1
+++ CI/appveyor.set-environment.ps1
@@ -6,7 +6,7 @@ function SetQtBaseDir([string] $logFile) {
     }
     catch
     {
-      $Env:QT_BASE_DIR = "C:\Qt\5.11.0\mingw53_32"
+      $Env:QT_BASE_DIR = "C:\Qt\5.12.1\mingw73_64"
     }
   }
   Write-Output "Using $Env:QT_BASE_DIR as QT base directory." | Tee-Object -File "$logFile" -Append
