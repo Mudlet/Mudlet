cb8c2270f1585754cf6bba0354dc687de1926fcd
Vadim Peretokin <vperetokin@gmail.com>
2019-03-11 16:49:09 +0100

    Deploy unzipped AppImage alongside zipped

--- CI/travis.linux.after_success.sh
+++ CI/travis.linux.after_success.sh
@@ -68,7 +68,10 @@ if [ "${Q_OR_C_MAKE}" = "qmake" ] && [ "${CC}" = "gcc" ]; then
 
     tar -czvf "Mudlet-${VERSION}-linux-x64.AppImage.tar" "Mudlet.AppImage"
 
+    cp "Mudlet.AppImage" "Mudlet-${VERSION}-linux-x64.AppImage"
+
     scp -i /tmp/mudlet-deploy-key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "Mudlet-${VERSION}-linux-x64.AppImage.tar" "keneanung@mudlet.org:${DEPLOY_PATH}"
+    scp -i /tmp/mudlet-deploy-key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "Mudlet-${VERSION}-linux-x64.AppImage" "keneanung@mudlet.org:${DEPLOY_PATH}"
     DEPLOY_URL="http://www.mudlet.org/wp-content/files/Mudlet-${VERSION}-linux-x64.AppImage.tar"
 
 
