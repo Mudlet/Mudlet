62ff58aeab0de9d341736819e2a8ca28ee614665
Vadim Peretokin <vperetokin@gmail.com>
2019-03-12 09:12:00 +0100

    Deploy unzipped AppImage alongside zipped (#2407)
    * Deploy unzipped AppImage alongside zipped
    
    * Don't version the file, appimage.github.io wants it that way

--- CI/travis.linux.after_success.sh
+++ CI/travis.linux.after_success.sh
@@ -69,6 +69,8 @@ if [ "${Q_OR_C_MAKE}" = "qmake" ] && [ "${CC}" = "gcc" ]; then
     tar -czvf "Mudlet-${VERSION}-linux-x64.AppImage.tar" "Mudlet.AppImage"
 
     scp -i /tmp/mudlet-deploy-key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "Mudlet-${VERSION}-linux-x64.AppImage.tar" "keneanung@mudlet.org:${DEPLOY_PATH}"
+    # upload an unzipped, unversioned release for appimage.github.io
+    scp -i /tmp/mudlet-deploy-key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "Mudlet.AppImage" "keneanung@mudlet.org:${DEPLOY_PATH}"
     DEPLOY_URL="http://www.mudlet.org/wp-content/files/Mudlet-${VERSION}-linux-x64.AppImage.tar"
 
 
