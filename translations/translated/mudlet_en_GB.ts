ea41c51035f3c5ee9060bd33adbc7479e9b82fb0
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2019-04-04 22:04:52 +0200

    New translations mudlet.ts (English, United Kingdom)

--- translations/translated/mudlet_en_GB.ts
+++ translations/translated/mudlet_en_GB.ts
@@ -8663,19 +8663,25 @@ Starting... Have fun!
       <location filename="../src/mudlet.cpp" line="4450"/>
       <source>&lt;p&gt;About Mudlet&lt;/p&gt;&lt;p&gt;&lt;i&gt;%n update(s) is/are now available!&lt;/i&gt;&lt;p&gt;</source>
       <comment>This is the tooltip text for the &apos;About&apos; Mudlet main toolbar button when it has been changed by adding a menu which now contains the original &apos;About Mudlet&apos; action and a new one to access the manual update process</comment>
-      <translation type="unfinished"/>
+      <translation type="unfinished">
+        <numerusform>&lt;p&gt;About Mudlet&lt;/p&gt;&lt;p&gt;&lt;i&gt;%n updates are now available!&lt;/i&gt;&lt;p&gt;</numerusform>
+      </translation>
     </message>
     <message numerus="yes">
       <location filename="../src/mudlet.cpp" line="4468"/>
       <source>Review %n update(s)...</source>
       <comment>Review update(s) menu item, %n is the the count of how many updates are available</comment>
-      <translation type="unfinished"/>
+      <translation type="unfinished">
+        <numerusform>Review %n updates...</numerusform>
+      </translation>
     </message>
     <message numerus="yes">
       <location filename="../src/mudlet.cpp" line="4473"/>
       <source>&lt;p&gt;Review the update(s) available...&lt;/p&gt;</source>
       <comment>Tool-tip for review update(s) menu item, given that the count of how many updates are available is already shown in the menu, the %n parameter that is that number need not be used here</comment>
-      <translation type="unfinished"/>
+      <translation type="unfinished">
+        <numerusform>&lt;p&gt;Review the updates available...&lt;/p&gt;</numerusform>
+      </translation>
     </message>
     <message>
       <location filename="../src/mudlet.cpp" line="4564"/>
