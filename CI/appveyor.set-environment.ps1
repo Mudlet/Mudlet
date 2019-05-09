51b82f4ea7c6d8977007da5cec4cbea20bf39526
Vadim Peretokin <vperetokin@gmail.com>
2019-05-09 13:25:49 +0200

    Use Qt 5.12.2 for official Windows releases (#2511)
    * Us Qt 5.12.2 for official Windows releases
    
    * Update appveyor.set-environment.ps1
    
    * Remove -lglut dependency
    
    See https://forum.qt.io/topic/102016/lglut-not-found-after-upgrading-from-qt-5-11-to-5-12-on-windows/2
    
    * Limit max number of printed errors to 5
    
    * Set the MINGW_BASE_DIR to the 32bit version
    
    * Revert "Limit max number of printed errors to 5"
    
    This reverts commit d9012bbb57aa04d4a06a65c50202365c352b7ec1.

--- CI/appveyor.set-environment.ps1
+++ CI/appveyor.set-environment.ps1
@@ -6,7 +6,7 @@ function SetQtBaseDir([string] $logFile) {
     }
     catch
     {
-      $Env:QT_BASE_DIR = "C:\Qt\5.11.0\mingw53_32"
+      $Env:QT_BASE_DIR = "C:\Qt\5.12.2\mingw73_32"
     }
   }
   Write-Output "Using $Env:QT_BASE_DIR as QT base directory." | Tee-Object -File "$logFile" -Append
