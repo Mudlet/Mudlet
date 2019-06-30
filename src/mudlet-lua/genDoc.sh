cdea39e08bb005cf7ceed595236922e5d3b43c7e
Vadim Peretokin <vperetokin@gmail.com>
2019-06-30 06:26:47 +0200

    Fixed shellcheck warnings

--- src/mudlet-lua/genDoc.sh
+++ src/mudlet-lua/genDoc.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # wipe all old html files
-cd mudlet-lua-doc/
+cd mudlet-lua-doc/ || exit
 find . -name "*.html" -type f
 
 # generate documentation
-cd ../lua/
-ldoc --style `pwd` --project Geyser --not_luadoc --dir ../mudlet-lua-doc/files geyser/
+cd ../lua/ || exit
+ldoc --style "$(pwd)" --project Geyser --not_luadoc --dir ../mudlet-lua-doc/files geyser/
 
-# maintain compatibility with old luadoc-generated links. 
+# maintain compatibility with old luadoc-generated links.
 # luadoc created geyser/, ldoc makes modules/
-cd ../mudlet-lua-doc/files
+cd ../mudlet-lua-doc/files || exit
 find . -type f -exec sed -i 's@modules/@geyser/@g' {} +
 rm -rf geyser/
 mv modules geyser
