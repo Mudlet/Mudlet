7e28212d6ca77b73d41b143e03cd5dd4921ae477
Vadim Peretokin <vperetokin@gmail.com>
2019-06-30 09:32:36 +0200

    Review feedback

--- src/mudlet-lua/genDoc.sh
+++ src/mudlet-lua/genDoc.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # wipe all old html files
-cd mudlet-lua-doc/ || exit
+cd mudlet-lua-doc/ || exit 1
 find . -name "*.html" -type f
 
 # generate documentation
-cd ../lua/ || exit
+cd ../lua/ || exit 1
 ldoc --style "$(pwd)" --project Geyser --not_luadoc --dir ../mudlet-lua-doc/files geyser/
 
 # maintain compatibility with old luadoc-generated links.
 # luadoc created geyser/, ldoc makes modules/
-cd ../mudlet-lua-doc/files || exit
+cd ../mudlet-lua-doc/files || exit 1
 find . -type f -exec sed -i 's@modules/@geyser/@g' {} +
 rm -rf geyser/
 mv modules geyser
