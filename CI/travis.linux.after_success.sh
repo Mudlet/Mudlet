3ce66ff72deb5a5b7647f7f7c30bcfab4b49c647
keneanung <keneanung@googlemail.com>
2019-07-22 12:57:04 +0200

    check out master branch for linux input methods

--- CI/travis.linux.after_success.sh
+++ CI/travis.linux.after_success.sh
@@ -27,7 +27,7 @@ if [ "${DEPLOY}" = "deploy" ]; then
     exit
   fi
 
-  git clone -b add-chinese-input-method https://github.com/Mudlet/installers.git "${TRAVIS_BUILD_DIR}/../installers"
+  git clone https://github.com/Mudlet/installers.git "${TRAVIS_BUILD_DIR}/../installers"
 
   cd "${TRAVIS_BUILD_DIR}/../installers/generic-linux"
 
