e2d2dda4952acfec2125faf8fbbe36c048058481
Vadim Peretokin <vperetokin@gmail.com>
2019-07-31 21:20:59 +0200

    Revert "Fix no patterns being set for triggers (#2891)"
    This reverts commit 0913755c091fef5a89ebd1a883f2e685061c6cc5.

--- src/TTrigger.cpp
+++ src/TTrigger.cpp
@@ -173,16 +173,14 @@ bool TTrigger::setRegexCodeList(QStringList regexList, QList<int> propertyList, 
         qDebug() << "[CRITICAL ERROR (plz report):] Trigger name=" << mName << " aborting reason: propertyList.size() != regexList.size()";
     }
 
-    if ((propertyList.empty()) && (!isFolder()) && (!mColorTrigger)) {
-        if (!addingTrigger) {
-            setError(QStringLiteral("<b><font color='blue'>%1</font></b>")
-                     .arg(tr("Error: This trigger has no patterns defined, yet. Add some to activate it.")));
-            mOK_init = false;
-            return false;
-        } else {
-            mOK_init = true;
-            return true;
-        }
+    if (!addingTrigger && ((propertyList.empty()) && (!isFolder()) && (!mColorTrigger))) {
+        setError(QStringLiteral("<b><font color='blue'>%1</font></b>")
+                .arg(tr("Error: This trigger has no patterns defined, yet. Add some to activate it.")));
+        mOK_init = false;
+        return false;
+    } else if (addingTrigger) {
+        mOK_init = true;
+        return true;
     }
 
     bool state = true;
