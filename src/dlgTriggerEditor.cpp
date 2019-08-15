87e43c1c37aee10040688091a5eb4375fe4e4cc6
Vadim Peretokin <vperetokin@gmail.com>
2019-08-15 18:43:39 +0200

    Remove "default_host" profile (#2950)

--- src/dlgTriggerEditor.cpp
+++ src/dlgTriggerEditor.cpp
@@ -7073,6 +7073,8 @@ void dlgTriggerEditor::slot_script_main_area_add_handler()
 
 void dlgTriggerEditor::slot_debug_mode()
 {
+    mudlet::self()->attachDebugArea(mpHost->getName());
+
     mudlet::mpDebugArea->setVisible(!mudlet::debugMode);
     mudlet::debugMode = !mudlet::debugMode;
     mudlet::mpDebugArea->setWindowTitle("Central Debug Console");
