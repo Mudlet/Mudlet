084683821f30e295b7b1b6e8f474be2a4d4719b0
Stephen Lyons <slysven@virginmedia.com>
2019-08-16 14:02:27 +0200

    BugFix: correct PR #2976
    That did a reasonable check for a non-null `TRoom` pointer but it was being
    performed in the wrong place. This commit moves the test to a better place
    (before it is first dereferenced) in the `TLuaInterpreter::searchRoom(...)`
    method.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/TLuaInterpreter.cpp
+++ src/TLuaInterpreter.cpp
@@ -4842,6 +4842,9 @@ int TLuaInterpreter::searchRoom(lua_State* L)
         lua_newtable(L);
         QList<int> roomIdsFound;
         for (auto pR : roomList) {
+            if (!pR) {
+                continue;
+            }
             if (exactMatch) {
                 if (pR->name.compare(room, caseSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive) == 0) {
                     roomIdsFound.append(pR->getId());
@@ -4855,10 +4858,6 @@ int TLuaInterpreter::searchRoom(lua_State* L)
         if (!roomIdsFound.isEmpty()) {
             for (int i : roomIdsFound) {
                 TRoom* pR = host.mpMap->mpRoomDB->getRoom(i);
-                if (!pR) {
-                    continue;
-                }
-
                 QString name = pR->name;
                 int roomID = pR->getId();
                 lua_pushnumber(L, roomID);
