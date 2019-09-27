6632e9ddea219f9c5de052c22b5fe096b18594ce
Vadim Peretokin <vperetokin@gmail.com>
2019-09-27 09:42:15 +0200

    Add missing newline in --version

--- src/main.cpp
+++ src/main.cpp
@@ -211,7 +211,7 @@ int main(int argc, char* argv[])
         texts << QCoreApplication::translate("main", "Copyright © 2008-%1  Mudlet developers\n").arg(QStringLiteral(__DATE__).mid(7, 4));
         texts << QCoreApplication::translate("main", "Licence GPLv2+: GNU GPL version 2 or later - http://gnu.org/licenses/gpl.html\n");
         texts << QCoreApplication::translate("main", "This is free software: you are free to change and redistribute it.\n"
-                                                     "There is NO WARRANTY, to the extent permitted by law.");
+                                                     "There is NO WARRANTY, to the extent permitted by law.\n");
         std::cout << texts.join(QString()).toStdString();
         return 0;
     } else if (startupAction & 1) {
