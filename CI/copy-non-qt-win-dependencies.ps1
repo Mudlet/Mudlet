7f06e38beb3a877365fcac42d030e5c921577d2e
Stephen Lyons <slysven@virginmedia.com>
2019-10-29 20:18:19 +0000

    BugFix: rename file to be copied in copy-non-qt-win-dependencies.ps1
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- CI/copy-non-qt-win-dependencies.ps1
+++ CI/copy-non-qt-win-dependencies.ps1
@@ -11,4 +11,4 @@ COPY ..\*.dic .
 COPY ..\*.aff .
 XCOPY /S /I /Q /Y $Env:MINGW_BASE_DIR\lib\lua\5.1 .
 XCOPY /S /I /Q /Y ..\..\3rdparty\lcf lcf
-COPY ..\..\3rdparty\discord\rpc\lib\discord-rpc.dll discord-rpc.dll
+COPY ..\..\3rdparty\discord\rpc\lib\discord-rpc32.dll discord-rpc32.dll
