7ec7ac919b7fb3844083eb49e096c6afb11d9aa0
Stephen Lyons <slysven@virginmedia.com>
2019-10-31 03:27:29 +0000

    WIP: Revise cmd script to work better with variables [skip travis]
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- CI/appveyor.install.cmd
+++ CI/appveyor.install.cmd
@@ -1,4 +1,3 @@
-ECHO "Bitness is set to %BUILD_BITNESS% and the base folder where we are working will be %APPVEYOR_BUILD_FOLDER%,"
-ECHO "The current path is: %path%"
-
-C:\msys64\msys2_shell.cmd -mingw%BUILD_BITNESS% -where %APPVEYOR_BUILD_FOLDER%\CI "appveyor.install.sh"
+ECHO "Bitness is set to %BUILD_BITNESS% and the base folder where we are working will be %APPVEYOR_BUILD_FOLDER% ."
+CD %APPVEYOR_BUILD_FOLDER%/CI
+IF %BUILD_BITNESS%==32 (C:\msys64\msys2_shell.cmd -mingw32 "appveyor.install.sh") ELSE IF %BUILD_BITNESS%==64 (C:\msys64\msys2_shell.cmd -mingw64 "appveyor.install.sh") ELSE EXIT /B -1
