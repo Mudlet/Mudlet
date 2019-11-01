ad1e15272c5d356dd112725ad9a244863d78812b
Stephen Lyons <slysven@virginmedia.com>
2019-11-01 01:32:32 +0000

    W.I.P. Revise sh if tests [skip travis]
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- CI/appveyor.build.sh
+++ CI/appveyor.build.sh
@@ -2,14 +2,14 @@
 
 echo "Running appveyor.build.sh shell script..."
 
-if [[ ${BUILD_BITNESS} != "32" -a ${BUILD_BITNESS} != "64" ]]; then
+if [ ${BUILD_BITNESS} != "32" -a ${BUILD_BITNESS} != "64" ] ; then
     echo "Requires environmental variable BUILD_BITNESS to exist and be set to \"32\" or \"64\" to specify bitness of target to be built."
     exit -1
 fi
 
 echo "Initial PATH is:"
 echo ${PATH}
-if [[ ${BUILD_BITNESS} == "32"]]; then
+if [ ${BUILD_BITNESS} == "32" ] ; then
     echo "Fixing it for 32-bit builds:"
     export PATH=/mingw32/bin:/usr/bin:${PATH}
 else
@@ -21,9 +21,9 @@ echo ${PATH}
 
 cd ${APPVEYOR_BUILD_FOLDER}
 
-if [[ ${APPVEYOR_REPO_TAG} == "false" ]] ; then
+if [ ${APPVEYOR_REPO_TAG} == "false" ] ; then
     MUDLET_VERSION_BUILD="-${BUILD_BITNESS}bit-testing"
-    if [[ -p ${APPVEYOR_PULL_REQUEST_NUMBER} ]] ; then
+    if [ -p ${APPVEYOR_PULL_REQUEST_NUMBER} ] ; then
         COMMIT="$(git rev-parse --short ${APPVEYOR_PULL_REQUEST_HEAD_COMMIT})"
         MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-PR${APPVEYOR_PULL_REQUEST_NUMBER}-${COMMIT}"
     else
@@ -36,7 +36,7 @@ echo "BUILDING MUDLET ${MUDLET_VERSION_BUILD} ..."
 
 # We could support debug builds in the future by adding as an argument to the qmake call:
 # CONFIG+=debug
-if [[ ${BUILD_BITNESS} == "32"]]; then
+if [ ${BUILD_BITNESS} == "32" ] ; then
     MINGW_BASE_DIR=C:/msys64/mingw64
     C:\msys64\mingw32\bin\qmake.exe CONFIG+=release ../src/mudlet.pro
     C:\msys64\mingw32\bin\mingw32-make-make.exe -f
--- CI/appveyor.install.sh
+++ CI/appveyor.install.sh
@@ -2,14 +2,14 @@
 
 echo "Running appveyor.build.sh shell script..."
 
-if [[ ${BUILD_BITNESS} != "32" -a ${BUILD_BITNESS} != "64" ]]; then
+if [ ${BUILD_BITNESS} != "32" -a ${BUILD_BITNESS} != "64" ] ; then
     echo "Requires environmental variable BUILD_BITNESS to exist and be set to \"32\" or \"64\" to specify bitness of target to be built."
     exit -1
 fi
 
 echo "Initial PATH is:"
 echo ${PATH}
-if [[ ${BUILD_BITNESS} == "32"]]; then
+if [ ${BUILD_BITNESS} == "32" ] ; then
     echo "Fixing it for 32-bit builds:"
     export PATH=/mingw32/bin:/usr/bin:${PATH}
 else
@@ -24,7 +24,7 @@ echo ${PATH}
 # --noconfirm = do not ask for user intervention
 # --noprogressbar = do not show progress bars as they are not useful in scripts
 
-if [[ ${BUILD_BITNESS} == "32"]]; then
+if [ ${BUILD_BITNESS} == "32"] ; then
     pacman -Syuu --noconfirm --noprogressbar base-devel git mercurial cvs wget ruby zip p7zip python2 mingw-w64-i686-toolchain mingw-w64-i686-qt5 mingw-w64-i686-libzip mingw-w64-i686-pugixml mingw-w64-i686-lua51 mingw-w64-i686-lua51-lpeg mingw-w64-i686-lua51-lsqlite3 mingw-w64-i686-lua51-luarocks mingw-w64-i686-hunspell mingw-w64-i686-zlib mingw-w64-i686-boost
 else
     pacman -Syuu --noconfirm --noprogressbar base-devel git mercurial cvs wget ruby zip p7zip python2 mingw-w64-x86_64-toolchain mingw-w64-x86_64-qt5 mingw-w64-x86_64-libzip mingw-w64-x86_64-pugixml mingw-w64-x86_64-lua51 mingw-w64-x86_64-lua51-lpeg mingw-x86_64-i686-lua51-lsqlite3 mingw-x86_64-i686-lua51-luarocks mingw-w64-x86_64-hunspell mingw-w64-x86_64-zlib mingw-x86_64-i686-boost
