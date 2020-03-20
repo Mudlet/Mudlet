ba1021334f20a86a77b1eac9a84f4df23a3dc2a0
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2020-03-20 05:35:34 +0100

    New translations mudlet.ts (Italian)

--- translations/translated/mudlet_it_IT.ts
+++ translations/translated/mudlet_it_IT.ts
@@ -5270,7 +5270,7 @@ Utilizzi</translation>
     <message>
       <location filename="../src/dlgConnectionProfiles.cpp" line="2357"/>
       <source>Port number must be above zero and below 65535.</source>
-      <translation type="unfinished"/>
+      <translation>Il numero di porta deve essere superiore a 0 e inferiore a 65535.</translation>
     </message>
     <message>
       <location filename="../src/dlgConnectionProfiles.cpp" line="2383"/>
