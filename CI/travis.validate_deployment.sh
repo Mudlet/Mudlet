4bc424afe0440fecf6b26109984af470bb0dd382
Vadim Peretokin <vperetokin@gmail.com>
2020-03-20 20:19:45 +0100

    Check the updater variable as well

--- CI/travis.validate_deployment.sh
+++ CI/travis.validate_deployment.sh
@@ -4,16 +4,31 @@ if [ -z "${TRAVIS_TAG}" ]; then
   exit 0
 fi
 
-VALID_QMAKE=$(cat src/mudlet.pro | pcregrep --only-matching=1 "^VERSION ? = ?(\d+\.\d+\.\d+)$")
+function validate_qmake() {
+  VALID_QMAKE=$(cat src/mudlet.pro | pcregrep --only-matching=1 "^VERSION ? = ?(\d+\.\d+\.\d+)$")
 
-if [ -z "${VALID_QMAKE}" ]; then
-  echo "mudlet.pro isn't set to a valid version - aborting build."
+  if [ -z "${VALID_QMAKE}" ]; then
+  echo "mudlet.pro isn't set to a valid version in a release build."
   exit 1
-fi
+  fi
+}
 
-VALID_CMAKE=$(cat CMakeLists.txt | pcregrep --only-matching=1 "set\(APP_VERSION (\d+\.\d+\.\d+)\)$")
+function validate_cmake() {
+  VALID_CMAKE=$(cat CMakeLists.txt | pcregrep --only-matching=1 "set\(APP_VERSION (\d+\.\d+\.\d+)\)$")
 
-if [ -z "${VALID_CMAKE}" ]; then
-  echo "CMakeLists.txt isn't set to a valid version - aborting build."
-  exit 1
-fi
+  if [ -z "${VALID_CMAKE}" ]; then
+    echo "CMakeLists.txt isn't set to a valid version in a release build."
+    exit 1
+  fi
+}
+
+function validate_updater_environment_variable() {
+  if [ "$WITH_UPDATER" != "NO" ]; then
+     echo "Updater is disabled in a release build."
+     exit 1
+  fi
+}
+
+validate_qmake
+validate_cmake
+validate_updater_environment_variable
