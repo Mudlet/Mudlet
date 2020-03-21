ec1ce62e5a669852c7847629fccb49cf5eb5cf94
Vadim Peretokin <vperetokin@gmail.com>
2020-03-21 07:03:38 +0100

    Fixed validation to actually work
    Added checks for BUILD, APP_BUILD variables.

--- CI/travis.validate_deployment.sh
+++ CI/travis.validate_deployment.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
 
-set -e
-
 if [ -z "${TRAVIS_TAG}" ]; then
+  echo "Not a release build - skipping release validation."
   exit 0
 fi
 
-function validate_qmake() {
-  VALID_QMAKE=$(cat src/mudlet.pro | pcregrep --only-matching=1 "^VERSION ? = ?(\d+\.\d+\.\d+)$")
-
-  if [ -z "${VALID_QMAKE}" ]; then
-  echo "mudlet.pro isn't set to a valid semantic version in a release build."
+error() {
+  # shellcheck disable=SC2059
+  printf "error: $1\n" "${@:2}" >&2
   exit 1
+}
+
+function validate_qmake() {
+  local VALID_QMAKE VALID_BUILD
+
+  VALID_QMAKE=$(pcregrep --only-matching=1 "^VERSION ? = ?(\d+\.\d+\.\d+)$" < src/mudlet.pro)
+  if [ -z "${VALID_QMAKE}" ]; then
+    error "mudlet.pro's VERSION variable isn't formatted following the semantic versioning rules in a release build."
+  fi
+
+  VALID_BUILD=$(pcregrep --only-matching=1 ' +BUILD ? = ? ("")' < src/mudlet.pro)
+  if [ "${VALID_BUILD}" != '""' ]; then
+    error "mudlet.pro's BUILD variable isn't set to \"\" as it should be in a release build."
   fi
 }
 
 function validate_cmake() {
-  VALID_CMAKE=$(cat CMakeLists.txt | pcregrep --only-matching=1 "set\(APP_VERSION (\d+\.\d+\.\d+)\)$")
+  local VALID_CMAKE VALID_BUILD
+  VALID_CMAKE=$(pcregrep --only-matching=1 "set\(APP_VERSION (\d+\.\d+\.\d+)\)$" < CMakeLists.txt)
 
   if [ -z "${VALID_CMAKE}" ]; then
-    echo "CMakeLists.txt isn't set to a valid semantic version in a release build."
-    exit 1
+    error "CMakeLists.txt VERSION variable isn't formatted following the semantic versioning rules in a release build."
+  fi
+
+  VALID_BUILD=$(pcregrep --only-matching=1 'set\(APP_BUILD ("")\)$' < CMakeLists.txt)
+  if [ "${VALID_BUILD}" != '""' ]; then
+    error "CMakeLists.txt APP_BUILD variable isn't set to \"\" as it should be in a release build."
   fi
 }
 
 function validate_updater_environment_variable() {
   if [ "$WITH_UPDATER" == "NO" ]; then
-     echo "Updater is disabled in a release build."
-     exit 1
+     error "Updater is disabled in a release build."
   fi
 }
 
