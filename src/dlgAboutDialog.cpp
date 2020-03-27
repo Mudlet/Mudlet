70d0062806bbd7f7d2c6560dfd60a94e0c177eae
Stephen Lyons <slysven@virginmedia.com>
2020-03-27 19:31:05 +0000

    Enhance: Add version release type Splash-screens (#3521)
    Also removes obsolete old un-extension-ed Splash-screen file from repo.
    
    An .svg original was not immediately available but I managed to regenerate
    something from another .png artwork.
    
    Also add a working copy of the GIMP native file that I was using to create
    the splash-screens so that it is on record.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/dlgAboutDialog.cpp
+++ src/dlgAboutDialog.cpp
@@ -27,6 +27,8 @@
 
 #include "dlgAboutDialog.h"
 
+#include "mudlet.h"
+
 #include "pre_guard.h"
 #include <QPainter>
 #include <QTextLayout>
@@ -39,7 +41,9 @@ dlgAboutDialog::dlgAboutDialog(QWidget* parent) : QDialog(parent)
 
     // Copied from main():
 
-    QImage splashImage(QStringLiteral(":/Mudlet_splashscreen_main.png"));
+    QImage splashImage(mudlet::scmIsReleaseVersion ? QStringLiteral(":/Mudlet_splashscreen_main.png")
+                                                   : mudlet::scmIsPublicTestVersion ? QStringLiteral(":/Mudlet_splashscreen_ptb.png")
+                                                                                    : QStringLiteral(":/Mudlet_splashscreen_development.png"));
 
     { // Brace code using painter to ensure it is freed at right time...
         QPainter painter(&splashImage);
