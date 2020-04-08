7afcf9f4b45dd01aeb63c1bf41344cd4e7d4c2ab
Vadim Peretokin <vperetokin@gmail.com>
2020-04-08 18:10:10 +0200

    Fix file read function

--- CI/generate-ptb-changelog.lua
+++ CI/generate-ptb-changelog.lua
@@ -35,7 +35,7 @@ function os.capture(cmd, raw)
 end
 
 function read_file(path)
-    local file = open(path, "rb") -- r read mode and b binary mode
+    local file = io.open(path, "rb") -- r read mode and b binary mode
     if not file then return nil end
     local content = file:read "*a" -- *a or *all reads the whole file
     file:close()
