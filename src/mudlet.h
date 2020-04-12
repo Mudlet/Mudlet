2bfe87dae10a9ddb2bb422dd5015b3115c7b7f1a
Edru2 <60551052+Edru2@users.noreply.github.com>
2020-04-12 20:23:22 +0200

    UserWindows: choosable dockPosition and autoDock disable (#3583)
    Addition to openUserWindow function:
    openUserWindow(string windowname,[bool restoreLayout], [bool autoDock], [string dockingArea])
    
    Geyser.UserWindow
    functions:
    window:setDockPosition(string dockPosition)
    window:enableAutoDock()
    window:disableAutoDock()
    Constraints to set at creation:
    autoDock = true
    dockPosition = "r"

--- src/mudlet.h
+++ src/mudlet.h
@@ -145,7 +145,7 @@ public:
     bool setWindowFontSize(Host *, const QString &, int);
     int getFontSize(Host*, const QString&);
     QSize calcFontSize(Host* pHost, const QString& windowName);
-    bool openWindow(Host*, const QString&, bool loadLayout = true);
+    std::pair<bool, QString> openWindow(Host*, const QString&, bool loadLayout, bool autoDock, const QString &area);
     bool setProfileStyleSheet(Host* pHost, const QString& styleSheet);
     std::pair<bool, QString> createMiniConsole(Host*, const QString& windowname, const QString& name, int, int, int, int);
     std::pair<bool, QString> createLabel(Host* pHost, const QString& windowname, const QString& name, int x, int y, int width, int height, bool fillBg, bool clickthrough);
