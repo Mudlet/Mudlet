86b3e9dac13c5c1b9d2660118e849a2602c6c10e
Stephen Lyons <slysven@virginmedia.com>
2020-04-17 04:45:59 +0100

    Revise: some more debugging tweaks [skip appveyor]
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- CI/travis.osx.install.sh
+++ CI/travis.osx.install.sh
@@ -7,21 +7,28 @@ shopt -s expand_aliases
 BREWS="cmake hunspell libzip libzzip lua51 pcre pkg-config qt5 yajl ccache pugixml luarocks"
 OUTDATED_BREWS=$(brew outdated)
 echo "The following brews are outdated:"
-for b in $BREWS; do
+for b in ${OUTDATED_BREWS}; do
     echo "    ${b}"
 done
+echo "End of outdated brew listing."
 
-for i in $BREWS; do
+for i in ${BREWS}; do
   for RETRIES in $(seq 1 3); do
     echo " "
     echo "Considering whether to upgrade: ${i}"
     #Added the -w (whole-word) option so that the grep will NOT match for pcre2
     #when we are considering pcre:
-    echo "${OUTDATED_BREWS}" | grep -w -q ${i}
+    #Removed the -q option to grep for debugging
+    echo "${OUTDATED_BREWS}" | grep -w ${i}
     STATUS="$?"
-    if [ "${STATUS}" -ne 0 ]; then
+    if [ "${STATUS}" -eq 1 ]; then
       echo "Already up to date or not installed."
       break
+    elif [ "${STATUS}" -eq 2 ]; then
+      echo "Error!"
+      exit 1
+    else
+      echo "Needs updating..."
     fi
 
     brew upgrade ${i}
