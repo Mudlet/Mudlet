7b98fe8a7306b327a6efaaae2c4a0eb0f41db412
Vadim Peretokin <vperetokin@gmail.com>
2020-04-23 07:29:34 +0200

    Debug print lack of 'modules'

--- src/mudlet-lua/genDoc.sh
+++ src/mudlet-lua/genDoc.sh
@@ -12,4 +12,5 @@ ldoc --style "$(pwd)" --project Geyser --not_luadoc --dir ../mudlet-lua-doc/file
 cd ../mudlet-lua-doc/files || exit 1
 find . -type f -exec sed -i 's@modules/@geyser/@g' {} +
 rm -rf geyser/
+find .
 mv modules geyser
