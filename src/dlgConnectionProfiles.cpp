68b7e22933fbc9001efb89b71adc2cb4cf780d72
Vadim Peretokin <vperetokin@gmail.com>
2020-05-02 09:05:43 +0200

    Remove extra braces

--- src/dlgConnectionProfiles.cpp
+++ src/dlgConnectionProfiles.cpp
@@ -2303,9 +2303,9 @@ void dlgConnectionProfiles::loadProfile(bool alsoConnect)
             || pHost->getUrl().contains(QStringLiteral("lusternia.com"), Qt::CaseInsensitive)
             || pHost->getUrl().contains(QStringLiteral("imperian.com"), Qt::CaseInsensitive)) {
             mudlet::self()->packagesToInstallList.append(QStringLiteral(":/mudlet-mapper.xml"));
-        } else if (pHost->getUrl().contains(QStringLiteral("carrionfields.net"), Qt::CaseInsensitive) ) {
+        } else if (pHost->getUrl().contains(QStringLiteral("carrionfields.net"), Qt::CaseInsensitive)) {
             mudlet::self()->packagesToInstallList.append(QStringLiteral(":/CF-loader.xml"));
-        } else if (!pHost->getUrl().contains(QStringLiteral("mudlet.org"), Qt::CaseInsensitive) ) {
+        } else if (!pHost->getUrl().contains(QStringLiteral("mudlet.org"), Qt::CaseInsensitive)) {
             mudlet::self()->packagesToInstallList.append(QStringLiteral(":/mudlet-lua/lua/generic-mapper/generic_mapper.xml"));
         }
         if (pHost->getUrl().contains(QStringLiteral("mudlet.org"), Qt::CaseInsensitive)) {
