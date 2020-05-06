70e3722499cff813912ccbd0a447939ba608e9ac
Gustavo Sousa <gustavocms@gmail.com>
2020-05-06 15:26:26 -0300

    Fixes #3723 - MXP &text; not working as expected (#3724)
    * Fixes #3723 - MXP &text; not working as expected
    
    - Fixes problem with interpolating `&text;` in standalone `<SEND>` tags
    
    * Fixes problem with semicolons not being replaced
    
    - Similar problem to the uppercased `&TEXT;`, the custom tag handler stripped the semicolons, but the handler for send tag didn't.

--- src/TBuffer.cpp
+++ src/TBuffer.cpp
@@ -1492,7 +1492,8 @@ void TBuffer::translateToPlainText(std::string& incoming, const bool isFromServe
                                 _t_ref_list << _t_ref;
                                 mLinkStore[mLinkID] = _t_ref_list;
                             } else {
-                                mLinkStore[mLinkID].replaceInStrings("&TEXT", mAssembleRef.c_str());
+                                mLinkStore[mLinkID].replaceInStrings("&TEXT", mAssembleRef.c_str(), Qt::CaseInsensitive);
+                                mHintStore[mLinkID].replaceInStrings("&TEXT", mAssembleRef.c_str(), Qt::CaseInsensitive);
                             }
                             mAssembleRef.clear();
                         }
@@ -1585,7 +1586,10 @@ void TBuffer::translateToPlainText(std::string& incoming, const bool isFromServe
 
 
                         mMXP_LINK_MODE = true;
-                        if (_t2.isEmpty() || _t2.contains("&TEXT")) {
+
+                        _t2 = _t2.replace("&text;", "&TEXT", Qt::CaseInsensitive);
+                        _t3 = _t3.replace("&text;", "&TEXT", Qt::CaseInsensitive);
+                        if (_t2.isEmpty() || _t2.contains("&TEXT", Qt::CaseInsensitive)) {
                             mMXP_SEND_NO_REF_MODE = true;
                         }
                         mLinkID++;
