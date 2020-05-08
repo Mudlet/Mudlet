9375c95408317540be2d9551aa9e020c5dda7596
Edru2 <alyven_87@hotmail.com>
2020-05-08 17:48:29 +0200

    first commit

--- src/mudlet-lua/lua/geyser/GeyserHBox.lua
+++ src/mudlet-lua/lua/geyser/GeyserHBox.lua
@@ -12,30 +12,29 @@ Geyser.HBox = Geyser.Container:new({
 function Geyser.HBox:add (window, cons)
   Geyser.add(self, window, cons)
   if not self.defer_updates then
-    self:reposition()
+    self:organize()
   end
 end
 
---- Responsible for placing/moving/resizing this window to the correct place/size.
--- Called on window resize events.
-function Geyser.HBox:reposition()
+--- Responsible for organizing the elements inside the HBox
+-- Called when a new element is added
+function Geyser.HBox:organize()
   self.parent:reposition()
-
-  local window_width = self:calculate_dynamic_window_size().width
+  local window_width = (self:calculate_dynamic_window_size().width / self:get_width()) * 100
   local start_x = 0
   for _, window_name in ipairs(self.windows) do
     local window = self.windowList[window_name]
-    local width = window.width
-    local height = window.height
-    window:move(start_x, 0)
+    local width = (window:get_width() / self:get_width()) * 100
+    local height = (window:get_height() / self:get_height()) * 100
+    window:move(start_x.."%", "0%")
     if window.h_policy == Geyser.Dynamic then
       width = window_width * window.h_stretch_factor
     end
     if window.v_policy == Geyser.Dynamic then
-      height = self:get_height()
+      height = 100
     end
-    window:resize(width, height)
-    start_x = start_x + window:get_width()
+    window:resize(width.."%", height.."%")
+    start_x = start_x + (window:get_width() / self:get_width()) * 100
   end
 end
 
@@ -50,6 +49,6 @@ function Geyser.HBox:new(cons, container)
   local me = self.parent:new(cons, container)
   setmetatable(me, self)
   self.__index = self
-  me:reposition()
+  me:organize()
   return me
 end
