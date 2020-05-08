d5390d0e9e97c6e008c6e0776ed66e8ac36923d9
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2020-05-08 15:33:36 +0200

    New translations mudlet.ts (English, United Kingdom)

--- translations/translated/mudlet_en_GB.ts
+++ translations/translated/mudlet_en_GB.ts
@@ -2403,9 +2403,9 @@ Mudlet - is your system running out of memory?</source>
  to be in this/these area(s)...</source>
       <comment>Making use of %n to allow quantity dependent message form 8-) !</comment>
       <translation>
-        <numerusform>[ ALERT ] - An area was detected as missing in the map: adding it in.
-Look for further messages related to the rooms that are supposed to
-be in this area...</numerusform>
+        <numerusform>[ ALERT ] - %n area(s) detected as missing in map: adding it/them in.
+ Look for further messages related to the rooms that are supposed
+ to be in this/these area(s)...</numerusform>
         <numerusform>[ ALERT ] - %n areas were detected as missing in map: adding them in.
 Look for further messages related to the rooms that are supposed
 to be in these areas...</numerusform>
