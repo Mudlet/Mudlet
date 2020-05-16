8cb73a1d5a81976697e8c873bf6322430e6cdc1c
Vadim Peretokin <vperetokin@gmail.com>
2020-05-16 05:23:26 +0200

    Upgrade to Qt 5.12.8 for Linux builds (#3781)
    * Upgrade to Qt 5.12.8 for Linux builds
    
    * Try not specifying flags

--- CI/travis.compile.sh
+++ CI/travis.compile.sh
@@ -18,10 +18,6 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   export LDFLAGS=" -L/usr/local/opt/qt5/lib ${LDFLAGS}"
   export CPPFLAGS=" -I/usr/local/opt/qt5/include ${CPPFLAGS}"
 fi
-if [ "${CC}" = "clang" ] && [ "${TRAVIS_OS_NAME}" = "linux" ] && [ "${Q_OR_C_MAKE}" = "qmake" ] ; then
-  # need to specify the clang specs for linux qmake builds
-  SPEC="-spec linux-clang"
-fi
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then # getting the number of processors is different for each OS
   PROCS=$(sysctl -n hw.physicalcpu)
 else
