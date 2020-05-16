304b640ba5ea6b1b425eb35c7a43a5fe2d5de10e
Vadim Peretokin <vperetokin@gmail.com>
2020-05-16 17:27:09 +0200

    Add Linux PTB builds (#3785)
    * Build PTB in Linux as well
    
    * Build PTBs
    
    * Update compile and other scripts
    
    * Only run coverity in certain cases
    
    * Fix post-deploy
    
    * Correctly guard condition in another if
    
    * Don't make snapshot builds for PTBs
    
    * Remove unneeded $app
    
    * Use PTBs branch
    
    * Correct input filename for the tar command
    
    * Only deploy sources for a release build
    
    * Plug in changelog generation
    
    * Only use mingw_base_dir when defined
    
    * Install LUa changelog dependencies
    
    * Download the 64bit version for Linux
    
    * Remove debug info

--- CI/travis.compile.sh
+++ CI/travis.compile.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 compile_line=()
-if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
-  if [ "${DEPLOY}" != "deploy" ]; then
-    echo Job not executed under cron run
-    exit 0
-  fi
+# macOS PTB is cron+deploy
+# linux coverity is cron+deploy
+# linux PTB is cron+clang+cmake
+if { [ "${TRAVIS_EVENT_TYPE}" = "cron" ] && [ "${TRAVIS_OS_NAME}" = "osx" ] && [ "${DEPLOY}" = "deploy" ]; } ||
+   { [ "${TRAVIS_EVENT_TYPE}" = "cron" ] && [ "${TRAVIS_OS_NAME}" = "linux" ] && [ "${DEPLOY}" = "deploy" ]; } ||
+   { [ "${TRAVIS_EVENT_TYPE}" = "cron" ] && [ "${TRAVIS_OS_NAME}" = "linux" ] &&  [ "${CC}" = "clang" ] && [ "${Q_OR_C_MAKE}" = "cmake" ]; } then
   export CCACHE_DISABLE=1
 
-  if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+  if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] && [ "${TRAVIS_OS_NAME}" = "linux" ] && [ "${DEPLOY}" = "deploy" ]; then
     compile_line+=(cov-build --dir cov-int)
   fi
+elif [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
+  echo Job not executed under cron run
+  exit 0
 fi
 
-cd build
+cd build || exit
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   # need to set additional compile flags on osx
   export LDFLAGS=" -L/usr/local/opt/qt5/lib ${LDFLAGS}"
--- CI/travis.set-build-info.sh
+++ CI/travis.set-build-info.sh
@@ -3,12 +3,12 @@
 MUDLET_VERSION_BUILD=""
 
 if [ -z "${TRAVIS_TAG}" ]; then
-  if [ "$TRAVIS_EVENT_TYPE" = "cron" ] && [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-    # The only scheduled macos builds are public test builds
+  if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
     MUDLET_VERSION_BUILD="-ptb"
   else
     MUDLET_VERSION_BUILD="-testing"
   fi
+
   if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then # building for a PR
     COMMIT=$(git rev-parse --short "${TRAVIS_PULL_REQUEST_SHA}")
     MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-PR${TRAVIS_PULL_REQUEST}-${COMMIT}"
@@ -17,7 +17,7 @@ if [ -z "${TRAVIS_TAG}" ]; then
 
     if [ "${MUDLET_VERSION_BUILD}" = "-ptb" ]; then
       DATE=$(date +'%Y-%m-%d')
-      # add a short commit to version for changelog generation know  what was last released
+      # add a short commit to version for changelog generation know what was last released
       SHORT_COMMIT=$(echo "${COMMIT}" | cut -c1-5)
       MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-${DATE}-${SHORT_COMMIT}"
     else
