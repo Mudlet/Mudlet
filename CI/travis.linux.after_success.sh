3e445c0bce7dbd1ca657a58728bc3147a554e4c2
Vadim Peretokin <vperetokin@gmail.com>
2020-05-28 22:23:28 +0200

    Fix Linux PTB changelog generation (#3839)
    * Run changelog generation in the same folder
    
    * Try a relative path
    
    * Load with help of environment variable
    
    * Add debug info
    
    * Debug whenever the script is started at all
    
    * Run script as in 87bcb7af0
    
    * Don't exit as 1
    
    * Cleanup

--- CI/travis.linux.after_success.sh
+++ CI/travis.linux.after_success.sh
@@ -108,6 +108,7 @@ if { [ "${TRAVIS_OS_NAME}" = "linux" ] && [ "${DEPLOY}" = "deploy" ]; } ||
       downloadedfeed=$(mktemp)
       wget "https://feeds.dblsqd.com/MKMMR7HNSP65PquQQbiDIw/public-test-build/linux/x86_64" --output-document="$downloadedfeed"
       echo "=== Generating a changelog ==="
+      cd "${TRAVIS_BUILD_DIR}"
       changelog=$(lua "${TRAVIS_BUILD_DIR}/CI/generate-ptb-changelog.lua" --releasefile "${downloadedfeed}")
 
       echo "=== Creating release in Dblsqd ==="
