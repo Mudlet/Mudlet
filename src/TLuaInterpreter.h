52803958064383a475dd6c7f761af6414769965a
Mike Conley <sousesider@gmail.com>
2020-08-25 14:59:45 -0400

    Introduce purgeMediaCache() [MCMP & MSP] (#4041)
    * Introduce purgeAllMediaFiles()
    
    * Introduce purgeMediaCache()
    
    * Updating lua-function-list
    
    * Removed TMedia:: prefix (SlySven)

--- src/TLuaInterpreter.h
+++ src/TLuaInterpreter.h
@@ -450,6 +450,7 @@ public:
     static int sendATCP(lua_State*);
     static int sendGMCP(lua_State*);
     static int receiveMSP(lua_State*);
+    static int purgeMediaCache(lua_State*);
     static int saveMap(lua_State* L);
     static int loadMap(lua_State* L);
     static int setExitStub(lua_State* L);
