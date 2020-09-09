Mudlet/Mudlet

commit 2a90a8afc74b4f203e6489a20252eb853ea08bd1

Enhance: enable builds in a full Windows MSYS2 environment (#3889)
* Enhance: enable builds in a full Windows MSYS2 environment (QMake only)

By defining `WITH_MAIN_BUILD_SYSTEM` to the value `NO` this PR makes enough
changes to the qmake project file to enable Mudlet to be compiled in a full
MSYS2 development environment (in the MSYS2 Qt Creator) - this will enable
easier development by Windows users (particularly those who also have some
familiarity with *nix systems) as I have documented at:
"Compiling on Windows 7+ (MSYS2_Alternative)" but it seems that the URL is
causing GitHub to forget about the PR as it seems to push and is recorded
in my local repository but never actually lands there!

It also makes some changes to the setting up of the LUA package paths for
the lua code formatter so that the paths are all entered with Unix style
directory separators but converted to whatever the Lua package handler is
set to use. In a Windows environment it is not unheard of to get both '\'
and '/' being used within the same path as different parts get generated
in stages - and using the backslash one can produce surprising error
messages if the back slash is not properly escaped when displaying those
messages in the main console or elsewhere (they dissappear entirely or
end up escaping following characters producing misleading information).

Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

* Update src/mudlet.pro

BugFix: fix a typo in qmake project file.

Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

* Revise: reduce almost duplicate comments

Fix an addition to the `package.cpath` which would not have worked as it
did not specify the file extension which is OS dependent.

Also remove LuaJIT remenent, which we dropped support for a long time back.

Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

* Apply suggestions from code review

Revise: fix an error in a comment.

src/ctelnet.cpp
@@ -109,7 +109,7 @@ cTelnet::cTelnet(Host* pH, const QString& profileName)
     // than that in the initialisation list so that it is processed as a change
     // to set up the initial encoder
     encodingChanged("UTF-8");
-    termType = QStringLiteral("Mudlet %1").arg(QStringLiteral(APP_VERSION));
+    termType = QStringLiteral("Mudlet" APP_VERSION);
     if (QByteArray(APP_BUILD).trimmed().length()) {
         termType.append(QStringLiteral(APP_BUILD));
     }

CMakeLists.txt
@@ -102,16 +102,22 @@ include_optional_module(ENVIRONMENT_VARIABLE WITH_UPDATER
                                           "Windows"
                                           "Darwin")
 
-include_optional_module(ENVIRONMENT_VARIABLE WITH_FONTS OPTION_VARIABLE
-                        USE_FONTS READABLE_NAME "fonts")
-include_optional_module(ENVIRONMENT_VARIABLE WITH_3DMAPPER OPTION_VARIABLE
-                        USE_3DMAPPER READABLE_NAME "3D mapper")
+include_optional_module(ENVIRONMENT_VARIABLE WITH_FONTS
+                        OPTION_VARIABLE USE_FONTS
+                        READABLE_NAME "fonts")
+include_optional_module(ENVIRONMENT_VARIABLE WITH_3DMAPPER
+                        OPTION_VARIABLE USE_3DMAPPER
+                        READABLE_NAME "3D mapper")
 include_optional_module(ENVIRONMENT_VARIABLE WITH_VARIABLE_SPLASH_SCREEN
                         OPTION_VARIABLE USE_VARIABLE_SPLASH_SCREEN
                         READABLE_NAME "build-type splash screen")
 include_optional_module(ENVIRONMENT_VARIABLE WITH_OWN_QTKEYCHAIN
                         OPTION_VARIABLE USE_OWN_QT5KEYCHAIN
                         READABLE_NAME "own QKeychain (Qt5) library")
+include_optional_module(ENVIRONMENT_VARIABLE WITH_MAIN_BUILD_SYSTEM
+                        OPTION_VARIABLE USE_MAIN_BUILD_SYSTEM
+                        READABLE_NAME "original CI (setup-windows-sdk.ps1) windows build system"
+                        SUPPORTED_SYSTEMS "Windows")
 
 include(InitGitSubmodule)
 

cmake/FindHUNSPELL.cmake
@@ -94,7 +94,7 @@ find_package_handle_standard_args(
 mark_as_advanced(HUNSPELL_INCLUDE_DIR HUNSPELL_LIBRARY HUNSPELL_LIBRARY_RELEASE
                  HUNSPELL_LIBRARY_DEBUG)
 
-get_filename_component(HUNSPELL_FILENAME ${HUNSPELL_LIBRARY} NAME_WE)
+get_filename_component(HUNSPELL_FILENAME ${HUNSPELL_LIBRARY} NAME)
 string(FIND ${HUNSPELL_FILENAME} .a HUNSPELL_STATIC)
 
 if(HUNSPELL_FOUND AND NOT TARGET HUNSPELL::HUNSPELL)

cmake/FindPCRE.cmake
@@ -81,7 +81,7 @@ find_package_handle_standard_args(PCRE REQUIRED_VARS PCRE_LIBRARY
 mark_as_advanced(PCRE_INCLUDE_DIR PCRE_LIBRARY PCRE_LIBRARY_RELEASE
                  PCRE_LIBRARY_DEBUG)
 
-get_filename_component(PCRE_FILENAME ${PCRE_LIBRARY} NAME_WE)
+get_filename_component(PCRE_FILENAME ${PCRE_LIBRARY} NAME)
 string(FIND ${PCRE_FILENAME} .a PCRE_STATIC)
 
 if(PCRE_FOUND AND NOT TARGET PCRE::PCRE)

cmake/FindPUGIXML.cmake
@@ -35,7 +35,7 @@ endif()
 
 include(FindPackageHandleStandardArgs)
 # Support the REQUIRED and QUIET arguments, and set PUGIXML_FOUND if found.
-find_package_handle_standard_args(PugiXML REQUIRED_VARS PUGIXML_LIBRARY
+find_package_handle_standard_args(PUGIXML REQUIRED_VARS PUGIXML_LIBRARY
                                   PUGIXML_INCLUDE_DIR VERSION_VAR PUGIXML_VER)
 
 if(PUGIXML_FOUND AND NOT TARGET PUGIXML::PUGIXML)

cmake/FindYAJL.cmake
@@ -58,13 +58,13 @@ find_library(
 if(YAJL_LIBRARY_DEBUG AND YAJL_LIBRARY_RELEASE)
   set(YAJL_LIBRARY optimized ${YAJL_LIBRARY_RELEASE} debug
                    ${YAJL_LIBRARY_DEBUG})
-  get_filename_component(YAJL_FILENAME ${YAJL_LIBRARY_RELEASE} NAME_WE)
+  get_filename_component(YAJL_FILENAME ${YAJL_LIBRARY_RELEASE} NAME)
 elseif(YAJL_LIBRARY_RELEASE)
   set(YAJL_LIBRARY ${YAJL_LIBRARY_RELEASE})
-  get_filename_component(YAJL_FILENAME ${YAJL_LIBRARY_RELEASE} NAME_WE)
+  get_filename_component(YAJL_FILENAME ${YAJL_LIBRARY_RELEASE} NAME)
 elseif(YAJL_LIBRARY_DEBUG)
   set(YAJL_LIBRARY ${YAJL_LIBRARY_DEBUG})
-  get_filename_component(YAJL_FILENAME ${YAJL_LIBRARY_DEBUG} NAME_WE)
+  get_filename_component(YAJL_FILENAME ${YAJL_LIBRARY_DEBUG} NAME)
 endif()
 
 if(PC_YAJL_yajl_FOUND)
@@ -85,6 +85,9 @@ mark_as_advanced(YAJL_INCLUDE_DIR YAJL_LIBRARY YAJL_LIBRARY_RELEASE
                  YAJL_LIBRARY_DEBUG)
 
 string(FIND ${YAJL_FILENAME} yajl_s YAJL_STATIC)
+if(YAJL_STATIC EQUAL -1)
+  string(FIND ${YAJL_FILENAME} .a YAJL_STATIC)
+endif()
 
 if(YAJL_FOUND AND NOT TARGET YAJL::YAJL)
   if(YAJL_STATIC EQUAL -1)

cmake/FindZIP.cmake
@@ -51,13 +51,13 @@ find_library(
 
 if(ZIP_LIBRARY_DEBUG AND ZIP_LIBRARY_RELEASE)
   set(ZIP_LIBRARY optimized ${ZIP_LIBRARY_RELEASE} debug ${ZIP_LIBRARY_DEBUG})
-  get_filename_component(ZIP_FILENAME ${ZIP_LIBRARY_RELEASE} NAME_WE)
+  get_filename_component(ZIP_FILENAME ${ZIP_LIBRARY_RELEASE} NAME)
 elseif(ZIP_LIBRARY_RELEASE)
   set(ZIP_LIBRARY ${ZIP_LIBRARY_RELEASE})
-  get_filename_component(ZIP_FILENAME ${ZIP_LIBRARY_RELEASE} NAME_WE)
+  get_filename_component(ZIP_FILENAME ${ZIP_LIBRARY_RELEASE} NAME)
 elseif(ZIP_LIBRARY_DEBUG)
   set(ZIP_LIBRARY ${ZIP_LIBRARY_DEBUG})
-  get_filename_component(ZIP_FILENAME ${ZIP_LIBRARY_DEBUG} NAME_WE)
+  get_filename_component(ZIP_FILENAME ${ZIP_LIBRARY_DEBUG} NAME)
 endif()
 
 if(PC_ZIP_zip_FOUND)
@@ -75,6 +75,9 @@ find_package_handle_standard_args(ZIP REQUIRED_VARS ZIP_LIBRARY ZIP_INCLUDE_DIR
                                   VERSION_VAR ZIP_VERSION)
 
 string(FIND ${ZIP_FILENAME} zip_s ZIP_STATIC)
+if(ZIP_STATIC EQUAL -1)
+  string(FIND ${ZIP_FILENAME} .a ZIP_STATIC)
+endif()
 
 mark_as_advanced(ZIP_INCLUDE_DIR ZIP_LIBRARY ZIP_LIBRARY_RELEASE
                  ZIP_LIBRARY_DEBUG)

cmake/IncludeOptionalModule.cmake
@@ -74,7 +74,7 @@ macro(include_optional_module)
         set(OPTIONAL_MODULE_OPTION_VALUE OFF)
         message(
           STATUS
-            "Excluding optional ${OPTIONAL_MODULE_READABLE_NAME} module explicitly"
+            "Excluding optional ${OPTIONAL_MODULE_READABLE_NAME} explicitly"
         )
       else()
         # Any other value was seen so ignore it and set "yes, include the
@@ -82,7 +82,7 @@ macro(include_optional_module)
         set(OPTIONAL_MODULE_OPTION_VALUE ON)
         message(
           STATUS
-            "Including optional ${OPTIONAL_MODULE_READABLE_NAME} module explicitly"
+            "Including optional ${OPTIONAL_MODULE_READABLE_NAME} explicitly"
         )
       endif()
     else()
@@ -90,10 +90,10 @@ macro(include_optional_module)
       # include the module"
       set(OPTIONAL_MODULE_OPTION_VALUE ON)
       message(
-        STATUS "Including optional ${OPTIONAL_MODULE_READABLE_NAME} module")
+        STATUS "Including optional ${OPTIONAL_MODULE_READABLE_NAME}")
     endif()
     option(${OPTIONAL_MODULE_OPTION_VARIABLE}
-           "Include optional ${OPTIONAL_MODULE_READABLE_NAME} module"
+           "Include optional ${OPTIONAL_MODULE_READABLE_NAME}"
            ${OPTIONAL_MODULE_OPTION_VALUE})
   else()
     # Don't offer option to enable the module since it's not supported on this
@@ -101,7 +101,7 @@ macro(include_optional_module)
     set(${OPTIONAL_MODULE_OPTION_VARIABLE} OFF)
     message(
       STATUS
-        "Excluding optional ${OPTIONAL_MODULE_READABLE_NAME} module as it is not supported on this platform"
+        "Excluding optional ${OPTIONAL_MODULE_READABLE_NAME} as it is not supported on this platform"
     )
   endif()
 

commit 15c52d00b02f78ec7157b3f9a38fbe2420cffc63

BugFix: TTYPE error
I introduced this error in #3889 and it was reported in, and this PR
will close #4066.

Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

src/ctelnet.cpp
@@ -109,7 +109,7 @@ cTelnet::cTelnet(Host* pH, const QString& profileName)
     // than that in the initialisation list so that it is processed as a change
     // to set up the initial encoder
     encodingChanged("UTF-8");
-    termType = QStringLiteral("Mudlet" APP_VERSION);
+    termType = QStringLiteral("Mudlet " APP_VERSION);
     if (QByteArray(APP_BUILD).trimmed().length()) {
         termType.append(QStringLiteral(APP_BUILD));
     }