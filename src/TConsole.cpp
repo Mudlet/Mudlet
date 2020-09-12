5ce3e4177df2b5f66b551065431a206f554a000d
Edru2 <alyven_87@hotmail.com>
2020-09-12 19:34:09 +0200

    change order for :lower as it caused strange issues

--- src/TConsole.cpp
+++ src/TConsole.cpp
@@ -2617,24 +2617,26 @@ bool TConsole::lowerWindow(const QString& name)
 
     if (pC) {
         pC->lower();
-        mpMainDisplay->lower();
         mpBackground->lower();
+        mpMainDisplay->lower();
         return true;
     }
     if (pL) {
         pL->lower();
-        mpMainDisplay->lower();
         mpBackground->lower();
+        mpMainDisplay->lower();
         return true;
     }
     if (pM && !name.compare(QLatin1String("mapper"), Qt::CaseInsensitive)) {
         pM->lower();
-        mpMainDisplay->lower();
         mpBackground->lower();
+        mpMainDisplay->lower();
         return true;
     }
     if (pN) {
         pN->lower();
+        mpBackground->lower();
+        mpMainDisplay->lower();
         return true;
     }
     return false;
