8b8b9ff248e28c828d787d04ead703ffd96246fc
Vadim Peretokin <vperetokin@gmail.com>
2020-09-30 12:52:33 +0200

    Update travis.osx.before_install.sh

--- CI/travis.osx.before_install.sh
+++ CI/travis.osx.before_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -ev
 
-echo "Running brew bundle cleanup --force..."
-brew bundle cleanup --force
-echo "Running brew bundle install..."
-brew bundle install
-echo "Done with brew bundle install.
+brew update
