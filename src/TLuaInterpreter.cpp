aa6cd330bd40b94f7c02aa637c50297d9b36b9a1
Matthias Urlichs <matthias@urlichs.de>
2020-10-04 21:59:39 +0200

    Fix tempAnsiColor trigger
    Closes #4109.

--- src/TLuaInterpreter.cpp
+++ src/TLuaInterpreter.cpp
@@ -8095,21 +8095,13 @@ int TLuaInterpreter::tempAnsiColorTrigger(lua_State* L)
     QString code;
     int ansiFgColor = TTrigger::scmIgnored;
     int ansiBgColor = TTrigger::scmIgnored;
-    if (lua_isstring(L, 1)) {
-        code = QString::fromUtf8(lua_tostring(L, 1));
-    } else if (lua_isfunction(L, 1)) {
-        // leave code as a null QString()
-    } else {
-        lua_pushfstring(L, "tempAnsiColorTrigger: bad argument #1 type (code to run as a string or a function expected, got %s!)", luaL_typename(L, 1));
-        return lua_error(L);
-    }
 
-    if (!lua_isnumber(L, 2)) {
+    if (!lua_isnumber(L, 1)) {
         lua_pushfstring(L, "tempAnsiColorTrigger: bad argument #2 type (foreground color as ANSI Color number {%d = ignore foreground color, %d = default colour, 0 to 255 ANSI colour} expected, got %s!)",
                         TTrigger::scmIgnored, TTrigger::scmDefault, luaL_typename(L, 2));
         return lua_error(L);
     } else {
-        int value = lua_tointeger(L, 2);
+        int value = lua_tointeger(L, 1);
         // At present we limit the range to (Trigger::scmIgnored),
         // (Trigger::scmDefault) and 0-255 ANSI colors - in the future we could
         // extend it to other "coded" values for locally generated textual
@@ -8127,7 +8119,7 @@ int TLuaInterpreter::tempAnsiColorTrigger(lua_State* L)
         }
     }
 
-    if (lua_gettop(L) > 2 && !lua_isnumber(L, 2)) {
+    if (!lua_isnumber(L, 2)) {
         lua_pushfstring(L, "tempAnsiColorTrigger: bad argument #3 type (background color as ANSI Color number {%d = ignore foreground color, %d = default colour, 0 to 255 ANSI colour} expected, got %s!)",
                         TTrigger::scmIgnored, TTrigger::scmDefault, luaL_typename(L, 3));
                  return lua_error(L);
@@ -8146,7 +8138,27 @@ int TLuaInterpreter::tempAnsiColorTrigger(lua_State* L)
         }
     }
 
-    int triggerID = pLuaInterpreter->startTempColorTrigger(ansiFgColor, ansiBgColor, code);
+    if (lua_isstring(L, 3)) {
+        code = QString::fromUtf8(lua_tostring(L, 1));
+    } else if (lua_isfunction(L, 3)) {
+        // leave code as a null QString()
+    } else {
+        lua_pushfstring(L, "tempAnsiColorTrigger: bad argument #1 type (code to run as a string or a function expected, got %s!)", luaL_typename(L, 3));
+        return lua_error(L);
+    }
+
+    int expiryCount = -1;
+    if (lua_isnumber(L, 4)) {
+        expiryCount = lua_tonumber(L, 4);
+
+        if (expiryCount < 1) {
+            lua_pushnil(L);
+            lua_pushfstring(L, "tempLineTrigger: bad argument #4 value (trigger expiration count must be greater than zero, got %d)", expiryCount);
+            return 2;
+        }
+    }
+
+    int triggerID = pLuaInterpreter->startTempColorTrigger(ansiFgColor, ansiBgColor, code, expiryCount);
     if (code.isNull()) {
         auto trigger = host.getTriggerUnit()->getTrigger(triggerID);
         trigger->mRegisteredAnonymousLuaFunction = true;
