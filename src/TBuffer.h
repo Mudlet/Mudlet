d13286f3fe23297bc5e227f154f12a5d46b61454
Matthias Urlichs <matthias@urlichs.de>
2020-10-26 09:50:14 +0100

    Various Coverity fixes (#4191)
    * Coverity fixes
    
    * Order fixes as per comments
    
    * That was broken
    
    * Initializer

--- src/TBuffer.h
+++ src/TBuffer.h
@@ -136,7 +136,7 @@ class TBuffer
 public:
     TBuffer(Host* pH);
     QPoint insert(QPoint&, const QString& text, int, int, int, int, int, int, bool bold, bool italics, bool underline, bool strikeout);
-    bool insertInLine(QPoint& cursor, const QString& what, TChar& format);
+    bool insertInLine(QPoint& cursor, const QString& what, const TChar& format);
     void expandLine(int y, int count, TChar&);
     int wrapLine(int startLine, int screenWidth, int indentSize, TChar& format);
     void log(int, int);
@@ -172,7 +172,7 @@ public:
     void updateColors();
     TBuffer copy(QPoint&, QPoint&);
     TBuffer cut(QPoint&, QPoint&);
-    void paste(QPoint&, TBuffer);
+    void paste(QPoint&, const TBuffer&);
     void setBufferSize(int requestedLinesLimit, int batch);
     int getMaxBufferSize();
     static const QList<QByteArray> getEncodingNames();
--- src/TMedia.h
+++ src/TMedia.h
@@ -92,16 +92,20 @@ private:
 class TMediaPlayer
 {
 public:
-    TMediaPlayer() {}
+    TMediaPlayer()
+        : mpHost(nullptr)
+        , mMediaData()
+        , mMediaPlayer(nullptr)
+        , initialized(false)
+        {}
     ~TMediaPlayer() {}
 
     TMediaPlayer(Host* pHost, TMediaData& mediaData)
-    {
-        mpHost = pHost;
-        mMediaPlayer = new QMediaPlayer(pHost);
-        mMediaData = mediaData;
-        initialized = true;
-    }
+        : mpHost(pHost)
+        , mMediaData(mediaData)
+        , mMediaPlayer(new QMediaPlayer(pHost))
+        , initialized(true)
+        {}
 
     TMediaData getMediaData() { return mMediaData; }
     void setMediaData(TMediaData& mediaData) { mMediaData = mediaData; }
@@ -112,7 +116,7 @@ private:
     QPointer<Host> mpHost;
     TMediaData mMediaData;
     QMediaPlayer* mMediaPlayer;
-    bool initialized = false;
+    bool initialized;
 };
 
 class TMedia : public QObject
