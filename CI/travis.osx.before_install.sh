03058512f94d3dec267c32885d9dd1aabe6df4dd
Vadim Peretokin <vperetokin@gmail.com>
2020-10-30 14:11:12 +0000

    Revert "Echo before checking"
    This reverts commit 895dacc4ec3252088843f1f5acbc13c4de2eb38e.

--- CI/travis.osx.before_install.sh
+++ CI/travis.osx.before_install.sh
@@ -3,12 +3,11 @@ set -ev
 brew update-reset
 
 # Ensure Homebrew's npm is used, instead of an outdated one
-PATH=/usr/local/bin:$PATH
 echo "Running npm install. PATH is $PATH"
+PATH=/usr/local/bin:$PATH
 npm install -g appdmg
 echo "Successfully ran npm install."
 
-echo "Making .npm-global..."
-mkdir /Users/travis/.npm-global
-echo "1 Checking /Users/travis/.npm-global..."
+"1 Checking /Users/travis/.npm-global..."
+mkdir ~/.npm-global
 file /Users/travis/.npm-global
