6e8a2bf4b2d30abd8e1d2a18e189a0d4fb5d5dd0
Vadim Peretokin <vperetokin@gmail.com>
2020-10-30 11:18:13 +0000

    Run npm install on start

--- CI/travis.osx.before_install.sh
+++ CI/travis.osx.before_install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -ev
 brew update-reset
+
+# Ensure Homebrew's npm is used, instead of an outdated one
+echo "Running npm install. PATH is $PATH"
+PATH=/usr/local/bin:$PATH
+npm install -g appdmg
+echo "Successfully ran npm install."
