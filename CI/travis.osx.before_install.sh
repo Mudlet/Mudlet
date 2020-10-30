895dacc4ec3252088843f1f5acbc13c4de2eb38e
Vadim Peretokin <vperetokin@gmail.com>
2020-10-30 13:44:01 +0000

    Echo before checking

--- CI/travis.osx.before_install.sh
+++ CI/travis.osx.before_install.sh
@@ -3,11 +3,12 @@ set -ev
 brew update-reset
 
 # Ensure Homebrew's npm is used, instead of an outdated one
-echo "Running npm install. PATH is $PATH"
 PATH=/usr/local/bin:$PATH
+echo "Running npm install. PATH is $PATH"
 npm install -g appdmg
 echo "Successfully ran npm install."
 
-"1 Checking /Users/travis/.npm-global..."
-mkdir ~/.npm-global
+echo "Making .npm-global..."
+mkdir /Users/travis/.npm-global
+echo "1 Checking /Users/travis/.npm-global..."
 file /Users/travis/.npm-global
