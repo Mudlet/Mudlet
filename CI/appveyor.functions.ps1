aae52fcfe1fe74a596a2104766cfec9181443b1b
Vadim Peretokin <vperetokin@gmail.com>
2020-11-04 11:20:52 +0100

    Update Qt to use latest 5.12.9 LTS version (#4256)
    * Update Qt to use latest LTS version
    
    * Change 'silent install' to use 5.12.9 as well

--- CI/appveyor.functions.ps1
+++ CI/appveyor.functions.ps1
@@ -22,7 +22,7 @@ function SetQtBaseDir([string] $logFile) {
     }
     catch
     {
-      $Env:QT_BASE_DIR = "C:\Qt\5.13.2\mingw73_32"
+      $Env:QT_BASE_DIR = "C:\Qt\5.12.9\mingw73_32"
     }
   }
   Write-Output "Using $Env:QT_BASE_DIR as QT base directory." | Tee-Object -File "$logFile" -Append
