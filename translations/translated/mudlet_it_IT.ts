ca4c9e8a75f459332ee681d21e6f10ce398dea1a
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2020-11-06 08:56:55 +0100

    New translations mudlet.ts (Italian)

--- translations/translated/mudlet_it_IT.ts
+++ translations/translated/mudlet_it_IT.ts
@@ -85,12 +85,12 @@ attendere prego...</translation>
     <message>
       <location filename="../src/Host.cpp" line="3521"/>
       <source>Pre-Map loading(3) report</source>
-      <translation type="unfinished"/>
+      <translation>Rapporto del pre-caricamento della Mappa(3)</translation>
     </message>
     <message>
       <location filename="../src/Host.cpp" line="3531"/>
       <source>Loading map(3) at %1 report</source>
-      <translation type="unfinished"/>
+      <translation>Rapporto del caricamento della mappa(3) a %1</translation>
     </message>
   </context>
   <context>
