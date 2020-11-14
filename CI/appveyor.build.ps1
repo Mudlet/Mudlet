d24e808717f363bae39c1b22b2a25bea45cf0d19
Vadim Peretokin <vperetokin@gmail.com>
2020-11-14 11:33:04 +0100

    Try packaging

--- CI/appveyor.build.ps1
+++ CI/appveyor.build.ps1
@@ -8,7 +8,7 @@ SetLuarocksPath "C:\src\verbose_output.log"
 . CI\set-build-info.ps1
 . CI\validate_deployment.ps1
 
-cd "$Env:APPVEYOR_BUILD_FOLDER\src"
+Set-Location "$Env:APPVEYOR_BUILD_FOLDER\src"
 
 $Env:PATH="C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;$Env:QT_BASE_DIR\bin;$Env:MINGW_BASE_DIR\bin;" + (($Env:PATH.Split(';') | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';')
 qmake CONFIG+=release mudlet.pro
