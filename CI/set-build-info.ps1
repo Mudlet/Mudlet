05ea6269d2ce7eed3a9d6d112169f524190c9e60
Vadim Peretokin <vperetokin@gmail.com>
2020-11-17 13:31:06 +0100

    Export BUILD_COMMIT

--- CI/set-build-info.ps1
+++ CI/set-build-info.ps1
@@ -12,21 +12,21 @@ if ($Env:APPVEYOR_REPO_TAG -ne "true" -and -not ((Test-Path Env:GITHUB_REF) -and
     $Env:MUDLET_VERSION_BUILD = "-testing"
   }
   if (Test-Path Env:APPVEYOR_PULL_REQUEST_NUMBER) {
-    $Script:Commit = git rev-parse --short $Env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT
+    $Env:BUILD_COMMIT = git rev-parse --short $Env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT
     $Env:MUDLET_VERSION_BUILD = "$Env:MUDLET_VERSION_BUILD-PR$Env:APPVEYOR_PULL_REQUEST_NUMBER-$Commit"
   }  elseif ($Env:GITHUB_EVENT_NAME -eq "pull_request") {
-    $Script:Commit = git rev-parse --short $Env:GITHUB_SHA
+    $Env:BUILD_COMMIT = git rev-parse --short $Env:GITHUB_SHA
     # This pulls eb58e2d0, but need 2901871
     # HEAD is now at eb58e2d0 Merge 2901871d7ad39482338863639f10b246623ff18d into c993ae52c25b2f9b6855398028588c1e3a4a20a1
     $Script:Pr_Pattern_Number = [regex]'refs/pull/(.+?)/'
     $Script:PR_NUMBER = ($Script:Pr_Pattern_Number.Matches($Env:GITHUB_REF) | ForEach-Object { $_.groups[1].value })
     $Env:MUDLET_VERSION_BUILD = "$Env:MUDLET_VERSION_BUILD-PR$Script:PR_NUMBER-$Commit"
   } else {
-    $Script:Commit = git rev-parse --short HEAD
+    $Env:BUILD_COMMIT = git rev-parse --short HEAD
 
     if ($Env:MUDLET_VERSION_BUILD -eq "-ptb") {
       $Script:Date = Get-Date -Format "yyyy-MM-dd"
-      $Script:Short_Commit = $Script:Commit.Substring(0, 5)
+      $Script:Short_Commit = $Env:BUILD_COMMIT.Substring(0, 5)
       $Env:MUDLET_VERSION_BUILD = "$Env:MUDLET_VERSION_BUILD-$Date-$Short_Commit"
     } else {
       $Env:MUDLET_VERSION_BUILD = "$Env:MUDLET_VERSION_BUILD-$Commit"
