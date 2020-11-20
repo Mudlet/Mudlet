e25ee7d3c1fdc6279e2608c504a5ea5c3105c56a
Vadim Peretokin <vperetokin@gmail.com>
2020-11-20 13:59:32 +0100

    Try without quotes

--- CI/appveyor.functions.ps1
+++ CI/appveyor.functions.ps1
@@ -396,7 +396,7 @@ function InstallPugixml() {
 function InstallLfs() {
   Set-Location $env:LUAROCKS_DIR
   ls $env:LUAROCKS_DIR
-  "$env:LUAROCKS_DIR\luarocks.bat --help"
+  .\luarocks.bat --help
   exec ".\luarocks" @("--tree=`"$Env:MINGW_BASE_DIR`"", "install", "LuaFileSystem")
 }
 
