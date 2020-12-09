04e40cbf50ed109dbfb3f4dc9a2172e818b7cdef
Vadim Peretokin <vperetokin@gmail.com>
2020-12-09 23:07:05 +0100

    Check for variable existence correctly

--- CI/travis.set-build-info.sh
+++ CI/travis.set-build-info.sh
@@ -45,7 +45,7 @@ elif [ "${Q_OR_C_MAKE}" = "qmake" ]; then
 fi
 
 # temporary - distinguish github-built-ones
-if [[ -v GITHUB_REPOSITORY ]]; then
+if [[ -v $GITHUB_REPOSITORY ]]; then
   MUDLET_VERSION_BUILD="$MUDLET_VERSION_BUILD-github"
 fi
 
@@ -53,7 +53,7 @@ fi
 MUDLET_VERSION_BUILD=$(echo "$MUDLET_VERSION_BUILD" | tr '[:upper:]' '[:lower:]')
 VERSION=$(echo "$VERSION" | tr '[:upper:]' '[:lower:]')
 
-if [[ -v GITHUB_REPOSITORY ]]; then
+if [[ -v $GITHUB_REPOSITORY ]]; then
   {
     echo "VERSION=$VERSION"
     echo "MUDLET_VERSION_BUILD=$MUDLET_VERSION_BUILD"
