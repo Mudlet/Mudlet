8f4026ffe4172f75e692f2302e3cb2adb219ff23
Vadim Peretokin <vperetokin@gmail.com>
2020-12-15 17:36:33 +0100

    Add Linux snapshot packaging to Github Actions (#4446)
    * Plug in release validation
    
    * Add pcregrep to list of installed packages
    
    * Negate tag check correctly for Github
    
    * Fix install line
    
    * Port set-build-info to Actions
    
    * Pass GITHUB_SHA as a variable
    
    * Check for variable existence correctly
    
    * Debug parsing build commit info
    
    * Debug getting build commits more
    
    * Correct check for building with Actions
    
    * Fix bash syntax in retrieving build commit
    
    * Try another syntax for pcregrep
    
    * Pull out PR_NUMBER in another way
    
    * Remove refs/ from PR number
    
    * Check for Github variable correctly
    
    * Fix check for macos
    
    * Pull out Mudlet version in github builds
    
    * Try a macOS-compatible way of checking if var exists
    
    * Cleanup
    
    * Ignore compiled cmake file
    
    * Plug in after-build packaging
    
    * Fix up more packaging
    
    * First draft of snapshot packages
    
    * Fix repository check
    
    * Switch to branch of installers
    
    * Show vcpkg folder
    
    * Install tree first
    
    * Add LS in
    
    * Try find instead
    
    * Add Lua to PATH
    
    * Install dependencies
    
    * Install Luarocks via apt
    
    * Clarify dependency philosophy
    
    * Install yajl last
    
    * Set YAJL_LIBDIR
    
    * Add sqlite to dependencies
    
    * Install sqlite
    
    * Install sqlite3
    
    * Install YAJL first & point to sqlite3 dir
    
    * Try parent of yajl
    
    * Revert "Try parent of yajl"
    
    This reverts commit 591e39f75b8a55b5d789109cfdc0ae6e00b31b14.
    
    * Try parent yajl directory
    
    * Set YAJL_INCDIR as instructed
    
    * Print include dir
    
    * Install tree first
    
    * Pass in LUA_INCDIR as well
    
    * Pass libdir
    
    * Pass all yajl variables in one ago
    
    * Find where libyajl is installed
    
    * Update to latest vcpkg
    
    * Workaround dodgy yajl build
    
    * Workaround in debug build as well
    
    * Revert "Update to latest vcpkg"
    
    This reverts commit e2b1a04dd55e77c3ce79b889cab66f387d7dead2.
    
    * Show the final /lib dir
    
    * Find all cases of yajl
    
    * Pass in LIBRARY_PATH
    
    * Set LIBRARY_PATH another way
    
    * Use LIBRARY_PATH : separatro
    
    * Echo $LIBRARY_PATH for debug
    
    * export LIBRARY_PATH
    
    * Revert "Use LIBRARY_PATH : separatro"
    
    This reverts commit ed5047eac05b750aa1a73639c21a60e1ac849e16.
    
    # Conflicts:
    #	.github/workflows/build-mudlet.yml
    
    * Find sqlite3.h
    
    * Swap to SQLITE_INCDIR
    
    * Don't install YAJL locally
    
    * Install locally again
    
    * Install libssl dependency
    
    * Change OS down to ubuntu-16.04
    
    * Change to latest GCC
    
    * Try manuallog logging that gcc9 is isntalled
    
    * See if update-alternatives --install is optional
    
    * Fix update-alternatives set syntax
    
    * Try config instead
    
    * Try configuring it first
    
    * Fix alternative install path
    
    * Sudo for --set
    
    * Only run on deploy flag
    
    * Use matrix
    
    * Try just matrix.deploy
    
    * Fix package Mudlet step
    
    * WIP
    
    * Remove SSL dependency
    
    * Try with braces
    
    * Convert booleans to strings
    
    * libreadline find
    
    * Skip checking libreadline
    
    * Don't check Lua version
    
    * Set DEPLOY to 'deploy', not 'true'
    
    * Fix up instance of 'deploy'
    
    * Cache LuaRocks
    
    * Find where the built file is
    
    * Upload the Mudlet we package
    
    * Improve step name
    
    * Use find correctly
    
    * Install libgstreamer-plugins-base1.0-0
    
    * cd to dir again after building
    
    * Fix FOLDER_TO_UPLOAD
    
    * Try $pwd
    
    * Try giving it the whole folder
    
    * Upload less files
    
    * Only package on Linux
    
    * Change back to development installers repo
    
    * Don't distinguish github-made builds
    
    * Remove duplicate

--- CI/travis.set-build-info.sh
+++ CI/travis.set-build-info.sh
@@ -2,26 +2,33 @@
 
 MUDLET_VERSION_BUILD=""
 
-if [ -z "${TRAVIS_TAG}" ]; then
-  if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+if [ -z "${TRAVIS_TAG}" ] && ! [[ "$GITHUB_REF" =~ ^"refs/tags/" ]]; then
+  if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [[ "$GITHUB_EVENT_NAME" = "schedule" ]]; then
     MUDLET_VERSION_BUILD="-ptb"
   else
     MUDLET_VERSION_BUILD="-testing"
   fi
 
-  if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then # building for a PR
-    COMMIT=$(git rev-parse --short "${TRAVIS_PULL_REQUEST_SHA}")
-    MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-PR${TRAVIS_PULL_REQUEST}-${COMMIT}"
+  if [ -n "$TRAVIS_PULL_REQUEST" ]; then # building for a PR
+    BUILD_COMMIT=$(git rev-parse --short "${TRAVIS_PULL_REQUEST_SHA}")
+    MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-PR${TRAVIS_PULL_REQUEST}-${BUILD_COMMIT}"
+    PR_NUMBER=${TRAVIS_PULL_REQUEST}
+    export PR_NUMBER
+  elif [ "${GITHUB_EVENT_NAME}" = "pull_request" ]; then
+    BUILD_COMMIT=$(git rev-parse --short "${GITHUB_SHA}^2")
+    PR_NUMBER=$(echo "$GITHUB_REF" | sed 's/refs\///' |sed 's/pull\///' | sed 's/\/merge//')
+    MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-PR${PR_NUMBER}-${BUILD_COMMIT}"
+    echo "PR_NUMBER=$PR_NUMBER" >> "$GITHUB_ENV"
   else
-    COMMIT=$(git rev-parse --short HEAD)
+    BUILD_COMMIT=$(git rev-parse --short HEAD)
 
     if [ "${MUDLET_VERSION_BUILD}" = "-ptb" ]; then
       DATE=$(date +'%Y-%m-%d')
       # add a short commit to version for changelog generation know what was last released
-      SHORT_COMMIT=$(echo "${COMMIT}" | cut -c1-5)
+      SHORT_COMMIT=$(echo "${BUILD_COMMIT}" | cut -c1-5)
       MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-${DATE}-${SHORT_COMMIT}"
     else
-      MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-${COMMIT}"
+      MUDLET_VERSION_BUILD="${MUDLET_VERSION_BUILD}-${BUILD_COMMIT}"
     fi
   fi
 fi
@@ -32,11 +39,22 @@ if [ "${Q_OR_C_MAKE}" = "cmake" ]; then
   VERSION=$(perl -lne 'print $1 if /^set\(APP_VERSION (.+)\)/' < "${TRAVIS_BUILD_DIR}/CMakeLists.txt")
 elif [ "${Q_OR_C_MAKE}" = "qmake" ]; then
   VERSION=$(perl -lne 'print $1 if /^VERSION = (.+)/' < "${TRAVIS_BUILD_DIR}/src/mudlet.pro")
+elif [ -n "$GITHUB_REPOSITORY" ]; then
+  VERSION=$(perl -lne 'print $1 if /^set\(APP_VERSION (.+)\)/' < "${GITHUB_WORKSPACE}/CMakeLists.txt")
 fi
 
 # not all systems we deal with allow uppercase ascii characters
 MUDLET_VERSION_BUILD=$(echo "$MUDLET_VERSION_BUILD" | tr '[:upper:]' '[:lower:]')
 VERSION=$(echo "$VERSION" | tr '[:upper:]' '[:lower:]')
 
+if [ -n "$GITHUB_REPOSITORY" ]; then
+  {
+    echo "VERSION=$VERSION"
+    echo "MUDLET_VERSION_BUILD=$MUDLET_VERSION_BUILD"
+    echo "BUILD_COMMIT=$BUILD_COMMIT"
+  } >> "$GITHUB_ENV"
+fi
+
 export VERSION
 export MUDLET_VERSION_BUILD
+export BUILD_COMMIT
