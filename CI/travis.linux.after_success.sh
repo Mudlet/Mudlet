8d31b5761cc1e4f63c8a002ca759a6d3f96cbc87
Vadim Peretokin <vperetokin@gmail.com>
2020-12-15 08:48:38 +0100

    Find where the built file is

--- CI/travis.linux.after_success.sh
+++ CI/travis.linux.after_success.sh
@@ -52,7 +52,8 @@ if { [ "${DEPLOY}" = "deploy" ]; } ||
   if [ -z "${TRAVIS_TAG}" ] && ! [[ "$GITHUB_REF" =~ ^"refs/tags/" ]] && [ "${public_test_build}" != "true" ]; then
     echo "== Creating a snapshot build =="
     ./make-installer.sh "${VERSION}${MUDLET_VERSION_BUILD}"
-    echo "INSTALLER BUILT!"
+    echo "INSTALLER BUILT! BUILD_DIR: $BUILD_DIR"
+    find / -name "${VERSION}${MUDLET_VERSION_BUILD}"
 
     chmod +x "Mudlet-${VERSION}${MUDLET_VERSION_BUILD}.AppImage"
 
