3bcaaf99067fff6658ecdd4e31d17db6b481da16
Stephen Lyons <slysven@virginmedia.com>
2020-12-17 12:28:19 +0000

    Cleanup: adopt "Rule of Three" requirements for TChar class (#4469)
    Briefly: since we have a user-defined copy constructor we need to also
    have an assignment operator and a destructor - though they can be default
    ones.
    
    I discovered this when getting a warning when using the Clang 10 compiler
    on FreeBSD and found out more about it on Stack Exchange at:
    https://stackoverflow.com/q/51863588/4805858
    
    Also:
    * Because the action of the previous default constructor and the one that
    takes a `Host*` is the same if the latter is give a `nullptr` argument the
    former is redundant and can be removed if we default the `Host*` argument
    for the latter and do NOT mark it as `explicit`.
    * remove the std::move(...) wrapper at end of `TBuffer::log(int, int) as it
    is causing Clang 10 to warn:
    "Moving a temporary object prevents copy elision [-Wpessimizing-move]"
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/TBuffer.cpp
+++ src/TBuffer.cpp
@@ -1,7 +1,8 @@
 /***************************************************************************
  *   Copyright (C) 2008-2013 by Heiko Koehn - KoehnHeiko@googlemail.com    *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
- *   Copyright (C) 2014-2018 by Stephen Lyons - slysven@virginmedia.com    *
+ *   Copyright (C) 2014-2018, 2020 by Stephen Lyons                        *
+ *                                               - slysven@virginmedia.com *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -51,16 +52,6 @@
 //#define DEBUG_MXP_PROCESSING
 
 
-// Default constructor:
-TChar::TChar()
-: mFgColor(Qt::white)
-, mBgColor(Qt::black)
-, mFlags(None)
-, mIsSelected(false)
-, mLinkIndex(0)
-{
-}
-
 TChar::TChar(const QColor& fg, const QColor& bg, const TChar::AttributeFlags flags, const int linkIndex)
 : mFgColor(fg)
 , mBgColor(bg)
@@ -106,7 +97,8 @@ bool TChar::operator==(const TChar& other)
     return true;
 }
 
-// Copy constructor:
+// Copy constructor - because it is resetting the mIsSelected flag it is NOT a
+// default copy constructor:
 TChar::TChar(const TChar& copy)
 : mFgColor(copy.mFgColor)
 , mBgColor(copy.mBgColor)
@@ -2701,7 +2693,7 @@ void TBuffer::log(int fromLine, int toLine)
 
     // record the last log call into a temporary buffer - we'll actually log
     // on the next iteration after duplication detection has run
-    lastTextToLog = std::move(linesToLog.join(QString()));
+    lastTextToLog = linesToLog.join(QString());
     lastLoggedFromLine = fromLine;
     lastloggedToLine = toLine;
 }
