ecff92a9cf5e6fe61f450b55c537a7d0c473b586
Stephen Lyons <slysven@virginmedia.com>
2020-12-21 15:43:00 +0000

    BugFix: correct QRegularExpression warnings on profile save (#4519)
    This was causing messages of the form:
    "QRegularExpressionPrivate::doMatch(): called on an invalid
    QRegularExpression object" whenever a colour trigger was saved. It was due
    to an error in the regular expression defined in:
    QStringList XMLexport::remapAnsiToColorNumber(const QStringList&,
                                                             const QList<int>&)
    
    Also remove unneeded #include <QRegularExpression>  in classes that do
    not need it.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/TKey.h
+++ src/TKey.h
@@ -4,7 +4,7 @@
 /***************************************************************************
  *   Copyright (C) 2008-2012 by Heiko Koehn - KoehnHeiko@googlemail.com    *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
- *   Copyright (C) 2018 by Stephen Lyons - slysven@virginmedia.com         *
+ *   Copyright (C) 2018, 2020 by Stephen Lyons - slysven@virginmedia.com   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -27,7 +27,6 @@
 
 #include "pre_guard.h"
 #include <QPointer>
-#include <QRegularExpression>
 #include "post_guard.h"
 
 class Host;
@@ -85,7 +84,6 @@ private:
     int mKeyModifier;
 
     QString mRegexCode;
-    QRegularExpression mRegex;
     QString mScript;
     QString mFuncName;
     QPointer<Host> mpHost;
