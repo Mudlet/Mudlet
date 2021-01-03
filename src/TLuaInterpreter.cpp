0c3d9ec58e39eb30a280c20e82418ae01a2f9507
Piotr Wilczynski <piotr.wilczynski@bisnode.com>
2021-01-03 16:56:44 +0100

    restore case insensitivity

--- src/TLuaInterpreter.cpp
+++ src/TLuaInterpreter.cpp
@@ -3224,7 +3224,7 @@ int TLuaInterpreter::setFont(lua_State* L)
     }
     QString font{lua_tostring(L, s)};
 
-    if (!mudlet::self()->getAvailableFonts().contains(font)) {
+    if (!mudlet::self()->getAvailableFonts().contains(font, Qt::CaseInsensitive)) {
         lua_pushnil(L);
         lua_pushfstring(L, "Font '%s' is not available)", font.toUtf8().constData());
         return 2;
