1fe11a823207b4aea31f31decf93aea8be19c14b
Kebap <kebap_spam@gmx.net>
2021-01-19 14:50:03 +0100

    Announce file name as well

--- src/mudlet-lua/lua/Other.lua
+++ src/mudlet-lua/lua/Other.lua
@@ -1030,14 +1030,16 @@ function packageDrop(event, fileName, suffix)
 
   mudlet.Locale = mudlet.Locale or loadTranslations("Mudlet")
   if installationSuccessful then
-    local successText = (mudlet.Locale.packageInstallSuccess 
-      and mudlet.Locale.packageInstallSuccess.message or "Package installed successfully.")
+    local successText = (mudlet.Locale.packageInstallSuccess
+      and mudlet.Locale.packageInstallSuccess.message or "Package '%s' installed successfully.")
+    sucessText = string.format(successText, fileName)
     local okPrefix = (mudlet.Locale.prefixOk and mudlet.Locale.prefixOk.message or "[  OK  ]  - ")
     decho('<0,160,0>' .. okPrefix .. '<190,100,50>' .. successText .. '\n')
     -- Light Green and Orange-ish; see cTelnet::postMessage for color comparison
   else
-    local failureText = (mudlet.Locale.packageInstallFail 
-      and mudlet.Locale.packageInstallFail.message or "Package installation failed.")
+    local failureText = (mudlet.Locale.packageInstallFail
+      and mudlet.Locale.packageInstallFail.message or "Package '%s' installation failed.")
+    failureText = string.format(failureText, fileName)
     local warnPrefix = (mudlet.Locale.prefixWarn and mudlet.Locale.prefixWarn.message or "[ WARN ]  - ")
     decho('<0,150,190>' .. warnPrefix .. '<190,150,0>' .. failureText .. '\n')
     -- Cyan and Orange; see cTelnet::postMessage for color comparison
