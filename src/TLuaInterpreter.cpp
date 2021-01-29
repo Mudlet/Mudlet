0b7aa35d94cb45817f98427089c26cf7703382a7
Kebap <kebap_spam@gmx.net>
2021-01-29 01:08:19 +0100

    don't speak when given whitespace

--- src/TLuaInterpreter.cpp
+++ src/TLuaInterpreter.cpp
@@ -12231,6 +12231,10 @@ int TLuaInterpreter::ttsSpeak(lua_State* L)
     QString textToSay;
     textToSay = lua_tostring(L, 1);
 
+    if (textToSay.trimmed().isEmpty()) { // there's nothing more to say
+        return 0;
+    }
+
     speechUnit->say(textToSay);
     speechCurrent = textToSay;
 
@@ -12521,6 +12525,11 @@ int TLuaInterpreter::ttsQueue(lua_State* L)
     }
 
     QString inputText = lua_tostring(L, 1);
+
+    if (inputText.trimmed().isEmpty()) { // there's nothing more to say
+        return 0;
+    }
+
     int index;
 
     if (lua_gettop(L) > 1) {
