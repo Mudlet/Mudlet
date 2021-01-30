7979458cdcf1b821f28216049e6827305b10bde8
Gustavo Sousa <gustavocms@gmail.com>
2021-01-30 07:20:51 -0300

    Infrastructure: Fix the TMxpSendTagHandlerTest and re-enable it (#4714)
    * Fixes problem blocking compilation of TMxpSendTagHandlerTest
    
    - Extracts TMediaData to a header in order to remove dependencies on MediaPlayer in testing environment
    
    * Adds override specifier to TMxpStubClient functions

--- src/TMxpSoundTagHandler.cpp
+++ src/TMxpSoundTagHandler.cpp
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "TMedia.h"
+#include "TMediaData.h"
 #include "TMxpSoundTagHandler.h"
 #include "TMxpClient.h"
 
