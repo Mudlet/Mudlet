4ecd229f3dc6ae7a89437f5a7d07a96cb1e2047c
Vadim Peretokin <vperetokin@gmail.com>
2021-02-02 21:25:40 +0100

    Fingers crossed, this does it for PTB changelog generated (#4734)

--- CI/generate-ptb-changelog.lua
+++ CI/generate-ptb-changelog.lua
@@ -65,7 +65,7 @@ end
 function extract_historical_sha1s()
   local history, command
   if github_workspace then
-    command = string.format("git log --pretty=%%H -n %d %s^2", MAX_COMMITS_PER_CHANGELOG, os.getenv("GITHUB_SHA"))
+    command = string.format("git log --pretty=%%H -n %d %s", MAX_COMMITS_PER_CHANGELOG, os.getenv("GITHUB_SHA"))
     print("[temporary debug information: "..command.."]")
     history = string.split(os.capture(command))
   else
