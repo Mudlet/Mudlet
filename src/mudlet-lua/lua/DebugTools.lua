9d3f4e9561d73842613c056a51393069df8ba446
Edru2 <alyven_87@hotmail.com>
2021-02-11 15:26:25 +0100

    make inspect global + replace prettywrite in miniconsole display

--- src/mudlet-lua/lua/DebugTools.lua
+++ src/mudlet-lua/lua/DebugTools.lua
@@ -29,7 +29,7 @@ function showMultimatches()
 end
 
 
-local inspect ={
+inspect ={
   _VERSION = 'inspect.lua 3.1.0',
   _URL     = 'http://github.com/kikito/inspect.lua',
   _DESCRIPTION = 'human-readable representations of tables',
