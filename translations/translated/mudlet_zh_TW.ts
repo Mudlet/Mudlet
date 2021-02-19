830e2312cd7ab934004d0c798d5a32b2c2fb9cfc
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2021-02-19 06:25:44 +0100

    New translations mudlet.ts (Chinese Traditional)

--- translations/translated/mudlet_zh_TW.ts
+++ translations/translated/mudlet_zh_TW.ts
@@ -1226,7 +1226,7 @@ rooms:</source>
       <location filename="../src/TArea.cpp" line="767"/>
       <source>no text</source>
       <comment>Default text if a label is created in mapper with no text</comment>
-      <translation type="unfinished"/>
+      <translation>沒有文字</translation>
     </message>
   </context>
   <context>
@@ -1775,12 +1775,12 @@ map with others relying on the original format.</source>
     <message>
       <location filename="../src/TMap.cpp" line="48"/>
       <source>Default Area</source>
-      <translation type="unfinished"/>
+      <translation>默认地区</translation>
     </message>
     <message>
       <location filename="../src/TMap.cpp" line="49"/>
       <source>Unnamed Area</source>
-      <translation type="unfinished"/>
+      <translation>未命名的地区</translation>
     </message>
     <message>
       <location filename="../src/TMap.cpp" line="439"/>
