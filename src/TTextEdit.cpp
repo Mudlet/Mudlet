17556bdbe6cea831f0ac7a218c546cdabdb7e627
Stephen Lyons <slysven@virginmedia.com>
2021-02-20 09:50:41 +0000

    Fix selection of replay message (#4650)
    * BugFix: improve handling of TConsole::mSystemMessageBgColor
    
    This `QColor` is initilised to the default value of `TConsole::mBgColor`
    which, since #4570, is now the transparent color. Unfortunely this is bad
    because when text printed on the main console with:
    `(void) TConsole::printSystemMessage(const QString)`
    the red foreground and the transparent background get swapped over and thus
    is rendered as a solid block of red.
    
    This PR attempts to solve this by ensuring that whne the
    `TConsole::mBgColor` member is set to a different color that the same
    colour is also stored for use as `TConsole::mSystemMessageBgColor`.
    
    The issue of how things are colored does seem to be made more complicated
    by the use of `QPalette` for some things for which it does not seem to be
    worthwhile; therefore in this PR I also remove some uses of the Qt palette
    system in places where it does not seem to be doing anything useful.
    
    I have also reviewed how things might be affected by setting an image as
    a background image for the main console but it seems that the changes here
    do not prevent the display of an image where there is NO text shown on
    screen.
    
    This might close #4635.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>
    
    * BugFix: part revert previous commit and correct spelling
    
    The modifications made to `(void) TConsole::changeColors()` to take away
    the manipulation of the command line palette should not have been done!
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/TTextEdit.cpp
+++ src/TTextEdit.cpp
@@ -118,11 +118,6 @@ TTextEdit::TTextEdit(TConsole* pC, QWidget* pW, TBuffer* pB, Host* pH, bool isLo
     setAttribute(Qt::WA_OpaquePaintEvent, false);
     setAttribute(Qt::WA_DeleteOnClose);
 
-    QPalette palette;
-    palette.setColor(QPalette::Text, mFgColor);
-    palette.setColor(QPalette::Highlight, QColor(55, 55, 255));
-    palette.setColor(QPalette::Base, mBgColor);
-    setPalette(palette);
     showNewLines();
     setMouseTracking(true); // test fix for MAC
     setEnabled(true);       //test fix for MAC
