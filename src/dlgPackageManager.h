d976b63c96e8201296673f58d79dce45f0502b41
Edru2 <alyven_87@hotmail.com>
2021-03-03 19:08:02 +0100

    copyright for headers

--- src/dlgPackageManager.h
+++ src/dlgPackageManager.h
@@ -2,6 +2,8 @@
 #define MUDLET_DLGPACKAGEMANAGER_H
 
 /***************************************************************************
+ *   Copyright (C) 2021 by Manuel Wegmann - wegmann.manuel@yahoo.com       *
+ *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
