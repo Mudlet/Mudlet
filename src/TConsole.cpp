fa1d04e28fb44b8c62a4d54fb4afcb30304c53d6
Stephen Lyons <slysven@virginmedia.com>
2021-03-17 02:36:18 +0000

    BugFix: prevent initial split view in TConsoles (#4953)
    * BugFix: prevent initial split view in TConsoles
    
    This was because the `(TTextEdit) TConsole::mLowerPane` was being shown
    and then hidden by a one shot "run when idle" timer when there is no
    need to AFAICT.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/TConsole.cpp
+++ src/TConsole.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2008-2013 by Heiko Koehn - KoehnHeiko@googlemail.com    *
- *   Copyright (C) 2014-2020 by Stephen Lyons - slysven@virginmedia.com    *
+ *   Copyright (C) 2014-2021 by Stephen Lyons - slysven@virginmedia.com    *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
  *   Copyright (C) 2016 by Ian Adkins - ieadkins@gmail.com                 *
  *                                                                         *
@@ -485,10 +485,7 @@ TConsole::TConsole(Host* pH, ConsoleType type, QWidget* parent)
     splitter->setSizes(sizeList);
 
     mUpperPane->show();
-    mLowerPane->show();
-    mLowerPane->updateScreenView();
-    // timer needed as updateScreenView doesn't seem to finish in time
-    QTimer::singleShot(0, [this]() { mLowerPane->hide(); });
+    mLowerPane->hide();
 
     connect(mpScrollBar, &QAbstractSlider::valueChanged, mUpperPane, &TTextEdit::slot_scrollBarMoved);
     connect(mpHScrollBar, &QAbstractSlider::valueChanged, mUpperPane, &TTextEdit::slot_hScrollBarMoved);
