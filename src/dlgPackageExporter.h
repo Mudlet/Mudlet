86941524a985c50bacad3dff7282886ae5b628e7
Manuel Wegmann <60551052+Edru2@users.noreply.github.com>
2021-04-03 08:45:43 +0200

    allow drag and drop and copy/paste of images onto package description in package exporter (#5058)
    * allow drag and drop of images onto description
    
    * return true got lost
    
    * copy packagePath/.mudlet
    
    * allow copy/paste of images
    
    * codefactor
    
    * Update dlgPackageExporter.h
    
    * didn't compile
    
    * disable copy_move
    
    * allow insert on cursor position if copy/paste
    
    if image is inserted with copy paste it will be insert at the cursor position, drag and drop images will always be inserted at the end.
    Images won't be duplicated anymore
    
    * image names as placeholder + svg support
    
    * clearer comment
    
    * Update placeholder to mention markdown and dnd support
    
    Co-authored-by: Vadim Peretokin <vperetokin@gmail.com>

--- src/dlgPackageExporter.h
+++ src/dlgPackageExporter.h
@@ -27,8 +27,10 @@
 
 #include "pre_guard.h"
 #include <QDialog>
+#include <QTextEdit>
 #include <zip.h>
 #include "post_guard.h"
+
 
 class QTreeWidget;
 class QTreeWidgetItem;
@@ -74,6 +76,8 @@ public:
     // This will hold the absolute pathFileName for the XML file that will
     // contain the Mudlet items to go into the package:
     QString mXmlPathFileName;
+    QString mPlainDescription;
+    QStringList mDescriptionImages;
 
 public slots:
     void slot_addFiles();
@@ -118,10 +122,25 @@ private:
     QString mPackagePathFileName;
     QString mPackageIconPath;
     QString mPackageConfig;
-    QString mPlainDescription;
 
 signals:
     void signal_exportLocationChanged(const QString& location);
+};
+
+
+class dlgPackageExporterDescription : public QTextEdit
+{
+    Q_OBJECT
+
+public:
+    Q_DISABLE_COPY(dlgPackageExporterDescription)
+#if (QT_VERSION) >= (QT_VERSION_CHECK(5, 13, 0))
+    Q_DISABLE_MOVE(dlgPackageExporterDescription)
+#endif
+    explicit dlgPackageExporterDescription(QWidget* pW = nullptr);
+    ~dlgPackageExporterDescription();
+    bool canInsertFromMimeData(const QMimeData* source) const override;
+    void insertFromMimeData(const QMimeData* source) override;
 };
 
 #endif // MUDLET_DLGPACKAGEEXPORTER_H
