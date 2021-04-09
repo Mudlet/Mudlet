17686446a9ec3f85146403af7473e1121b360be3
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2021-04-09 08:35:56 +0200

    New translations mudlet.ts (Chinese Simplified)

--- translations/translated/mudlet_zh_CN.ts
+++ translations/translated/mudlet_zh_CN.ts
@@ -5895,12 +5895,12 @@ Count</source>
     <message>
       <location filename="../src/dlgPackageManager.cpp" line="171"/>
       <source>Author</source>
-      <translation type="unfinished"/>
+      <translation>作者</translation>
     </message>
     <message>
       <location filename="../src/dlgPackageManager.cpp" line="171"/>
       <source>Version</source>
-      <translation type="unfinished"/>
+      <translation>版本</translation>
     </message>
     <message>
       <location filename="../src/dlgPackageManager.cpp" line="171"/>
