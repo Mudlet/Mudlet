441814b16effc04c4711f6bb1794cf415d56cd85
Stephen Lyons <slysven@virginmedia.com>
2021-04-10 05:59:18 +0100

    Coverity: fix issue registered as CID 1503742 (#5126)
    An (lack of) initialisation issue.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/TConsole.h
+++ src/TConsole.h
@@ -209,7 +209,7 @@ public:
     QString mDisplayFontName;
     int mDisplayFontSize;
     QFont mDisplayFont;
-    int mEngineCursor;
+    int mEngineCursor = -1;
     QColor mFgColor;
     TChar mFormatBasic;
     TChar mFormatSystemMessage;
