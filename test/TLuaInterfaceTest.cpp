cbd0f74eb55127e3d5641a12230f30622f70a484
Chris Mitchell <chris.mit7@gmail.com>
2021-04-11 12:24:08 -0400

    Fix tests

--- test/TLuaInterfaceTest.cpp
+++ test/TLuaInterfaceTest.cpp
@@ -1,41 +1,56 @@
-//#include <QList>
-//#include <QMap>
-
 #include <LuaInterface.h>
-//#include <TVar.h>
+#include <TVar.h>
 #include <VarUnit.h>
 #include <QtTest/QtTest>
 
 extern "C" {
-#include <lauxlib.h>
-#include <lua.h>
-#include <lualib.h>
+    #include <lauxlib.h>
+    #include <lua.h>
+    #include <lualib.h>
 }
 
 class TVarTest : public QObject {
 Q_OBJECT
 
 private:
+    lua_State* L;
+    LuaInterface* interface;
 
 private slots:
 
     void initTestCase()
     {
+        L = luaL_newstate();
+        interface = new LuaInterface(L);
+    }
 
+    void execLua(QString string) {
+        luaL_loadstring(L, string.toUtf8().constData());
+        lua_pcall(L, 0, 0, 0);
     }
 
     void testRetrieveStrings()
     {
-        lua_State* L = luaL_newstate();
-        LuaInterface* interface = new LuaInterface(L);
-        luaL_loadstring(L, QString("test = 1").toUtf8().constData());
-        lua_pcall(L, 0, 0, 0);
+        execLua("test = '1'");
         interface->getVars(false);
         VarUnit* vu = interface->getVarUnit();
         TVar* base = vu->getBase();
         QList<TVar*> children = base->getChildren();
-        qDebug() << "children are" << children;
+        TVar* testVar = children.first();
+        QCOMPARE(testVar->getValue(), "1");
+        QCOMPARE(testVar->getValueType(), LUA_TSTRING);
+    }
 
+    void testRetrieveNumber()
+    {
+        execLua("test = 1");
+        interface->getVars(false);
+        VarUnit* vu = interface->getVarUnit();
+        TVar* base = vu->getBase();
+        QList<TVar*> children = base->getChildren();
+        TVar* testVar = children.first();
+        QCOMPARE(testVar->getValue(), "1");
+        QCOMPARE(testVar->getValueType(), LUA_TNUMBER);
     }
 
     void cleanupTestCase()
