0ef8c6c3f81de1a46be5b0c56674472cac2321be
Stephen Lyons <slysven@virginmedia.com>
2021-04-14 04:09:59 +0100

    Revise: some changes to Server GUI downloader handling (CID 1478854) (#5147)
    Initially this was going to address Coverity CID 1478854 - like Vadi's
    PR #5130. However I spotted some improvements that could be made as well:
    * rename `(void) cTelnet::replyFinished(QNetworkReply*)` to
    `(void) cTelnet::slot_replyFinished(QNetworkReply*)` as it *is* a SLOT.
    * rename `(QNetworkReply*) cTelnet::packageDownloadReply` to
    `(QNetworkReply*) mpPackageDownloadReply`
    * check that the argument passed to the slot method matches the one that is
    expected (issues a warning if it doesn't)
    * clean up the argument supplied to the slot method in ALL cases.
    * reset the stored `QNetworkReply` point to a `nullptr` upon success (and
    thus returns it to the state it now has on class creation.
    
    Revised warning message to give better context.
    
    Signed-off by: Stephen Lyons <slysven@virginmedia.com>
    Co-authored-by: Vadim Peretokin <vperetokin@gmail.com>

--- src/ctelnet.h
+++ src/ctelnet.h
@@ -6,7 +6,7 @@
  *   Copyright (C) 2008-2013 by Heiko Koehn - KoehnHeiko@googlemail.com    *
  *   Copyright (C) 2014-2017 by Ahmed Charles - acharles@outlook.com       *
  *   Copyright (C) 2014-2015 by Florian Scheel - keneanung@googlemail.com  *
- *   Copyright (C) 2015, 2017-2019 by Stephen Lyons                        *
+ *   Copyright (C) 2015, 2017-2019, 2021 by Stephen Lyons                  *
  *                                               - slysven@virginmedia.com *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -205,7 +205,7 @@ public:
 
 public slots:
     void setDownloadProgress(qint64, qint64);
-    void replyFinished(QNetworkReply*);
+    void slot_replyFinished(QNetworkReply*);
     void slot_processReplayChunk();
     void handle_socket_signal_hostFound(QHostInfo);
     void handle_socket_signal_connected();
@@ -283,7 +283,7 @@ private:
     QTimer* mpPostingTimer;
     bool mUSE_IRE_DRIVER_BUGFIX;
 
-    QNetworkReply* packageDownloadReply;
+    QNetworkReply* mpPackageDownloadReply = nullptr;
 
     int mCommands;
     bool mMCCP_version_1;
