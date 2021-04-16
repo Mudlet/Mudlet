945c52b54069971df061eb8f0291dfbe64324cec
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2021-04-16 08:25:11 +0200

    New translations mudlet.ts (German)

--- translations/translated/mudlet_de_DE.ts
+++ translations/translated/mudlet_de_DE.ts
@@ -5891,7 +5891,7 @@ Nutzungen</translation>
       <location filename="../src/dlgPackageExporter.cpp" line="828"/>
       <source>Failed to open package file. Error is: &quot;%1&quot;.</source>
       <comment>This zipError message is shown when the libzip library code is unable to open the file that was to be the end result of the export process. As this may be an existing file anywhere in the computer&apos;s file-system(s) it is possible that permissions on the directory or an existing file that is to be overwritten may be a source of problems here.</comment>
-      <translation type="unfinished"/>
+      <translation>Fehler beim Öffnen der Paketdatei. Fehlermeldung: &quot;%1&quot;.</translation>
     </message>
     <message>
       <location filename="../src/dlgPackageExporter.cpp" line="978"/>
