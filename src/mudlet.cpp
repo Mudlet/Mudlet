6d54f8e44c5b3afcfd6c1487519e2b32673b0ce2
Vadim Peretokin <vperetokin@gmail.com>
2021-08-16 07:51:37 +0200

    Remove masses of if's

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -2424,7 +2424,7 @@ void mudlet::deleteProfileData(const QString& profile, const QString& item)
 void mudlet::startAutoLogin(const QString& cliProfile)
 {
     QStringList hostList = QDir(getMudletPath(profilesPath)).entryList(QDir::Dirs | QDir::NoDotAndDotDot, QDir::Name);
-    hostList += mudlet::scmDefaultGames;
+    hostList += mudlet::scmDefaultGames.keys();
     hostList << QStringLiteral("Mudlet self-test");
     hostList.removeDuplicates();
     bool openedProfile = false;
