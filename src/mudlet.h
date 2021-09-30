35970e3482c2e8556b1ba74b7f47ecef556cd97c
Vadim Peretokin <vperetokin@gmail.com>
2021-09-30 09:56:03 +0200

    Add Lua tests to CI (#3795)
    * Add a Ubuntu build
    
    * Install homebrew dependencies only on macOS
    
    * Add Linux dependencies separate
    
    * Don't stop other builds on fail
    
    * Install libzip-dev on Linux
    
    * Sudo please
    
    * Try installing libglu1-mesa-dev
    
    * Install Pulse dev libraries
    
    * Add a workaround for Lua linking
    
    * Correct YAML
    
    * Back to a single line
    
    * Debug locations of libraries
    
    * Correct an extra /
    
    * Build Mudlet with tracing
    
    * Try -GNinja Multi-Config
    
    * Try not specifying a generator
    
    * Add link information against libdl.so as well (#33)
    
    * Add link information against libdl.so as well
    
    * Remove (harmful?) lua library path definition
    
    * Show built output in the end
    
    * Added a run tests flag
    
    * Try appending gcc argument as well
    
    * Debug plugin loading
    
    * Use a single, generic compiler line
    
    * Remove the x's
    
    * Install libxkbcommon-x11-0
    
    * Try launching Achaea profile
    
    * Update .github/workflows/build-mudlet.yml
    
    Co-authored-by: Damian Monogue <3660+demonnic@users.noreply.github.com>
    
    * Try taking a screenshot of what's running
    
    * Fix YAML syntax
    
    * Clarify the step name
    
    * Take a screenshot even on build 'time out'
    
    * Install imagemagick
    
    * Fix whitespaces
    
    * Upgrade to latest 1.5 of the xvfb action
    
    * Pre-set environment variables to be used
    
    * Increase the timeout
    
    * Run with the self-test profile
    
    * Re-enable mudlet.org's connection info
    
    * Mock test runs
    
    * Add an installing debug echo
    
    * Debug with tmate
    
    * Show error when script couldn't be loaded
    
    * Print all script compile errors
    
    * Setup tmate at the end
    
    * Add debug echoes
    
    * Move Mudlet to the right location
    
    * Split out packaging steps
    
    * Cleanup
    
    * Auto-run tests
    
    * Install busted as well
    
    * Fix setup() call
    
    * Log everything sent to all consoles
    
    * Actually log everything printed
    
    * Pass in the test location
    
    * Comment out a crash
    
    * Tidier console print message
    
    * Echo cleanup
    
    * Better echo style
    
    * Wrap closeMudlet in a timer to avoid a crash
    
    * Comment out mSpellDic crash as well
    
    * Set environment variable when test failed
    
    * Fail workflow when tests fail
    
    * Write to GITHUB_ENV properly
    
    * Add setEnv
    
    * Make use of setEnv
    
    * Launch tmate
    
    * Write to file instead
    
    * Add comments
    
    * Improve error message
    
    * Remove tmate
    
    * Fix merge error
    
    * Add the new --mirror option
    
    * Don't deploy Windows builds
    
    * Only check Lua tests when we need to
    
    * Update job name
    
    * Remove unusued setEnv
    
    Co-authored-by: keneanung <keneanung@googlemail.com>
    Co-authored-by: Damian Monogue <3660+demonnic@users.noreply.github.com>

--- src/mudlet.h
+++ src/mudlet.h
@@ -485,7 +485,7 @@ public:
                                  "<center><a href='https://www.reinosdeleyenda.es/foro/'>Forums</a></center>\n"
                                  "<center><a href='https://wiki.reinosdeleyenda.es/'>Wiki</a></center>\n", ":/icons/reinosdeleyenda_mud.png"}},
         {"Fierymud", {"fierymud.org", 4000, false, "<center><a href='https://www.fierymud.org/'>https://www.fierymud.org</a></center>", ":/icons/fiery_mud.png"}},
-        // {"Mudlet self-test", {"mudlet.org", 23, false,}},
+        {"Mudlet self-test", {"mudlet.org", 23, false,}},
         {"Carrion Fields", {"carrionfields.net", 4449, false, "<center><a href='http://www.carrionfields.net'>www.carrionfields.net</a></center>", ":/icons/carrionfields.png"}},
         {"Cleft of Dimensions", {"cleftofdimensions.net", 4354, false, "<center><a href='https://www.cleftofdimensions.net/'>cleftofdimensions.net</a></center>", ":/icons/cleftofdimensions.png"}},
         {"Legends of the Jedi", {"legendsofthejedi.com", 5656, false, "<center><a href='https://www.legendsofthejedi.com/'>legendsofthejedi.com</a></center>", ":/icons/legendsofthejedi_120x30.png"}},
