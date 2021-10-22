3da8a95a538e7a36b837855b5cf72137c5e5e873
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2021-10-22 09:27:38 +0200

    New translations mudlet.ts (Swedish)

--- translations/translated/mudlet_sv_SE.ts
+++ translations/translated/mudlet_sv_SE.ts
@@ -132,7 +132,8 @@ Vänligen vänta...</translation>
       <source>Area:%1%2 ID:%1%3 x:%1%4%1&lt;‑&gt;%1%5 y:%1%6%1&lt;‑&gt;%1%7 z:%1%8%1&lt;‑&gt;%1%9
 </source>
       <comment>This text uses non-breaking spaces (as &apos;%1&apos;s, as Qt Creator cannot handle them literally in raw strings) and non-breaking hyphens which are used to prevent the line being split at some places it might otherwise be; when translating please consider at which points the text may be divided to fit onto more than one line. %2 is the (text) name of the area, %3 is the number for it, %4 to %9 are pairs (min &lt;-&gt; max) of extremes for each of x,y and z coordinates</comment>
-      <translation type="unfinished"/>
+      <translation>Område:%1%2 ID:%1%3 x:%1%4%1&lt;-&gt;%1%5 y:%1%6%1&lt;-&gt;%1%7 z:%1%8%1&lt;-&gt;%1%9
+</translation>
     </message>
     <message>
       <location filename="../src/mapInfoContributorManager.cpp" line="140"/>
