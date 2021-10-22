8d91e6bd7f22276de8dfb92299d9a51e127d3e23
keneanung <keneanung@googlemail.com>
2021-10-22 21:32:27 +0200

    Remove unneeded files

--- test/RunTestsOnWindows.cmd
+++
@@ -1,4 +0,0 @@
-echo If it doesn't work, check if the version numbers in the paths are correct!
-path C:\Qt\Tools\mingw730_32\bin;C:\Qt\5.13.2\mingw73_32\bin;%path%
-
-mingw32-make -f Makefile.TestsOnWindows %1%
