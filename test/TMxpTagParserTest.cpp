85924daa2e0a1f8e5844332e9d8e3b6c84cee155
keneanung <keneanung@googlemail.com>
2021-10-23 21:22:08 +0200

    Fix codefactor issues

--- test/TMxpTagParserTest.cpp
+++ test/TMxpTagParserTest.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   Copyright (C) 2021 by Florian Scheel - keneanung@gmail.com            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #include "TMxpTagParser.h"
 #include <MxpTag.h>
@@ -316,7 +334,8 @@ private slots:
     {
         QString tagLine = R"(<FRAME Name="Map" Left="-20c" Top="0" Width="20c" Height="20c">)";
 
-        auto node = parseNode(tagLine);QVERIFY(node->isStartTag());
+        auto node = parseNode(tagLine);
+        QVERIFY(node->isStartTag());
         MxpStartTag* tag = node->asStartTag();
 
         QCOMPARE(tag->getName(), "FRAME");
