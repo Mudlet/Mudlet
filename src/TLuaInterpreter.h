48318ad04b509362614229ab94812722d43ef36e
Piotr <delwing@gmail.com>
2021-11-07 22:12:44 +0100

    Add: Custom right click menu items for command lines (#5582)
    * Add: Custom menu items for command lines
    
    * better error message
    
    * more uniform handling when items do not exist
    
    * clear clang tidy errors
    
    * clear clang tidy errors
    
    * Update src/TLuaInterpreter.cpp
    
    Co-authored-by: Tim Johnson <29287358+atari2600tim@users.noreply.github.com>
    
    * rework command line menu items, they are backed up by events system now
    
    Co-authored-by: Tim Johnson <29287358+atari2600tim@users.noreply.github.com>

--- src/TLuaInterpreter.h
+++ src/TLuaInterpreter.h
@@ -618,6 +618,8 @@ public:
     static int addMouseEvent(lua_State* L);
     static int removeMouseEvent(lua_State* L);
     static int getMouseEvents(lua_State* L);
+    static int addCommandLineMenuEvent(lua_State* L);
+    static int removeCommandLineMenuEvent(lua_State* L);
     // PLACEMARKER: End of Lua functions declarations
 
 
