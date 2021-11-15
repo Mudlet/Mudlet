fe215b1dbab883416e9a64e642904015512dd2fb
Vadim Peretokin <vperetokin@gmail.com>
2021-11-15 21:15:02 +0100

    Add: show unicode characters which can change how code is interpreted in editor (#5650)
    #### Brief overview of PR changes/additions
    See https://www.trojansource.codes, it is possible to sneak in Unicode control characters which re-order how source code is understood - and if the editor doesn't show them (none did by default), bad things can happen.
    
    ![image](https://user-images.githubusercontent.com/110988/141780698-b4ea0a0d-0640-4aab-b1d3-5515739e7368.png)
    
    ![image](https://user-images.githubusercontent.com/110988/141780734-f538b572-d3bc-4f1e-a35b-4e672621d105.png)
    
    #### Motivation for adding to Mudlet
    Better security.
    #### Other info (issues closed, discussion etc)
    Defaults to on, but there is an option to disable it should someone have a legitimate usecase for doing so.
    
    See https://github.com/edbee/edbee-lib/issues/127
    
    Co-authored-by: Stephen Lyons <slysven@virginmedia.com>

--- src/XMLimport.cpp
+++ src/XMLimport.cpp
@@ -825,6 +825,9 @@ void XMLimport::readHostPackage(Host* pHost)
     pHost->mMapperUseAntiAlias = attributes().value(QStringLiteral("mMapperUseAntiAlias")) == YES;
     pHost->mMapperShowRoomBorders = readDefaultTrueBool(QStringLiteral("mMapperShowRoomBorders"));
     pHost->mEditorAutoComplete = (attributes().value(QStringLiteral("mEditorAutoComplete")) == YES);
+    if (!attributes().hasAttribute("mEditorShowBidi") || (attributes().value(QStringLiteral("mEditorShowBidi")) == YES)) {
+        pHost->mEditorShowBidi = true;
+    }
     pHost->mEditorTheme = attributes().value(QLatin1String("mEditorTheme")).toString();
     pHost->mEditorThemeFile = attributes().value(QLatin1String("mEditorThemeFile")).toString();
     pHost->mThemePreviewItemID = attributes().value(QLatin1String("mThemePreviewItemID")).toInt();
--- src/dlgProfilePreferences.cpp
+++ src/dlgProfilePreferences.cpp
@@ -1380,6 +1380,7 @@ void dlgProfilePreferences::loadEditorTab()
     config->setUseLineSeparator(mudlet::self()->mEditorTextOptions & QTextOption::ShowLineAndParagraphSeparators);
     config->setFont(pHost->getDisplayFont());
     config->setAutocompleteAutoShow(pHost->mEditorAutoComplete);
+    config->setRenderBidiContolCharacters(pHost->mEditorShowBidi);
     config->setAutocompleteMinimalCharacters(3);
     config->endChanges();
     edbeePreviewWidget->textDocument()->setLanguageGrammar(edbee::Edbee::instance()->grammarManager()->detectGrammarWithFilename(QStringLiteral("Buck.lua")));
@@ -1412,6 +1413,7 @@ void dlgProfilePreferences::loadEditorTab()
     theme_download_label->hide();
 
     checkBox_autocompleteLuaCode->setChecked(pHost->mEditorAutoComplete);
+    checkBox_showBidi->setChecked(pHost->mEditorShowBidi);
 
     // changes the theme being previewed
     connect(code_editor_theme_selection_combobox, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), this, &dlgProfilePreferences::slot_theme_selected);
@@ -2670,6 +2672,7 @@ void dlgProfilePreferences::slot_save_and_exit()
         pHost->mEditorTheme = code_editor_theme_selection_combobox->currentText();
         pHost->mEditorThemeFile = code_editor_theme_selection_combobox->currentData().toString();
         pHost->mEditorAutoComplete = checkBox_autocompleteLuaCode->isChecked();
+        pHost->mEditorShowBidi = checkBox_showBidi->isChecked();
         if (pHost->mpEditorDialog) {
             pHost->mpEditorDialog->setThemeAndOtherSettings(pHost->mEditorTheme);
         }
