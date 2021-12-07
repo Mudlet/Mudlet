b4211963e1b30764a98f59f96595f569d427b083
Vadim Peretokin <vperetokin@gmail.com>
2021-12-07 07:17:56 +0100

    Add `getBackgroundColor()` (#5721)

--- src/TLuaInterpreter.h
+++ src/TLuaInterpreter.h
@@ -623,6 +623,7 @@ public:
     static int deleteMap(lua_State*);
     static int windowType(lua_State*);
     static int getProfileStats(lua_State* L);
+    static int getBackgroundColor(lua_State* L);
     static int getLabelStylesheet(lua_State* L);
     // PLACEMARKER: End of Lua functions declarations
 
