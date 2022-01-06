152a93ad699252cd008ebc50619e3ab4d3a16b95
Manuel Wegmann <60551052+Edru2@users.noreply.github.com>
2022-01-06 21:39:45 +0100

    improve: get Label Size hint and Geyser.Label adjustSize (#5815)
    * add label size hint to primitive and Geyser
    * add autoWidth and autoHeight as constraints
    * also adds enableAutoAdjustSize and disableAutoAdjustSize functions

--- src/mudlet-lua/lua/geyser/GeyserLabel.lua
+++ src/mudlet-lua/lua/geyser/GeyserLabel.lua
@@ -64,12 +64,14 @@ function Geyser.Label:echo(message, color, format)
   message = [[<div ]] .. alignment .. color .. fs ..
   [[">]] .. message .. [[</div>]]
   echo(self.name, message)
+  self:autoAdjustSize()
 end
 
 --- raw Echo without formatting/handholding stuff that Geyser.Label:echo() does
 -- @param message The message to print. Can contain html formatting.
 function Geyser.Label:rawEcho(message)
   echo(self.name, message)
+  self:autoAdjustSize()
 end
 
 --- sets the color of the text on the label
@@ -122,6 +124,72 @@ function Geyser.Label:setFont(font)
   end
   self.font = font
   self:echo()
+end
+
+--- return the size hint (the suggested size) of the label
+function Geyser.Label:getSizeHint()
+  return getLabelSizeHint(self.name)
+end
+
+--- adjust size of the Label to the suggested size (probably the content size)
+function Geyser.Label:adjustSize()
+  local width, height = self:getSizeHint()
+  self:resize(width, height)
+  return true
+end
+
+--- adjust size of the Label to the suggested height (probably the content height)
+function Geyser.Label:adjustHeight()
+  local width, height = self:getSizeHint()
+  self:resize(nil, height)
+  return true
+end
+
+--- adjust size of the Label to the suggested width (probably the content width)
+function Geyser.Label:adjustWidth()
+  local width, height = self:getSizeHint()
+  self:resize(width, nil)
+  return true
+end
+
+--internal function to auto adjust label size to content
+function Geyser.Label:autoAdjustSize()
+  local width = self.autoWidth
+  local height = self.autoHeight
+  if not width and not height then
+    return
+  end
+
+  if height then
+    self:adjustHeight()
+  end
+
+  if width then
+    self:adjustWidth()
+  end
+end
+
+---Enable autoAdjustSize
+-- @param set width to false if just autoAdjust height
+-- @param set height to false if just autoAdjust width
+function Geyser.Label:enableAutoAdjustSize(width, height)
+  self.autoHeight = true
+  self.autoWidth = true
+  if width == false then
+    self.autoWidth = false
+  end
+
+  if height == false then 
+    self.autoHeight = false
+  end
+  return true
+end
+
+--- Disable autoAdjustSize 
+function Geyser.Label:disableAutoAdjustSize()
+  self.autoHeight = false
+  self.autoWidth = false
+  return true
 end
 
 --- Set whether or not the text in the label should be bold
@@ -229,6 +297,7 @@ end
 -- @param imageFileName The image to use for a background image.
 function Geyser.Label:setBackgroundImage (imageFileName)
   setBackgroundImage(self.name, imageFileName)
+  self:autoAdjustSize()
 end
 
 --- Sets a tiled background image for this label.
@@ -317,6 +386,7 @@ function Geyser.Label:setStyleSheet(css)
   css = css or self.stylesheet
   setLabelStyleSheet(self.name, css)
   self.stylesheet = css
+  self:autoAdjustSize()
 end
 --- Sets the tooltip of the label
 -- @param txt the tooltip txt
@@ -809,7 +879,7 @@ function Geyser.Label:new (cons, container)
   if cons.clickthrough then me:enableClickthrough() end
 
   if me.stylesheet then me:setStyleSheet() end
-
+  me:autoAdjustSize()
   --print("  New in " .. self.name .. " : " .. me.name)
   return me
 end
