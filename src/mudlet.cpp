cbb1338c2e302dc0c0534f2b8fc42f28288a35de
Vadim Peretokin <vperetokin@gmail.com>
2022-01-13 11:37:42 +0100

    Update mudlet.cpp

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -1184,8 +1184,8 @@ void mudlet::scanForMudletTranslations(const QString& path)
                 currentTranslation.mNativeName = qsl("Türkçe");
             } else if (!languageCode.compare(QLatin1String("fi_FI"), Qt::CaseInsensitive)) {
                 currentTranslation.mNativeName = qsl("Suomeksi");
-            } else if (!languageCode.compare(QLatin1String("ar_EG"), Qt::CaseInsensitive)) {
-                currentTranslation.mNativeName = qsl("العربية (مصر)");
+            } else if (!languageCode.compare(QLatin1String("ar_AR"), Qt::CaseInsensitive)) {
+                currentTranslation.mNativeName = qsl("العربية");
             } else {
                 currentTranslation.mNativeName = languageCode;
             }
