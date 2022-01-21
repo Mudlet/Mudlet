6e23518f3d29d51f44b1703ebc6392a2a794fcaf
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2022-01-21 08:36:07 +0100

    New translations mudlet.ts (Polish)

--- translations/translated/mudlet_pl_PL.ts
+++ translations/translated/mudlet_pl_PL.ts
@@ -5753,7 +5753,7 @@ Count</source>
       <location filename="../src/dlgPackageExporter.cpp" line="1464"/>
       <source>Select what to export</source>
       <comment>This is the text shown at the top of a groupbox initially and when there is NO items to export in the Package exporter dialogue.</comment>
-      <translation type="unfinished"/>
+      <translation>Wybierz elementy do wyeksportowania</translation>
     </message>
     <message>
       <location filename="../src/ui/dlgPackageExporter.ui" line="216"/>
