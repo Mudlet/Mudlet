d41090542d3c33164646a159267689a6ab9add38
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2022-02-12 01:56:12 +0100

    New translations mudlet.ts (English, United Kingdom)

--- translations/translated/mudlet_en_GB.ts
+++ translations/translated/mudlet_en_GB.ts
@@ -11278,7 +11278,7 @@ try again when it has finished.</source>
     <message>
       <location filename="../src/ui/profile_preferences.ui" line="2143"/>
       <source>Use large area exit arrows in 2D view</source>
-      <translation type="unfinished"/>
+      <translation>Use large area exit arrows in 2D view</translation>
     </message>
     <message>
       <location filename="../src/ui/profile_preferences.ui" line="2762"/>
