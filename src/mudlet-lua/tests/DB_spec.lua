181091bd2057114f074d14e506e40e28c249ceaf
Rollanz <39258026+RollanzMushing@users.noreply.github.com>
2022-04-09 18:11:51 -0400

    Update DB_spec.lua
    - added more extensive tests for null handling

--- src/mudlet-lua/tests/DB_spec.lua
+++ src/mudlet-lua/tests/DB_spec.lua
@@ -105,7 +105,7 @@ describe("Tests DB.lua functions", function()
         local results = db:fetch(mydb.enemies)
         assert.is_true(#results == 1)
       end)
-
+			
       it("Should replace a db entry if add_unique is used and the unique index matches", function()
         db:add(mydb.enemies, {name="Bob", city="Sacramento"})
         db:add(mydb.enemies, {name="Bob", city="San Francisco"})
@@ -907,5 +907,130 @@ describe("Tests DB.lua functions", function()
       assert.is.same(exp_count, count)
     end)
   end)
-
+	
+	describe("Tests, if NULL handling works as intended",
+	function()
+		before_each(function()
+			mydb = db:create("mydbt_nulltesting",
+				{
+					sheet = {
+						name = "",
+						level = 0,
+						motto = "",
+						_unique = { "name" },
+					}
+				})
+		end)
+		
+		after_each(function()
+			db:close()
+			local filename = getMudletHomeDir() .. "/Database_mydbtnulltesting.db"
+      os.remove(filename)
+      mydb = nil
+		end)
+		
+		it("should be able to insert NULL values",
+		function()
+			local test = {name = "Bellman", level = db:Null(), motto = ""}
+			db:add(mydb.sheet, test)
+			test.level = nil
+			local results = db:fetch(mydb.sheet)
+			assert.is_true(#results == 1)
+			results[1]._row_id = nil
+			assert.are.same(results[1], test)
+		end)
+		
+		it("should be able to fetch by NULL conditions",
+		function()
+			local test1 = {name = "Boots", level = 1, motto = ""}
+			local test2 = {name = "Bellman", level = db:Null(), motto = ""}
+			db:add(mydb.sheet, test1, test2)
+			test2.level = nil
+			local results_null = db:fetch(mydb.sheet, db:is_nil(mydb.sheet.level))
+			assert.is_true(#results_null == 1)
+			results_null[1]._row_id = nil
+			assert.are.same(results_null[1], test2)
+			local results_not_null = db:fetch(mydb.sheet, db:is_not_nil(mydb.sheet.level))
+			assert.is_true(#results_not_null == 1)
+			results_not_null[1]._row_id = nil
+			assert.are.same(results_not_null[1], test1)
+		end)
+		
+		it("should be able to set an existing field to NULL",
+		function()
+			local test = {name = "Bellman", level = 1, motto = "Four weeks to the month you may mark"}
+			db:add(mydb.sheet, test)
+			db:set(mydb.sheet.motto, db:Null(), db:eq(mydb.sheet.name, "Bellman"))
+			test.motto = nil
+			local results = db:fetch(mydb.sheet)
+			assert.is_true(#results == 1)
+			results[1]._row_id = nil
+			assert.are.same(results[1], test)
+		end)
+	end)
+	
+	describe("Tests, if default NULL works as intended",
+	function()
+		after_each(function()
+			db:close()
+			local filename = getMudletHomeDir() .. "/Database_mydbtnulltesting.db"
+			os.remove(filename)
+			mydb = nil
+		end)
+			
+		it("should be able to create a table with default NULL",
+		function()
+			mydb = db:create("mydbt_nulltesting",
+			{
+				sheet = {
+					name = "",
+					house = db:Null(),
+					_unique = { "name" },
+				}
+			})
+			local test1 = {name = "Hermione", house = "Griffindor"}
+			local test2 = {name = "Viktor"}
+			db:add(mydb.sheet, test1, test2)
+			test2.house = nil
+			results1 = db:fetch(mydb.sheet, db:is_not_nil(mydb.sheet.house))
+			results2 = db:fetch(mydb.sheet, db:is_nil(mydb.sheet.house))
+			assert.is_true(#results1 == 1)
+			results1[1]._row_id = nil
+			assert.are.same(results1[1], test1)
+			assert.is_true(#results2 == 1)
+			results2[1]._row_id = nil
+			assert.are.same(results2[1], test2)
+		end)
+		
+		it("should be able to add a column with default NULL to a table",
+		function()
+			mydb = db:create("mydbt_nulltesting",
+			{
+				sheet = {
+					name = "",
+					level = 1,
+					_unique = { "name" },
+				}
+			})
+			
+			mydb = db:create("mydbt_nulltesting",
+			{
+				sheet = {
+					name = "",
+					level = 1,
+					house = db:Null(),
+					_unique = { "name" },
+				}
+			})
+			local test =  { {name = "Laergon", level = 1, house = "Kharon"}, {name = "Mymla", level = 5} }
+			db:add(mydb.sheet, test[1], test[2])
+			local results = db:fetch(mydb.sheet)
+			assert.is_true(#results == 2)
+			results[1]._row_id = nil
+			results[2]._row_id = nil
+			assert.is_true(table.size(results[1]) == 3)
+			assert.is_true(table.size(results[2]) == 2)
+			assert.are.same(results, test)
+		end)
+	end)
 end)
