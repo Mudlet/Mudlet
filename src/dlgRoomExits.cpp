7619fb3297c1dfee89a5b8534e7a9e7f66877642
Stephen Lyons <slysven@virginmedia.com>
2022-06-19 20:43:33 +0100

    Revise: remove phrase held to be redundant in a couple of tooltips.
    This was suggested/requested by @vadi2.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/dlgRoomExits.cpp
+++ src/dlgRoomExits.cpp
@@ -1523,7 +1523,7 @@ void dlgRoomExits::init()
     roomID->setText(QString::number(mRoomID));
     if (pR->isLocked) {
         // Revise the tool tip:
-        roomID->setToolTip(utils::richText(tr("This is the Room ID number for this room; this <b>room is locked</b> so it will not be used for speed-walks at all. Neither of these details can be changed here!",
+        roomID->setToolTip(utils::richText(tr("This is the Room ID number for this room; this <b>room is locked</b> so it will not be used for speed-walks at all.",
                                               // Intentional comment to separate arguments
                                               "This text is a revision to the default tooltip text set for this widget in the 'room_exits.ui' file. Bold HTML tags are used to emphasis that this room's locked status overrides any weight or lock (\"No route\") setting of any exit that comes to it.")));
     } else {
