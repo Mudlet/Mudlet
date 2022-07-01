5c310f700e24edb395019a4560cba742edda0db3
Vadim Peretokin <vperetokin@hey.com>
2022-07-01 09:10:13 +0200

    Surgery with provider - ongoing

--- src/Announcer.h
+++ src/Announcer.h
@@ -1,109 +1,155 @@
 /***************************************************************************
-*   Copyright (C) 2022 by Vadim Peretokin - vadim.peretokin@mudlet.org    *
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-*   This program is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-*   GNU General Public License for more details.                          *
-*                                                                         *
-*   You should have received a copy of the GNU General Public License     *
-*   along with this program; if not, write to the                         *
-*   Free Software Foundation, Inc.,                                       *
-*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
-***************************************************************************/
+ *   Copyright (C) 2022 by Vadim Peretokin - vadim.peretokin@mudlet.org    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #ifndef ANNOUNCER_H
 #define ANNOUNCER_H
 
-#include <QWidget>
-#include <QObject>
 #include <QAccessible>
 #include <QAccessibleInterface>
 #include <QAccessibleWidget>
+#include <QLibrary>
+#include <QObject>
+#include <QWidget>
 
-#if defined(Q_OS_LINUX) || defined (Q_OS_WIN32)
+#if defined(Q_OS_WIN)
+#include <oleacc.h>
+#include <uiautomationclient.h>
+#include <uiautomationcore.h>
+#include <uiautomationcoreapi.h>
+#endif
+
+#if defined(Q_OS_LINUX)
 class InvisibleNotification : public QWidget {
-Q_OBJECT
+  Q_OBJECT
 
 public:
-    Q_DISABLE_COPY(InvisibleNotification)
-    explicit InvisibleNotification(QWidget *parent);
+  Q_DISABLE_COPY(InvisibleNotification)
+  explicit InvisibleNotification(QWidget *parent);
 
-    void setText(const QString &text);
-    QString text();
+  void setText(const QString &text);
+  QString text();
 
 private:
-    QString mText;
+  QString mText;
 };
-
 
 // create a new class InvisibleStatusbar based on QWidget
 class InvisibleStatusbar : public QWidget {
-Q_OBJECT
+  Q_OBJECT
 
 public:
-    Q_DISABLE_COPY(InvisibleStatusbar)
-    explicit InvisibleStatusbar(QWidget *parent);
+  Q_DISABLE_COPY(InvisibleStatusbar)
+  explicit InvisibleStatusbar(QWidget *parent);
 };
 
-class InvisibleAccessibleNotification: public QAccessibleWidget
-{
+class InvisibleAccessibleNotification : public QAccessibleWidget {
 public:
-#if (QT_VERSION) >= (QT_VERSION_CHECK(5, 12, 0))
-    explicit InvisibleAccessibleNotification(QWidget* w) : QAccessibleWidget(w, QAccessible::Role::Notification) { }
-#else
-    explicit InvisibleAccessibleNotification(QWidget* w) : QAccessibleWidget(w, 0x00000086) { }
-#endif
+  explicit InvisibleAccessibleNotification(QWidget *w)
+      : QAccessibleWidget(w, QAccessible::Role::Notification) {}
 
 private:
-    InvisibleNotification *notification() const;
+  InvisibleNotification *notification() const;
 
 protected:
-    QString text(QAccessible::Text t) const override;
-
+  QString text(QAccessible::Text t) const override;
 };
 
-class FakeAccessibleStatusbar: public QAccessibleWidget
-{
+class FakeAccessibleStatusbar : public QAccessibleWidget {
 public:
-    explicit FakeAccessibleStatusbar(QWidget* w) : QAccessibleWidget(w, QAccessible::Role::StatusBar) { }
+  explicit FakeAccessibleStatusbar(QWidget *w)
+      : QAccessibleWidget(w, QAccessible::Role::StatusBar) {}
 };
 #endif
 
-class Announcer : public QWidget
-{
-    Q_OBJECT
-public:
-    Q_DISABLE_COPY_MOVE(Announcer)
-    explicit Announcer(QWidget *parent = nullptr);
-    void announce(const QString text);
+enum NotificationProcessing {
+  NotificationProcessing_ImportantAll = 0,
+  NotificationProcessing_ImportantMostRecent = 1,
+  NotificationProcessing_All = 2,
+  NotificationProcessing_MostRecent = 3,
+  NotificationProcessing_CurrentThenMostRecent = 4
+};
 
-#if defined(Q_OS_LINUX) || defined (Q_OS_WIN32)
-    static QAccessibleInterface* accessibleFactory(const QString &classname, QObject *object)
-    {
+enum NotificationKind {
+  NotificationKind_ItemAdded = 0,
+  NotificationKind_ItemRemoved = 1,
+  NotificationKind_ActionCompleted = 2,
+  NotificationKind_ActionAborted = 3,
+  NotificationKind_Other = 4
+};
+
+class Announcer : public QWidget {
+  Q_OBJECT
+public:
+  Q_DISABLE_COPY_MOVE(Announcer)
+  explicit Announcer(QWidget *parent = nullptr);
+  void announce(const QString text);
+
+#if defined(Q_OS_LINUX)
+  static QAccessibleInterface *accessibleFactory(const QString &classname,
+                                                 QObject *object) {
 // mingw compilation breaks without this
 #undef interface
-        QAccessibleInterface *interface = nullptr;
+    QAccessibleInterface *interface = nullptr;
 
-        if (classname == QLatin1String("InvisibleNotification") && object && object->isWidgetType()) {
-            interface = new InvisibleAccessibleNotification(static_cast<QWidget *>(object));
-        } else if (classname == QLatin1String("InvisibleStatusbar") && object && object->isWidgetType()) {
-            interface = new FakeAccessibleStatusbar(static_cast<QWidget *>(object));
-        }
-
-        return interface;
+    if (classname == QLatin1String("InvisibleNotification") && object &&
+        object->isWidgetType()) {
+      interface =
+          new InvisibleAccessibleNotification(static_cast<QWidget *>(object));
+    } else if (classname == QLatin1String("InvisibleStatusbar") && object &&
+               object->isWidgetType()) {
+      interface = new FakeAccessibleStatusbar(static_cast<QWidget *>(object));
     }
+
+    return interface;
+  }
 #endif
 
 private:
-#if defined(Q_OS_LINUX) || defined (Q_OS_WIN32)
-    InvisibleNotification* notification;
-    InvisibleStatusbar* statusbar;
+#if defined(Q_OS_LINUX)
+  InvisibleNotification *notification;
+  InvisibleStatusbar *statusbar;
+#endif
+
+#if defined(Q_OS_WIN)
+  QScopedPointer<QLibrary> mpUiaLibrary;
+  typedef HRESULT(WINAPI *PtrUiaRaiseNotificationEvent)(
+      IRawElementProviderSimple *, NotificationKind, NotificationProcessing,
+      BSTR, BSTR);
+  typedef HRESULT(WINAPI *PtrUiaDisconnectAllProviders)();
+  typedef HRESULT(WINAPI *PtrUiaDisconnectProvider)(
+      IRawElementProviderSimple *);
+  PtrUiaRaiseNotificationEvent m_pUiaRaiseNotificationEvent = nullptr;
+  PtrUiaDisconnectAllProviders m_pUiaDisconnectAllProviders = nullptr;
+  PtrUiaDisconnectProvider m_pUiaDisconnectProvider = nullptr;
+
+  HRESULT raiseNotificationEvent(IRawElementProviderSimple *provider,
+                                 NotificationKind notificationKind,
+                                 NotificationProcessing notificationProcessing,
+                                 BSTR displayString, BSTR activityId);
+  HRESULT disconnectAllProviders();
+  HRESULT disconnectProvider(IRawElementProviderSimple *pProvider);
+
+  bool initializeUia();
+  bool terminateUia();
+
+  class UiaProvider;
+  UiaProvider *uiaProvider{};
 #endif
 };
 #endif // ANNOUNCER_H
--- src/AnnouncerWindows.cpp
+++ src/AnnouncerWindows.cpp
@@ -1,69 +1,229 @@
 /***************************************************************************
-*   Copyright (C) 2022 by Vadim Peretokin - vadim.peretokin@mudlet.org    *
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-*   This program is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-*   GNU General Public License for more details.                          *
-*                                                                         *
-*   You should have received a copy of the GNU General Public License     *
-*   along with this program; if not, write to the                         *
-*   Free Software Foundation, Inc.,                                       *
-*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
-***************************************************************************/
+ *   Copyright 2019-2022 Leonard de Ruijter, James Teh - OSARA             *
+ *   Copyright (C) 2022 by Vadim Peretokin - vadim.peretokin@mudlet.org    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #include "Announcer.h"
+#include "mudlet.h"
 
-#include <QDebug>
 #include <QAccessible>
+#include <QDebug>
+#include <QLibrary>
 
-InvisibleNotification::InvisibleNotification(QWidget *parent) : QWidget(parent) {
-    setObjectName("InvisibleNotification");
-    setAccessibleName("InvisibleNotification");
-    setAccessibleDescription("An invisible widget used as a workaround to announce text to the screen reader");
+#include <memory>
+#include <ole2.h>
+#include <tlhelp32.h>
+#include <uiautomation.h>
+#include <utility>
+
+#include <oleacc.h>
+#include <uiautomationclient.h>
+#include <uiautomationcore.h>
+#include <uiautomationcoreapi.h>
+
+///////////////////
+using namespace std;
+
+HWND uiaWnd = nullptr;
+
+// mingw 7.30's uiautomationclient.h is outdated, lacks this define
+#define UIA_CustomControlTypeId (50025)
+
+// Some UIA functions aren't available in earlier versions of Windows, so we
+// must fetch those at runtime. Otherwise, it will fail to load. This class
+// handles loading/freeing the dll and getting the required functions.
+class UiaCore {
+private:
+  HMODULE dll = LoadLibraryA("UIAutomationCore.dll");
+
+  template <typename FuncType> FuncType *getFunc(const char *funcName) {
+    return (FuncType *)GetProcAddress(this->dll, funcName);
+  }
+
+public:
+  ~UiaCore() { FreeLibrary(this->dll); }
+};
+
+unique_ptr<UiaCore> uiaCore;
+
+// Provider code based on Microsoft's uiautomationSimpleProvider example.
+class Announcer::UiaProvider : public IRawElementProviderSimple {
+public:
+  UiaProvider(_In_ HWND hwnd) : refCount(0), controlHWnd(hwnd) {}
+
+  // IUnknown methods
+  ULONG STDMETHODCALLTYPE AddRef() { return InterlockedIncrement(&refCount); }
+
+  ULONG STDMETHODCALLTYPE Release() {
+    long val = InterlockedDecrement(&refCount);
+    if (val == 0) {
+      delete this;
+    }
+    return val;
+  }
+
+  HRESULT STDMETHODCALLTYPE QueryInterface(_In_ REFIID riid,
+                                           _Outptr_ void **ppInterface) {
+    if (ppInterface) {
+      return E_INVALIDARG;
+    }
+    if (riid == __uuidof(IUnknown)) {
+      *ppInterface = static_cast<IRawElementProviderSimple *>(this);
+    } else if (riid == __uuidof(IRawElementProviderSimple)) {
+      *ppInterface = static_cast<IRawElementProviderSimple *>(this);
+    } else {
+      *ppInterface = nullptr;
+      return E_NOINTERFACE;
+    }
+    (static_cast<IUnknown *>(*ppInterface))->AddRef();
+    return S_OK;
+  }
+
+  // IRawElementProviderSimple methods
+  HRESULT STDMETHODCALLTYPE
+  get_ProviderOptions(_Out_ ProviderOptions *pRetVal) {
+    if (!pRetVal)
+      return E_INVALIDARG;
+    // We are STA, (OleInitialize()).
+    *pRetVal = static_cast<ProviderOptions>(ProviderOptions_ServerSideProvider |
+                                            ProviderOptions_UseComThreading);
+    return S_OK;
+  }
+
+  HRESULT STDMETHODCALLTYPE GetPatternProvider(
+      PATTERNID patternId, _Outptr_result_maybenull_ IUnknown **pRetVal) {
+    // We do not support any pattern.
+    *pRetVal = NULL;
+    return S_OK;
+  }
+
+  HRESULT STDMETHODCALLTYPE GetPropertyValue(PROPERTYID propertyId,
+                                             _Out_ VARIANT *pRetVal) {
+    switch (propertyId) {
+    case UIA_ControlTypePropertyId:
+      // Stop Narrator from ever speaking this as a window
+      pRetVal->vt = VT_I4;
+      pRetVal->lVal = UIA_CustomControlTypeId;
+      break;
+    case UIA_IsControlElementPropertyId:
+    case UIA_IsContentElementPropertyId:
+    case UIA_IsKeyboardFocusablePropertyId:
+      pRetVal->vt = VT_BOOL;
+      pRetVal->boolVal = VARIANT_FALSE;
+      break;
+    case UIA_ProviderDescriptionPropertyId:
+      pRetVal->vt = VT_BSTR;
+      pRetVal->bstrVal = SysAllocString(L"REAPER OSARA");
+      break;
+    default:
+      pRetVal->vt = VT_EMPTY;
+    }
+    return S_OK;
+  }
+
+  HRESULT STDMETHODCALLTYPE
+  get_HostRawElementProvider(IRawElementProviderSimple **pRetVal) {
+    return UiaHostProviderFromHwnd(controlHWnd, pRetVal);
+  }
+
+private:
+  virtual ~UiaProvider() {}
+
+  ULONG refCount;   // Ref Count for this COM object
+  HWND controlHWnd; // The HWND for the control.
+};
+
+bool Announcer::initializeUia() {
+  uiaCore = make_unique<UiaCore>();
+
+  // Constructor  initializes refcount to 0, assignment to a CComPtr
+  // takes it to 1.
+  uiaProvider = new UiaProvider((HWND)mudlet::self()->effectiveWinId());
+  return true;
 }
 
-void InvisibleNotification::setText(const QString &text) {
-    this->mText = text;
+bool Announcer::terminateUia() {
+  if (uiaProvider) {
+    // Null out uiaProvider so it can't be returned by WM_GETOBJECT during
+    // disconnection.
+    UiaProvider *tmpProv = std::move(uiaProvider);
+    uiaProvider = nullptr;
+    disconnectProvider(tmpProv);
+  }
+
+  disconnectAllProviders();
+  uiaCore = nullptr;
+  return true;
 }
 
-QString InvisibleNotification::text() {
-    return mText;
+// bool Announcer::sendUiaNotification(const string& message, bool interrupt) {
+//    if (!UiaClientsAreListening() || message.empty()) {
+//        return true;
+//    }
+//    return (raiseNotificationEvent(
+//        uiaProvider,
+//        NotificationKind::NotificationKind_Other,
+//        interrupt ? NotificationProcessing::NotificationProcessing_MostRecent
+//        : NotificationProcessing::NotificationProcessing_All,
+//        SysAllocString(widen(message).c_str()),
+//        SysAllocString(L"REAPER_OSARA")
+//    ) == S_OK);
+//}
+
+///////////////////
+
+Announcer::Announcer(QWidget *parent) : QWidget{parent} {
+  mpUiaLibrary.reset(new QLibrary(QStringLiteral("UIAutomationCore")));
+  if (mpUiaLibrary->load()) {
+    m_pUiaRaiseNotificationEvent =
+        reinterpret_cast<PtrUiaRaiseNotificationEvent>(
+            mpUiaLibrary->resolve("UiaRaiseNotificationEvent"));
+  }
+  initializeUia();
 }
 
-InvisibleNotification* InvisibleAccessibleNotification::notification() const
-{
-    return static_cast<InvisibleNotification*>(object());
+BSTR bStrFromQString(const QString &value) {
+  return SysAllocString(reinterpret_cast<const wchar_t *>(value.utf16()));
 }
 
-QString InvisibleAccessibleNotification::text(QAccessible::Text t) const {
-    Q_UNUSED(t)
-
-    // return the notifications contents regardless of the request as part of the workaround
-    return notification()->text();
+HRESULT
+Announcer::raiseNotificationEvent(IRawElementProviderSimple *provider,
+                                  NotificationKind notificationKind,
+                                  NotificationProcessing notificationProcessing,
+                                  BSTR displayString, BSTR activityId) {
+  if (!m_pUiaRaiseNotificationEvent)
+    return UIA_E_NOTSUPPORTED;
+  return m_pUiaRaiseNotificationEvent(provider, notificationKind,
+                                      notificationProcessing, displayString,
+                                      activityId);
 }
 
-InvisibleStatusbar::InvisibleStatusbar(QWidget *parent) : QWidget(parent) {
-    setObjectName("InvisibleStatusbar");
-    setAccessibleName("InvisibleStatusbar");
-    setAccessibleDescription("An invisible widget used as part as a workaround to announce text to the screen reader");
-}
+void Announcer::announce(const QString text) {
+  // check UiaClientsAreListening here or much earlier on
+  qDebug() << "announce" << text;
+  QAccessibleValueChangeEvent event(this, "hi");
 
-Announcer::Announcer(QWidget *parent): QWidget{parent}, statusbar(new InvisibleStatusbar(this))
-{
-    notification = new InvisibleNotification(statusbar);
-}
+  BSTR displayString = bStrFromQString(text);
+  BSTR activityId = bStrFromQString(qsl("Mudlet"));
 
-void Announcer::announce(const QString text)
-{
-    notification->setText(text);
+  raiseNotificationEvent(uiaProvider, NotificationKind_ItemAdded,
+                         NotificationProcessing_All, displayString, activityId);
 
-    // implemented per recommendation from Orca dev: https://mail.gnome.org/archives/orca-list/2022-June/msg00027.html
-    QAccessibleEvent event(notification, QAccessible::ObjectShow);
-    QAccessible::updateAccessibility(&event);
+  ::SysFreeString(displayString);
+  ::SysFreeString(activityId);
 }
