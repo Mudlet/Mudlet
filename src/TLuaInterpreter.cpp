752c05eea70cc5fdabdb8598c362f0770fb9cf12
Vadim Peretokin <vperetokin@gmail.com>
2022-07-09 08:33:17 +0200

    Comment out volume/rate/pitch temporarily

--- src/TLuaInterpreter.cpp
+++ src/TLuaInterpreter.cpp
@@ -12304,9 +12304,9 @@ void TLuaInterpreter::ttsBuild()
     bSpeechQueueing = false;
 
     connect(speechUnit, &QTextToSpeech::stateChanged, &TLuaInterpreter::ttsStateChanged);
-    speechUnit->setVolume(1.0);
-    speechUnit->setRate(0.0);
-    speechUnit->setPitch(0.0);
+//    speechUnit->setVolume(1.0);
+//    speechUnit->setRate(0.0);
+//    speechUnit->setPitch(0.0);
     return;
 }
 
