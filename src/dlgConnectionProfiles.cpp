4d06e13b94e185e38507c2d1c0e40bcbb2006479
Vadim Peretokin <vperetokin@hey.com>
2022-07-09 09:55:33 +0200

    Update description

--- src/dlgConnectionProfiles.cpp
+++ src/dlgConnectionProfiles.cpp
@@ -868,12 +868,12 @@ QString dlgConnectionProfiles::getDescription(const QString& hostUrl, const quin
                 "role-play, acquire achievements, and complete quests across many challenging locations across Middle-earth such as Lothlórien, the Shire, Bree, Rivendell, Goblin-town,"
                 " Mirkwood, Dol Guldur, and the Mines of Moria. The game is completely at no cost to play and has been continually enhanced since its inception in the fall of 1991.");
     } else if (hostUrl == qsl("mud.ren")) {
-        return qsl("天下风云出我辈,一入江湖岁月催。"
-                "皇图霸业谈笑中,不胜人生一场醉。"
-                "提剑跨骑挥鬼雨,白骨如山鸟惊飞。"
-                "尘事如潮人如水,只叹江湖几人回。"
+        return qsl("天下风云出我辈，一入江湖岁月催。"
+                "皇图霸业谈笑中，不胜人生一场醉。"
+                "提剑跨骑挥鬼雨，白骨如山鸟惊飞。"
+                "尘事如潮人如水，只叹江湖几人回。"
                 ""
-                "中文武侠MUD炎黄群侠传，游戏包括25个门派和5大世家，正邪只在一念间；近千门武学等你学习，上百种任务随你体验；让自己成为一代宗师，江湖笑，恩怨了。"
+                "中文开源武侠MUD炎黄群侠传，游戏包括25大门派和5大世家，正邪只在一念间；近千门武学等你学习，上百种任务随你体验；让自己成为一代宗师，江湖笑，恩怨了。"
                 );
     } else {
         return readProfileData(profile_name, qsl("description"));
