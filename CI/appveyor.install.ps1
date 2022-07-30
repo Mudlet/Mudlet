5b4e09ebe00d31a28d517006ed102cc78c64f4f0
Vadim Peretokin <vperetokin@gmail.com>
2022-07-30 09:47:31 +0200

    Remove ccache debugging

--- CI/appveyor.install.ps1
+++ CI/appveyor.install.ps1
@@ -14,8 +14,6 @@ $Env:PATH = "$CMakePath;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;C:\Program Files\7-Zi
 # install dependencies
 
 CheckAndInstallCcache
-ls "C:\Program Files\ccache"
-ccache -s
 CheckAndInstall7z
 CheckAndInstallCmake
 CheckAndInstallMingwGet
