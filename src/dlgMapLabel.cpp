2743746511a10833eb55533a73b438ea2bbcc6b5
Stephen Lyons <slysven@virginmedia.com>
2022-08-27 10:47:01 +0200

    Infrastructure: tidy up naming of SLOT methods and their usage - Part 2 (#6255)
    When Qt's slot/signal system is used to invoke a method there is some
    overhead - so it makes sense to ensure developers can spot all such
    methods (functions). We have tried to do this with a `slot_` prefix to
    the methods we create but it has not been applied uniformly. This PR
    (and one or more to follow) is intended to help with this by more
    rigorously doing so - the names changed herein should all follow a
    `slot_`*camelCaseMethodName* style. In addition some methods were
    detected that are not currently used or which are not actually used via
    the signal/slot system, these have been commented out or have had the
    prefix removed and the declaration in the relevant header file moved as
    appropriate.
    
    For reference the changes made are:
    * `TLuaInterpreter::slotDeleteSender(...)` ==>
    `TLuaInterpreter::slot_deleteSender(...)`
    * `TLuaInterpreter::slotPurge()` ==> `TLuaInterpreter::slot_purge()`
    * `cTelnet::handle_socket_signal_connected()` ==>
    `cTelnet::slot_socketConnected()`
    * `cTelnet::handle_socket_signal_disconnected()` ==>
    `cTelnet::slot_socketDisconnected()`
    * `cTelnet::handle_socket_signal_hostFound(...)` ==>
    `cTelnet::slot_socketHostFound(...)`
    * `cTelnet::handle_socket_signal_readyRead()` ==>
    `cTelnet::slot_socketReadyToBeRead()`
    * `cTelnet::handle_socket_signal_sslError(...)` ==>
    `cTelnet::slot_socketSslError(...)`
    * `cTelnet::setDownloadProgress(...)` ==>
    `cTelnet::slot_setDownloadProgress(...)`
    * `dlgComposer::cancel()` ==> `dlgComposer::slot_cancel()`
    * `dlgComposer::save()` ==> `dlgComposer::slot_save()`
    * `dlgConnectionProfiles::slot_item_clicked(...)` ==>
    `dlgConnectionProfiles::slot_itemClicked(...)`
    * `dlgConnectionProfiles::slot_save_name()` ==>
    `dlgConnectionProfiles::slot_saveName()`
    * `dlgConnectionProfiles::slot_update_login(...)` ==>
    `dlgConnectionProfiles::slot_updateLogin(...)`
    * `dlgConnectionProfiles::slot_update_name(...)` ==>
    `dlgConnectionProfiles::slot_updateName(...)`
    * `dlgConnectionProfiles::slot_update_pass(...)` ==>
    `dlgConnectionProfiles::slot_updatePassword(...)`
    * `dlgConnectionProfiles::slot_update_port(...)` ==>
    `dlgConnectionProfiles::slot_updatePort(...)`
    * `dlgConnectionProfiles::slot_update_SSL_TSL_port(...)` ==>
    `dlgConnectionProfiles::slot_updateSslTslPort(...)`
    * `dlgConnectionProfiles::slot_update_url(...)` ==>
    `dlgConnectionProfiles::slot_updateUrl(...)`
    * `dlgMapLabel::pickBgColor()` ==> `dlgMapLabel::slot_pickBgColor()`
    * `dlgMapLabel::pickFgColor()` ==> `dlgMapLabel::slot_pickFgColor()`
    * `dlgMapLabel::pickFile()` ==> `dlgMapLabel::slot_pickFile()`
    * `dlgMapLabel::pickFont()` ==> `dlgMapLabel::slot_pickFont()`
    * `dlgMapLabel::save()` ==> `dlgMapLabel::slot_save()`
    * `dlgMapLabel::updateControls()` ==>
    `dlgMapLabel::slot_updateControls()`
    * `dlgMapLabel::updateControlsVisibility()` ==>
    `dlgMapLabel::slot_updateControlsVisibility()`
    
    Commented out as not being used:
    * `cTelnet::handle_socket_signal_error()` ==>
    `cTelnet::slot_socketError()`
    * `dlgConnectionProfiles::slot_update_website(...) ==>
    `dlgConnectionProfiles::slot_updateWebsite(const QString& url)`
    
    I have my doubts about whether the
    `dlgMapLabel::save()`/`dlgMapLabel::slot_save()` method is needed at
    all, as it might be that the `QDialog::accept()` slot could have been
    inserted into the `QObject::connect(...)` call directly instead...?
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/dlgMapLabel.cpp
+++ src/dlgMapLabel.cpp
@@ -32,28 +32,28 @@ dlgMapLabel::dlgMapLabel(QWidget* pF) : QDialog(pF), fgColor(QColor(255, 255, 50
     setAttribute(Qt::WA_DeleteOnClose);
     setWindowTitle(tr("Create label", "Create label dialog title"));
 
-    connect(comboBox_type, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &dlgMapLabel::updateControlsVisibility);
+    connect(comboBox_type, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &dlgMapLabel::slot_updateControlsVisibility);
     connect(comboBox_type, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &dlgMapLabel::updated);
-    connect(toolButton_imagePick, &QToolButton::released, this, &dlgMapLabel::pickFile);
+    connect(toolButton_imagePick, &QToolButton::released, this, &dlgMapLabel::slot_pickFile);
     connect(checkBox_stretchImage, &QCheckBox::stateChanged, this, &dlgMapLabel::updated);
     connect(lineEdit_text, &QLineEdit::textChanged, this, [&](const QString& pText) {
         text = pText;
         emit updated();
     });
-    connect(pushButton_bgColor, &QPushButton::released, this, &dlgMapLabel::pickBgColor);
-    connect(pushButton_fgColor, &QPushButton::released, this, &dlgMapLabel::pickFgColor);
-    connect(toolButton_fontPick, &QToolButton::released, this, &dlgMapLabel::pickFont);
-    connect(pushButton_save, &QPushButton::released, this, &dlgMapLabel::save);
+    connect(pushButton_bgColor, &QPushButton::released, this, &dlgMapLabel::slot_pickBgColor);
+    connect(pushButton_fgColor, &QPushButton::released, this, &dlgMapLabel::slot_pickFgColor);
+    connect(toolButton_fontPick, &QToolButton::released, this, &dlgMapLabel::slot_pickFont);
+    connect(pushButton_save, &QPushButton::released, this, &dlgMapLabel::slot_save);
     connect(pushButton_cancel, &QPushButton::released, this, &dlgMapLabel::close);
     connect(comboBox_position, QOverload<int>::of(&QComboBox::currentIndexChanged), this, [=]() { emit updated(); });
     connect(checkBox_scaling, &QCheckBox::stateChanged, this, [=]() { emit updated(); });
-    connect(this, &dlgMapLabel::updated, this, &dlgMapLabel::updateControls);
+    connect(this, &dlgMapLabel::updated, this, &dlgMapLabel::slot_updateControls);
 
     font = QApplication::font();
     font.setStyle(QFont::StyleNormal);
     text = lineEdit_text->placeholderText();
-    updateControls();
-    updateControlsVisibility();
+    slot_updateControls();
+    slot_updateControlsVisibility();
 }
 
 dlgMapLabel::~dlgMapLabel() {}
@@ -68,7 +68,7 @@ QString dlgMapLabel::getImagePath()
     return imagePath;
 }
 
-void dlgMapLabel::pickFgColor()
+void dlgMapLabel::slot_pickFgColor()
 {
     fgColorDialog = new QColorDialog(this);
     fgColorDialog->setAttribute(Qt::WA_DeleteOnClose);
@@ -87,7 +87,7 @@ void dlgMapLabel::pickFgColor()
     fgColorDialog->raise();
 }
 
-void dlgMapLabel::pickBgColor()
+void dlgMapLabel::slot_pickBgColor()
 {
     auto originalColor = QColor(bgColor);
     bgColorDialog = new QColorDialog(this);
@@ -106,7 +106,7 @@ void dlgMapLabel::pickBgColor()
     bgColorDialog->raise();
 }
 
-void dlgMapLabel::pickFont()
+void dlgMapLabel::slot_pickFont()
 {
     auto originalFont = QFont(font);
     fontDialog = new QFontDialog(font, this);
@@ -126,13 +126,13 @@ void dlgMapLabel::pickFont()
     fontDialog->raise();
 }
 
-void dlgMapLabel::pickFile()
+void dlgMapLabel::slot_pickFile()
 {
     imagePath = QFileDialog::getOpenFileName(nullptr, tr("Select image", "2D Mapper create label file dialog title"));
     emit updated();
 }
 
-void dlgMapLabel::save()
+void dlgMapLabel::slot_save()
 {
     accept();
 }
@@ -172,7 +172,7 @@ bool dlgMapLabel::stretchImage()
     return checkBox_stretchImage->isChecked();
 }
 
-void dlgMapLabel::updateControls()
+void dlgMapLabel::slot_updateControls()
 {
     lineEdit_font->setText(QString("%1, %2pt %3").arg(font.family(), QString::number(font.pointSize()), font.styleName()));
     pushButton_fgColor->setStyleSheet(BUTTON_STYLESHEET.arg(QString::number(fgColor.red()), QString::number(fgColor.green()), QString::number(fgColor.blue()), QString::number(fgColor.alpha())));
@@ -180,7 +180,7 @@ void dlgMapLabel::updateControls()
     lineEdit_image->setText(imagePath);
 }
 
-void dlgMapLabel::updateControlsVisibility()
+void dlgMapLabel::slot_updateControlsVisibility()
 {
     bool isText = isTextLabel();
     label_image->setVisible(!isText);
