b9147d23c46170a564495b52f1da638be2586351
Kebap <kebap_spam@gmx.net>
2022-10-06 23:13:15 +0200

    copied and modified dlgRoomSymbol

---
+++ src/dlgRoomProperties.h
@@ -0,0 +1,68 @@
+#ifndef MUDLET_DLGROOMPROPERTIES_H
+#define MUDLET_DLGROOMPROPERTIES_H
+
+/***************************************************************************
+ *   Copyright (C) 2021 by Piotr Wilczynski - delwing@gmail.com            *
+ *   Copyright (C) 2022 by Stephen Lyons - slysven@virginmedia.com         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "Host.h"
+
+#include "pre_guard.h"
+#include "ui_room_properties.h"
+#include "post_guard.h"
+
+
+class dlgRoomProperties : public QDialog, public Ui::room_properties
+{
+    Q_OBJECT
+
+public:
+    Q_DISABLE_COPY(dlgRoomProperties)
+    explicit dlgRoomProperties(Host*, QWidget* parent = nullptr);
+    void init(QHash<QString, int>& pSymbols, QSet<TRoom*>& pRooms);
+    void accept() override;
+
+signals:
+    void signal_save_symbol(QString symbol, QColor color, QSet<TRoom*> rooms);
+
+private:
+    QColor backgroundBasedColor(QColor);
+    QColor defaultColor();
+    QString getNewSymbol();
+    void initInstructionLabel();
+    QStringList getComboBoxItems();
+    QFont getFontForPreview(QString);
+
+    Host* mpHost;
+    QSet<TRoom*> mpRooms;
+    QHash<QString, int> mpSymbols;
+    QColor selectedColor;
+    QColor previewColor;
+    QColor roomColor;
+
+private slots:
+    void slot_openColorSelector();
+    void slot_currentColorChanged(const QColor&);
+    void slot_colorSelected(const QColor&);
+    void slot_colorRejected();
+    void slot_updatePreview();
+    void slot_resetColors();
+};
+
+#endif // MUDLET_DLGROOMPROPERTIES_H
