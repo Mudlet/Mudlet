eac91f42be8bd5095a012a5f6c07d11c5f7c5ccc
Kebap <kebap_spam@gmx.net>
2022-10-17 14:10:31 +0200

    name kind of color saved

--- src/dlgRoomProperties.h
+++ src/dlgRoomProperties.h
@@ -39,7 +39,7 @@ public:
     void accept() override;
 
 signals:
-    void signal_save_symbol(QString symbol, QColor color, QSet<TRoom*> rooms);
+    void signal_save_symbol(QString symbol, QColor symbolColor, QSet<TRoom*> rooms);
 
 private:
     QColor backgroundBasedColor(QColor);
