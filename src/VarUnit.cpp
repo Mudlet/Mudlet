c20a12c4a902bf95530fd2279c23135753247242
Stephen Lyons <slysven@virginmedia.com>
2022-10-24 22:35:10 +0100

    Infrastructure: unify class constructor initialisers (Part 2 0f 2) (#6388)
    This is so they all have each member initialiser on a single line - so
    that changes can be more easily seen/comprehended in Git differences (as
    opposed to them all being on a long single line...!)
    
    This is a response to the comment:
    https://github.com/Mudlet/Mudlet/pull/6379#issuecomment-1287238558
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/VarUnit.cpp
+++ src/VarUnit.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *   Copyright (C) 2013 by Chris Mitchell                                  *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
- *   Copyright (C) 2021 by Stephen Lyons - slysven@virginmedia..com        *
+ *   Copyright (C) 2021-2022 by Stephen Lyons - slysven@virginmedia..com   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -29,7 +29,8 @@
 #include "post_guard.h"
 
 
-VarUnit::VarUnit() : base()
+VarUnit::VarUnit()
+: base()
 {
 }
 
