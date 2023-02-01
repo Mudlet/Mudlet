3eeb5fd57fcbcd2b27160ff40175c8608ed6065c
Kebap <kebap_spam@gmx.net>
2023-02-01 17:23:21 +0100

    RoomColorSelector: DeleteOnClose

--- src/dlgRoomProperties.cpp
+++ src/dlgRoomProperties.cpp
@@ -482,6 +482,7 @@ void dlgRoomProperties::slot_defineNewColor()
 void dlgRoomProperties::slot_openRoomColorSelector()
 {
     auto dialog = new QDialog(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
     dialog->setWindowTitle(tr("Set room color"));
     auto vboxLayout = new QVBoxLayout;
     dialog->setLayout(vboxLayout);
