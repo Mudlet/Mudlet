1b2d29509aae0df8429e04af0fbedc2e02669766
keneanung <keneanung@googlemail.com>
2023-02-06 21:38:47 +0100

    Infrastructure: Re-Enable release automations that were disabled due to SSH key issues (#6092)
    <!-- Keep the title short & concise so anyone non-technical can
    understand it,
         the title appears in PTB changelogs -->
    #### Brief overview of PR changes/additions
    
    This PR re-enables automations that were disabled due to issues with SSH
    keys, namely upload of AppImages, macOS dmgs, windows installers, and
    source tarballs as well as registering uploaded artifacts with DBLSQD.
    
    #### Motivation for adding to Mudlet
    
    More release automation means less tedious release process, which means
    happier developers.
    
    #### Other info (issues closed, discussion etc)
    
    closes #5864
    restores part of #5865, #5866

--- CI/travis.after_success.sh
+++ CI/travis.after_success.sh
@@ -2,29 +2,18 @@
 
 set -x
 
-if { [ -n "$TRAVIS_REPO_SLUG" ] && [ "${TRAVIS_REPO_SLUG}" != "Mudlet/Mudlet" ]; } ||
-   { [ -n "$GITHUB_REPOSITORY" ] && [ "${GITHUB_REPOSITORY}" != "Mudlet/Mudlet" ]; } then
+if { [ -n "$GITHUB_REPOSITORY" ] && [ "${GITHUB_REPOSITORY}" != "Mudlet/Mudlet" ]; } then
   exit 0
 fi
 
-if [ "${TRAVIS_OS_NAME}" = "linux" ] || [ "${RUNNER_OS}" = "Linux" ]; then
+if [ "${RUNNER_OS}" = "Linux" ]; then
   echo Deploy on Linux.
   . CI/travis.linux.after_success.sh;
   echo $?
   echo "^ worked?"
-elif [ "${TRAVIS_OS_NAME}" = "osx" ]  || [ "${RUNNER_OS}" = "macOS" ]; then
+elif [ "${RUNNER_OS}" = "macOS" ]; then
   echo Deploy on macOS.
   . CI/travis.osx.after_success.sh;
-fi
-
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  prId=" ,#${TRAVIS_PULL_REQUEST}"
-fi
-
-if [ ! -z "${DEPLOY_URL}" ]; then
-  curl \
-    --data-urlencode "message=Deployed Mudlet \`${VERSION}${MUDLET_VERSION_BUILD}\` (${TRAVIS_OS_NAME}${prId}) to [${DEPLOY_URL}](${DEPLOY_URL})" \
-    https://webhooks.gitter.im/e/cc99072d43b642c4673a
 fi
 
 echo ""
