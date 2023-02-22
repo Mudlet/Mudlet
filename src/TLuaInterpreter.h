f12b4ff634f7892693c66a11da2f802cb10fa68d
atari2600tim <29287358+atari2600tim@users.noreply.github.com>
2023-02-21 19:27:13 -0500

    Update TLuaInterpreter.h
    token edit after ubuntu builder is supposed to be back

--- src/TLuaInterpreter.h
+++ src/TLuaInterpreter.h
@@ -645,6 +645,7 @@ public:
     static int announce(lua_State*);
     static int scrollTo(lua_State*);
     static int getScroll(lua_State*);
+// TIM
     static int getConfig(lua_State*);
     // PLACEMARKER: End of Lua functions declarations
     // check new functions against https://www.linguistic-antipatterns.com when creating them
