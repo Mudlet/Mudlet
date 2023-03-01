b88a507d69b031019983c068c75a85db4cbf5579
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2023-03-01 10:41:06 +0100

    New translations mudlet.ts (Turkish)

--- translations/translated/mudlet_tr_TR.ts
+++ translations/translated/mudlet_tr_TR.ts
@@ -1268,6 +1268,11 @@ factor of:</source>
       <translation type="unfinished"/>
     </message>
     <message>
+      <location filename="../src/TConsole.cpp" line="2063"/>
+      <source>Debug messages from all profiles are shown here.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
       <location filename="../src/TConsole.cpp" line="2094"/>
       <source>Game content is shown here. It may contain subconsoles and a mapper window.</source>
       <translation type="unfinished"/>
@@ -1342,11 +1347,6 @@ factor of:</source>
     <message>
       <location filename="../src/TConsole.cpp" line="2062"/>
       <source>Debug Console.</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
-      <location filename="../src/TConsole.cpp" line="2063"/>
-      <source>Debug messages from all profile are shown here.</source>
       <translation type="unfinished"/>
     </message>
     <message>
