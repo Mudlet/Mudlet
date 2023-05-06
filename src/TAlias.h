0345c494d54369a99685fb9a981f7a86b8721f90
Vadim Peretokin <vperetokin@hey.com>
2023-05-06 05:58:00 +0200

    Add debug operator for alias

--- src/TAlias.h
+++ src/TAlias.h
@@ -27,6 +27,7 @@
 
 #include "pre_guard.h"
 #include <QApplication>
+#include <QDebug>
 #include <QPointer>
 #include <QSharedPointer>
 #include "post_guard.h"
@@ -49,17 +50,17 @@ public:
     TAlias(const QString& name, Host* pHost);
     void compileAll();
     void compileRegex();
-    QString getName() { return mName; }
+    QString getName() const { return mName; }
     void setName(const QString& name);
     void compile();
     bool compileScript();
     void execute();
-    QString getScript() { return mScript; }
+    QString getScript() const { return mScript; }
     bool setScript(const QString& script);
-    QString getRegexCode() { return mRegexCode; }
+    QString getRegexCode() const { return mRegexCode; }
     void setRegexCode(const QString&);
     void setCommand(const QString& command) { mCommand = command; }
-    QString getCommand() { return mCommand; }
+    QString getCommand() const { return mCommand; }
 
     bool match(const QString& toMatch);
     bool registerAlias();
@@ -79,5 +80,29 @@ public:
     bool exportItem = true;
     bool mRegisteredAnonymousLuaFunction = false;
 };
+
+#ifndef QT_NO_DEBUG_STREAM
+inline QDebug& operator<<(QDebug& debug, const TAlias* alias)
+{
+    QDebugStateSaver saver(debug);
+    Q_UNUSED(saver);
+
+    if (!alias) {
+        return debug << "TAlias(0x0) ";
+    }
+    debug.nospace() << "TAlias(" << alias->getName() << ")";
+    debug.nospace() << ", command=" << alias->getCommand();
+    debug.nospace() << ", regexCode=" << alias->getRegexCode();
+    debug.nospace() << ", funcName=" << alias->mFuncName;
+    debug.nospace() << ", script=" << alias->getScript();
+    debug.nospace() << ", needsCompiling=" << alias->mNeedsToBeCompiled;
+    debug.nospace() << ", moduleMember=" << alias->mModuleMember;
+    debug.nospace() << ", moduleMasterFolder=" << alias->mModuleMasterFolder;
+    debug.nospace() << ", exportItem=" << alias->exportItem;
+    debug.nospace() << ", registeredAnonymousLuaFunction=" << alias->mRegisteredAnonymousLuaFunction;
+    debug.nospace() << ')';
+    return debug;
+}
+#endif // QT_NO_DEBUG_STREAM
 
 #endif // MUDLET_TALIAS_H
