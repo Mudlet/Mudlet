8b587c51fdc502608bc01911a016b4bf32402ec9
Tim Johnson <29287358+atari2600tim@users.noreply.github.com>
2023-05-09 13:08:26 -0400

    watch for return value of copy

--- src/dlgPackageExporter.cpp
+++ src/dlgPackageExporter.cpp
@@ -860,7 +860,9 @@ std::pair<bool, QString> dlgPackageExporter::copyAssetsToTmp(const QStringList& 
         }
         if (asset.isFile()) {
             QFile::remove(filePath);
-            QFile::copy(asset.absoluteFilePath(), filePath);
+            if (!QFile::copy(asset.absoluteFilePath(), filePath)) {
+                return {false, tr("cannot copy %1 to the temporary location %2 - can you double-check it?").arg(asset.absoluteFilePath().toHtmlEscaped(), tempPath.toHtmlEscaped())};
+            }
         } else if (asset.isDir()) {
             copy_directory(asset.absoluteFilePath(), filePath, false);
         }
