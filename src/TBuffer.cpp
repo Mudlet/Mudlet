8a82cf2666ec7f173b38f45bee22d51d9bbabc16
eowmob <72653851+eowmob@users.noreply.github.com>
2023-05-31 19:44:48 +0200

    Improvement: MXP text effects: bold, underline, italics, strikethrough (#6802)
    <!-- Keep the title short & concise so anyone non-technical can
    understand it,
         the title appears in PTB changelogs -->
    #### Brief overview of PR changes/additions
    MXP text effects (incl. all aliases) are now rendered.
    Colour and text effects in open mode auto-close at end of line 
    #### Motivation for adding to Mudlet
    Improve MXP compliance
    #### Other info (issues closed, discussion etc)
    [MXP enhancements, was PR
    #4200](https://forums.mudlet.org/viewtopic.php?f=7&t=23206)

--- src/TBuffer.cpp
+++ src/TBuffer.cpp
@@ -542,12 +542,12 @@ void TBuffer::translateToPlainText(std::string& incoming, const bool isFromServe
                     const int spacesNeeded = temp.toInt(&isOk);
                     if (isOk && spacesNeeded > 0) {
                         const TChar::AttributeFlags attributeFlags =
-                                ((mIsDefaultColor ? mBold : false) ? TChar::Bold : TChar::None)
-                                | (mItalics ? TChar::Italic : TChar::None)
+                                ((mIsDefaultColor ? mBold || mpHost->mMxpClient.bold() : false) ? TChar::Bold : TChar::None)
+                                | (mItalics || mpHost->mMxpClient.italic() ? TChar::Italic : TChar::None)
                                 | (mOverline ? TChar::Overline : TChar::None)
                                 | (mReverse ? TChar::Reverse : TChar::None)
-                                | (mStrikeOut ? TChar::StrikeOut : TChar::None)
-                                | (mUnderline ? TChar::Underline : TChar::None);
+                                | (mStrikeOut || mpHost->mMxpClient.strikeOut() ? TChar::StrikeOut : TChar::None)
+                                | (mUnderline || mpHost->mMxpClient.underline() ? TChar::Underline : TChar::None);
 
                         // Note: we are using the background color for the
                         // foreground color as well so that we are transparent:
@@ -693,12 +693,12 @@ COMMIT_LINE:
                     continue;
                 } else if (mpHost->mBlankLineBehaviour == Host::BlankLineBehaviour::ReplaceWithSpace) {
                     const TChar::AttributeFlags attributeFlags =
-                            ((mIsDefaultColor ? mBold : false) ? TChar::Bold : TChar::None)
-                            | (mItalics ? TChar::Italic : TChar::None)
+                            ((mIsDefaultColor ? mBold || mpHost->mMxpClient.bold(): false) ? TChar::Bold : TChar::None)
+                            | (mItalics || mpHost->mMxpClient.italic() ? TChar::Italic : TChar::None)
                             | (mOverline ? TChar::Overline : TChar::None)
                             | (mReverse ? TChar::Reverse : TChar::None)
-                            | (mStrikeOut ? TChar::StrikeOut : TChar::None)
-                            | (mUnderline ? TChar::Underline : TChar::None);
+                            | (mStrikeOut || mpHost->mMxpClient.strikeOut() ? TChar::StrikeOut : TChar::None)
+                            | (mUnderline || mpHost->mMxpClient.underline() ? TChar::Underline : TChar::None);
 
                     // Note: we are using the background color for the
                     // foreground color as well so that we are transparent:
@@ -836,12 +836,12 @@ COMMIT_LINE:
         }
 
         const TChar::AttributeFlags attributeFlags =
-                ((mIsDefaultColor ? mBold : false) ? TChar::Bold : TChar::None)
-                | (mItalics ? TChar::Italic : TChar::None)
+                ((mIsDefaultColor ? mBold || mpHost->mMxpClient.bold() : false) ? TChar::Bold : TChar::None)
+                | (mItalics || mpHost->mMxpClient.italic() ? TChar::Italic : TChar::None)
                 | (mOverline ? TChar::Overline : TChar::None)
                 | (mReverse ? TChar::Reverse : TChar::None)
-                | (mStrikeOut ? TChar::StrikeOut : TChar::None)
-                | (mUnderline ? TChar::Underline : TChar::None);
+                | (mStrikeOut || mpHost->mMxpClient.strikeOut() ? TChar::StrikeOut : TChar::None)
+                | (mUnderline || mpHost->mMxpClient.underline() ? TChar::Underline : TChar::None);
 
         TChar c((!mIsDefaultColor && mBold) ? mForeGroundColorLight : mForeGroundColor, mBackGroundColor, attributeFlags);
 
