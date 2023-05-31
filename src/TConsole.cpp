7e0ac3b7cd50198304e0eddbc6d852ab3ffd74c7
Stephen Lyons <slysven@virginmedia.com>
2023-05-31 17:26:07 +0100

    Improve: clear split TConsole when disabling scrolling (#6874)
    #### Brief overview of PR changes/additions
    As I identified the code required was already used in two separate
    places I refactored it out to a new method `(void)
    TConsole::clearSplit()` so it could be used in a new, third place as
    well.
    
    #### Motivation for adding to Mudlet
    As I mentioned in
    https://github.com/Mudlet/Mudlet/pull/6848#issuecomment-1569192566
    disabling scrolling when the `TConsole` had already been scrolled (and
    had thus been split) locked the split in place and if the window was
    cleared only the upper pane had all it's content removed. This addition
    - as well as refactoring the code that clears the split from two
    existing places - adds it to the code that disables scrolling so when
    that happens any existing split is cleared.
    
    #### Other info (issues closed, discussion etc)
    DRY!
    
    ---------
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>
    Co-authored-by: Vadim Peretokin <vperetokin@hey.com>

--- src/TConsole.cpp
+++ src/TConsole.cpp
@@ -1724,10 +1724,13 @@ void TConsole::setHorizontalScrollBar(bool isEnabled)
     }
 }
 
-void TConsole::setScrolling(bool enabled)
+void TConsole::setScrolling(const bool state)
 {
     if (mType & (UserWindow | SubConsole)) {
-        mScrollingEnabled = enabled;
+        mScrollingEnabled = state;
+        if (!mScrollingEnabled) {
+            clearSplit();
+        }
     }
 }
 
@@ -2411,4 +2414,16 @@ void TConsole::handleLinesOverflowEvent(const int lineCount)
     sysWindowOverflow.mArgumentList.append(QString::number(-linesSpare));
     sysWindowOverflow.mArgumentTypeList.append(ARGUMENT_TYPE_NUMBER);
     mpHost->raiseEvent(sysWindowOverflow);
+}
+
+void TConsole::clearSplit()
+{
+    mLowerPane->mCursorY = buffer.size();
+    mLowerPane->hide();
+    buffer.mCursorY = buffer.size();
+    mUpperPane->mCursorY = buffer.size();
+    mUpperPane->mCursorX = 0;
+    mUpperPane->mIsTailMode = true;
+    mUpperPane->updateScreenView();
+    mUpperPane->forceUpdate();
 }
