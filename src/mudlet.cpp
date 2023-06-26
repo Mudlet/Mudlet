Mudlet/Mudlet

commit 356fe9dc515e2d53c0c2a5e0d293b2092046f09c

Improve: don't allow profiles that failed to load to overwrite good profiles (#6791)
<!-- Keep the title short & concise so anyone non-technical can
understand it,
     the title appears in PTB changelogs -->
#### Brief overview of PR changes/additions
If a profile failed to load due to an error, Mudlet would still allow
this failed profile to be _saved_. Because nothing was actually loaded,
a blank profile would be saved - meaning next time a player loaded
Mudlet, nothing would be there.

This improvement prevents profiles that failed to load from saving a
blank profile on top - instead, the same issue would persist until a
player chose the last working profile from the history.

This way, instead of seeing a blank profile with all of their stuff
gone, they just see a broken one with instructions on how to fix:

![Screenshot from 2023-04-27
20-11-11](https://user-images.githubusercontent.com/110988/234955989-5177350b-62aa-4bb9-8854-b1c532f2fd92.png)

#### Motivation for adding to Mudlet
Better player experience even in the very rare cases.
#### Other info (issues closed, discussion etc)
A technically-minded player can look in the terminal to find the exact
error message:

> dlgConnectionProfiles::loadProfile: ERROR loading
"/home/vadi/.config/mudlet/profiles/Achaea/current/2023-04-20#07-17-24.xml"
due to: "Start tag expected."

---------

Co-authored-by: Stephen Lyons <slysven@virginmedia.com>

src/mudlet.cpp
@@ -2698,11 +2698,20 @@ void mudlet::doAutoLogin(const QString& profile_name)
             pHost->mSslTsl = (*it).tlsEnabled;
         }
     } else {
-        QFile file(qsl("%1/%2").arg(folder, entries.at(0)));
+        QFile file(qsl("%1%2").arg(folder, entries.at(0)));
         file.open(QFile::ReadOnly | QFile::Text);
         XMLimport importer(pHost);
         qDebug() << "[LOADING PROFILE]:" << file.fileName();
-        importer.importPackage(&file); // TODO: Missing false return value handler
+        importer.importPackage(&file);
+        if (auto [success, message] = importer.importPackage(&file); !success) {
+            pHost->postMessage(tr("[ ERROR ] - Something went wrong loading your Mudlet profile and it could not be loaded.\n"
+                "Try loading an older version in 'Connect - Options - Profile history' or double-check that %1 looks correct.").arg(file.fileName()));
+        
+            qDebug().nospace().noquote() << "mudlet::doAutoLogin(\"" << profile_name << "\") ERROR - loading \"" << file.fileName() << "\" failed, reason: \"" << message << "\".";
+        } else {
+            pHost->mLoadedOk = true;
+        }
+
         pHost->refreshPackageFonts();
 
         // Is this a new profile created through 'copy profile (settings only)'? install default packages into it

src/Host.cpp
@@ -802,6 +802,9 @@ std::tuple<bool, QString, QString> Host::saveProfile(const QString& saveFolder, 
         filename_xml = qsl("%1/%2.xml").arg(directory_xml, saveName);
     }
 
+    if (!mLoadedOk) {
+        return {false, filename_xml, qsl("profile was not loaded correctly to begin with")};
+    }
 
     if (mIsProfileLoadingSequence) {
         //If we're inside of profile loading sequence modules might not be loaded yet, thus we can accidetnally clear their contents

src/Host.h
@@ -520,6 +520,10 @@ public:
     int mScreenHeight;
     int mScreenWidth;
 
+    // has the profile save data been loaded without issues?
+    // if there were issues during loading, we should not save anything on close
+    bool mLoadedOk = false;
+
     int mTimeout;
 
     QString mUrl;
@@ -744,7 +748,6 @@ private:
     int mHostID;
     QString mHostName;
     QString mDiscordGameName; // Discord self-reported game name
-
     bool mIsClosingDown;
 
     QString mLine;

src/dlgConnectionProfiles.cpp
@@ -1547,17 +1547,19 @@ void dlgConnectionProfiles::loadProfile(bool alsoConnect)
     if (entries.isEmpty()) {
         firstTimeLoad = true;
     } else {
-        QString fileName{qsl("%1%2").arg(folder, profile_history->itemData(profile_history->currentIndex()).toString())};
-        QFile file(fileName);
+        QFile file(qsl("%1%2").arg(folder, profile_history->itemData(profile_history->currentIndex()).toString()));
         file.open(QFile::ReadOnly | QFile::Text);
         XMLimport importer(pHost);
 
         qDebug() << "[LOADING PROFILE]:" << file.fileName();
         if (auto [success, message] = importer.importPackage(&file, nullptr); !success) {
+            //: %1 is the path and file name (i.e. the location) of the problem fil
             pHost->postMessage(tr("[ ERROR ] - Something went wrong loading your Mudlet profile and it could not be loaded.\n"
-                "Try loading an older version in 'Connect - Options - Profile history' or double-check that %1 looks correct.", "%1 is the filename").arg(fileName));
+                "Try loading an older version in 'Connect - Options - Profile history' or double-check that %1 looks correct.").arg(file.fileName()));
         
-            qDebug() << "dlgConnectionProfiles::loadProfile: ERROR loading" << fileName << "due to:" << message;
+            qDebug().nospace().noquote() << "dlgConnectionProfiles::loadProfile(" << alsoConnect << ") ERROR - loading \"" << file.fileName() << "\" failed, reason: \"" << message << "\".";
+        } else {
+            pHost->mLoadedOk = true;
         }
 
         pHost->refreshPackageFonts();

src/dlgTriggerEditor.cpp
@@ -505,7 +505,6 @@ dlgTriggerEditor::dlgTriggerEditor(Host* pH)
     connect(mpExportAction, &QAction::triggered, this, &dlgTriggerEditor::slot_export);
 
     mProfileSaveAction = new QAction(QIcon(qsl(":/icons/document-save-all.png")), tr("Save Profile"), this);
-    mProfileSaveAction->setEnabled(true);
     mProfileSaveAction->setToolTip(tr("<p>Saves your profile. (Ctrl+Shift+S)</p>"
                                       "<p>Saves your entire profile (triggers, aliases, scripts, timers, buttons and "
                                       "keys, but not the map or script-specific settings) to your computer disk, so "
@@ -517,14 +516,23 @@ dlgTriggerEditor::dlgTriggerEditor(Host* pH)
                                       "also cause them to be saved and reloaded into other profiles if they too are "
                                       "active.</p>"));
     mProfileSaveAction->setStatusTip(tr(R"(Saves your entire profile (triggers, aliases, scripts, timers, buttons and keys, but not the map or script-specific settings); also "synchronizes" modules that are so marked.)"));
-    connect(mProfileSaveAction, &QAction::triggered, this, &dlgTriggerEditor::slot_profileSaveAction);
 
     mProfileSaveAsAction = new QAction(QIcon(qsl(":/icons/utilities-file-archiver.png")), tr("Save Profile As"), this);
-    mProfileSaveAsAction->setEnabled(true);
-    connect(mProfileSaveAsAction, &QAction::triggered, this, &dlgTriggerEditor::slot_profileSaveAsAction);
 
-    auto *nextSectionShortcut = new QShortcut(QKeySequence(Qt::CTRL | Qt::Key_Tab), this);
-    connect(nextSectionShortcut, &QShortcut::activated, this, &dlgTriggerEditor::slot_nextSection);
+    if (mpHost->mLoadedOk) {
+        connect(mProfileSaveAction, &QAction::triggered, this, &dlgTriggerEditor::slot_profileSaveAction);
+        connect(mProfileSaveAsAction, &QAction::triggered, this, &dlgTriggerEditor::slot_profileSaveAsAction);
+    } else {
+        mProfileSaveAction->setDisabled(true);
+        mProfileSaveAsAction->setDisabled(true);
+        auto disabledSaving = tr("Something went wrong loading your Mudlet profile and it could not be loaded. "
+            "Try loading an older version in 'Connect - Options - Profile history'");
+        mProfileSaveAction->setToolTip(disabledSaving);
+        mProfileSaveAsAction->setToolTip(disabledSaving);
+    }
+
+    auto *nextSectionShortcut = new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Tab), this);
+    QObject::connect(nextSectionShortcut, &QShortcut::activated, this, &dlgTriggerEditor::slot_nextSection);
 
     QShortcut *previousSectionShortcut = new QShortcut(QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_Tab), this);
     connect(previousSectionShortcut, &QShortcut::activated, this, &dlgTriggerEditor::slot_previousSection);

commit a0f16d25d8a4041a1181bf8e0e5129ce4cb15c14

Fix: remove bogus double read of profile's game save data
This looks to be an error introduced in #6791 where a single line that did
an unchecked load of the profile's game save was to be replaced with a
checking version that reports any problems - but the original unchecked one
didn't get removed.

Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

src/mudlet.cpp
@@ -2749,7 +2749,6 @@ void mudlet::doAutoLogin(const QString& profile_name)
         file.open(QFile::ReadOnly | QFile::Text);
         XMLimport importer(pHost);
         qDebug() << "[LOADING PROFILE]:" << file.fileName();
-        importer.importPackage(&file);
         if (auto [success, message] = importer.importPackage(&file); !success) {
             pHost->postMessage(tr("[ ERROR ] - Something went wrong loading your Mudlet profile and it could not be loaded.\n"
                 "Try loading an older version in 'Connect - Options - Profile history' or double-check that %1 looks correct.").arg(file.fileName()));