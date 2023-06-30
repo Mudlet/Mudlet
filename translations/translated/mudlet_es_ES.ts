ed03c9b37526feaec30f34e845afabc82824ad57
mudlet-machine-account <39947211+mudlet-machine-account@users.noreply.github.com>
2023-06-30 09:43:31 +0200

    New translations mudlet.ts (Spanish)

--- translations/translated/mudlet_es_ES.ts
+++ translations/translated/mudlet_es_ES.ts
@@ -1090,13 +1090,13 @@ See the &quot;[MAP ERROR:]&quot; message for the reason.</source>
       <location filename="../src/TCommandLine.cpp" line="707"/>
       <source>▼Mudlet▼ │ dictionary suggestions │ ▲User▲</source>
       <comment>This line is shown in the list of spelling suggestions on the profile&apos;s command-line context menu to clearly divide up where the suggestions for correct spellings are coming from.  The precise format might be modified as long as it is clear that the entries below this line in the menu come from the spelling dictionary that the user has chosen in the profile setting which we have bundled with Mudlet; the entries about this line are the ones that the user has personally added.</comment>
-      <translation type="unfinished"/>
+      <translation>▼Mudlet▼ │ sugerencias del diccionario │ ▲Usuario▲</translation>
     </message>
     <message>
       <location filename="../src/TCommandLine.cpp" line="717"/>
       <source>▼System▼ │ dictionary suggestions │ ▲User▲</source>
       <comment>This line is shown in the list of spelling suggestions on the profile&apos;s command-line context menu to clearly divide up where the suggestions for correct spellings are coming from.  The precise format might be modified as long as it is clear that the entries below this line in the menu come from the spelling dictionary that the user has chosen in the profile setting which is provided as part of the OS; the entries about this line are the ones that the user has personally added.</comment>
-      <translation type="unfinished"/>
+      <translation>▼Sistema▼ │ sugerencias del diccionario │ ▲Usuario▲</translation>
     </message>
     <message>
       <location filename="../src/TCommandLine.cpp" line="791"/>
@@ -7615,7 +7615,7 @@ Bold HTML tags are used to emphasis that the value is destination room&apos;s we
     <message>
       <location filename="../src/dlgTriggerEditor.cpp" line="470"/>
       <source>Add Item</source>
-      <translation type="unfinished"/>
+      <translation>Añadir un elemento</translation>
     </message>
     <message>
       <location filename="../src/dlgTriggerEditor.cpp" line="484"/>
