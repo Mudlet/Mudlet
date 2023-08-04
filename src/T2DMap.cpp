Mudlet/Mudlet

commit bab6599bb692f27becf35b2a93e55370bd566af0

Infrastructure: Delete popup window on close (#6531)
#### Brief overview of PR changes/additions
Make sure to actually delete the popup windows after the player closes
them, so they do not linger on.

#### Motivation for adding to Mudlet
Fix #6490 for some but not all popups:

- [x] Move to position (in `T2DMap::slot_movePosition()`)
- [x] Set exits (in `T2DMap::slot_setExits()`)
- [x] Move to area (in `T2DMap::slot_setArea()`)
- [x] Create exit line (in `T2DMap::slot_setCustomLine()`)
- [x] Custom line properties (in `T2DMap::slot_customLineProperties()`)
- [x] Select room color (in
`dlgRoomProperties::slot_openRoomColorSelector()`)
- [ ] "Package exporter" (in `mudlet::slot_packageExporter()`) -
postponed after failed attempt below to #6545
- [x] "downloading game GUI from server" (ATCP) (in `void
cTelnet::processTelnetCommand(const std::string& command)`)
- [x] "downloading game GUI from server" (GMCP) (in `void
cTelnet::setGMCPVariables(const QByteArray& msg)`)

#### Other info (issues closed, discussion etc)
Thanks to atari2600tim and SlySven!

src/T2DMap.cpp
@@ -4,7 +4,7 @@
  *                                               - slysven@virginmedia.com *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
  *   Copyright (C) 2021-2022 by Piotr Wilczynski - delwing@gmail.com       *
- *   Copyright (C) 2022 by Lecker Kebap - Leris@mudlet.org                 *
+ *   Copyright (C) 2022-2023 by Lecker Kebap - Leris@mudlet.org            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -3262,6 +3262,7 @@ void T2DMap::slot_customLineProperties()
                 qWarning("T2DMap::slot_customLineProperties() ERROR: failed to create the dialog!");
                 return;
             }
+            dialog->setAttribute(Qt::WA_DeleteOnClose);
             dialog->setWindowIcon(QIcon(qsl(":/icons/mudlet_custom_exit_properties.png")));
             auto* le_toId = dialog->findChild<QLineEdit*>(qsl("toId"));
             auto* le_fromId = dialog->findChild<QLineEdit*>(qsl("fromId"));
@@ -3597,6 +3598,7 @@ void T2DMap::slot_movePosition()
 
     auto dialog = new QDialog(this);
     auto gridLayout = new QGridLayout;
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
     dialog->setLayout(gridLayout);
     dialog->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding));
     dialog->setContentsMargins(0, 0, 0, 0);
@@ -3982,6 +3984,7 @@ void T2DMap::slot_setExits()
         auto pD = new dlgRoomExits(mpHost, mMultiSelectionHighlightRoomId, this);
         pD->show();
         pD->raise();
+        pD->setAttribute(Qt::WA_DeleteOnClose);
     }
 }
 
@@ -4057,6 +4060,7 @@ void T2DMap::slot_setArea()
     if (!set_room_area_dialog) {
         return;
     }
+    set_room_area_dialog->setAttribute(Qt::WA_DeleteOnClose);
     arealist_combobox = set_room_area_dialog->findChild<QComboBox*>("arealist_combobox");
     if (!arealist_combobox) {
         return;
@@ -4533,6 +4537,7 @@ void T2DMap::slot_setCustomLine()
     if (!dialog) {
         return;
     }
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
     dialog->setWindowIcon(QIcon(qsl(":/icons/mudlet_custom_exit.png")));
     mCustomLinesRoomFrom = mMultiSelectionHighlightRoomId;
     mCustomLinesRoomTo = 0;

src/ctelnet.cpp
@@ -8,6 +8,7 @@
  *   Copyright (C) 2016 by Ian Adkins - ieadkins@gmail.com                 *
  *   Copyright (C) 2017 by Michael Hupp - darksix@northfire.org            *
  *   Copyright (C) 2017 by Colton Rasbury - rasbury.colton@gmail.com       *
+ *   Copyright (C) 2023 by Lecker Kebap - Leris@mudlet.org                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -1674,6 +1675,7 @@ void cTelnet::processTelnetCommand(const std::string& command)
                 mpProgressDialog = new QProgressDialog(tr("downloading game GUI from server"), tr("Cancel", "Cancel download of GUI package from Server"), 0, 4000000, mpHost->mpConsole);
                 connect(mpPackageDownloadReply, &QNetworkReply::downloadProgress, this, &cTelnet::slot_setDownloadProgress);
                 connect(mpProgressDialog, &QProgressDialog::canceled, mpPackageDownloadReply, &QNetworkReply::abort);
+                mpProgressDialog->setAttribute(Qt::WA_DeleteOnClose);
                 mpProgressDialog->show();
             }
             return;
@@ -2036,6 +2038,7 @@ void cTelnet::setGMCPVariables(const QByteArray& msg)
         mpProgressDialog = new QProgressDialog(tr("downloading game GUI from server"), tr("Cancel", "Cancel download of GUI package from Server"), 0, 4000000, mpHost->mpConsole);
         connect(mpPackageDownloadReply, &QNetworkReply::downloadProgress, this, &cTelnet::slot_setDownloadProgress);
         connect(mpProgressDialog, &QProgressDialog::canceled, mpPackageDownloadReply, &QNetworkReply::abort);
+        mpProgressDialog->setAttribute(Qt::WA_DeleteOnClose);
         mpProgressDialog->show();
         return;
     } else if (transcodedMsg.startsWith(QLatin1String("Client.Map"), Qt::CaseInsensitive)) {

src/dlgRoomProperties.cpp
@@ -496,6 +496,7 @@ void dlgRoomProperties::slot_defineNewColor()
 void dlgRoomProperties::slot_openRoomColorSelector()
 {
     auto dialog = new QDialog(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
     dialog->setWindowTitle(tr("Set room color"));
     auto vboxLayout = new QVBoxLayout;
     dialog->setLayout(vboxLayout);

src/mudlet.cpp
@@ -7,6 +7,7 @@
  *   Copyright (C) 2017 by Tom Scheper - scheper@gmail.com                 *
  *   Copyright (C) 2011-2021 by Vadim Peretokin - vperetokin@gmail.com     *
  *   Copyright (C) 2022 by Thiago Jung Bauermann - bauermann@kolabnow.com  *
+ *   Copyright (C) 2023 by Lecker Kebap - Leris@mudlet.org                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *

commit adb001332ab96d5126ffea46fa1763c97990faf4

Fix: crash on moving rooms (#6933)
#### Brief overview of PR changes/additions
In fact this ought to be happening in other cases as well when using the
"Move to position" 2D Mapper context menu option. It is a result of
setting the `Qt::WA_DeleteOnClose` attribute on the `QDialog` used to
get the user's input and using `QDialog::exect()` in order to "run" the
dialog. As a result it deletes itself when the user clicks the "Okay"
button but Mudlet has been trying (and crashing when it cannot) to read
the text contents of the `QLineEdit`s that the user has typed their
input into after that point!

The fix is to NOT set that attribute. To ensure that the dialog is
cleared from memory after use (as #6531 was trying to do) we must ensure
that the dialog is destroyed AFTER the details have been gathered - or
even if they are not because "Cancel" has been clicked instead and the
safest method to do that is to call `QObject::deleteLater()` on it when
we have finished with it - then it gets scheduled for destruction and
the underlying Qt code does that when "control returns to the main event
loop" - importantly it is safe to call that more than once on the same
object which is not always the case for the core C++ `delete X`
operation).

#### Motivation for adding to Mudlet
As the title and related issue suggests, to stop a fatal seg. fault when
one or more rooms are moved in the 2D mapper with the "Move to position"
context menu action is used.

#### Other info (issues closed, discussion etc)
This should close #6932. As a check I have reviewed all the other places
where `QDialog::exec()` is used and I have determined to my satisfaction
that there are no other cases in our code where this could also happen.
Further investigation shows that the bug was introduced by #6531
inserted into the code base by @Kebap in February this year (so will
affect **4.17.x** releases up until this fix goes in).

src/T2DMap.cpp
@@ -3648,7 +3648,11 @@ void T2DMap::slot_movePosition()
 
     auto dialog = new QDialog(this);
     auto gridLayout = new QGridLayout;
-    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    // Do NOT try to set the Qt::WA_DeleteOnClose attribute on the dialogue,
+    // because we want to read the details from the QLineEdits on it after the
+    // user has clicked "OK" on the dialog - and setting that flag will cause
+    // it (and those QLineEdits) to be destroyed by the time the QDialog:exec()
+    // call returns which is before we've got those details!
     dialog->setLayout(gridLayout);
     dialog->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding));
     dialog->setContentsMargins(0, 0, 0, 0);
@@ -3718,6 +3722,7 @@ void T2DMap::slot_movePosition()
             room->z += dz;
         }
     }
+    dialog->deleteLater();
     repaint();
     mpMap->setUnsaved(__func__);
 }