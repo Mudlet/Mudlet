d8903dc6a89aae27d7d73063f9aacf22f577eca5
Stephen Lyons <slysven@virginmedia.com>
2023-12-18 02:10:48 +0000

    Revise: fix a couple of typos in warning messages
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/XMLimport.cpp
+++ src/XMLimport.cpp
@@ -829,15 +829,14 @@ void XMLimport::readHost(Host* pHost)
             if (!match.hasMatch()) {
                 // We have a user name that contains characters that do not
                 // match the post 2023/05 Discord user-name changes
-                QString warningMessage{tr("[ WARN ]  - A Discord-RPC username has been specified in the preferences for this\n"
-                                          "profile of: \"%1\"\n"
-                                          "however, from May 2023, Discord is changing the user name format to be a unique\n"
-                                          "string for each user that contains only lower case ASCII alphabet ('a' to 'z'),\n"
-                                          "digits ('0' to '9'), '_' and '.'. This name does not meet that requirement and\n"
-                                          "will need to be changed when your Discord name is updated to the new format.\n"
-                                          "Once that happens you will need to adjust this value in the \"Chat\" tab of\n"
-                                          "the settings otherwise the Discord-RPC integration for this profile will stop\n"
-                                          "working.\n")
+                QString warningMessage{tr("[ WARN ]  - A Discord-RPC username has been specified in the preferences for\n"
+                                          "this profile of: \"%1\" however, from May 2023, Discord is\n""
+                                          "changing the user name format to be a unique string for each user that contains\n"
+                                          "only lower case ASCII alphabet ('a' to 'z'), digits ('0' to '9'), '_' and '.'.\n"
+                                          "This name does not meet that requirement and will need to be changed when your\n"
+                                          "Discord name is updated to the new format. Once that happens you will need to\n"
+                                          "adjust this value in the \"Chat\" tab of the settings otherwise the Discord-RPC\n"
+                                          "integration for this profile will stop working.\n")
                             .arg(pHost->mRequiredDiscordUserName)};
                 pHost->postMessage(warningMessage);
             }
@@ -851,7 +850,7 @@ void XMLimport::readHost(Host* pHost)
         if (!pHost->mRequiredDiscordUserDiscriminator.isEmpty() && pHost->mRequiredDiscordUserDiscriminator.compare(QLatin1String("0"))) {
             // We have a discriminator which won't work after the 2023/05
             // Discord user name changes
-            QString warningMessage{tr("[ WARN ]  - A Discord-RPC 'discriminator has been specified in the preferences\n"
+            QString warningMessage{tr("[ WARN ]  - A Discord-RPC discriminator has been specified in the preferences\n"
                                       "for this profile of: \"%1\" however, from May 2023, Discord is removing this\n"
                                       "and changing the user name format to be a unique string for each user. So when\n"
                                       "your Discord name is updated to the new format you will also need to remove\n"
