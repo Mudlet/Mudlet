2180a34303e60f3f4a2bb5d46df5b36527bc87a5
Vadim Peretokin <vperetokin@hey.com>
2023-12-24 16:30:02 +0100

    Clean up comments

--- src/T2DMap.h
+++ src/T2DMap.h
@@ -148,11 +148,8 @@ public:
     // This is the area of the map that is being shown, it need not be that
     // which contains the player room:
     int mAreaID = 0;
-    // These next three represent the room coordinates at the middle of the map
-    // the first pair needs to not be integer types as a more flexible zoom
-    // in/out mechanism was adopted that meant non-integral coordinates were
-    // needed, OTOH a "snap to a fractional (power of 2?) value" might help to
-    // keep the decimal numbers reasonable:
+    // These represent the map center coordinates.
+    // The first two are non-integer to enable flexible zooming:
     qreal mMapCenterX = 0.0;
     qreal mMapCenterY = 0.0;
     int mMapCenterZ = 0;
@@ -271,19 +268,10 @@ private:
         int y;
     } mContextMenuClickPosition;
 
-    // When more than zero rooms are selected this
-    // is either the first (only) room in the set
-    // or if getCenterSelectionId() is used the
-    // room that is selected - this is so that it
-    // can be painted in yellow rather than orange
-    // when more than one room is selected to
-    // indicate the particular room that will be
-    // modified or be the center of those
-    // modifications. {for slot_spread(),
-    // slot_shrink(), slot_setUserData() - if ever
-    // implemented, slot_setExits(),
-    // slot_movePosition(), etc.} - previously have
-    // used -1 but is now reset to 0 if it is not valid.
+    // This holds the ID of the room highlighted in yellow when multiple
+    // rooms are selected. It is either the first selected room, or the
+    // room at the center of the selection. This indicates the room that
+    // will be modified by actions like spread, shrink, set exits, move position, etc.
     int mMultiSelectionHighlightRoomId = 0;
 
     bool mIsSelectionSorting = true;
