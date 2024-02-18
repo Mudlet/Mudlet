42545622bad5a0472bb57a97e64c566d27fcc427
Vadim Peretokin <vadim.peretokin@carasent.com>
2024-02-18 20:59:45 +0100

    Re-add missing IAC SB GMCP

--- src/ctelnet.cpp
+++ src/ctelnet.cpp
@@ -1785,6 +1785,7 @@ void cTelnet::processTelnetCommand(const std::string& telnetCommand)
 
             output = TN_IAC;
             output += TN_SB;
+            output += OPT_GMCP;
             output += R"(Core.Supports.Set [ "Char 1", "Char.Skills 1", "Char.Items 1", "Room 1", "IRE.Rift 1", "IRE.Composer 1", "External.Discord 1", "Client.Media 1", "Client.Authenticate 1"])";
             output += TN_IAC;
             output += TN_SE;
