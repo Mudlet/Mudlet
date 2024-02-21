Mudlet/Mudlet

commit 8e80630c44038585cba0af989212c02f24f81e65

Infrastructure: add ccache to Appveyor (#6193)
#### Brief overview of PR changes/additions
Add ccache to Appveyor.
#### Motivation for adding to Mudlet
Faster than half hour builds for Windows, hopefully!
#### Other info (issues closed, discussion etc)
Ccache has [level B](https://ccache.dev/platform-compiler-language-support.html) support for Windows, but from testing so far that seems to be fine.

Co-authored-by: Stephen Lyons <slysven@virginmedia.com>

CI/appveyor.functions.ps1
@@ -445,6 +445,15 @@ function InstallLuaZip () {
   Copy-Item "zip.dll" "$Env:MINGW_BASE_DIR\lib\lua\5.1"
 }
 
+function InstallCcache() {
+    Step "installing ccache"
+    DownloadFile "https://github.com/ccache/ccache/releases/download/v4.6.1/ccache-4.6.1-windows-x86_64.zip" "ccache.zip"
+    ExtractZip "ccache.zip" "ccache"
+    Set-Location "ccache/ccache-4.6.1-windows-x86_64"
+    New-Item "C:\Program Files\ccache" -ItemType "directory" -Force
+    Copy-Item "ccache.exe" "C:\Program Files\ccache\ccache.exe" -Force
+}
+
 function InstallLuaModules(){
   CheckAndInstall "lfs" "$Env:MINGW_BASE_DIR\\lib\lua\5.1\lfs.dll" { InstallLfs }
   CheckAndInstall "luasql.sqlite3" "$Env:MINGW_BASE_DIR\\lib\lua\5.1\luasql\sqlite3.dll" { InstallLuasql }
@@ -454,6 +463,11 @@ function InstallLuaModules(){
   CheckAndInstall "luazip" "$Env:MINGW_BASE_DIR\\lib\lua\5.1\zip.dll" { InstallLuaZip }
   CheckAndInstall "argparse" "$Env:MINGW_BASE_DIR\\lib\lua\5.1\argparse" { InstallLuaArgparse }
   CheckAndInstall "lunajson" "$Env:MINGW_BASE_DIR\\lib\luarocks\rocks-5.1\lunajson" { InstallLuaLunajson }
+}
+
+function CheckAndInstallCcache(){
+  CheckAndInstall "ccache" "C:\Program Files\ccache\ccache.exe" { InstallCcache }
+
 }
 
 function CheckAndInstall7z(){

CI/appveyor.install.ps1
@@ -9,10 +9,10 @@ $global:ErrorActionPreference = "Stop"
 
 SetQtBaseDir "$logFile"
 
-$Env:PATH = "$CMakePath;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;C:\Program Files\7-Zip;$Env:PATH"
+$Env:PATH = "$CMakePath;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;C:\Program Files\7-Zip;C:\Program Files\ccache;$Env:PATH"
 
 # install dependencies
-
+CheckAndInstallCcache
 CheckAndInstall7z
 CheckAndInstallCmake
 CheckAndInstallMingwGet

.appveyor.yml
@@ -6,9 +6,17 @@ branches:
   - /^Mudlet-.+/   # build release tags (yes, the option also applies to tags)
 init:
 - cmd: mkdir C:\src\
+# enable to debug build process on start
+# - ps: iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 
 image: Visual Studio 2019
 environment:
+  global:
+    CCACHE_DIR: "%APPVEYOR_BUILD_FOLDER%\\.ccache"
+    # our appveyor cache size is 1G
+    CCACHE_MAXSIZE: 1G
+    # APP_BUILD embeds the git sha which changes on every commit, throwing the cache off - ignore it
+    CCACHE_IGNOREOPTIONS: "-DAPP_BUILD=*"
   signing_password:
     secure: JJDxNdreMgNn/IOcY+UVmlaqgDT4a7vcxsY3nfcgWY4=
   DEPLOY_KEY_PASS:
@@ -26,18 +34,32 @@ install:
 - cd "%APPVEYOR_BUILD_FOLDER%\CI"
 - powershell ".\appveyor.install.ps1"
 
-build_script:
+before_build:
+- set PATH=C:\Program Files\ccache;%PATH%
 - cd "%APPVEYOR_BUILD_FOLDER%\CI"
+- echo "ccache stats before building:"
+- ccache --zero-stats --show-stats --verbose
+
+build_script:
 - powershell ".\appveyor.build.ps1"
+
+after_build:
+- echo "ccache stats after building:"
+- ccache --show-stats --verbose
 
 on_failure:
 - cd C:\src
 - powershell "Push-AppveyorArtifact ./verbose_output.log"
 - echo "Build failed - see verbose_output.log artifact upload for more information."
 
+# enable to debug build process on completion
+#on_finish:
+#  - ps: $blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+
 cache:
   - '%MINGW_BASE_DIR% -> CI\appveyor.install.ps1, CI\appveyor.functions.ps1'
   - '%QT_BASE_DIR% -> CI\appveyor.install.ps1, CI\appveyor.functions.ps1'
+  - .ccache
 
 notifications:
 - provider: Webhook

CI/appveyor.build.ps1
@@ -10,7 +10,7 @@ SetLuarocksPath "C:\src\verbose_output.log"
 
 cd "$Env:APPVEYOR_BUILD_FOLDER\src"
 
-$Env:PATH="C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;$Env:QT_BASE_DIR\bin;$Env:MINGW_BASE_DIR\bin;" + (($Env:PATH.Split(';') | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';')
+$Env:PATH="C:\Program Files (x86)\CMake\bin;C:\Program Files\7-Zip;C:\Program Files\ccache;$Env:QT_BASE_DIR\bin;$Env:MINGW_BASE_DIR\bin;" + (($Env:PATH.Split(';') | Where-Object { $_ -ne 'C:\Program Files\Git\usr\bin' }) -join ';')
 # enable for a debug build (1/2)
 # qmake CONFIG+=release CONFIG+=force_debug_info mudlet.pro
 qmake CONFIG+=release mudlet.pro

src/mudlet.pro
@@ -366,12 +366,12 @@ macx {
 }
 
 # use ccache if available
-unix {
-    BASE_CXX = $$QMAKE_CXX
-    # common linux location
-    exists(/usr/bin/ccache):QMAKE_CXX = ccache $$BASE_CXX
-    # common macos location
-    exists(/usr/local/bin/ccache):QMAKE_CXX = ccache $$BASE_CXX
+BASE_CXX = $$QMAKE_CXX
+BASE_C = $$QMAKE_C
+# common linux location
+exists(/usr/bin/ccache)|exists(/usr/local/bin/ccache)|exists(C:/Program Files/ccache/ccache.exe) {
+    QMAKE_CXX = ccache $$BASE_CXX
+    QMAKE_C = ccache $$BASE_C
 }
 
 # There does not seem to be an obvious pkg-config option for this one, it is

commit fed5d75ce6a341689581e93d51c0fbabf7d2267f

Infrastructure: Update Powershell scripts (#7043)
#### Brief overview of PR changes/additions
- Install 7z before ccache, as this is needed to extract that
- Improve error reporting parameters

#### Motivation for adding to Mudlet
-
[setup-windows-sdk.ps1](https://github.com/Mudlet/Mudlet/blob/development/setup-windows-sdk.ps1)
fails otherwise with "file not found" error mentioning 7z:
```
.\setup-windows-sdk.ps1
workingBaseDir is C:\src\
Using C:\Qt\5.14.2\mingw73_32 as QT base directory.
==== compiling and installing ccache ====
---- installing ccache ----
---- Downloading ----
---- Extracting source distribution ----
Start-Process: C:\dev\GitHub\Mudlet\CI\appveyor.functions.ps1:79
Line |
  79 |  … exitCode = (Start-Process -FilePath $cmd -ArgumentList $parameter -Wa …
     |                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     | This command cannot be run due to the error: Das System kann die angegebene Datei nicht finden.
Exception: C:\dev\GitHub\Mudlet\CI\appveyor.functions.ps1:84
Line |
  84 |      throw $errorMessage
     |      ~~~~~~~~~~~~~~~~~~~
     | Error executing command: 7z
```

#### Other info (issues closed, discussion etc)
- Probably broken since #6193
- Bug however only appears if system does not have 7z installed prior

---------

Co-authored-by: Vadim Peretokin <vperetokin@hey.com>

CI/appveyor.functions.ps1
@@ -67,7 +67,7 @@ function script:exec {
   param(
     [Parameter(Position=0,Mandatory=1)][string]$cmd,
     [Parameter(Position=1,Mandatory=0)][string[]]$parameter = @(),
-    [Parameter(Position=2,Mandatory=0)][string]$errorMessage = ("Error executing command: {0}" -f $cmd)
+    [Parameter(Position=2,Mandatory=0)][string]$errorMessage = ("Error executing command '{0}' with {1} parameters: {2}" -f $cmd, $parameter.Length, ($parameter -join ', '))
   )
   # ignore standard error for external programs
   $global:ErrorActionPreference = "Continue"

CI/appveyor.install.ps1
@@ -12,8 +12,8 @@ SetQtBaseDir "$logFile"
 $Env:PATH = "$CMakePath;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;C:\Program Files\7-Zip;C:\Program Files\ccache;$Env:PATH"
 
 # install dependencies
-CheckAndInstallCcache
 CheckAndInstall7z
+CheckAndInstallCcache
 CheckAndInstallCmake
 CheckAndInstallMingwGet
 CheckAndInstallMsys