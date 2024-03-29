e224103c538165965ddf0f589e3326bb493442ce
Vadim Peretokin <vadim.peretokin@carasent.com>
2024-03-29 15:50:02 +0100

    Revert "Update src/TTextEdit.cpp"
    This reverts commit a2d28ee7598d2fac323402eef5ac51818395b485.

--- src/TTextEdit.cpp
+++ src/TTextEdit.cpp
@@ -1109,14 +1109,14 @@ void TTextEdit::expandSelectionToWords()
     if (yind >= 0 && yind < static_cast<int>(mpBuffer->lineBuffer.size())) {
         for (; xind >= 0; --xind) {
             // Ensure xind is within the valid range for the current line
-            if not (xind < static_cast<int>(mpBuffer->lineBuffer.at(yind).size())) {
+            if (xind < static_cast<int>(mpBuffer->lineBuffer.at(yind).size())) {
+                const QChar currentChar = mpBuffer->lineBuffer.at(yind).at(xind);
+                if (currentChar == QChar::Space
+                    || mpHost->mDoubleClickIgnore.contains(currentChar)) {
+                    break;
+                }
+            } else {
                 break; // xind is out of bounds, break the loop
-            }
-            
-            const QChar currentChar = mpBuffer->lineBuffer.at(yind).at(xind);
-            if (currentChar == QChar::Space
-                || mpHost->mDoubleClickIgnore.contains(currentChar)) {
-                break;
             }
         }
     }
