66847210d8666f7204137dcba1616bb5b653bdaa
Stephen Lyons <slysven@virginmedia.com>
2024-04-19 00:52:40 +0100

    Fix: add needed argument to autoreconf call
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- CI/appveyor.functions.ps1
+++ CI/appveyor.functions.ps1
@@ -133,7 +133,7 @@ function ExtractZip([string] $zipFile, [string] $outputPath) {
 
 function RunAutoReconfig(){
   Step "Running autoreconf"
-  exec "bash" @("-c", "`"/c/msys64/usr/bin/autoreconf -i`"")
+  exec "bash" @("-c", "`"/c/msys64/usr/bin/autoreconf -f -i -v`"")
 }
 
 function RunConfigure([string] $configureArguments = "--prefix=$Env:MINGW_BASE_DIR_BASH") {
