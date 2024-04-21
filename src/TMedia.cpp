5a8647fa151d34902afdeb792021a1e93b67eb17
Michael Conley <sousesider@gmail.com>
2024-04-21 11:20:15 -0400

    Remove include

--- src/TMedia.cpp
+++ src/TMedia.cpp
@@ -28,9 +28,6 @@
 #include <QFileInfo>
 #include <QJsonDocument>
 #include <QJsonObject>
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-#include <QMediaPlaylist>
-#endif
 #include <QNetworkDiskCache>
 #include <QRandomGenerator>
 #include <QStandardPaths>
