a110c1334edaf395b4fa99fb03b12530bb99d101
Vadim Peretokin <vperetokin@hey.com>
2024-05-09 08:22:57 +0200

    Install general ccache instead
    Per https://packages.msys2.org/package/ccache?repo=msys&variant=x86_64 it should be available in /usr/bin automatically.

--- CI/setup-windows-sdk.sh
+++ CI/setup-windows-sdk.sh
@@ -115,6 +115,7 @@ fi
     git \
     man \
     rsync \
+    ccache \
     "mingw-w64-${BUILDCOMPONENT}-toolchain" \
     "mingw-w64-${BUILDCOMPONENT}-pcre" \
     "mingw-w64-${BUILDCOMPONENT}-libzip" \
@@ -128,8 +129,7 @@ fi
     "mingw-w64-${BUILDCOMPONENT}-boost" \
     "mingw-w64-${BUILDCOMPONENT}-yajl" \
     "mingw-w64-${BUILDCOMPONENT}-lua-luarocks" \
-    "mingw-w64-${BUILDCOMPONENT}-7zip" \
-    "mingw-w64-${BUILDCOMPONENT}-ccache" 
+    "mingw-w64-${BUILDCOMPONENT}-7zip" 
 
 echo ""
 echo "    Completed"
