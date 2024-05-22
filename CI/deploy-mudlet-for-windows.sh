78b789cc4387cad01c35037788ae08bf300a9913
John McKisson <john.mckisson@gmail.com>
2024-05-22 16:19:55 -0400

    file_cat should be 0 for Windows per the appveyor script

--- CI/deploy-mudlet-for-windows.sh
+++ CI/deploy-mudlet-for-windows.sh
@@ -290,14 +290,14 @@ else
 
     SHA256SUM=$(shasum -a 256 "$setupExePath" | awk '{print $1}')
 
-    # file_cat=3 asuming Windows is the 3rd item in WP-Download-Manager category
+    # file_cat=0 asuming Windows is the 0th item in WP-Download-Manager category
     curl -X POST 'https://www.mudlet.org/wp-content/plugins/wp-downloadmanager/download-add.php' \
     -H "x-wp-download-token: $X_WP_DOWNLOAD_TOKEN" \
     -F "file_type=2" \
     -F "file_remote=$DEPLOY_URL" \
     -F "file_name=Mudlet-${VERSION} (windows-$BUILD_BITNESS)" \
     -F "file_des=sha256: $SHA256SUM" \
-    -F "file_cat=3" \
+    -F "file_cat=0" \
     -F "file_permission=-1" \
     -F "output=json" \
     -F "do=Add File"
