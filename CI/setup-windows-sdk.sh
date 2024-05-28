30f73f2685dd913870a5127622248a92028f7b95
John McKisson <john.mckisson@gmail.com>
2024-05-28 08:44:52 -0400

    Infrastructure: Fix ccache for Windows msys2 builds (#7249)
    <!-- Keep the title short & concise so anyone non-technical can
    understand it,
         the title appears in PTB changelogs -->
    #### Brief overview of PR changes/additions
    
    The mudlet.pro checks for existence of ccache binary in a few places
    before setting the makefile to use ccache, this checks for ccache in the
    mingw32/mingw64 bin directories where it should be installed by pacman.
    
    Also changed the CCACHE_DIR definition to use a unix path just in case.
    
    #### Motivation for adding to Mudlet
    Fixes https://github.com/Mudlet/Mudlet/issues/7243
    #### Other info (issues closed, discussion etc)

--- CI/setup-windows-sdk.sh
+++ CI/setup-windows-sdk.sh
@@ -148,7 +148,7 @@ while true; do
     git \
     man \
     rsync \
-    ccache \
+    "mingw-w64-${BUILDCOMPONENT}-ccache" \
     "mingw-w64-${BUILDCOMPONENT}-toolchain" \
     "mingw-w64-${BUILDCOMPONENT}-pcre" \
     "mingw-w64-${BUILDCOMPONENT}-libzip" \
