79250416389242cba762e5cf7d75721dc6587ef3
Vadim Peretokin <vperetokin@hey.com>
2024-07-26 09:03:39 +0200

    Fix: editor not showing any items (#7359)
    <!-- Keep the title short & concise so anyone non-technical can
    understand it,
         the title appears in PTB changelogs -->
    #### Brief overview of PR changes/additions
    Fix the editor not showing any items on launch.
    #### Motivation for adding to Mudlet
    Bugfix, regression after https://github.com/Mudlet/Mudlet/pull/7337.
    #### Other info (issues closed, discussion etc)
    Missing Rainforest QA tests that tested the editor visually for us here,
    they would have caught this.
    
    Closes https://github.com/Mudlet/Mudlet/issues/7358.

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -1496,6 +1496,7 @@ void mudlet::addConsoleForNewHost(Host* pH)
     pH->mpEditorDialog = pEditor;
     connect(pH, &Host::profileSaveStarted,  pH->mpEditorDialog, &dlgTriggerEditor::slot_profileSaveStarted);
     connect(pH, &Host::profileSaveFinished,  pH->mpEditorDialog, &dlgTriggerEditor::slot_profileSaveFinished);
+    pEditor->fillout_form();
 
     pH->getActionUnit()->updateToolbar();
 
