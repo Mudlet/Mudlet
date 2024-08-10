bbcb87eafed07a955c00af1f6bb6b8f9adfb29e9
John McKisson <john.mckisson@gmail.com>
2024-08-10 12:17:47 -0400

    move allocator print to mudletg dtor

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -102,6 +102,7 @@ namespace coreMacOS {
 
 // PLACEMARKER: sample benchmarking code
 // #include <nanobench.h>
+#include <jemalloc/jemalloc.h>
 #include "post_guard.h"
 
 using namespace std::chrono_literals;
@@ -3093,6 +3094,9 @@ mudlet::~mudlet()
         }
     }
     mudlet::smpSelf = nullptr;
+    
+    // Dump allocator statistics to stderr.
+    je_malloc_stats_print(NULL, NULL, NULL);
 }
 
 void mudlet::slot_toggleFullScreenView()
