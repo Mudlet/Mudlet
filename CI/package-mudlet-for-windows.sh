df1dd3efd5c6db189ac5e93335869ff0514a7cbe
j30 <46060104+joaquin30@users.noreply.github.com>
2024-08-12 18:05:47 -0500

    removing tabs in package-mudlet-for-windows.sh

--- CI/package-mudlet-for-windows.sh
+++ CI/package-mudlet-for-windows.sh
@@ -119,16 +119,16 @@ ZIP_FILE_NAME="Mudlet-${MSYSTEM}"
 
 # Making sure that the WMF backend DLL is being copied
 if [ "${BUILD_BITNESS}" = "64" ]; then
-	if [ ! -f "${MINGW_INTERNAL_BASE_DIR}/share/qt6/plugins/multimedia/windowsmediaplugin.dll" ]; then
-	  echo "ERROR: windowsmediaplugin.dll not found - Mudlet will no have sound in windows"
-	  echo "Install package mingw-w64-x86_64-qt6-multimedia-wmf to solve"
-	  exit 7
-	fi
+    if [ ! -f "${MINGW_INTERNAL_BASE_DIR}/share/qt6/plugins/multimedia/windowsmediaplugin.dll" ]; then
+      echo "ERROR: windowsmediaplugin.dll not found - Mudlet will no have sound in windows"
+      echo "Install package mingw-w64-x86_64-qt6-multimedia-wmf to solve"
+      exit 7
+    fi
 
-	mkdir -p "./multimedia/"
-	cp -v -p -t "./multimedia/" \
-		"${MINGW_INTERNAL_BASE_DIR}/share/qt6/plugins/multimedia/windowsmediaplugin.dll"
-	echo "Copied ${MINGW_INTERNAL_BASE_DIR}/share/qt6/plugins/multimedia/windowsmediaplugin.dll"
+    mkdir -p "./multimedia/"
+    cp -v -p -t "./multimedia/" \
+        "${MINGW_INTERNAL_BASE_DIR}/share/qt6/plugins/multimedia/windowsmediaplugin.dll"
+    echo "Copied ${MINGW_INTERNAL_BASE_DIR}/share/qt6/plugins/multimedia/windowsmediaplugin.dll"
 fi
 
 # To determine which system libraries have to be copied in it requires
