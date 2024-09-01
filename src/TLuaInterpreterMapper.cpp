Mudlet/Mudlet

commit 44bf5fea087a5f93bce5792797d5e1eb2a74739d

Improve: speed up bulk room creation (#7184)
<!-- Keep the title short & concise so anyone non-technical can
understand it,
     the title appears in PTB changelogs -->
#### Brief overview of PR changes/additions
Speed up bulk room creation so people can make million-map rooms
quickly:

* addRoom now accepts the area to place the room in right away
* setRoomArea accepts an optional table of rooms instead of one room to
work in bulk
* setRoomArea will no longer update the mapper view automatically - but
this is fixed by https://github.com/Mudlet/Mudlet/pull/7187
#### Motivation for adding to Mudlet

https://discord.com/channels/283581582550237184/1215109133150199889/1217359192835358741
#### Other info (issues closed, discussion etc)
Somewhere along the line we forgot that
https://wiki.mudlet.org/w/Manual:Mapper_Functions#updateMap needs to be
called from scripts to update the map for this very reason - performance

src/TLuaInterpreterMapper.cpp
@@ -579,7 +579,12 @@ int TLuaInterpreter::addRoom(lua_State* L)
     const bool added = host.mpMap->addRoom(id);
     lua_pushboolean(L, added);
     if (added) {
-        host.mpMap->setRoomArea(id, -1, false);
+        int areaID = -1;
+        if (lua_gettop(L) >= 2) {
+            areaID = getVerifiedInt(L, __func__, 2, "areaID");
+        }
+        // defer area calculations as all new rooms are initialised at 0,0,0 anyway
+        host.mpMap->setRoomArea(id, areaID, true);
         host.mpMap->setUnsaved(__func__);
         host.mpMap->update();
         host.mpMap->mMapGraphNeedsUpdate = true;
@@ -3476,12 +3481,32 @@ int TLuaInterpreter::setRoomArea(lua_State* L)
         return warnArgumentValue(L, __func__, "no map present or loaded");
     }
 
-    const int id = getVerifiedInt(L, __func__, 1, "roomID");
-    if (!host.mpMap->mpRoomDB->getRoomIDList().contains(id)) {
-        return warnArgumentValue(L, __func__, csmInvalidRoomID.arg(id));
+    QVector<int> roomIds;
+    if (lua_isnumber(L, 1)) {
+        const int id = getVerifiedInt(L, __func__, 1, "roomID");
+        if (!host.mpMap->mpRoomDB->getRoomIDList().contains(id)) {
+            return warnArgumentValue(L, __func__, csmInvalidRoomID.arg(id));
+        }
+        roomIds.append(id);
+    } else if (lua_istable(L, 1)) {
+        lua_pushnil(L);
+        while (lua_next(L, 1) != 0) {
+            const int id = getVerifiedInt(L, __func__, -1, "roomID");
+            if (!host.mpMap->mpRoomDB->getRoomIDList().contains(id)) {
+                return warnArgumentValue(L, __func__, csmInvalidRoomID.arg(id));
+            }
+            roomIds.append(id);
+            lua_pop(L, 1);
+        }
+    } else {
+        lua_pushfstring(L,
+                        "setRoomArea: bad argument #1 type (roomID as number or table of roomIDs\n"
+                        "expected, got %s!)",
+                        luaL_typename(L, 1));
+        return lua_error(L);
     }
 
-    int areaId;
+    int areaId = -1;
     QString areaName;
     if (lua_isnumber(L, 2)) {
         areaId = lua_tonumber(L, 2);
@@ -3511,9 +3536,11 @@ int TLuaInterpreter::setRoomArea(lua_State* L)
         return lua_error(L);
     }
 
-    // Can set the room to an area which does not have a TArea instance but does
-    // appear in the TRoomDB::areaNamesMap...
-    const bool result = host.mpMap->setRoomArea(id, areaId, false);
+    const bool result = std::all_of(roomIds.begin(), roomIds.end(), [&](int id) {
+        // defer area recalculation on all rooms until the last room (.back())
+        return host.mpMap->setRoomArea(id, areaId, id != roomIds.back());
+    });
+
     if (result) {
         host.mpMap->update();
     }

src/TArea.cpp
@@ -471,7 +471,7 @@ void TArea::calcSpan()
 // bool TRoom::setArea( int, bool )  -- the second arg there can be used for this
 // bool TRoomDB::__removeRoom( int ) -- automatically skipped for area deletion
 //                                      (when this would not be needed)
-void TArea::removeRoom(int room, bool isToDeferAreaRelatedRecalculations)
+void TArea::removeRoom(int room, bool deferAreaRecalculations)
 {
     static double cumulativeMean = 0.0;
     static quint64 runCount = 0;
@@ -480,7 +480,7 @@ void TArea::removeRoom(int room, bool isToDeferAreaRelatedRecalculations)
 
     // Will use to flag whether some things have to be recalculated.
     bool isOnExtreme = false;
-    if (rooms.contains(room) && !isToDeferAreaRelatedRecalculations) {
+    if (rooms.contains(room) && !deferAreaRecalculations) {
         // just a check, if the area DOESN'T have the room then it is not wise
         // to behave as if it did
         TRoom* pR = mpRoomDB->getRoom(room);

src/TArea.h
@@ -57,7 +57,7 @@ public:
     void fast_calcSpan(int);
     void determineAreaExits();
     void determineAreaExitsOfRoom(int);
-    void removeRoom(int, bool isToDeferAreaRelatedRecalculations = false);
+    void removeRoom(int, bool deferAreaRecalculations = false);
     QList<int> getCollisionNodes();
     QList<int> getRoomsByPosition(int x, int y, int z);
     QMap<int, QMap<int, QMultiMap<int, int>>> koordinatenSystem();

src/TMap.cpp
@@ -129,7 +129,7 @@ void TMap::logError(QString& msg)
 //    mpHost->mLuaInterpreter.compileAndExecuteScript( script );
 //}
 
-bool TMap::setRoomArea(int id, int area, bool isToDeferAreaRelatedRecalculations)
+bool TMap::setRoomArea(int id, int area, bool deferAreaRecalculations)
 {
     TRoom* pR = mpRoomDB->getRoom(id);
     if (!pR) {
@@ -155,7 +155,7 @@ bool TMap::setRoomArea(int id, int area, bool isToDeferAreaRelatedRecalculations
         // to retain the API for the lua subsystem...
     }
 
-    const bool result = pR->setArea(area, isToDeferAreaRelatedRecalculations);
+    const bool result = pR->setArea(area, deferAreaRecalculations);
     if (result) {
         mMapGraphNeedsUpdate = true;
         setUnsaved(__func__);

src/TMap.h
@@ -111,7 +111,7 @@ public:
                        std::optional<QString> fontName = std::nullopt);
     void deleteMapLabel(int area, int labelID);
     bool addRoom(int id = 0);
-    bool setRoomArea(int id, int area, bool isToDeferAreaRelatedRecalculations = false);
+    bool setRoomArea(int id, int area, bool deferAreaRecalculations = false);
     void deleteArea(int id);
     int createNewRoomID(int minimumId = 1);
     void logError(QString& msg);

src/TRoom.cpp
@@ -297,7 +297,7 @@ void TRoom::setId(const int roomId)
 // There IS a theoretical risk that if the last called room "doesn't exist" then
 // the area related recalculations won't get done - so had better provide an
 // alternative means to do them as a fault recovery
-bool TRoom::setArea(int areaID, bool isToDeferAreaRelatedRecalculations)
+bool TRoom::setArea(int areaID, bool deferAreaRecalculations)
 {
     static QSet<TArea*> dirtyAreas;
     TArea* pA = mpRoomDB->getArea(areaID);
@@ -316,7 +316,7 @@ bool TRoom::setArea(int areaID, bool isToDeferAreaRelatedRecalculations)
     //remove from the old area
     TArea* pA2 = mpRoomDB->getArea(area);
     if (pA2) {
-        pA2->removeRoom(id, isToDeferAreaRelatedRecalculations);
+        pA2->removeRoom(id, deferAreaRecalculations);
         // Ah, all rooms in the OLD area that led to the room now become area
         // exits for that OLD area {so must run determineAreaExits() for the
         // old area after the room has moved to the new area see other
@@ -339,7 +339,7 @@ bool TRoom::setArea(int areaID, bool isToDeferAreaRelatedRecalculations)
     dirtyAreas.insert(pA);
     pA->mIsDirty = true;
 
-    if (!isToDeferAreaRelatedRecalculations) {
+    if (!deferAreaRecalculations) {
         QSetIterator<TArea*> itpArea = dirtyAreas;
         while (itpArea.hasNext()) {
             TArea* pArea = itpArea.next();

src/TRoom.h
@@ -73,7 +73,7 @@ public:
     bool hasExitStub(int direction);
     void setExitStub(int direction, bool status);
     void calcRoomDimensions();
-    bool setArea(int, bool isToDeferAreaRelatedRecalculations = false);
+    bool setArea(int, bool deferAreaRecalculations = false);
     int getExitWeight(const QString& cmd);
 
     int getWeight() const { return weight; }

src/TRoomDB.cpp
@@ -65,13 +65,12 @@ bool TRoomDB::addRoom(int id)
         rooms[id]->setId(id);
         // there is no point in updating the entranceMap here, as the room has no exit information
         return true;
-    } else {
-        if (id <= 0) {
-            QString error = qsl("addRoom: illegal room id=%1. roomID must be > 0").arg(id);
-            mpMap->logError(error);
-        }
-        return false;
     }
+    if (id <= 0) {
+        QString error = qsl("addRoom: illegal room id=%1. roomID must be > 0").arg(id);
+        mpMap->logError(error);
+    }
+    return false;
 }
 
 bool TRoomDB::addRoom(int id, TRoom* pR, bool isMapLoading)
@@ -81,9 +80,8 @@ bool TRoomDB::addRoom(int id, TRoom* pR, bool isMapLoading)
         pR->setId(id);
         updateEntranceMap(pR, isMapLoading);
         return true;
-    } else {
-        return false;
     }
+    return false;
 }
 
 void TRoomDB::deleteValuesFromEntranceMap(int value)

commit 8097a47160ff7eb4010d145a28dc5a111781e711

Fix: allow revised addRoom(...) to work without an area as second argument
#### Brief overview of PR changes/additions
Moves a line of code that was clobbering the Lua <==> C argument passing
"stack" (it was outputting a boolean value and overwriting a value that
was needed as an input for code later in the function) so that it now acts
after the inputting parts are done.

#### Motivation for adding to Mudlet
To fix a defect introduced by #7184 which meant that not providing a second
argument to the revised `addRoom(...)` function meant it read a `true`
value that it had itself placed into the argument stack.

#### Other info (issues closed, discussion etc)
Also fixed an more minor issue where providing an invalid area still seemed
to work correctly in that a `true` value was returned to the caller. The
room was and now still is created but it now causes a nil+error message
to be returned. The room would instead be placed in the "Default Area" -
HOWEVER deferring the recalculations for that area seems to hide the room
in the map - it becomes a "ghost" that does have the right coordinates and
area number (as revealed by `getRoomCoordinates(...)` and `getRoomArea(...)`
but it does not show in the mapper until the map is saved and reloaded (or
the Lua `auditAreas()` function is used - which happens during a map load).

Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

src/TLuaInterpreterMapper.cpp
@@ -577,18 +577,53 @@ int TLuaInterpreter::addRoom(lua_State* L)
     const int id = getVerifiedInt(L, __func__, 1, "roomID");
     const Host& host = getHostFromLua(L);
     const bool added = host.mpMap->addRoom(id);
-    lua_pushboolean(L, added);
     if (added) {
         int areaID = -1;
-        if (lua_gettop(L) >= 2) {
+        if (lua_gettop(L) > 1) {
             areaID = getVerifiedInt(L, __func__, 2, "areaID");
         }
         // defer area calculations as all new rooms are initialised at 0,0,0 anyway
-        host.mpMap->setRoomArea(id, areaID, true);
-        host.mpMap->setUnsaved(__func__);
+        bool setArea = false;
+        if (areaID != -1) {
+            // The TMap::addRoom(...) call will have put the room in the -1 area
+            if (host.mpMap->setRoomArea(id, areaID, true)) {
+                setArea = true;
+            } else {
+                // However setRoomArea(...) fails when the area does not exist
+                // so force the area to -1 as a failback - though this happens
+                // automatically merely by creating the room we need to call
+                // the setRoomArea(...) WITHOUT deferring the area
+                // recalculations otherwise the room becomes a ghost that is not
+                // revealed without reloading the map or running the auditArea()
+                // Lua API function:
+                areaID = -1;
+            }
+
+        } else {
+            // setting the Area to the default area always works but we need to
+            // set the flag that says we managed it to avoid returning the nil +
+            // error message in the other case where we failed to set another
+            // area:
+            setArea = true;
+        }
+
+        if (!setArea) {
+            lua_pushnil(L);
+            lua_pushfstring(L, "addRoom: created roomID %d but failed to place it in areaID %d, does that area actually exist?", id, areaID);
+            return 2;
+        }
+
+        // in testing it was found that deferring the recalcualation for
+        // the default area ended up hiding the rooms even if (as it is by
+        // default) the default area is viewable - they only became visible
+        // after saving and reloading the map or by running auditAreas()
+        host.mpMap->setRoomArea(id, areaID, false);
+
+        // a previous version had calls to functions that would be done anyway
+        // so they do not need repeating here
         host.mpMap->update();
-        host.mpMap->mMapGraphNeedsUpdate = true;
     }
+    lua_pushboolean(L, added);
     return 1;
 }
 