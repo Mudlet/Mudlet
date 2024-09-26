f567e771e7a5577dd5c6bd2ead330d054afb1939
jmckisson <john.mckisson@gmail.com>
2024-09-26 14:53:17 -0400

    add bootstrap script entry

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -4877,6 +4877,7 @@ void mudlet::setupPreInstallPackages(const QString& gameUrl)
                                                       qsl("imperian.com"),
                                                       qsl("starmourn.com"),
                                                       qsl("stickmud.com")}},
+        {qsl(":/MedBootstrap.xml"),       {qsl("medievia.com")}}
         // clang-format on
     };
 
