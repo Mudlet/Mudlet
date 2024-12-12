99e20151ecd68b9b5ebf8d72a550c1ebea03ec09
Vadim Peretokin <vperetokin@hey.com>
2024-12-12 13:58:55 +0100

    Update src/dlgTriggerEditor.cpp
    Co-authored-by: Stephen Lyons <slysven@virginmedia.com>

--- src/dlgTriggerEditor.cpp
+++ src/dlgTriggerEditor.cpp
@@ -2861,7 +2861,7 @@ void dlgTriggerEditor::activeToggle_trigger()
 
     if (!pT->state()) {
         pT->setIsActive(false);
-        showError(tr(R"(<p>Unable to activate "<tt>%1</tt>"; %2</p>
+        showError(tr(R"(<p>Unable to activate "<tt>%1</tt>": %2</p>
                      <p><i>You will need to reactivate this after the problem has been corrected.</i></p>)").arg(pT->getName().toHtmlEscaped(), pT->getError()));
         icon.addPixmap(QPixmap(qsl(":/icons/tools-report-bug.png")), QIcon::Normal, QIcon::Off);
         itemDescription = descError;
