40c6f7d2b360aa4ad1c885091cfd79ac9fe122e2
Vadim Peretokin <vadi2@users.noreply.github.com>
2024-12-13 07:08:47 +0100

    Fix: set Mudlet version build to public test build

--- CI/build-mudlet-for-windows.sh
+++ CI/build-mudlet-for-windows.sh
@@ -70,6 +70,9 @@ if [[ "$GITHUB_REPO_TAG" == "false" ]]; then
     MUDLET_VERSION_BUILD="-testing"
   fi
 
+  # REMOVEME
+  MUDLET_VERSION_BUILD="-ptb"
+
   # Check if this is a pull request
   if [[ -n "$GITHUB_PULL_REQUEST_NUMBER" ]]; then
     # Use the specific commit SHA from the pull request head, since GitHub Actions merges the PR
