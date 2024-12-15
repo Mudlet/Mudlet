5afee6e69b4e570227e1d22c73578287d504d869
Vadim Peretokin <vperetokin@hey.com>
2024-12-15 20:02:16 +0100

    Reset both values together

--- src/XMLimport.cpp
+++ src/XMLimport.cpp
@@ -785,11 +785,9 @@ void XMLimport::readHost(Host* pHost)
     pHost->setUserDictionaryOptions(enableUserDictionary, useSharedDictionary);
     pHost->mMapperShowRoomBorders = readDefaultTrueBool(qsl("mMapperShowRoomBorders"));
     pHost->mEditorTheme = attributes().value(QLatin1String("mEditorTheme")).toString();
-    if (pHost->mEditorTheme.isEmpty()) {
-        pHost->mEditorTheme = qsl("Mudlet");
-    }
     pHost->mEditorThemeFile = attributes().value(QLatin1String("mEditorThemeFile")).toString();
-    if (pHost->mEditorThemeFile.isEmpty()) {
+    if (pHost->mEditorTheme.isEmpty() || pHost->mEditorThemeFile.isEmpty()) {
+        pHost->mEditorTheme = qsl("Mudlet");
         pHost->mEditorThemeFile = qsl("Mudlet.tmTheme");
     }
     pHost->mThemePreviewItemID = attributes().value(QLatin1String("mThemePreviewItemID")).toInt();
