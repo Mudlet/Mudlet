71cb1dccf4f82022e7f99892467b47424a92cbc7
ZookaOnGit <136661366+ZookaOnGit@users.noreply.github.com>
2024-12-19 09:40:55 +0400

    remove all formatting, just lower the vertical alignment

--- src/dlgTriggerEditor.cpp
+++ src/dlgTriggerEditor.cpp
@@ -870,11 +870,13 @@ dlgTriggerEditor::dlgTriggerEditor(Host* pH)
         // Populate default of false
         lineEditShouldMarkSpaces[pItem->singleLineTextEdit_pattern] = false;
 
+/*
         auto mpFont = mpHost->getDisplayFont();
         mpFont.setPixelSize(pItem->singleLineTextEdit_pattern->height() / 2);
         pItem->singleLineTextEdit_pattern->setFont(mpFont);
+*/
         // align a fraction lower
-        pItem->singleLineTextEdit_pattern->setStyleSheet("SingleLineTextEdit { padding-top: 2px; }");
+        pItem->singleLineTextEdit_pattern->setStyleSheet("SingleLineTextEdit { padding-top: 3px; }");
 
         if (i == 0) {
             pItem->singleLineTextEdit_pattern->setPlaceholderText(tr("Text to find (trigger pattern)"));
