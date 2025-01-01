01fe227b8eaa9dd6961168d94ecc4c3f2acf5f59
Stephen Lyons <slysven@virginmedia.com>
2025-01-01 18:28:19 +0000

    Infrastructure: update date stamps for 2025
    #### Brief overview of PR changes/additions
    Updates Copyright lines of text where they are presented to the end-user.
    
    #### Motivation for adding to Mudlet
    Its 2025 so Happy New Mudlet.
    
    #### Other info (issues closed, discussion etc)
    This is a companion to the same name PR https://github.com/Mudlet/installers/pull/122
    in the Mudlet installers repository.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/dlgAboutDialog.cpp
+++ src/dlgAboutDialog.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2008-2009 by Heiko Koehn - KoehnHeiko@googlemail.com    *
- *   Copyright (C) 2013-2014, 2017-2019, 2022, 2024 by Stephen Lyons       *
+ *   Copyright (C) 2013-2014, 2017-2019, 2022, 2024-2025 by Stephen Lyons  *
  *                                               - slysven@virginmedia.com *
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
  *                                                                         *
@@ -87,7 +87,7 @@ dlgAboutDialog::dlgAboutDialog(QWidget* parent)
 
         // Repeat for other text, but we know it will fit at given size
         // PLACEMARKER: Date-stamp needing annual update
-        QString sourceCopyrightText = qsl("©️ Mudlet makers 2008-2024");
+        QString sourceCopyrightText = qsl("©️ Mudlet makers 2008-2025");
         QFont font(qsl("Bitstream Vera Serif"), 16, QFont::Bold | QFont::Serif | QFont::PreferMatch | QFont::PreferAntialias);
         QTextLayout copyrightTextLayout(sourceCopyrightText, font, painter.device());
         copyrightTextLayout.beginLayout();
