67353e9096280031b456bfa098fddf7d91aae4f5
missionz3r0 <github_xu8w@missionz3r0.com>
2025-05-22 10:41:09 -0400

    remove duplicate declarations of string.starts and string.ends from DB.lua

--- src/mudlet-lua/lua/DB.lua
+++ src/mudlet-lua/lua/DB.lua
@@ -3,18 +3,6 @@
 ----------------------------------------------------------------------------------
 
 
--- TODO those functions are already definde elsewhere
-function string.starts(String, Start)
-  return string.sub(String, 1, string.len(Start)) == Start
-end
-
-function string.ends(String, End)
-  return End == '' or string.sub(String, -string.len(End)) == End
-end
-
-
-
-
 -- TODO move to StringUtils?
 -----------------------------------------------------------------------------
 -- Some Date / Time parsing functions.
