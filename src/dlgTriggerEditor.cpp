fb2d70759df3d9360b4a3ec9fcb743ad3082cb00
Vadim Peretokin <vperetokin@hey.com>
2025-08-17 10:16:14 +0200

    Fix: save any pending edits before saving profile to disk to prevent data loss (#8017)
    <!-- Keep the title short & concise so anyone non-technical can
    understand it,
         the title appears in PTB changelogs -->
    #### Brief overview of PR changes/additions
    Save any pending edits before saving profile to disk to prevent data
    loss
    #### Motivation for adding to Mudlet
    Fix https://github.com/Mudlet/Mudlet/issues/8014
    #### Other info (issues closed, discussion etc)
    
    Co-authored-by: Vadim Peretokin <vadi2@users.noreply.github.com>
    Co-authored-by: Zooka <136661366+ZookaOnGit@users.noreply.github.com>

--- src/dlgTriggerEditor.cpp
+++ src/dlgTriggerEditor.cpp
@@ -9578,6 +9578,8 @@ void dlgTriggerEditor::runScheduledCleanReset()
 
 void dlgTriggerEditor::slot_profileSaveAction()
 {
+    slot_saveEdits();
+    
     auto [ok, filename, error] = mpHost->saveProfile(nullptr, nullptr, true);
 
     if (!ok) {
@@ -9604,7 +9606,8 @@ void dlgTriggerEditor::slot_profileSaveAsAction()
     if (!fileName.endsWith(qsl(".xml"), Qt::CaseInsensitive) && !fileName.endsWith(qsl(".trigger"), Qt::CaseInsensitive)) {
         fileName.append(qsl(".xml"));
     }
-
+    slot_saveEdits();
+    
     mpHost->saveProfileAs(fileName);
     mSavingAs = false;
 }
