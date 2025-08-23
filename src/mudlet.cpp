9b47f2aa7f473f1be6371cb736fa712994cba1f1
Vadim Peretokin <vadi2@users.noreply.github.com>
2025-08-23 18:05:07 +0200

    Align 2D map zoom in/out with other applications
    Add an optional toggle to revert to old behaviour if desired

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -2652,6 +2652,7 @@ void mudlet::readLateSettings(const QSettings& settings)
     mEditorTextOptions = static_cast<QTextOption::Flags>(settings.value("editorTextOptions", QVariant(0)).toInt());
 
     mShowMapAuditErrors = settings.value("reportMapIssuesToConsole", QVariant(false)).toBool();
+    mInvertMapZoom = settings.value("invertMapZoom", QVariant(false)).toBool(); // Default to false for modern (non-inverted) behavior
     mStorePasswordsSecurely = settings.value("storePasswordsSecurely", QVariant(true)).toBool();
     mShowTabConnectionIndicators = settings.value("showTabConnectionIndicators", QVariant(false)).toBool();
 
@@ -2825,6 +2826,7 @@ void mudlet::writeSettings()
     settings.setValue("fullScreen", static_cast<bool>(windowState() & Qt::WindowFullScreen));
     settings.setValue("editorTextOptions", static_cast<int>(mEditorTextOptions));
     settings.setValue("reportMapIssuesToConsole", mShowMapAuditErrors);
+    settings.setValue("invertMapZoom", mInvertMapZoom);
     settings.setValue("storePasswordsSecurely", mStorePasswordsSecurely);
     settings.setValue("showTabConnectionIndicators", mShowTabConnectionIndicators);
     settings.setValue("showIconsInMenus", mShowIconsOnMenuCheckedState);
@@ -5307,6 +5309,13 @@ void mudlet::setShowMapAuditErrors(const bool state)
         mShowMapAuditErrors = state;
 
         emit signal_showMapAuditErrorsChanged(state);
+    }
+}
+
+void mudlet::setInvertMapZoom(const bool state)
+{
+    if (mInvertMapZoom != state) {
+        mInvertMapZoom = state;
     }
 }
 
