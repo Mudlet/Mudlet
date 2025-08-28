ecedc0c945650d586704e021896fec8c0b3ffaa0
Vadim Peretokin <vperetokin@hey.com>
2025-08-28 21:39:49 +0200

    Update src/TDetachedWindow.cpp
    Co-authored-by: Stephen Lyons <slysven@virginmedia.com>

--- src/TDetachedWindow.cpp
+++ src/TDetachedWindow.cpp
@@ -967,7 +967,6 @@ void TDetachedWindow::createToolBar()
 
 
     mpButtonDiscord->addAction(mpActionDiscord);
-    mpButtonDiscord->setDefaultAction(mpActionDiscord);
 
     // Map and other tools
     mpActionMapper = new QAction(QIcon(qsl(":/icons/applications-internet.png")), tr("Map"), this);
