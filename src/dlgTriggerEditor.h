2ff824443ca2f7b9d1668de724fa9dc5cd48b3d1
Sahelisaha04 <sahasaheli04@gmail.com>
2025-09-03 15:08:24 +0530

    Revert "Fix build error: Add missing Qt widget includes"
    This reverts commit 4da28e1d40fc8e1594445513a913aaddff2fbe62.

--- src/dlgTriggerEditor.h
+++ src/dlgTriggerEditor.h
@@ -304,7 +304,6 @@ public slots:
     void slot_redo();
     void slot_updateUndoRedoActions();
     void configureTextWidgetUndoBehavior();
-    void configureUIElementUndoBehavior();
 
 private slots:
     void slot_changeEditorTextOptions(QTextOption::Flags);
