bf083c33cc9349426ccec1433af6e6679a1e8c15
Vadim Peretokin <vadi2@users.noreply.github.com>
2025-09-05 13:25:37 +0200

    Fix crash importing packages

--- src/LuaInterface.cpp
+++ src/LuaInterface.cpp
@@ -140,6 +140,21 @@ bool LuaInterface::loadValue(lua_State* L, TVar* var, int index)
             //everything is tabled in lua, we need to just find what table
             //we're using, if index == 0, we iterate to the closest table
             if (index) {
+                // Validate stack before attempting table access
+                const int stackTop = lua_gettop(L);
+                const int actualIndex = (index < 0) ? stackTop + index + 1 : index;
+                
+                if (actualIndex <= 0 || actualIndex > stackTop) {
+                    qWarning() << "LuaInterface::loadValue() - Invalid stack index:" << index 
+                               << "Stack size:" << stackTop << "Actual index:" << actualIndex;
+                    return false;
+                }
+                
+                if (!lua_istable(L, index)) {
+                    qWarning() << "LuaInterface::loadValue() - Value at index" << index << "is not a table, type:" << lua_typename(L, lua_type(L, index));
+                    return false;
+                }
+                
                 lua_gettable(L, index);
             } else {
                 for (int j = 1; j <= lua_gettop(L); j++) {
