1f8e00aba8930567f3df7c28d0c442b579f03acd
Excellencedev <ademiluyisuccessandexcellence@gmail.com>
2025-09-09 05:05:39 +0100

    CMake add

--- src/TutorialProfile.cpp
+++ src/TutorialProfile.cpp
@@ -1,4 +1,3 @@
-#error "COMPILING TUTORIALPROFILE.CPP"
 /***************************************************************************
  *   Copyright (C) 2025 by Mudlet Team                                     *
  *                                                                         *
@@ -16,7 +15,7 @@
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+/***************************************************************************/
 
 #include "TutorialProfile.h"
 
