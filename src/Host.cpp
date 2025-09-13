8e450da068b6330c64a74659c129bd0682e6f63c
Harrison-Teeg <Harrison.Martin@gmail.com>
2025-09-13 10:06:40 -0700

    Discard old volume exits flag

--- src/Host.cpp
+++ src/Host.cpp
@@ -4595,7 +4595,6 @@ const QSet<QString> Host::mValidExperiments = {
     qsl("experiment.rendering.originalish"),
     qsl("experiment.rendering.more-transparent"),
     qsl("experiment.3dmap.modernmapper"),
-    qsl("experiment.room-connection-volume.basic"),
     qsl("experiment.render-in-out-exits"),
 };
 
