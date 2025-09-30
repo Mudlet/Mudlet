22f55b32e174e30c9a3a7f602d5b1d335548575c
Kebap <kebap_spam@gmx.net>
2025-09-30 10:44:59 +0200

    (c)

--- src/XMLimport.cpp
+++ src/XMLimport.cpp
@@ -3,6 +3,7 @@
  *   Copyright (C) 2014 by Ahmed Charles - acharles@outlook.com            *
  *   Copyright (C) 2016-2023 by Stephen Lyons - slysven@virginmedia.com    *
  *   Copyright (C) 2016-2017 by Ian Adkins - ieadkins@gmail.com            *
+ *   Copyright (C) 2025 by Lecker Kebap - Leris@mudlet.org                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
