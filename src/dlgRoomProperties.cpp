9f65b7e6009cdb794164e32e114bdb2a9c1b9a01
ZookaOnGit <136661366+ZookaOnGit@users.noreply.github.com>
2025-10-14 14:10:11 +1100

    fix: configure room font size and wording

--- src/dlgRoomProperties.cpp
+++ src/dlgRoomProperties.cpp
@@ -92,11 +92,13 @@ void dlgRoomProperties::init(
             comboBox_roomSymbol->lineEdit()->selectAll();
         }
     }
+    auto mapSymbolFont = mpHost->mpMap->mMapSymbolFont;
+    mapSymbolFont.setPointSize(qApp->font().pointSize());
     if (!lineEdit_roomSymbol->isHidden()) {
-        lineEdit_roomSymbol->setFont(mpHost->mpMap->mMapSymbolFont);
+        lineEdit_roomSymbol->setFont(mapSymbolFont);
     }
     if (!comboBox_roomSymbol->isHidden()) {
-        comboBox_roomSymbol->setFont(mpHost->mpMap->mMapSymbolFont);
+        comboBox_roomSymbol->setFont(mapSymbolFont);
     }
     initSymbolInstructions();
 
@@ -200,15 +202,14 @@ void dlgRoomProperties::initSymbolInstructions()
     QString instructions;
     if (mpSymbols.size() == 1) {
         instructions = tr("Type one or more graphemes (\"visible characters\") to use as a symbol "
-                          "for all of the %n selected room(s), or enter a space to clear the symbol:",
+                          "for all of the %n selected room(s):",
                           // Intentional comment to separate arguments!
                           "%n is the total number of rooms involved.",
                           mpRooms.size());
     } else {
         instructions = tr("To change the symbol for all of the %n selected room(s), please choose:\n"
                           " • an existing symbol from the list below (sorted by most commonly used first)\n"
-                          " • enter one or more graphemes (\"visible characters\") as a new symbol\n"
-                          " • enter a space to clear any existing symbols",
+                          " • enter one or more graphemes (\"visible characters\") as a new symbol\n",
                           // Intentional comment to separate arguments!
                           "This is for when applying a new room symbol to one or more rooms "
                           "and some have different symbols or no symbol at present. "
