74c8e0736a3a8849b328c2210b4e0df3a970e9d9
Michael Conley <sousesider@gmail.com>
2025-11-14 07:51:18 -0500

    Fix: Add missing closing brace in TMxpMudlet::insertText()

--- src/TMxpMudlet.cpp
+++ src/TMxpMudlet.cpp
@@ -279,6 +279,8 @@ void TMxpMudlet::insertText(const QString& text)
         std::string textToInsert = text.toStdString();
         mpHost->mpConsole->buffer.translateToPlainText(textToInsert, false);
     }
+}
+
 bool TMxpMudlet::shouldLockModeToSecure() const
 {
     return mpHost && mpHost->getForceMXPProcessorOn();
