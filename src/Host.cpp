608d947646d39ef9c63d7117354fa8bdfced2543
Michael Conley <sousesider@gmail.com>
2025-12-01 21:14:03 -0500

    initial

--- src/Host.cpp
+++ src/Host.cpp
@@ -4487,7 +4487,7 @@ void Host::setFocusOnHostActiveCommandLine()
 
 void Host::recordActiveCommandLine(TCommandLine* pCommandLine)
 {
-    if (!pCommandLine) {
+    if (!pCommandLine || mIsClosingDown) {
         return;
     }
     mpLastCommandLineUsed.removeAll(QPointer<TCommandLine>(pCommandLine));
@@ -4496,7 +4496,7 @@ void Host::recordActiveCommandLine(TCommandLine* pCommandLine)
 
 void Host::forgetCommandLine(TCommandLine* pCommandLine)
 {
-    if (pCommandLine) {
+    if (pCommandLine && !mIsClosingDown) {
         mpLastCommandLineUsed.removeAll(QPointer<TCommandLine>(pCommandLine));
     }
 }
@@ -4505,7 +4505,7 @@ void Host::forgetCommandLine(TCommandLine* pCommandLine)
 TCommandLine* Host::activeCommandLine()
 {
     TCommandLine* pCommandLine = nullptr;
-    if (mpLastCommandLineUsed.isEmpty()) {
+    if (mIsClosingDown || mpLastCommandLineUsed.isEmpty()) {
         return nullptr;
     }
 
