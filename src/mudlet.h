643df1b246a415d0258714343b75482b29db2960
Excellencedev <ademiluyisuccessandexcellence@gmail.com>
2026-01-11 16:04:23 +0100

    make telnet links on macos work even if mudlet is already running

--- src/mudlet.h
+++ src/mudlet.h
@@ -500,6 +500,7 @@ protected:
     void dragEnterEvent(QDragEnterEvent* event) override;
     void dragMoveEvent(QDragMoveEvent* event) override;
     void dropEvent(QDropEvent* event) override;
+    bool eventFilter(QObject* obj, QEvent* event) override;
 
 
 signals:
