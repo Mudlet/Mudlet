f425cb68ea60a55da0e2698059abac216980d3be
Vadim Peretokin <vperetokin@hey.com>
2026-01-25 12:20:23 +0100

    fix: Use custom dark theme on all Windows versions (#8819)
    #### Brief overview of PR changes/additions
    Always use custom Fusion-based dark theme on Windows instead of only on
    Windows 10.
    
    #### Motivation for adding to Mudlet
    The built-in Qt dark theme on Windows 11 hasn't received praise but has
    received some criticism.
    
    #### Other info (issues closed, discussion etc)
    Mudlet 4.19.1 used the custom dark theme on all platforms - the switch
    to native Qt theming for macOS and Windows 11 was introduced after
    4.19.1.
    
    **Test case:** Enable dark mode on Windows 11, verify the custom Fusion
    dark theme is applied instead of the native Qt theme.
    
    Co-authored-by: Vadim Peretokin <vadi2@users.noreply.github.com>
    (cherry picked from commit cc07a97129855bd20743f294f5e312c058498a21)

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -5839,9 +5839,7 @@ void mudlet::setShowIconsOnMenu(const Qt::CheckState state)
 
 bool mudlet::needsCustomDarkTheme()
 {
-#if defined(Q_OS_WINDOWS)
-    return QSysInfo::productVersion() == qsl("10");
-#elif defined(Q_OS_LINUX)
+#if defined(Q_OS_WINDOWS) || defined(Q_OS_LINUX)
     return true;
 #else
     return false;
