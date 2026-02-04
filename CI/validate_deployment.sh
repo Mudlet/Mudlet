1a0531b6d77374329ae33400b3a597a83609102e
Vadim Peretokin <vadi2@users.noreply.github.com>
2026-02-04 16:40:43 +0100

    use pcre2grep in CI validation

--- CI/validate_deployment.sh
+++ CI/validate_deployment.sh
@@ -14,13 +14,13 @@ else
 
   function validate_cmake() {
     local VALID_CMAKE VALID_BUILD
-    VALID_CMAKE=$(pcregrep --only-matching=1 "set\(APP_VERSION (\d+\.\d+\.\d+)\)$" < CMakeLists.txt)
+    VALID_CMAKE=$(pcre2grep --only-matching=1 "set\(APP_VERSION (\d+\.\d+\.\d+)\)$" < CMakeLists.txt)
 
     if [ -z "${VALID_CMAKE}" ]; then
       error "CMakeLists.txt VERSION variable isn't formatted following the semantic versioning rules in a release build."
     fi
 
-    VALID_BUILD=$(pcregrep --only-matching=1 'set\(APP_BUILD ("")' < CMakeLists.txt)
+    VALID_BUILD=$(pcre2grep --only-matching=1 'set\(APP_BUILD ("")' < CMakeLists.txt)
     if [ "${VALID_BUILD}" != '""' ]; then
       error "CMakeLists.txt APP_BUILD variable isn't set to \"\" as it should be in a release build."
     fi
