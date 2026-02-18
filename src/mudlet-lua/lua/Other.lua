e55786a12138ae520b97dacd075fe1b058fb9b85
missionz3r0 <github_xu8w@missionz3r0.com>
2026-02-18 09:56:04 -0500

    fix odd quirk with how inserts can occur

--- src/mudlet-lua/lua/Other.lua
+++ src/mudlet-lua/lua/Other.lua
@@ -952,9 +952,12 @@ function timeframe(vname, true_time, nil_time, ...)
   local timerlist = {
     {0, nil},
     (type(true_time) == "number" and {true_time, true}) or (type(true_time) == "table" and true_time),
-    ...,
-    (type(nil_time) == "number" and {nil_time, nil}) or (type(nil_time) == "table" and nil_time) or nil,
+    ...
   }
+  table.insert(
+    timerlist,
+    (type(nil_time) == "number" and {nil_time, nil}) or (type(nil_time) == "table" and nil_time) or nil
+  )
 
   -- reinitialise timeframe for vname
   killtimeframe(vname)
