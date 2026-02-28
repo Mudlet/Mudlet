8c460b5fd4347b1032d10aeadeb0961934237daf
missionz3r0 <3701326+missionz3r0@users.noreply.github.com>
2026-02-28 14:38:00 +0000

    Update mudlet.cpp's comment.

--- src/mudlet.cpp
+++ src/mudlet.cpp
@@ -5306,7 +5306,8 @@ QString mudlet::getMudletPath(const enums::mudletPathType mode, const QString& e
 void mudlet::checkUpdatesOnStart()
 {
     if (!qEnvironmentVariableIsSet("MUDLET_TEST_MODE") && (releaseVersion || publicTestVersion || qEnvironmentVariableIsSet("DEV_UPDATER"))) {
-        // Only try and create an updater (which checks for updates online) if
+        // Doesn't check for updates during test runs.
+        // Otherwise, try and create an updater (which checks for updates online) if
         // this is a release/public test version, or if you are testing Sparkle (env flag set).
         pUpdater->checkUpdatesOnStart();
     }
