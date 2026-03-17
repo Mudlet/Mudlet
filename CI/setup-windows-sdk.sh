d53779e20ce1a6e1c4b21c00dd5b3f4f44bfedd1
John McKisson <john.mckisson@gmail.com>
2026-03-17 13:08:03 -0400

    Revert "slysven review suggestions"
    This reverts commit 518cf09ee1e1f9cd896a9133e65a9be9bf0b44d0.

--- CI/setup-windows-sdk.sh
+++ CI/setup-windows-sdk.sh
@@ -54,16 +54,15 @@
 
 
 if [ "${MSYSTEM}" = "MSYS" ]; then
-  echo "Please run this script from a MINGW64 or CLANG64 type bash terminal as the MSYS one"
+  echo "Please run this script from a CLANG64 type bash terminal as the MSYS one"
   echo "does not supported what is needed."
   exit 2
-elif [ "${MSYSTEM}" = "MINGW64" ]; then
-  export BUILDCOMPONENT="x86_64"
 elif [ "${MSYSTEM}" = "CLANG64" ]; then
+  export BUILD_BITNESS="64"
   export BUILDCOMPONENT="clang-x86_64"
 else
   echo "This script is not set up to handle systems of type ${MSYSTEM}, only"
-  echo "MINGW64 and CLANG64 are currently supported. Please rerun this in a bash terminal of"
+  echo "CLANG64 is currently supported. Please rerun this in a bash terminal of"
   echo "that type."
   exit 2
 fi
