ee30bbf835669ab9549d8b214f2a57791ffa5289
Vadim Peretokin <vperetokin@hey.com>
2026-03-21 09:00:55 +0100

    Fix setSaveCommandHistory inverting boolean in 2-arg form
    The ! negation on the getVerifiedBool result caused
    setSaveCommandHistory("name", true) to disable saving and
    setSaveCommandHistory("name", false) to enable it. The 1-arg
    form worked correctly without negation, making this inconsistent.

--- src/TLuaInterpreter.cpp
+++ src/TLuaInterpreter.cpp
@@ -8252,7 +8252,7 @@ int TLuaInterpreter::setSaveCommandHistory(lua_State* L)
             // First argument is a string so is presumably a command line name
             name = CMDLINE_NAME(L, 1);
             if (n > 1) {
-                saveCommands = !getVerifiedBool(L, __func__, 2, "save command history", true);
+                saveCommands = getVerifiedBool(L, __func__, 2, "save command history", true);
             }
 
         } else {
@@ -8261,7 +8261,7 @@ int TLuaInterpreter::setSaveCommandHistory(lua_State* L)
                 return lua_error(L); // Dummy return!
             }
 
-            saveCommands = !getVerifiedBool(L, __func__, 1, "save command history", true);
+            saveCommands = getVerifiedBool(L, __func__, 1, "save command history", true);
         }
     }
 
