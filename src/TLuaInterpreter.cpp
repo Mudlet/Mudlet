Mudlet/Mudlet

commit 9f55e7ded1cc5abc8ef12b78c2feca980429e9a1

Add persistent command history (#6767)
#### Brief overview of PR changes/additions
Saves each command line's history when a profile is closed and restores
the data the next time the profile is opened. Each command line is
handled separately.

#### Motivation for adding to Mudlet
It is a bounty item!

#### Other info (issues closed, discussion etc)
This works for command lines of all types, and saves them all separately
in the profile's base directory.

This should close https://github.com/Mudlet/Mudlet/issues/2007.

We'll avoid using the names for the extra command lines as part of a
filename by instead storing them in a `QSettings` per profile
`profile.ini` format file.

Using such a file to store per-profile details is something that has
been on my TODO list for years (I prototyped code to do it at least as
far back as 2016 I think) - so I have added two functions to the Host
class that hopefully can eventually replace the existing
`readProfileData(...)` and `writeProfileData(...)`.

Also added code to fix a corner case where a TCommandLine has a name
containing '/' or '\'.

Also added a limit to the number of command line history entries to
save. Otherwise it will grow indefinitely large as every single entry is
retained. A knob for this has been provided on the profile preferences
and it is saved with the profile's XML game save file (not in the base
directory of the profile). It covers the range of 0 to 10,000 entries
with a default of 500 and a logarithmic step size (1, 2, 5, 10) for each
multiple of 10. This value is applied to ALL command lines in a profile.

Note that this limit is only applied when the commend history is saved,
it can still grow to be larger than the limit whilst the profile is
active!

A pair of lua API  functions are added:
* `setSaveCommandHistory([commandLineName,] save)`  to enable
(`true`) or disable (`false`) the main, or if specified, any other
command-line

src/TLuaInterpreter.cpp
@@ -15697,6 +15697,8 @@ void TLuaInterpreter::initLuaGlobals()
     lua_register(pGlobalLua, "scrollTo", TLuaInterpreter::scrollTo);
     lua_register(pGlobalLua, "getScroll", TLuaInterpreter::getScroll);
     lua_register(pGlobalLua, "getConfig", TLuaInterpreter::getConfig);
+    lua_register(pGlobalLua, "setSaveCommandHistory", TLuaInterpreter::setSaveCommandHistory);
+    lua_register(pGlobalLua, "getSaveCommandHistory", TLuaInterpreter::getSaveCommandHistory);
     // PLACEMARKER: End of main Lua interpreter functions registration
     // check new functions against https://www.linguistic-antipatterns.com when creating them
 
@@ -17724,6 +17726,30 @@ int TLuaInterpreter::setConfig(lua_State * L)
         }
         return success();
     }
+    if (key == qsl("commandLineHistorySaveSize")) {
+        // This set of values needs to be the same as those put in the
+        // (QComboBox) dlgProfilePreferences::comboBox_commandLineHistorySaveSize
+        // widget:
+        static const QList<int> values{0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000};
+        const auto value = getVerifiedInt(L, __func__, 2, "value");
+        if (!values.contains(value)) {
+            static QStringList valuesAsStrings;
+            if (valuesAsStrings.isEmpty()) {
+                for (const auto& potentialValue : values) {
+                    valuesAsStrings << QString::number(potentialValue);
+                }
+            }
+            lua_pushnil(L);
+            // Use the original argument as a string, not what the
+            // getVerifiedInt(...) returns in case it is not a pure integer to
+            // start with:
+            lua_pushfstring(L, "invalid commandLineHistorySaveSize value '%s', it should be one of %s",
+                            lua_tostring(L, 2), valuesAsStrings.join(qsl(", ")).toUtf8().constData());
+            return 2;
+        }
+        host.setCommandLineHistorySaveSize(value);
+        return success();
+    }
 
     return warnArgumentValue(L, __func__, qsl("'%1' isn't a valid configuration option").arg(key));
 }
@@ -18077,6 +18103,7 @@ int TLuaInterpreter::getConfig(lua_State *L)
                 lua_pushstring(L, "f6");
             }
         } },
+        { qsl("commandLineHistorySaveSize"), [&](){ lua_pushnumber(L, host.getCommandLineHistorySaveSize()); } },
     };
 
     auto it = configMap.find(key);
@@ -18086,4 +18113,70 @@ int TLuaInterpreter::getConfig(lua_State *L)
     }
 
     return warnArgumentValue(L, __func__, qsl("'%1' isn't a valid configuration option").arg(key));
+}
+
+// Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#getSaveCommandHistory
+int TLuaInterpreter::getSaveCommandHistory(lua_State* L)
+{
+    auto& host = getHostFromLua(L);
+    auto numberOfLines = host.getCommandLineHistorySaveSize();
+    if (!numberOfLines) {
+        // We do not use warnArgumentValue(...) because it is valid to have
+        // this disabled and we do not want a message to be painted on the
+        // Central Debug Console:
+        lua_pushboolean(L, false);
+        lua_pushstring(L, "disabled by profile global preference");
+        return 2;
+    }
+    QString name = QLatin1String("main");
+    if (lua_gettop(L)) {
+        name = CMDLINE_NAME(L, 1);
+    }
+    auto pCommandline = COMMANDLINE(L, name);
+    lua_pushboolean(L, pCommandline->mSaveCommands);
+    lua_pushstring(L, (pCommandline->mSaveCommands ? qsl("enabled (%1 lines will be saved)").arg(QString::number(numberOfLines)) : qsl("disabled")).toUtf8().constData());
+    return 2;
+}
+
+// Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#setSaveCommandHistory
+int TLuaInterpreter::setSaveCommandHistory(lua_State* L)
+{
+    auto n = lua_gettop(L);
+    auto& host = getHostFromLua(L);
+    auto numberOfLines = host.getCommandLineHistorySaveSize();
+    if (!numberOfLines) {
+        // Unlike for the getter we do want to alert on trying to set the
+        // per-commandLine option when things are disabled globally for the
+        // profile:
+        return warnArgumentValue(L, __func__, "disabled by profile global preference");
+    }
+    QString name = QLatin1String("main");
+    bool saveCommands = true;
+    // if there is no arguments we will set the "save command history" on the
+    // main  command line:
+    if (n == 1) {
+        saveCommands = getVerifiedBool(L, __func__, 1, "save command history", true);
+    } else {
+        if (lua_type(L, 1) == LUA_TSTRING) {
+            // First argument is a string so is presumably a command line name
+            name = CMDLINE_NAME(L, 1);
+            if (n > 1) {
+                saveCommands = !getVerifiedBool(L, __func__, 2, "save command history", true);
+            }
+
+        } else {
+            if (lua_type(L, 1) != LUA_TBOOLEAN) {
+                lua_pushfstring(L, "%s: bad argument #1 type (command line name as string or save history as boolean is optional, got %s!)",
+                                __func__, luaL_typename(L, 1));
+                return lua_error(L); // Dummy return!
+            }
+
+            saveCommands = !getVerifiedBool(L, __func__, 1, "save command history", true);
+        }
+    }
+
+    auto pCommandline = COMMANDLINE(L, name);
+    pCommandline->mSaveCommands = saveCommands;
+    lua_pushboolean(L, true);
+    return 1;
 }

src/Host.cpp
@@ -818,6 +818,13 @@ std::tuple<bool, QString, QString> Host::saveProfile(const QString& saveFolder, 
         return std::make_tuple(false, QString(), qsl("a save is already in progress"));
     }
 
+    if (saveFolder.isEmpty() && saveName.isEmpty()) {
+        // This is likely to be the save as the profile is closed
+        qDebug().noquote().nospace() << "Host::saveProfile(...) INFO - called with no saveFolder or saveName arguments "
+                                        "so assuming it is a end of session save and the TCommandLines' histories need saving...";
+        emit signal_saveCommandLinesHistory();
+    }
+
     emit profileSaveStarted();
     qApp->processEvents();
 
@@ -2140,6 +2147,115 @@ QString Host::getPackageConfig(const QString& luaConfig, bool isModule)
     lua_pop(L, -1);
     lua_close(L);
     return QString();
+}
+
+// writeProfileIniData(...) and readProfileIniData(...) might eventually
+// replace writeProfileData(...) and readProfileData(...) but for now are just
+// used to store some information about one or more TCommandLine's mHistoryData:
+bool Host::writeProfileIniData(const QString& item, const QString& what)
+{
+    QSettings settings(mudlet::getMudletPath(mudlet::profileDataItemPath, getName(), qsl("profile.ini")), QSettings::IniFormat);
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    // This will ensure compatibility going forward and backward
+    settings.setIniCodec(QTextCodec::codecForName("UTF-8"));
+#endif
+    settings.setValue(item, what);
+    settings.sync();
+    switch (settings.status()) {
+    case QSettings::NoError:
+        return true;
+    case QSettings::FormatError:
+        qWarning().nospace().noquote() << "Host::writeProfileIniData(\"" << item << "\", \"" << what << "\") ERROR - failed to save this detail, reason: \"Format error\".";
+        return false;
+    case QSettings::AccessError:
+        qWarning().nospace().noquote() << "Host::writeProfileIniData(\"" << item << "\", \"" << what << "\") ERROR - failed to save this detail, reason: \"Access error\".";
+        return false;
+    }
+    Q_UNREACHABLE();
+}
+
+QString Host::readProfileIniData(const QString& item)
+{
+    QSettings settings(mudlet::getMudletPath(mudlet::profileDataItemPath, getName(), qsl("profile.ini")), QSettings::IniFormat);
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    // This will ensure compatibility going forward and backward
+    settings.setIniCodec(QTextCodec::codecForName("UTF-8"));
+#endif
+    return settings.value(item).toString();
+}
+
+// This function retrieves command line history settings based on the given
+// command line type and name. It reads the saveCommands setting from the
+// profile.ini file, which is intended to replace all other single data item
+// files in the profile's home directory. For the main command line, a
+// predefined file name is used, while for any other command line, the function
+// first looks for a mapping from a number-suffixed file to a command line
+// name. If the mapping is not found, a new file name and default setting for
+// saveCommands is created, stored, and returned.
+// Because '/' and '\\' are used by the QSettings class in key names for
+// special purposes we MUST filter them out in the name, we'll replace them
+// with '_'s:
+std::tuple<QString, bool> Host::getCmdLineSettings(const TCommandLine::CommandLineType type, const QString& name)
+{
+    if (type == TCommandLine::MainCommandLine) {
+        // This one does not need the name to be kept in a QSettings but we
+        // still need to retrieve the other setting:
+        auto saveCommands = static_cast<bool>(readProfileIniData(qsl("CommandLines/SaveHistory/main")).compare(qsl("false"), Qt::CaseInsensitive));
+        return {qsl("command_history_main"), saveCommands};
+    }
+    QString localName{name};
+    localName.replace(QRegularExpression(qsl("[\\/]")), qsl("_"));
+    // We use a '/' in the name as that denotes a grouping (section) within the
+    // QSetting's (INI) format with the left-most side of the first '/' as a
+    // section header. They actually get converted to `\\` (a single backslash)
+    // inside the actual file but are accessed correctly only if given as a
+    // forward slash in the code:
+    auto fileName = readProfileIniData(qsl("CommandLines/NameMapping/%1").arg(localName));
+    // We want this to default to true if the setting doesn't exist, so we will
+    // compare it to the opposite (which will be zero for a match) and convert
+    // it to a boolean - so that a missing value will give a non-zero value
+    // which becomes a true:
+    auto saveCommands = static_cast<bool>(readProfileIniData(qsl("CommandLines/SaveHistory/%1").arg(localName)).compare(qsl("false"), Qt::CaseInsensitive));
+    if (!fileName.isEmpty()) {
+        // Ah, we've used this name before, so return the details:
+        return {fileName, saveCommands};
+    }
+
+    // Else the name is not in the settings so we will have to create one:
+    // Get the highest number used so far:
+    bool isOk = false;
+    auto usedIndex = readProfileIniData(qsl("CommandLines/UsedIndexes")).toInt(&isOk);
+    if (!isOk || !usedIndex) {
+        // The value was not found / is null - so force it to be the right one
+        // to start with, remembering that it will be incremented before use:
+        usedIndex = 0;
+    }
+    // Increment it and save the new value
+    writeProfileIniData(qsl("CommandLines/UsedIndexes"), QString::number(++usedIndex));
+    // Generate the name
+    fileName = qsl("command_history_%1").arg(usedIndex, 2, 10, QLatin1Char('0'));
+    // Save it:
+    writeProfileIniData(qsl("CommandLines/NameMapping/%1").arg(localName), fileName);
+    // And a default setting:
+    writeProfileIniData(qsl("CommandLines/SaveHistory/%1").arg(localName), saveCommands ? qsl("true") : qsl("false"));
+    // And return it - with the defaulted other setting:
+    return {fileName, saveCommands};
+}
+
+void Host::setCmdLineSettings(const TCommandLine::CommandLineType type, const bool saveCommands, const QString& name)
+{
+    if (type == TCommandLine::MainCommandLine) {
+        writeProfileIniData(qsl("CommandLines/SaveHistory/main"), saveCommands ? qsl("true") : qsl("false"));
+        return;
+    }
+    QString localName{name};
+    localName.replace(QRegularExpression(qsl("[\\/]")), qsl("_"));
+    // We use a '/' in the name as that donotes a grouping (section) within the
+    // QSetting's (INI) format with the left-most side of the first '/' as a
+    // section header. They actually get converted to `\\` (a single backslash)
+    // inside the actual file but are accessed correctly only if given as a
+    // forward slash in the code:
+    writeProfileIniData(qsl("CommandLines/SaveHistory/%1").arg(localName), saveCommands ? qsl("true") : qsl("false"));
 }
 
 // Derived from the one in dlgConnectionProfile class - but it does not need a
@@ -4184,4 +4300,11 @@ void Host::setBorders(QMargins borders)
     QResizeEvent event(s, s);
     QApplication::sendEvent(mpConsole, &event);
     mpConsole->raiseMudletSysWindowResizeEvent(x, y);
+}
+
+void Host::setCommandLineHistorySaveSize(const int lines)
+{
+    if (mCommandLineHistorySaveSize != lines) {
+        mCommandLineHistorySaveSize = lines;
+    }
 }

src/Host.h
@@ -30,6 +30,7 @@
 #include "AliasUnit.h"
 #include "KeyUnit.h"
 #include "ScriptUnit.h"
+#include "TCommandLine.h"
 #include "TLuaInterpreter.h"
 #include "TimerUnit.h"
 #include "TMainConsole.h"
@@ -312,7 +313,9 @@ public:
     void postMessage(const QString message) { mTelnet.postMessage(message); }
     QColor getAnsiColor(const int ansiCode, const bool isBackground = false) const;
     QPair<bool, QString> writeProfileData(const QString&, const QString&);
+    bool writeProfileIniData(const QString& item, const QString& what);
     QString readProfileData(const QString&);
+    QString readProfileIniData(const QString& item);
     void xmlSaved(const QString& xmlName);
     bool currentlySavingProfile();
     void processDiscordGMCP(const QString& packageMessage, const QString& data);
@@ -408,6 +411,10 @@ public:
     QMargins borders() const { return mBorders; }
     void setBorders(const QMargins);
     void loadMap();
+    std::tuple<QString, bool> getCmdLineSettings(const TCommandLine::CommandLineType, const QString&);
+    void setCmdLineSettings(const TCommandLine::CommandLineType, const bool, const QString&);
+    int getCommandLineHistorySaveSize() const { return mCommandLineHistorySaveSize; }
+    void setCommandLineHistorySaveSize(const int lines);
 
 
     cTelnet mTelnet;
@@ -701,6 +708,8 @@ signals:
     // Tells all consoles associated with this Host (but NOT the Central Debug
     // one) to change the way they show  control characters:
     void signal_controlCharacterHandlingChanged(const ControlCharacterMode);
+    // Tells all command lines to save their history:
+    void signal_saveCommandLinesHistory();
 
 private slots:
     void slot_purgeTemps();
@@ -878,6 +887,15 @@ private:
     bool mFocusTimerRunning = false;
 
     QMargins mBorders;
+
+    // The range - applied to ALL command lines - is 0 to 10000, with the knob
+    // on the profile preferences having a log-step action with multiples
+    // of 10 to integer powers and steps of (0,) 10, 20, 50, 100. Prior to the
+    // introduction of this feature the control would effectively have been
+    // zero - and whilst the knob shows the special value of "None" then
+    // to reproduce that behavior there is little reason to not enable it
+    // by default:
+    int mCommandLineHistorySaveSize = 500;
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(Host::DiscordOptionFlags)

src/TCommandLine.cpp
@@ -37,6 +37,7 @@
 #include <QKeyEvent>
 #include <QRegularExpression>
 #include <QScrollBar>
+#include <QSaveFile>
 #include "post_guard.h"
 
 TCommandLine::TCommandLine(Host* pHost, const QString& name, CommandLineType type, TConsole* pConsole, QWidget* parent)
@@ -80,6 +81,14 @@ TCommandLine::TCommandLine(Host* pHost, const QString& name, CommandLineType typ
 
     connect(mudlet::self(), &mudlet::signal_adjustAccessibleNames, this, &TCommandLine::slot_adjustAccessibleNames);
     slot_adjustAccessibleNames();
+    // Restore the history settings:
+    std::tie(mBackingFileName, mSaveCommands) = mpHost->getCmdLineSettings(mType, name);
+
+    // Restore any previous historic commands even if we are not going to save
+    // them under current settings:
+    restoreHistory();
+
+    connect(pHost, &Host::signal_saveCommandLinesHistory, this, &TCommandLine::slot_saveHistory);
 }
 
 void TCommandLine::processNormalKey(QEvent* event)
@@ -693,6 +702,7 @@ void TCommandLine::mousePressEvent(QMouseEvent* event)
             QAction* action_addWord = nullptr;
             QAction* action_removeWord = nullptr;
             QAction* action_dictionarySeparatorLine = nullptr;
+
             if (handle_profile) {
                 // TODO: Make icons for these?
 //                if (!qApp->testAttribute(Qt::AA_DontShowIconsInMenus)) {
@@ -844,7 +854,7 @@ void TCommandLine::mousePressEvent(QMouseEvent* event)
             * only) argument given.
             */
 
-            auto separator_aboveStandardMenu = popup->insertSeparator(popup->actions().first());
+            auto separator_aboveStandardMenu = popup->insertSeparator(popup->actions().constFirst());
             if (handle_profile) {
                 popup->insertAction(separator_aboveStandardMenu, action_removeWord);
                 popup->insertAction(action_removeWord, action_addWord);
@@ -857,9 +867,6 @@ void TCommandLine::mousePressEvent(QMouseEvent* event)
             } else {
                 popup->insertActions(separator_aboveStandardMenu, spellings_system);
             }
-            // else the word is in the dictionary - in either ca`se show the context
-            // menu - either the one with the prefixed spellings, or the standard
-            // one:
         }
 
         popup->addSeparator();
@@ -887,41 +894,41 @@ void TCommandLine::mousePressEvent(QMouseEvent* event)
         event->accept();
     }
 
-    // Process any other possible mousePressEvent - which is default popup
-    // handling - and which accepts the event:
+    // Process any other possible mousePressEvent - which is default context
+    // menu handling - and which accepts the event:
     QPlainTextEdit::mousePressEvent(event);
     mudlet::self()->activateProfile(mpHost);
 }
 
 void TCommandLine::mouseReleaseEvent(QMouseEvent* event)
 {
-    // Process any other possible mousePressEvent - which is default popup
-    // handling - and which accepts the event:
-    QPlainTextEdit::mousePressEvent(event);
+    // Process any other possible mouseReleaseEvent - which is default context
+    // menu handling - and which accepts the event:
+    QPlainTextEdit::mouseReleaseEvent(event);
     mudlet::self()->activateProfile(mpHost);
 }
 
 void TCommandLine::enterCommand(QKeyEvent* event)
 {
     Q_UNUSED(event)
-    const QString _t = toPlainText();
     mTabCompletionCount = -1;
     mAutoCompletionCount = -1;
     mTabCompletionTyped.clear();
     mLastCompletion.clear();
     mUserKeptOnTyping = false;
 
-    QStringList _l = _t.split(QChar::LineFeed);
+    QStringList commandList = toPlainText().split(QChar::LineFeed);
 
-    for (int i = 0; i < _l.size(); i++) {
+    for (int i = 0; i < commandList.size(); ++i) {
         if (mType != MainCommandLine && mActionFunction) {
-            mpHost->getLuaInterpreter()->callCmdLineAction(mActionFunction, _l[i]);
+            mpHost->getLuaInterpreter()->callCmdLineAction(mActionFunction, commandList.at(i));
         } else {
-            mpHost->send(_l[i]);
+            mpHost->send(commandList.at(i));
         }
         // send command to your MiniConsole
         if (mType == ConsoleCommandLine && !mActionFunction && mpHost->mPrintCommand){
-            mpConsole->printCommand(_l[i]);
+            // This usage of commandList modifies the content!!!
+            mpConsole->printCommand(commandList[i]);
         }
     }
 
@@ -940,6 +947,7 @@ void TCommandLine::enterCommand(QKeyEvent* event)
         }
         mHistoryList.push_front(QString());
     }
+
     if (mpHost->mAutoClearCommandLineAfterSend) {
 #if defined (Q_OS_MACOS)
         // clearing the input line on macOS 11.6 makes VoiceOver announce the removed text,
@@ -1424,5 +1432,94 @@ void TCommandLine::slot_adjustAccessibleNames()
         break;
     case UnknownType:
         Q_UNREACHABLE();
+    }
+}
+
+void TCommandLine::restoreHistory()
+{
+    auto pHost = mpHost;
+    if (!pHost) {
+        qWarning().nospace().noquote() << "TCommandLine::restoreHistory() ERROR - got a Host pointer that was null - unable to save command history for the command line called: " << mCommandLineName
+                                       << " of type: " << mType;
+        return;
+    }
+
+    QString pathFileName{mudlet::self()->mudlet::getMudletPath(mudlet::profileDataItemPath, pHost->getName(), mBackingFileName)};
+    QFile historyFile(pathFileName, this);
+    if (historyFile.exists()) {
+        if (historyFile.open(QIODevice::ReadOnly | QIODevice::Unbuffered)) {
+            // In Qt6 the default encoding is UTF-8
+            QTextStream ifs(&historyFile);
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+            ifs.setCodec(QTextCodec::codecForName("UTF-8"));
+#endif
+            QString buffer;
+            while (!ifs.atEnd() && ifs.status() == QTextStream::Ok) {
+                ifs.readLineInto(&buffer);
+                mHistoryList.append(buffer);
+            }
+
+            if (historyFile.error() != QFileDevice::NoError) {
+                qWarning() << "TCommandLine::restoreHistory() ERROR - unable to read command history from file for the command line called: " << mCommandLineName << " of type: " << mType
+                           << " reason: " << historyFile.errorString();
+                historyFile.close();
+                return;
+            }
+
+            historyFile.close();
+            // Success!
+            return;
+        }
+
+        // else failed to open the file despite it existing
+        if (historyFile.error() != QFileDevice::NoError) {
+            qWarning() << "TCommandLine::restoreHistory() ERROR - unable to open command history for the command line called: "
+                       << mCommandLineName << " of type: " << mType
+                       << " reason: " << historyFile.errorString();
+            return;
+        }
+    }
+    // else no such file - which will be the case for the first time the
+    // command line is created - so it might not be an error:
+    qDebug() << "TCommandLine::restoreHistory() ALERT - unable to open command history for the command line called: "
+             << mCommandLineName << " of type: " << mType
+             << " because the file: " << mBackingFileName
+             << " does not exist in the profile's home directory, unless this is a new command line then this is an unexpected error.";
+}
+
+void TCommandLine::slot_saveHistory()
+{
+    auto pHost = mpHost;
+    if (!pHost) {
+        qWarning().nospace().noquote() << "TCommandLine::slot_saveHistory() ERROR - got a Host pointer that was null - unable to save command history for the command line called: " << mCommandLineName
+                                       << " of type: " << mType;
+        return;
+    }
+
+    pHost->setCmdLineSettings(mType, mSaveCommands, mCommandLineName);
+    auto saveSize = pHost->getCommandLineHistorySaveSize();
+    if (!saveSize || !mSaveCommands) {
+        // Option has been disabled so do nothing (won't delete the previous one
+        // though!):
+        return;
+    }
+
+    QString pathFileName{mudlet::self()->mudlet::getMudletPath(mudlet::profileDataItemPath, pHost->getName(), mBackingFileName)};
+    QSaveFile historyFile(pathFileName, this);
+    if (historyFile.open(QIODevice::WriteOnly | QIODevice::Unbuffered)) {
+        QTextStream ofs(&historyFile);
+        // In Qt6 the default encoding is UTF-8
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+        ofs.setCodec(QTextCodec::codecForName("UTF-8"));
+#endif
+        // We need to add one here because usually the first line in
+        // mHistoryList is an empty one - maybe it might represent the current
+        // line and will get captured/saved if the profile is closed with some
+        // unsent text in the command line?
+        ofs << mHistoryList.mid(0, saveSize + 1).join(QChar::LineFeed);
+        if (!historyFile.commit()) {
+            qDebug().nospace().noquote() << "TCommandLine::slot_saveHistory() ERROR - unable to save command history for the command line called: " << mCommandLineName
+                                         << " of type: " << mType << " reason: " << historyFile.errorString();
+        }
     }
 }

src/TCommandLine.h
@@ -83,12 +83,18 @@ public:
 
     QMap<QString, QString> contextMenuItems;
 
+    // Set to true (by default) to save the commands in the mHistoryList at the
+    // end of the session:
+    bool mSaveCommands = true;
+
+
 public slots:
     void slot_popupMenu();
     void slot_addWord();
     void slot_removeWord();
     void slot_clearSelection(bool yes);
     void slot_adjustAccessibleNames();
+    void slot_saveHistory();
 
 private:
     bool event(QEvent*) override;
@@ -103,6 +109,7 @@ private:
     bool keybindingMatched(QKeyEvent*);
     void spellCheckWord(QTextCursor& c);
     bool handleCtrlTabChange(QKeyEvent* key, int tabNumber);
+    void restoreHistory();
 
     QPointer<Host> mpHost;
     CommandLineType mType = UnknownType;
@@ -127,9 +134,27 @@ private:
     char** mpUserSuggestionsList = nullptr;
     QSet<QString> commandLineSuggestions;
     QSet<QString> tabCompleteBlacklist;
-
+    // The file used to store the command history between sessions:
+    QString mBackingFileName;
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(TCommandLine::CommandLineType)
+
+#if !defined(QT_NO_DEBUG)
+inline QDebug& operator<<(QDebug& debug, const TCommandLine::CommandLineType& type)
+{
+    QString text;
+    QDebugStateSaver saver(debug);
+    switch (type) {
+    case TCommandLine::UnknownType:        text = qsl("Unknown"); break;
+    case TCommandLine::SubCommandLine:     text = qsl("SubCommandLine"); break;
+    case TCommandLine::ConsoleCommandLine: text = qsl("ConsoleCommandLine"); break;
+    case TCommandLine::MainCommandLine:    text = qsl("MainCommandLine"); break;
+    default:                               text = qsl("Non-coded Type");
+    }
+    debug.nospace() << text;
+    return debug;
+}
+#endif // !defined(QT_NO_DEBUG)
 
 #endif // MUDLET_TCOMMANDLINE_H

commit 8b0175bf7a459c57cb15bcd04f7e05698d37c2f8

fix: setSaveCommandHistory inverting the boolean when a command line name is given (#9098)
#### Brief overview of PR changes/additions
Remove erroneous ! negation on the getVerifiedBool result in the 2-arg
paths of setSaveCommandHistory (lines 8255 and 8264)

#### Motivation for adding to Mudlet
setSaveCommandHistory("main", true) was disabling command history
instead of enabling it, while setSaveCommandHistory(true) worked
correctly - the 1-arg and 2-arg forms had opposite behavior for the same
boolean value.

#### Other info (issues closed, discussion etc)
Bug has been present since the function was introduced in PR #6767.

**Test case:** Run setSaveCommandHistory("main", true) then
getSaveCommandHistory("main") - should return true (previously returned
false).

src/TLuaInterpreter.cpp
@@ -8253,7 +8253,7 @@ int TLuaInterpreter::setSaveCommandHistory(lua_State* L)
             // First argument is a string so is presumably a command line name
             name = CMDLINE_NAME(L, 1);
             if (n > 1) {
-                saveCommands = !getVerifiedBool(L, __func__, 2, "save command history", true);
+                saveCommands = getVerifiedBool(L, __func__, 2, "save command history", true);
             }
 
         } else {
@@ -8262,7 +8262,7 @@ int TLuaInterpreter::setSaveCommandHistory(lua_State* L)
                 return lua_error(L); // Dummy return!
             }
 
-            saveCommands = !getVerifiedBool(L, __func__, 1, "save command history", true);
+            saveCommands = getVerifiedBool(L, __func__, 1, "save command history", true);
         }
     }
 