Mudlet/Mudlet

commit 35ec27678c2cab35475550c1928cfd4671684cf4

Improve: split map mouse interaction code, for better readability, improve handling of user interactions (#8356)
#### Brief overview of PR changes/additions
I split mouse interaction logic into separate small handlers, that are
checking whether they can act on interaction in priority order.
There is separate struct that is responsible fore keeping current map
interaction context, that is used by the handlers.
Solution might not be perfect at the time, but it is first step to
improving quality and consistency of interactions with map interface.
Deleting label clears it's selection state, since now if you delete
label and right click, you will still see context menu for that label.
There are some improvements in map interaction to be more intuitive.
Right click now will select room/label that is clicked.
Right click anywhere will deselect room and bring general popup menu.
Rectangle selection with shift will add elements to list.
Rectangle selection with ctrl will flip elements selection.

#### Motivation for adding to Mudlet
Code easier to comprehend, allowing further changes and fun additions,
like room link tool.
More cosistent and more intuitive way of map interactions.

closes #6033

src/LabelInteractionHandler.cpp
@@ -1,0 +1,280 @@
+/***************************************************************************
+ *   Copyright (C) 2025 by Piotr Wilczynski - delwing@gmail.com            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "LabelInteractionHandler.h"
+
+#include "TArea.h"
+#include "TMap.h"
+#include "TMapLabel.h"
+#include "TRoomDB.h"
+
+#include "pre_guard.h"
+#include <QMap>
+#include <QAction>
+#include <QMenu>
+#include <QMouseEvent>
+#include <QMutableMapIterator>
+#include <QRectF>
+#include <QtGlobal>
+#include "post_guard.h"
+
+LabelInteractionHandler::LabelInteractionHandler(T2DMap& mapWidget)
+: mMapWidget(mapWidget)
+{
+}
+
+bool LabelInteractionHandler::matches(const T2DMap::MapInteractionContext& context) const
+{
+    if (!context.event || !mMapWidget.mpMap) {
+        return false;
+    }
+
+    switch (context.event->type()) {
+    case QEvent::MouseButtonPress:
+        if (!context.area || context.isCustomLineDrawing) {
+            return false;
+        }
+
+        if (context.button == Qt::LeftButton) {
+            return !context.isMapViewOnly;
+        }
+
+        if (context.button == Qt::RightButton) {
+            return true;
+        }
+
+        return false;
+    case QEvent::MouseMove:
+        return context.isLabelHighlighted || context.isMoveLabelActive;
+    case QEvent::MouseButtonRelease:
+        return context.button == Qt::LeftButton && context.isMoveLabelActive;
+    default:
+        return false;
+    }
+}
+
+bool LabelInteractionHandler::handle(T2DMap::MapInteractionContext& context)
+{
+    if (!context.event) {
+        return false;
+    }
+
+    switch (context.event->type()) {
+    case QEvent::MouseButtonPress:
+        return handleMousePress(context);
+    case QEvent::MouseMove:
+        return handleMouseMove(context);
+    case QEvent::MouseButtonRelease:
+        return handleMouseRelease(context);
+    default:
+        return false;
+    }
+}
+
+bool LabelInteractionHandler::handleMousePress(T2DMap::MapInteractionContext& context) const
+{
+    auto* area = context.area;
+    if (!area) {
+        if (context.button == Qt::RightButton) {
+            mMapWidget.mLabelHighlighted = false;
+            mMapWidget.update();
+        }
+        context.isLabelHighlighted = false;
+        return false;
+    }
+
+    if (context.button == Qt::LeftButton) {
+        if (area->mMapLabels.isEmpty()) {
+            return false;
+        }
+
+        QMutableMapIterator<int, TMapLabel> iterator(area->mMapLabels);
+        while (iterator.hasNext()) {
+            iterator.next();
+            auto mapLabel = iterator.value();
+            if (mapLabel.pos.z() != mMapWidget.mMapCenterZ) {
+                continue;
+            }
+
+            const float labelX = mapLabel.pos.x() * mMapWidget.mRoomWidth + mMapWidget.mRX;
+            const float labelY = mapLabel.pos.y() * mMapWidget.mRoomHeight * -1 + mMapWidget.mRY;
+            const QPoint click = context.widgetPosition;
+            const QRectF boundingRect(labelX, labelY, mapLabel.clickSize.width(), mapLabel.clickSize.height());
+            if (boundingRect.contains(click)) {
+                mapLabel.highlight = !mapLabel.highlight;
+                mMapWidget.mLabelHighlighted = mapLabel.highlight;
+                context.isLabelHighlighted = mMapWidget.mLabelHighlighted;
+                iterator.setValue(mapLabel);
+                mMapWidget.update();
+                return true;
+            }
+        }
+
+        mMapWidget.mLabelHighlighted = false;
+        context.isLabelHighlighted = false;
+        mMapWidget.update();
+
+        return false;
+    }
+
+    if (context.button == Qt::RightButton) {
+        bool highlightChanged = false;
+        bool labelFound = false;
+
+        QMutableMapIterator<int, TMapLabel> iterator(area->mMapLabels);
+        while (iterator.hasNext()) {
+            iterator.next();
+            auto mapLabel = iterator.value();
+
+            const bool isSameZLevel = qRound(mapLabel.pos.z()) == mMapWidget.mMapCenterZ;
+            bool shouldHighlight = false;
+            if (isSameZLevel) {
+                const float labelX = mapLabel.pos.x() * mMapWidget.mRoomWidth + mMapWidget.mRX;
+                const float labelY = mapLabel.pos.y() * mMapWidget.mRoomHeight * -1 + mMapWidget.mRY;
+                const QRectF boundingRect(labelX, labelY, mapLabel.clickSize.width(), mapLabel.clickSize.height());
+                shouldHighlight = boundingRect.contains(context.widgetPosition);
+            }
+
+            if (mapLabel.highlight != shouldHighlight) {
+                mapLabel.highlight = shouldHighlight;
+                iterator.setValue(mapLabel);
+                highlightChanged = true;
+            }
+
+            if (shouldHighlight) {
+                labelFound = true;
+            }
+        }
+
+        mMapWidget.mLabelHighlighted = labelFound;
+        context.isLabelHighlighted = labelFound;
+
+        if (highlightChanged || !labelFound) {
+            mMapWidget.update();
+        }
+
+        if (!labelFound) {
+            return false;
+        }
+
+        bool clearedRoomSelection = false;
+        if (!mMapWidget.mMultiSelectionSet.isEmpty()) {
+            mMapWidget.clearSelection();
+            mMapWidget.mMultiSelectionSet.clear();
+            clearedRoomSelection = true;
+        }
+
+        if (mMapWidget.mMultiSelectionHighlightRoomId != 0) {
+            mMapWidget.mMultiSelectionHighlightRoomId = 0;
+            clearedRoomSelection = true;
+        }
+
+        if (clearedRoomSelection) {
+            mMapWidget.updateSelectionWidget();
+        }
+
+        auto* popup = new QMenu(&mMapWidget);
+        popup->setToolTipsVisible(true);
+        popup->setAttribute(Qt::WA_DeleteOnClose);
+        mMapWidget.registerContextMenu(popup);
+
+        //: 2D Mapper context menu (label) item
+        auto* moveLabel = new QAction(mMapWidget.tr("Move"), popup);
+        //: 2D Mapper context menu item (label) tooltip
+        moveLabel->setToolTip(mMapWidget.tr("Move label"));
+        QObject::connect(moveLabel, &QAction::triggered, &mMapWidget, &T2DMap::slot_moveLabel);
+
+        //: 2D Mapper context menu (label) item
+        auto* deleteLabel = new QAction(mMapWidget.tr("Delete"), popup);
+        //: 2D Mapper context menu (label) item tooltip
+        deleteLabel->setToolTip(mMapWidget.tr("Delete label"));
+        QObject::connect(deleteLabel, &QAction::triggered, &mMapWidget, &T2DMap::slot_deleteLabel);
+
+        popup->addAction(moveLabel);
+        popup->addAction(deleteLabel);
+
+        mMapWidget.mPopupMenu = true;
+        popup->popup(mMapWidget.mapToGlobal(context.widgetPosition));
+        mMapWidget.update();
+
+        return true;
+    }
+
+    return false;
+}
+
+bool LabelInteractionHandler::handleMouseMove(T2DMap::MapInteractionContext& context) const
+{
+    if (!mMapWidget.mpMap || !mMapWidget.mpMap->mpRoomDB) {
+        return false;
+    }
+
+    if (mMapWidget.mLabelHighlighted) {
+        auto* area = mMapWidget.mpMap->mpRoomDB->getArea(mMapWidget.mAreaID);
+        if (!area || area->mMapLabels.isEmpty()) {
+            return false;
+        }
+
+        bool needUpdate = false;
+        bool needToSave = false;
+        QMapIterator<int, TMapLabel> iterator(area->mMapLabels);
+        while (iterator.hasNext()) {
+            iterator.next();
+            auto mapLabel = iterator.value();
+            if (qRound(mapLabel.pos.z()) != mMapWidget.mMapCenterZ) {
+                continue;
+            }
+            if (!mapLabel.highlight) {
+                continue;
+            }
+
+            mapLabel.pos = QVector3D(static_cast<float>(context.mapX), static_cast<float>(context.mapY), static_cast<float>(mMapWidget.mMapCenterZ));
+            area->mMapLabels[iterator.key()] = mapLabel;
+            needUpdate = true;
+            if (!mapLabel.temporary) {
+                needToSave = true;
+            }
+        }
+        if (needUpdate) {
+            mMapWidget.update();
+            if (needToSave) {
+                mMapWidget.mpMap->setUnsaved(__func__);
+            }
+        }
+        return true;
+    }
+
+    if (mMapWidget.mMoveLabel) {
+        mMapWidget.mMoveLabel = false;
+        return false;
+    }
+
+    return false;
+}
+
+bool LabelInteractionHandler::handleMouseRelease(T2DMap::MapInteractionContext& context) const
+{
+    if (context.button == Qt::LeftButton) {
+        if (mMapWidget.mMoveLabel) {
+            mMapWidget.mMoveLabel = false;
+        }
+    }
+
+    return false;
+}

src/CMakeLists.txt
@@ -108,6 +108,16 @@ set(mudlet_SRCS
     ShortcutsManager.cpp
     SingleLineTextEdit.cpp
     T2DMap.cpp
+    CustomLineDrawContextMenuHandler.cpp
+    CustomLineDrawHandler.cpp
+    CustomLineEditContextMenuHandler.cpp
+    CustomLineEditHandler.cpp
+    LabelInteractionHandler.cpp
+    PanInteractionHandler.cpp
+    RoomContextMenuHandler.cpp
+    RoomMoveActivationHandler.cpp
+    RoomMoveDragHandler.cpp
+    SelectionRectangleHandler.cpp
     TAccessibleTextEdit.cpp
     TAction.cpp
     TAlias.cpp
@@ -291,6 +301,15 @@ set(mudlet_HDRS
     ShortcutsManager.h
     SingleLineTextEdit.h
     T2DMap.h
+    CustomLineDrawContextMenuHandler.h
+    CustomLineDrawHandler.h
+    CustomLineEditContextMenuHandler.h
+    CustomLineEditHandler.h
+    LabelInteractionHandler.h
+    PanInteractionHandler.h
+    RoomMoveActivationHandler.h
+    RoomMoveDragHandler.h
+    SelectionRectangleHandler.h
     TAccessibleConsole.h
     TAccessibleTextEdit.h
     TAction.h

src/CustomLineDrawContextMenuHandler.cpp
@@ -1,0 +1,115 @@
+/***************************************************************************
+ *   Copyright (C) 2025 by Piotr Wilczynski - delwing@gmail.com            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "CustomLineDrawContextMenuHandler.h"
+
+#include "TMap.h"
+#include "TRoom.h"
+#include "TRoomDB.h"
+#include "utils.h"
+
+#include "pre_guard.h"
+#include <QAction>
+#include <QEvent>
+#include <QMenu>
+#include <QMouseEvent>
+#include "post_guard.h"
+
+CustomLineDrawContextMenuHandler::CustomLineDrawContextMenuHandler(T2DMap& mapWidget)
+: mMapWidget(mapWidget)
+{
+}
+
+bool CustomLineDrawContextMenuHandler::matches(const T2DMap::MapInteractionContext& context) const
+{
+    if (!context.event || !mMapWidget.mpMap) {
+        return false;
+    }
+
+    if (context.event->type() != QEvent::MouseButtonRelease) {
+        return false;
+    }
+
+    if (context.button != Qt::RightButton) {
+        return false;
+    }
+
+    if (!context.isCustomLineDrawing || context.customLinesRoomFrom <= 0) {
+        return false;
+    }
+
+    return true;
+}
+
+bool CustomLineDrawContextMenuHandler::handle(T2DMap::MapInteractionContext& context)
+{
+    if (!mMapWidget.mpMap || !mMapWidget.mpMap->mpRoomDB) {
+        return false;
+    }
+
+    if (context.isDialogLocked) {
+        return true;
+    }
+
+    TRoom* room = mMapWidget.mpMap->mpRoomDB->getRoom(context.customLinesRoomFrom);
+    if (!room) {
+        return false;
+    }
+
+    auto popup = new QMenu(&mMapWidget);
+    popup->setToolTipsVisible(true);
+    popup->setAttribute(Qt::WA_DeleteOnClose);
+    mMapWidget.registerContextMenu(popup);
+
+    //: 2D Mapper context menu (drawing custom exit line) item
+    auto customLineUndoLastPoint = new QAction(T2DMap::tr("Undo"), &mMapWidget);
+    //: 2D Mapper context menu (drawing custom exit line) item tooltip
+    customLineUndoLastPoint->setToolTip(T2DMap::tr("Undo last point"));
+    if (room->customLines.value(context.customLinesRoomExit).count() > 1) {
+        QObject::connect(customLineUndoLastPoint, &QAction::triggered, &mMapWidget, &T2DMap::slot_undoCustomLineLastPoint);
+    } else {
+        customLineUndoLastPoint->setEnabled(false);
+    }
+
+    //: 2D Mapper context menu (drawing custom exit line) item name (but not used as display text as that is set separately)
+    auto customLineProperties = new QAction(T2DMap::tr("Properties"), &mMapWidget);
+    //: 2D Mapper context menu (drawing custom exit line) item display text (has to be entered separately as the ... would get stripped off otherwise)
+    customLineProperties->setText(T2DMap::tr("properties..."));
+    //: 2D Mapper context menu (drawing custom exit line) item tooltip
+    customLineProperties->setToolTip(utils::richText(T2DMap::tr("Change the properties of this line")));
+    QObject::connect(customLineProperties, &QAction::triggered, &mMapWidget, &T2DMap::slot_customLineProperties);
+
+    //: 2D Mapper context menu (drawing custom exit line) item
+    auto customLineFinish = new QAction(T2DMap::tr("Finish"), &mMapWidget);
+    //: 2D Mapper context menu (drawing custom exit line) item tooltip
+    customLineFinish->setToolTip(utils::richText(T2DMap::tr("Finish drawing this line")));
+    QObject::connect(customLineFinish, &QAction::triggered, &mMapWidget, &T2DMap::slot_doneCustomLine);
+
+    room->calcRoomDimensions();
+
+    popup->addAction(customLineUndoLastPoint);
+    popup->addAction(customLineProperties);
+    popup->addAction(customLineFinish);
+
+    mMapWidget.mPopupMenu = true;
+    popup->popup(mMapWidget.mapToGlobal(context.widgetPosition));
+    mMapWidget.update();
+
+    return true;
+}

src/CustomLineDrawContextMenuHandler.h
@@ -1,0 +1,34 @@
+/***************************************************************************
+ *   Copyright (C) 2025 by Piotr Wilczynski - delwing@gmail.com            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#pragma once
+
+#include "T2DMap.h"
+
+class CustomLineDrawContextMenuHandler : public T2DMap::IInteractionHandler
+{
+public:
+    explicit CustomLineDrawContextMenuHandler(T2DMap& mapWidget);
+
+    bool matches(const T2DMap::MapInteractionContext& context) const override;
+    bool handle(T2DMap::MapInteractionContext& context) override;
+
+private:
+    T2DMap& mMapWidget;
+};

src/CustomLineDrawHandler.cpp
@@ -1,0 +1,86 @@
+/***************************************************************************
+ *   Copyright (C) 2025 by Piotr Wilczynski - delwing@gmail.com            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "CustomLineDrawHandler.h"
+
+#include "TMap.h"
+#include "TRoom.h"
+#include "TRoomDB.h"
+
+#include "pre_guard.h"
+#include <QEvent>
+#include <QMouseEvent>
+#include <QPointF>
+#include "post_guard.h"
+
+CustomLineDrawHandler::CustomLineDrawHandler(T2DMap& mapWidget)
+: mMapWidget(mapWidget)
+{
+}
+
+bool CustomLineDrawHandler::matches(const T2DMap::MapInteractionContext& context) const
+{
+    if (!context.event || !mMapWidget.mpMap) {
+        return false;
+    }
+
+    if (context.event->type() != QEvent::MouseButtonPress) {
+        return false;
+    }
+
+    if (context.button != Qt::LeftButton) {
+        return false;
+    }
+
+    if (!context.isCustomLineDrawing) {
+        return false;
+    }
+
+    if (context.customLinesRoomFrom <= 0) {
+        return false;
+    }
+
+    return true;
+}
+
+bool CustomLineDrawHandler::handle(T2DMap::MapInteractionContext& context)
+{
+    if (!mMapWidget.mpMap || !mMapWidget.mpMap->mpRoomDB) {
+        return false;
+    }
+
+    if (context.isDialogLocked) {
+        return true;
+    }
+
+    TRoom* room = mMapWidget.mpMap->mpRoomDB->getRoom(context.customLinesRoomFrom);
+
+    if (!room) {
+        return false;
+    }
+
+    const float mapX = static_cast<float>(context.mapX);
+    const float mapY = static_cast<float>(context.mapY);
+
+    room->customLines[context.customLinesRoomExit].push_back(QPointF(mapX, mapY));
+    room->calcRoomDimensions();
+    mMapWidget.repaint();
+
+    return true;
+}

src/CustomLineDrawHandler.h
@@ -1,0 +1,37 @@
+/***************************************************************************
+ *   Copyright (C) 2025 by Piotr Wilczynski - delwing@gmail.com            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef MUDLET_CUSTOMLINEDRAWHANDLER_H
+#define MUDLET_CUSTOMLINEDRAWHANDLER_H
+
+#include "T2DMap.h"
+
+class CustomLineDrawHandler : public T2DMap::IInteractionHandler
+{
+public:
+    explicit CustomLineDrawHandler(T2DMap& mapWidget);
+
+    bool matches(const T2DMap::MapInteractionContext& context) const override;
+    bool handle(T2DMap::MapInteractionContext& context) override;
+
+private:
+    T2DMap& mMapWidget;
+};
+
+#endif // MUDLET_CUSTOMLINEDRAWHANDLER_H

commit 44a7d9ff2216ceee51d99d4f0af7bb3c6f8f8285

fix label move in 2D map, improve highlight visual
- enable mouse tracking in slot_moveLabel() so mouse moves are
  received without holding a button
- snap label to cursor immediately when Move is selected
- left-click during move mode places the label and ends move
- show help message during label move
- change label highlight from orange overlay to orange outline
- extract paintLabelHighlight() to deduplicate four rendering sites

Broken since 35ec27678 which split map mouse interaction code
into separate handler classes.

src/LabelInteractionHandler.cpp
@@ -61,7 +61,7 @@ bool LabelInteractionHandler::matches(const T2DMap::MapInteractionContext& conte
     case QEvent::MouseMove:
         return context.isLabelHighlighted || context.isMoveLabelActive;
     case QEvent::MouseButtonRelease:
-        return context.button == Qt::LeftButton && context.isMoveLabelActive;
+        return false;
     default:
         return false;
     }
@@ -98,6 +98,29 @@ bool LabelInteractionHandler::handleMousePress(T2DMap::MapInteractionContext& co
     }
 
     if (context.button == Qt::LeftButton) {
+        if (mMapWidget.mMoveLabel) {
+            mMapWidget.mMoveLabel = false;
+            context.isMoveLabelActive = false;
+            mMapWidget.setMouseTracking(false);
+            mMapWidget.mLabelHighlighted = false;
+            context.isLabelHighlighted = false;
+            mMapWidget.mHelpMsg.clear();
+
+            if (!area->mMapLabels.isEmpty()) {
+                QMutableMapIterator<int, TMapLabel> iterator(area->mMapLabels);
+                while (iterator.hasNext()) {
+                    iterator.next();
+                    auto& mapLabel = iterator.value();
+                    if (mapLabel.highlight) {
+                        mapLabel.highlight = false;
+                    }
+                }
+            }
+
+            mMapWidget.update();
+            return true;
+        }
+
         if (area->mMapLabels.isEmpty()) {
             return false;
         }
@@ -258,21 +281,11 @@ bool LabelInteractionHandler::handleMouseMove(T2DMap::MapInteractionContext& con
         return true;
     }
 
-    if (mMapWidget.mMoveLabel) {
-        mMapWidget.mMoveLabel = false;
-        return false;
-    }
-
     return false;
 }
 
 bool LabelInteractionHandler::handleMouseRelease(T2DMap::MapInteractionContext& context) const
 {
-    if (context.button == Qt::LeftButton) {
-        if (mMapWidget.mMoveLabel) {
-            mMapWidget.mMoveLabel = false;
-        }
-    }
-
+    Q_UNUSED(context)
     return false;
 }

src/T2DMap.cpp
@@ -3652,6 +3652,21 @@ void T2DMap::slot_deleteCustomExitLine()
 void T2DMap::slot_moveLabel()
 {
     mMoveLabel = true;
+    setMouseTracking(true);
+    //: 2D Mapper big, bottom of screen help message when moving a label
+    mHelpMsg = tr("Click to finish moving the label.");
+
+    auto [mx, my] = getMousePosition();
+    auto* area = mpMap->mpRoomDB->getArea(mAreaID);
+    if (area) {
+        for (auto& mapLabel : area->mMapLabels) {
+            if (mapLabel.highlight) {
+                mapLabel.pos = QVector3D(static_cast<float>(mx), static_cast<float>(my), static_cast<float>(mMapCenterZ));
+            }
+        }
+    }
+
+    update();
 }
 
 void T2DMap::slot_deleteLabel()