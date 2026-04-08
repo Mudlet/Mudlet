c29ab2cfb56da691bed73efb1f9f4b717007f881
Vadim Peretokin <vperetokin@hey.com>
2026-04-08 19:02:18 +0200

    Revert ineffective NOLINT comment

--- test/functional_tests/dlgTriggerEditorUndoRedoTest.cpp
+++ test/functional_tests/dlgTriggerEditorUndoRedoTest.cpp
@@ -17,8 +17,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-// NOLINTBEGIN(clang-diagnostic-error) - clangd falsely flags private member access; dlgTriggerEditorUndoRedoTest is a friend class of dlgTriggerEditor
-
 #include <QtTest/QtTest>
 
 #include "EditorUndoStack.h"
