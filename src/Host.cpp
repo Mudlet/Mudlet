b2bee6822bf8e991dc150936c25544a225bd48f5
John McKisson <john.mckisson@gmail.com>
2026-04-08 14:55:54 -0400

    use qsl for font name + package construction
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- src/Host.cpp
+++ src/Host.cpp
@@ -2678,7 +2678,7 @@ void Host::installPackageFonts(const QString& packageName)
 
         if (filePath.endsWith(QLatin1String(".otf"), Qt::CaseInsensitive) || filePath.endsWith(QLatin1String(".ttf"), Qt::CaseInsensitive)
             || filePath.endsWith(QLatin1String(".ttc"), Qt::CaseInsensitive) || filePath.endsWith(QLatin1String(".otc"), Qt::CaseInsensitive)) {
-            mudlet::self()->mFontManager.loadFont(filePath, getName() + QChar('/') + packageName);
+            mudlet::self()->mFontManager.loadFont(filePath, qsl("%1/%2").arg(getName(), packageName));
         }
     }
 }
