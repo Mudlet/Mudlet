Mudlet/Mudlet

commit 09054e513a49ce835954bfd254cd9dce360e36c7

Fix: Several Wordwrap issues - Indent, HangingIndent, international character widths  (#7714)
<!-- Keep the title short & concise so anyone non-technical can
understand it,
     the title appears in PTB changelogs -->
#### Brief overview of PR changes/additions

- Created `TTextProperties.h` so TBuffer and TTextEdit both share the
same functions for checking the width of graphemes.
- Added the functions necessary to set hanging indent for
(mini-)consoles (exposes Lua function:
`setWindowWrapHangingIndent(str:window, int:hangingIndent)`)
- Deduplicated the process of appending and wrapping text in TBuffer to
follow, generally:
- `append` calls `appendLine` and `wrapLine` and handles buffer
shrinking.
    - `appendLine`  adds text to buffer.
- `wrapLine` calls `getWrapInfo` then applies wrap, also logs the
wrapped lines.
- `getWrapInfo` checks the width of each grapheme for determining wrap
points instead of the previous method of treating each QChar as width
one.

<details>
<summary>More specifically</summary>

Created TTextProperties.h for one shared source of grapheme width for
both TTextEdit and TBuffer:
- `graphemeInfo::getBaseCharacter` copied over from
`TTextEdit::getGraphemeBaseCharacter`
- `graphemeInfo::getWidth(unicode, mWideAmbigousWidthGlyphs)` copied
from `TTextEdit::getGraphemeWidth(unicode)` (with some modification)


Added `TLuaInterpreter::setWindowWrapHangingIndent` /
`TConsole::setHangingIndentCount` / `TBuffer::setWrapHangingIndent` to
allow setting hanging indents for any (mini)console


Added `WrapInfo(bool isNewline, bool needsIndent, int firstChar, int
lastChar)` class to TBuffer.h for storing information on whether to
indent / where to snip a line to properly wrap it.

Added the inline method `TBuffer::getWrapInfo(const QString& lineText,
bool isNewline, const int maxWidth, const int indent, const int
hangingIndent)` which takes information on the current line of text and
returns a `QList<WrapInfo>` indicating the start / end indices and

src/TConsole.cpp
@@ -1144,7 +1144,7 @@ void TConsole::insertLink(const QString& text, QStringList& func, QStringList& h
             buffer.applyLink(P, P2, func, hint, luaReference);
             if (text.indexOf("\n") != -1) {
                 const int y_tmp = mUserCursor.y();
-                const int down = buffer.wrapLine(mUserCursor.y(), mpHost->mScreenWidth, mpHost->mWrapIndentCount, mFormatCurrent);
+                const int down = buffer.wrapLine(mUserCursor.y(), mpHost->mScreenWidth, mpHost->mWrapIndentCount, mpHost->mWrapHangingIndentCount);
                 mUpperPane->needUpdate(y_tmp, y_tmp + down + 1);
                 const int y_neu = y_tmp + down;
                 const int x_adjust = text.lastIndexOf("\n");
@@ -1181,7 +1181,7 @@ void TConsole::insertText(const QString& text, QPoint P)
             buffer.insertInLine(mUserCursor, text, mFormatCurrent);
             const int y_tmp = mUserCursor.y();
             if (text.indexOf(QChar::LineFeed) != -1) {
-                const int down = buffer.wrapLine(y_tmp, mpHost->mScreenWidth, mpHost->mWrapIndentCount, mFormatCurrent);
+                const int down = buffer.wrapLine(y_tmp, mpHost->mScreenWidth, mpHost->mWrapIndentCount, mpHost->mWrapHangingIndentCount);
                 mUpperPane->needUpdate(y_tmp, y_tmp + down + 1);
             } else {
                 mUpperPane->needUpdate(y_tmp, y_tmp + 1);
@@ -1355,8 +1355,7 @@ void TConsole::luaWrapLine(int line)
     if (!mpHost) {
         return;
     }
-    TChar ch(this);
-    buffer.wrapLine(line, mWrapAt, mIndentCount, ch);
+    buffer.wrapLine(line, mWrapAt, mIndentCount, mHangingIndentCount);
 }
 
 bool TConsole::setFontSize(int size)
@@ -1699,7 +1698,7 @@ void TConsole::printCommand(QString& msg)
                 QPoint P(promptEnd, lineBeforeNewContent);
                 const TChar format(mCommandFgColor, mCommandBgColor);
                 buffer.insertInLine(P, msg, format);
-                const int down = buffer.wrapLine(lineBeforeNewContent, mpHost->mScreenWidth, mpHost->mWrapIndentCount, mFormatCurrent);
+                const int down = buffer.wrapLine(lineBeforeNewContent, mpHost->mScreenWidth, mpHost->mWrapIndentCount, mpHost->mWrapHangingIndentCount);
 
                 mUpperPane->needUpdate(lineBeforeNewContent, lineBeforeNewContent + 1 + down);
                 mLowerPane->needUpdate(lineBeforeNewContent, lineBeforeNewContent + 1 + down);

src/TBuffer.h
@@ -53,6 +53,18 @@ class Host;
 class QTextCodec;
 class TConsole;
 
+class WrapInfo
+{
+    friend class TBuffer;
+
+public:
+    const bool isNewline;
+    const bool needsIndent;
+    const int firstChar;
+    const int lastChar;
+    WrapInfo(bool isNewline, bool needsIndent, int firstChar, int lastChar) : isNewline(isNewline), needsIndent(needsIndent), firstChar(firstChar), lastChar(lastChar) {}
+};
+
 class TChar
 {
     friend class TBuffer;
@@ -253,7 +265,7 @@ public:
     QPoint insert(QPoint&, const QString& text, int, int, int, int, int, int, bool bold, bool italics, bool underline, bool strikeout);
     bool insertInLine(QPoint& cursor, const QString& what, const TChar& format);
     void expandLine(int y, int count, TChar&);
-    int wrapLine(int startLine, int screenWidth, int indentSize, TChar& format);
+    int wrapLine(int startLine, int maxWidth, int indentSize, int hangingIndentSize);
     void log(int, int);
     int skipSpacesAtBeginOfLine(const int row, const int column);
     void addLink(bool, const QString& text, QStringList& command, QStringList& hint, TChar format, QVector<int> luaReference = QVector<int>());
@@ -262,7 +274,6 @@ public:
     bool isEmpty() const { return buffer.size() == 0; }
     QString& line(int lineNumber);
     int find(int line, const QString& what, int pos);
-    int wrap(int);
     QStringList split(int line, const QString& splitter);
     QStringList split(int line, const QRegularExpression& splitter);
     bool replaceInLine(QPoint& start, QPoint& end, const QString& with, TChar& format);
@@ -283,8 +294,10 @@ public:
     // Only the bits within TChar::TestMask are considered for formatting:
     void append(const QString& chunk, const int sub_start, const int sub_end, const TChar format, const int linkID = 0);
     void appendLine(const QString& chunk, const int sub_start, const int sub_end, const QColor& fg, const QColor& bg, TChar::AttributeFlags flags = TChar::None, const int linkID = 0);
+    void appendEmptyLine();
     void setWrapAt(int i) { mWrapAt = i; }
     void setWrapIndent(int i) { mWrapIndent = i; }
+    void setWrapHangingIndent(int i) { mWrapHangingIndent = i; }
     void updateColors();
     TBuffer copy(QPoint&, QPoint&);
     TBuffer cut(QPoint&, QPoint&);
@@ -323,6 +336,7 @@ public:
 
 
 private:
+    inline QList<WrapInfo> getWrapInfo(const QString& lineText, bool isNewline, const int maxWidth, const int indent, const int hangingIndent);
     void shrinkBuffer();
     int calculateWrapPosition(int lineNumber, int begin, int end);
     void handleNewLine();

src/TConsole.h
@@ -145,6 +145,12 @@ public:
         buffer.setWrapIndent(count);
     }
 
+    void setHangingIndentCount(int count)
+    {
+        mHangingIndentCount = count;
+        buffer.setWrapHangingIndent(count);
+    }
+
     TLinkStore &getLinkStore() { return buffer.mLinkStore; }
     void echo(const QString&);
     bool moveCursor(int x, int y);

src/TLuaInterpreter.cpp
@@ -5103,6 +5103,7 @@ void TLuaInterpreter::initLuaGlobals()
     lua_register(pGlobalLua, "setWindowWrap", TLuaInterpreter::setWindowWrap);
     lua_register(pGlobalLua, "getWindowWrap", TLuaInterpreter::getWindowWrap);
     lua_register(pGlobalLua, "setWindowWrapIndent", TLuaInterpreter::setWindowWrapIndent);
+    lua_register(pGlobalLua, "setWindowWrapHangingIndent", TLuaInterpreter::setWindowWrapHangingIndent);
     lua_register(pGlobalLua, "resetFormat", TLuaInterpreter::resetFormat);
     lua_register(pGlobalLua, "moveCursorEnd", TLuaInterpreter::moveCursorEnd);
     lua_register(pGlobalLua, "getLastLineNumber", TLuaInterpreter::getLastLineNumber);

src/TLuaInterpreter.h
@@ -365,6 +365,7 @@ public:
     static int setWindowWrap(lua_State*);
     static int getWindowWrap(lua_State*);
     static int setWindowWrapIndent(lua_State*);
+    static int setWindowWrapHangingIndent(lua_State*);
     static int resetFormat(lua_State*);
     static int moveCursorEnd(lua_State*);
     static int getLastLineNumber(lua_State*);

src/TLuaInterpreterUI.cpp
@@ -3047,6 +3047,16 @@ int TLuaInterpreter::setWindowWrapIndent(lua_State* L)
     return 0;
 }
 
+//Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#setWindowWrapHangingIndent
+int TLuaInterpreter::setWindowWrapHangingIndent(lua_State* L)
+{
+    const QString windowName {WINDOW_NAME(L, 1)};
+    const int luaFrom = getVerifiedInt(L, __func__, 2, "wrapTo");
+    auto console = CONSOLE(L, windowName);
+    console->setHangingIndentCount(luaFrom);
+    return 0;
+}
+
 // Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#showWindow
 int TLuaInterpreter::showWindow(lua_State* L)
 {

src/TTextEdit.cpp
@@ -36,6 +36,7 @@
 #include "uiawrapper.h"
 #endif
 #include "widechar_width.h"
+#include "TTextProperties.h"
 
 #include "pre_guard.h"
 #include <chrono>
@@ -427,35 +428,6 @@ void TTextEdit::scrollDown(int lines)
     }
 }
 
-// Extract the base (first) part which will be one or two QChars
-// and if they ARE a surrogate pair convert them back to the single
-// Unicode codepoint (needs around 21 bits, can be contained in a
-// 32bit unsigned integer) value:
-inline uint TTextEdit::getGraphemeBaseCharacter(const QString& str) const
-{
-    if (str.isEmpty()) {
-        return 0;
-    }
-
-    QChar first = str.at(0);
-    if (first.isSurrogate() && str.size() >= 2) {
-        QChar second = str.at(1);
-        if (first.isHighSurrogate() && second.isLowSurrogate()) {
-            return QChar::surrogateToUcs4(first, second);
-        }
-
-        if (Q_UNLIKELY(first.isLowSurrogate() && second.isHighSurrogate())) {
-            qDebug().noquote().nospace() << "TTextEdit::getGraphemeBaseCharacter(\"str\") INFO - passed a QString comprising a Low followed by a High surrogate QChar, this is not expected, they will be swapped around to try and recover but if this causes mojibake (text corrupted into meaningless symbols) please report this to the developers!";
-            return QChar::surrogateToUcs4(second, first);
-        }
-
-        // str format error ?
-        return first.unicode();
-    }
-
-    return first.unicode();
-}
-
 void TTextEdit::drawLine(QPainter& painter, int lineNumber, int lineOfScreen, int* offset) const
 {
     QPoint cursor(-mCursorX, lineOfScreen);
@@ -614,7 +586,7 @@ void TTextEdit::drawLine(QPainter& painter, int lineNumber, int lineOfScreen, in
 
 int TTextEdit::drawGraphemeBackground(QPainter& painter, QVector<QColor>& fgColors, QVector<QRect>& textRects, QVector<QString>& graphemes, QVector<int>& charWidths, QPoint& cursor, const QString& grapheme, const int column, const int line, TChar& charStyle) const
 {
-    uint unicode = getGraphemeBaseCharacter(grapheme);
+    uint unicode = graphemeInfo::getBaseCharacter(grapheme);
     int charWidth = 0;
 
     switch (mpConsole->mControlCharacter) {
@@ -634,7 +606,7 @@ int TTextEdit::drawGraphemeBackground(QPainter& painter, QVector<QColor>& fgColo
             }
 
         } else {
-            charWidth = getGraphemeWidth(unicode);
+            charWidth = graphemeInfo::getWidth(unicode, mWideAmbigousWidthGlyphs);
             graphemes.append((charWidth < 1) ? QChar() : grapheme);
         }
         break;
@@ -721,18 +693,9 @@ void TTextEdit::drawGraphemeForeground(QPainter& painter, const QColor& fgColor,
 
 int TTextEdit::getGraphemeWidth(uint unicode) const
 {
-    // https://github.com/ridiculousfish/widecharwidth/issues/11
-    if (unicode == 0x1F6E1 || unicode == 0x2318) {
-        return 2;
-    }
-
-    switch (widechar_wcwidth(unicode)) {
-    case 1: // Draw as normal/narrow
-        return 1;
-    case 2: // Draw as wide
-        return 2;
-    case widechar_nonprint:
 #if defined(DEBUG_CODEPOINT_PROBLEMS)
+    switch (widechar_wcwidth(unicode)) {
+    case widechar_nonprint:
         // -1 = The character is not printable - so put in a replacement
         // character instead - and so it can be seen it need a space:
         if (!mIsLowerPane) {
@@ -748,10 +711,8 @@ int TTextEdit::getGraphemeWidth(uint unicode) const
                 mProblemCodepoints.insert(unicode, std::tuple{++count, reason});
             }
         }
-#endif
         return 0;
     case widechar_non_character:
-#if defined(DEBUG_CODEPOINT_PROBLEMS)
         // -7 = The character is a non-character - we might make use of some of them for
         // internal purposes in the future (in which case we might need additional code here
         // or elsewhere) but we don't right now:
@@ -768,10 +729,8 @@ int TTextEdit::getGraphemeWidth(uint unicode) const
                 mProblemCodepoints.insert(unicode, std::tuple{++count, reason});
             }
         }
-#endif
         return 0;
     case widechar_combining:
-#if defined(DEBUG_CODEPOINT_PROBLEMS)
         // -2 = The character is a zero-width combiner - and should not be
         // present as the FIRST codepoint in a grapheme so this indicates an
         // error somewhere - so put in the replacement character
@@ -788,13 +747,8 @@ int TTextEdit::getGraphemeWidth(uint unicode) const
                 mProblemCodepoints.insert(unicode, std::tuple{++count, reason});
             }
         }
-#endif
         return 0;
-    case widechar_ambiguous:
-        // -3 = The character is East-Asian ambiguous width.
-        return mWideAmbigousWidthGlyphs ? 2 : 1;
     case widechar_private_use:
-#if defined(DEBUG_CODEPOINT_PROBLEMS)
         // -4 = The character is for private use - we cannot know for certain
         // what width to used - let's assume 1 for the moment:
         if (!mIsLowerPane) {
@@ -810,10 +764,8 @@ int TTextEdit::getGraphemeWidth(uint unicode) const
                 mProblemCodepoints.insert(unicode, std::tuple{++count, reason});
             }
         }
-#endif
         return 1;
     case widechar_unassigned:
-#if defined(DEBUG_CODEPOINT_PROBLEMS)
         // -5 = The character is unassigned - at least for the Unicode version
         // that our widechar_wcwidth(...) was built for - assume 1:
         if (!mIsLowerPane) {
@@ -829,15 +781,11 @@ int TTextEdit::getGraphemeWidth(uint unicode) const
                 mProblemCodepoints.insert(unicode, std::tuple{++count, reason});
             }
         }
-    #endif
         return 1;
-    case widechar_widened_in_9: // -6 = Width is 1 in Unicode 8, 2 in Unicode 9+.
-        return 2;
-    default:
-        return 1; // Got an uncoded return value from widechar_wcwidth(...)
     }
+#endif
+    return graphemeInfo::getWidth(unicode, mWideAmbigousWidthGlyphs);
 }
-
 void TTextEdit::drawForeground(QPainter& painter, const QRect& r)
 {
     qreal dpr = devicePixelRatioF();
@@ -1276,7 +1224,7 @@ int TTextEdit::convertMouseXToBufferX(const int mouseX, const int lineNumber, bo
             // This could contain a surrogate pair (i.e. pair of QChars) and/or
             // include suffixed combining diacritical marks (additional QChars):
             const QString grapheme = lineText.mid(indexOfChar, nextBoundary - indexOfChar);
-            const uint unicode = getGraphemeBaseCharacter(grapheme);
+            const uint unicode = graphemeInfo::getBaseCharacter(grapheme);
             if (unicode == '\t') {
                 charWidth = mTabStopwidth - (column % mTabStopwidth);
             } else {
@@ -1687,7 +1635,7 @@ void TTextEdit::slot_copySelectionToClipboardImage()
             // Width in "normal" width equivalent of this grapheme:
             int charWidth{};
             const QString grapheme = lineText.mid(indexOfChar, nextBoundary - indexOfChar);
-            const uint unicode = getGraphemeBaseCharacter(grapheme);
+            const uint unicode = graphemeInfo::getBaseCharacter(grapheme);
             if (unicode == '\t') {
                 charWidth = mTabStopwidth - (column % mTabStopwidth);
             } else {

commit 65c6a28d364f85db5dbdebe4299a1d02dc1d46f4

Fix: text wrapping uses full available column width on resize (#9280)
#### Brief overview of PR changes/additions

When the main Mudlet window was resized, text kept wrapping at the
previous width instead of using the new one, leaving unused space on the
right. This PR fixes that so wrapping always matches the actual window
size.

It also handles the multi-profile case: when several profiles share one
Mudlet window, resizing while viewing one profile now also corrects the
wrap width for the other profiles (which are hidden in background tabs),
so their text wraps correctly the moment you switch to them.

Profiles opened in their own detached window are unaffected — they
already manage their own size.

#### Motivation for adding to Mudlet

Text wrapping at a fraction of the available width wastes screen space
and makes the main console harder to read, particularly on wider screens
or layouts. This restores the behaviour present in v4.19.1, including
for users who run multiple profiles in the same Mudlet window.

#### Other info (issues closed, discussion etc)

Regression introduced by #7714.

<img width="1728" height="1031" alt="Screenshot 2026-05-16 at 3 43
55 PM"
src="https://github.com/user-attachments/assets/897fe60c-698a-4732-93fe-6f0fb1f0062e"
/>

---------

Signed-off-by: Michael Conley <sousesider@gmail.com>
Co-authored-by: Stephen Lyons <slysven@virginmedia.com>

src/TConsole.cpp
@@ -715,6 +715,51 @@ void TConsole::resizeEvent(QResizeEvent* event)
         layerCommandLine->move(0, mpBaseVFrame->height() - layerCommandLine->height());
     }
 
+    // Sync Host dimensions on resize so wraps and NAWS reflect the current pane width.
+    if ((mType & MainConsole) && !mpHost.isNull() && mUpperPane && !mUpperPane->visibleRegion().isEmpty()) {
+        const int paneWidthPx = mUpperPane->visibleRegion().boundingRect().width();
+        auto syncHost = [paneWidthPx](Host* host, const QWidget* paneForFont) {
+            if (!host || !paneForFont) {
+                return;
+            }
+            const int fontWidth = QFontMetrics(paneForFont->font()).averageCharWidth();
+            if (fontWidth <= 0) {
+                return;
+            }
+            const int cols = qMax(40, paneWidthPx / fontWidth);
+            if (cols > 0 && cols != host->mScreenWidth) {
+                host->setScreenDimensions(cols, host->mScreenHeight);
+                QTimer::singleShot(0, host, &Host::updateDisplayDimensions);
+            }
+        };
+
+        syncHost(mpHost.data(), mUpperPane);
+
+        // Detached profiles have their own pixel width; only propagate from a main-window console.
+        mudlet* const app = mudlet::self();
+        const bool inMainWindow = app && !app->getDetachedWindows().contains(mpHost->getName());
+        if (inMainWindow) {
+            for (const auto& otherHostPtr : app->getHostManager()) {
+                Host* otherHost = otherHostPtr.data();
+                if (!otherHost || otherHost == mpHost.data()) {
+                    continue;
+                }
+                // Skip detached profiles: different container, different width.
+                if (app->getDetachedWindows().contains(otherHost->getName())) {
+                    continue;
+                }
+                if (!otherHost->mpConsole || !otherHost->mpConsole->mUpperPane) {
+                    continue;
+                }
+                // Visible siblings (multi-view) handle their own resizeEvent.
+                if (!otherHost->mpConsole->mUpperPane->visibleRegion().isEmpty()) {
+                    continue;
+                }
+                syncHost(otherHost, otherHost->mpConsole->mUpperPane);
+            }
+        }
+    }
+
     emit resized(event);
     QWidget::resizeEvent(event);
 