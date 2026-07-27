Mudlet/Mudlet

commit acd82333aeb6dcb1078685c84f57092a74ee8da9

Add multiple map views support (#8743)
#### Brief overview of PR changes/additions
Open additional 2D map windows showing different areas of the same map.
Views are dockable and controlled via menu or Lua.

  New Lua functions:
  - `createMapView([areaId])` - open a new map window
  - `closeMapView(viewId)` - close a specific window
  - `closeAllMapViews()` - close all extra windows
  - `getMapViewIds()` - list open windows
  - `getMapViewInfo(viewId)` - get window state

  Extended with optional `viewId` parameter:
  - `centerview(roomId, [viewId])`
  - `setMapZoom(zoom, [areaId], [viewId])`
  - `getMapZoom([areaId], [viewId])`

  #### Motivation for adding to Mudlet
Allows viewing multiple map areas simultaneously - useful for navigation
planning, comparing areas, or keeping a zoomed-out overview while
exploring.

Upvoted suggestion in
https://discord.com/channels/283581582550237184/792073945922142259/1402147549585866854

  #### Other info (issues closed, discussion etc)
  **Test case:**
  1. Load a profile with a map
  2. Window → New map window (or run `createMapView()`)
  3. Select different area in new window's dropdown
  4. Verify both views update when moving rooms
  5. Dock/undock the new window
  6. Close via X button or `closeMapView(id)`


https://github.com/user-attachments/assets/1cd38f37-5b3e-4ae2-83e4-ea9478b9f68c

src/T2DMap.cpp
@@ -765,6 +765,70 @@ void T2DMap::switchArea(const QString& newAreaName)
     }
 }
 
+void T2DMap::switchArea(int areaId)
+{
+    if (!mpMap || !mpMap->mpRoomDB) {
+        qWarning() << "T2DMap::switchArea(int) - cannot switch to area" << areaId << "- map or roomDB is null";
+        return;
+    }
+
+    const QString areaName = mpMap->mpRoomDB->getAreaNamesMap().value(areaId);
+    if (areaName.isEmpty()) {
+        qWarning() << "T2DMap::switchArea(int) - area" << areaId << "not found in area names map";
+        return;
+    }
+    switchArea(areaName);
+}
+
+std::pair<bool, QString> T2DMap::centerview(int roomId)
+{
+    if (!mpMap || !mpMap->mpRoomDB) {
+        return {false, qsl("map or roomDB is null")};
+    }
+
+    TRoom* pR = mpMap->mpRoomDB->getRoom(roomId);
+    if (!pR) {
+        return {false, qsl("room %1 not found").arg(roomId)};
+    }
+
+    const int areaId = pR->getArea();
+    TArea* pArea = mpMap->mpRoomDB->getArea(areaId);
+    if (!pArea) {
+        return {false, qsl("room %1 references non-existent area %2").arg(roomId).arg(areaId)};
+    }
+
+    // For secondary views, don't raise sysMapAreaChanged event
+    if (!mIsSecondaryView) {
+        TEvent areaViewedChangedEvent{};
+        if (mAreaID != areaId) {
+            areaViewedChangedEvent.mArgumentList.append(qsl("sysMapAreaChanged"));
+            areaViewedChangedEvent.mArgumentTypeList.append(ARGUMENT_TYPE_STRING);
+            areaViewedChangedEvent.mArgumentList.append(QString::number(areaId));
+            areaViewedChangedEvent.mArgumentTypeList.append(ARGUMENT_TYPE_NUMBER);
+            areaViewedChangedEvent.mArgumentList.append(QString::number(mAreaID));
+            areaViewedChangedEvent.mArgumentTypeList.append(ARGUMENT_TYPE_NUMBER);
+        }
+
+        if (!areaViewedChangedEvent.mArgumentList.isEmpty() && mpHost) {
+            mpHost->raiseEvent(areaViewedChangedEvent);
+        }
+    }
+
+    mAreaID = areaId;
+    mLastViewedAreaID = areaId;
+    mRoomID = roomId;
+    mMapCenterX = pR->x();
+    mMapCenterY = -pR->y();  // Map y coordinates are reversed
+    mMapCenterZ = pR->z();
+    xyzoom = mpMap->mpRoomDB->get2DMapZoom(areaId);
+
+    isCenterViewCall = true;
+    update();
+    isCenterViewCall = false;
+
+    return {true, QString()};
+}
+
 // key format: <QColor.name()><QString of one or more QChars>
 void T2DMap::addSymbolToPixmapCache(const QString key, const QString text, const QColor symbolColor, const bool gridMode)
 {
@@ -1627,7 +1691,8 @@ void T2DMap::paintEvent(QPaintEvent* e)
     // it at the end of the paintEvent:
     TEvent areaViewedChangedEvent{};
 
-    if ((!mPick && !mShiftMode) || mpMap->mNewMove) {
+    // Secondary views don't follow the player - they maintain their own independent view
+    if (!mIsSecondaryView && ((!mPick && !mShiftMode) || mpMap->mNewMove)) {
         mShiftMode = true;
         // that's of interest only here because the map editor is here ->
         // map might not be updated, thus I force a map update on centerview()
@@ -2949,7 +3014,7 @@ void T2DMap::updateMapLabel(QRectF labelRectangle, int labelId, TArea* pArea)
 
     if (Q_LIKELY(labelId >= 0)) {
         pArea->mMapLabels.insert(labelId, label);
-        update();
+        mpMap->updateArea(mAreaID);
         if (!label.temporary) {
             mpMap->setUnsaved(__func__);
         }
@@ -3149,13 +3214,8 @@ void T2DMap::slot_createRoom()
     mpMap->setRoomCoordinates(roomID, mContextMenuClickPosition.x, mContextMenuClickPosition.y, mMapCenterZ);
 
     mpMap->mMapGraphNeedsUpdate = true;
-#if defined(INCLUDE_3DMAPPER)
-    if (mpMap->mpM) {
-        mpMap->mpM->update();
-    }
-#endif
     isCenterViewCall = true;
-    update();
+    mpMap->updateArea(mAreaID);
     isCenterViewCall = false;
     mpMap->setUnsaved(__func__);
 }
@@ -3440,7 +3500,7 @@ void T2DMap::slot_doneCustomLine()
             room->calcRoomDimensions();
         }
     }
-    update();
+    mpMap->updateArea(mAreaID);
     mpMap->setUnsaved(__func__);
 }
 
@@ -3502,7 +3562,7 @@ void T2DMap::slot_deleteLabel()
     }
 
     if (updateNeeded) {
-        update();
+        mpMap->updateArea(mAreaID);
         if (saveNeeded) {
             mpMap->setUnsaved(__func__);
         }
@@ -3520,7 +3580,7 @@ void T2DMap::slot_setPlayerLocation()
     }
 
     const int _newRoomId = *(mMultiSelectionSet.constBegin());
-    if (mpMap->mpRoomDB->getRoom(_newRoomId)) {
+    if (auto* pR = mpMap->mpRoomDB->getRoom(_newRoomId)) {
         // No need to check it is a DIFFERENT room - that is taken care of by en/dis-abling the control
         mpMap->mRoomIdHash[mpMap->mProfileName] = _newRoomId;
         mpMap->mNewMove = true;
@@ -3530,9 +3590,7 @@ void T2DMap::slot_setPlayerLocation()
         manualSetEvent.mArgumentList.append(QString::number(_newRoomId));
         manualSetEvent.mArgumentTypeList.append(ARGUMENT_TYPE_NUMBER);
         mpHost->raiseEvent(manualSetEvent);
-        update();
-        // don't update map on player location change, as this would cause unnecessary
-        // autosaves while just speedwalking around
+        mpMap->updateArea(pR->getArea());
     }
 }
 
@@ -3912,8 +3970,7 @@ void T2DMap::slot_setRoomProperties(
     if (changeWeight || changeLockStatus) {
         mpMap->mMapGraphNeedsUpdate = true;
     }
-    repaint();
-    update();
+    mpMap->updateArea(mAreaID);
     mpMap->setUnsaved(__func__);
 }
 
@@ -4153,14 +4210,8 @@ void T2DMap::slot_newMap()
     mpMap->mNewMove = true;
     slot_toggleMapViewOnly();
 
-#if defined(INCLUDE_3DMAPPER)
-    if (mpMap->mpM) {
-        mpMap->mpM->update();
-    }
-#endif
-
     isCenterViewCall = true;
-    update();
+    mpMap->updateArea(-1);
     isCenterViewCall = false;
     mpMap->setUnsaved(__func__);
     mpMap->mpMapper->resetAreaComboBoxToPlayerRoomArea();

src/T2DMap.h
@@ -171,7 +171,21 @@ public:
     void addSymbolToPixmapCache(const QString, const QString, const QColor, const bool);
     void setPlayerRoomStyle(const int style);
     void switchArea(const QString& newAreaName);
+    void switchArea(int areaId);
     void clearSelection();
+
+    // Secondary view support (for multiple map views feature)
+    void setSecondaryView(bool isSecondary) { mIsSecondaryView = isSecondary; }
+    bool isSecondaryView() const { return mIsSecondaryView; }
+
+    // View state getters
+    int getAreaId() const { return mAreaID; }
+    int getCenterRoomId() const { return mRoomID; }
+    qreal getZoom() const { return xyzoom; }
+    int getZLevel() const { return mMapCenterZ; }
+
+    // Center view on a room. For secondary views, skips raising sysMapAreaChanged events.
+    std::pair<bool, QString> centerview(int roomId);
     std::pair<bool, QString> exportAreaToImage(int areaId, const QString& filePath, std::optional<int> zLevel = std::nullopt, qreal zoom = 2.0, bool exportAllZLevels = false);
 
 
@@ -424,6 +438,8 @@ private:
     // is initialised to - so that the xyzoom gets read for the first area that
     // is shown - because the value of these two are different:
     int mLastViewedAreaID = -2;
+
+    bool mIsSecondaryView = false;
 
 private slots:
     void slot_createRoom();

src/TMap.h
@@ -61,6 +61,7 @@ class Host;
 class QOpenGLWidget;
 #endif
 class TArea;
+class TMapViewManager;
 class TRoom;
 class TRoomDB;
 class QFile;
@@ -74,6 +75,7 @@ class TMap : public QObject
 
 signals:
     void signal_saveErrorChanged(bool hasError);
+    void signal_areaChanged(int areaId);
 
 private:
     QString mDefaultAreaName;
@@ -116,7 +118,7 @@ public:
     void logError(QString& msg);
     bool setExit(int from, int to, int dir);
     bool setRoomCoordinates(int id, int x, int y, int z);
-    void update();
+    void updateArea(int areaId);
 
     void audit();
 
@@ -202,9 +204,12 @@ public:
 
 
     TRoomDB* mpRoomDB = nullptr;
+    TMapViewManager* mpViewManager = nullptr;
     QMap<int, int> mEnvColors;
     QPointer<Host> mpHost;
     QString mProfileName;
+
+    TMapViewManager* getViewManager() { return mpViewManager; }
 
     // Was a single int mRoomId but that breaks things when maps are
     // copied/shared between profiles - so now we track the profile name

src/CMakeLists.txt
@@ -151,6 +151,8 @@ set(mudlet_SRCS
     TMainConsole.cpp
     TMap.cpp
     TMapLabel.cpp
+    TMapView.cpp
+    TMapViewManager.cpp
     TMedia.cpp
     TMediaPlaylist.cpp
     TMxpElementDefinitionHandler.cpp
@@ -356,6 +358,8 @@ set(mudlet_HDRS
     TMainConsole.h
     TMap.h
     TMapLabel.h
+    TMapView.h
+    TMapViewManager.h
     TMatchState.h
     TMedia.h
     TMediaPlaylist.h

src/Host.cpp
@@ -43,6 +43,7 @@
 #include "TLabel.h"
 #include "TMainConsole.h"
 #include "TMap.h"
+#include "TMapViewManager.h"
 #include "TMedia.h"
 #include "TRoomDB.h"
 #include "TScript.h"
@@ -3231,6 +3232,64 @@ std::pair<bool, QString> Host::setMapperTitle(const QString& title)
     }
 
     return {true, QString()};
+}
+
+std::pair<int, QString> Host::createMapView(int areaId)
+{
+    if (!mpMap) {
+        return {0, qsl("no map present or loaded")};
+    }
+
+    auto* viewManager = mpMap->getViewManager();
+    if (!viewManager) {
+        return {0, qsl("no view manager available")};
+    }
+
+    return viewManager->createView(areaId);
+}
+
+std::pair<bool, QString> Host::closeMapView(int viewId)
+{
+    if (!mpMap) {
+        return {false, qsl("no map present or loaded")};
+    }
+
+    auto* viewManager = mpMap->getViewManager();
+    if (!viewManager) {
+        return {false, qsl("no view manager available")};
+    }
+
+    return viewManager->closeView(viewId);
+}
+
+std::pair<int, QString> Host::closeAllMapViews()
+{
+    if (!mpMap) {
+        return {0, qsl("no map present or loaded")};
+    }
+
+    auto* viewManager = mpMap->getViewManager();
+    if (!viewManager) {
+        return {0, qsl("no view manager available")};
+    }
+
+    return {viewManager->closeAllViews(), QString()};
+}
+
+QList<int> Host::getMapViewIds() const
+{
+    if (!mpMap) {
+        qWarning() << "Host::getMapViewIds() - no map present or loaded";
+        return {};
+    }
+
+    auto* viewManager = mpMap->getViewManager();
+    if (!viewManager) {
+        qWarning() << "Host::getMapViewIds() - no view manager available";
+        return {};
+    }
+
+    return viewManager->getViewIds();
 }
 
 void Host::setDebugShowAllProblemCodepoints(const bool state)

src/Host.h
@@ -371,6 +371,13 @@ public:
     void setSearchOptions(const dlgTriggerEditor::SearchOptions);
     void setBufferSearchOptions(const TConsole::SearchOptions);
     std::pair<bool, QString> setMapperTitle(const QString&);
+
+    // Multiple map views support
+    std::pair<int, QString> createMapView(int areaId = 0);
+    std::pair<bool, QString> closeMapView(int viewId);
+    std::pair<int, QString> closeAllMapViews();
+    QList<int> getMapViewIds() const;
+
     void setDebugShowAllProblemCodepoints(const bool);
     bool debugShowAllProblemCodepoints() const { return mDebugShowAllProblemCodepoints; }
     void setCompactInputLine(const bool state);

commit e30ecf9a4704caa008b431599685f33261116d29

Fix: mapper API error reporting for getMapEvents and addRoom (#9473)
#### Brief overview of PR changes/additions

Fixes two mapper API error-reporting bugs:

- `getMapEvents` called before the map widget was ever opened returned
bare zero values (nothing usable). It now returns `nil` plus `"you
haven't opened a map yet"`, matching `getMapMenus` (the PR #5121
convention). The restructure also removed a latent empty-stack return.
- `addRoom`'s failure message always blamed "areaID -1" instead of the
actual bad area ID the caller passed - a regression from PR #8743. The
intentional park-in-default-area behavior (PR #7419) is unchanged; only
the message is honest again.

#### Motivation for adding to Mudlet

Both cases misled scripters: one returned no usable error at all, the
other reported the wrong area ID. Accurate errors make the mapper API
easier to debug.

#### Other info (issues closed, discussion etc)

Both fixes are pinned in `Mapper_spec` with fail-without-fix proof.

---

Tested by hand. Squash-merge with:
```
Assisted-by: Claude:claude-fable-5
Signed-off-by: Vadim Peretokin <vadim.peretokin@mudlet.org>
```


https://github.com/user-attachments/assets/607b8c36-8b0b-4b5f-a5fb-5dfac2450883

src/T2DMap.cpp
@@ -4550,8 +4550,11 @@ void T2DMap::slot_toggleMapViewOnly()
 
 void T2DMap::populateUserContextMenus(QMenu& menu)
 {
+    if (!mpMap) {
+        return;
+    }
     QMap<QString, QMenu*> userMenus;
-    QMapIterator<QString, QStringList> menuIterator(mUserMenus);
+    QMapIterator<QString, QStringList> menuIterator(mpMap->mUserMenus);
 
     while (menuIterator.hasNext()) {
         menuIterator.next();
@@ -4576,7 +4579,7 @@ void T2DMap::populateUserContextMenus(QMenu& menu)
         }
     }
 
-    QMapIterator<QString, QStringList> actionIterator(mUserActions);
+    QMapIterator<QString, QStringList> actionIterator(mpMap->mUserActions);
     while (actionIterator.hasNext()) {
         actionIterator.next();
         const QString uniqueName = actionIterator.key();
@@ -4602,8 +4605,11 @@ void T2DMap::populateUserContextMenus(QMenu& menu)
 
 void T2DMap::slot_userAction(QString uniqueName)
 {
+    if (!mpMap) {
+        return;
+    }
     TEvent event{};
-    QStringList userEvent = mUserActions[uniqueName];
+    const QStringList userEvent = mpMap->mUserActions.value(uniqueName);
     event.mArgumentList.append(userEvent[0]);
     event.mArgumentTypeList.append(ARGUMENT_TYPE_STRING);
     event.mArgumentList.append(uniqueName);

src/T2DMap.h
@@ -220,13 +220,6 @@ public:
     int mTargetRoomId = 0;
     bool mStartSpeedWalk = false;
 
-
-    // string list: 0 is event name, 1 is menu it is under if it is
-    QMap<QString, QStringList> mUserActions;
-
-    // unique name, List:parent name ("" if null), display name
-    QMap<QString, QStringList> mUserMenus;
-
     bool mRoomBeingMoved = false;
     QPointF mRoomMoveLastMapPoint;
     bool mHasRoomMoveLastMapPoint = false;

src/TLuaInterpreterMapper.cpp
@@ -531,14 +531,12 @@ int TLuaInterpreter::addMapEvent(lua_State* L)
         actionInfo << lua_tostring(L, i);
     }
     const Host& host = getHostFromLua(L);
-    if (host.mpMap) {
-        if (host.mpMap->mpMapper) {
-            if (host.mpMap->mpMapper->mp2dMap) {
-                host.mpMap->mpMapper->mp2dMap->mUserActions.insert(uniqueName, actionInfo);
-            }
-        }
+    if (!host.mpMap) {
+        return warnArgumentValue(L, __func__, "no map present or loaded");
     }
-    return 0;
+    host.mpMap->mUserActions.insert(uniqueName, actionInfo);
+    lua_pushboolean(L, true);
+    return 1;
 }
 
 // Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#addMapMenu
@@ -559,14 +557,12 @@ int TLuaInterpreter::addMapMenu(lua_State* L)
         menuList << lua_tostring(L, 3);
     }
     const Host& host = getHostFromLua(L);
-    if (host.mpMap) {
-        if (host.mpMap->mpMapper) {
-            if (host.mpMap->mpMapper->mp2dMap) {
-                host.mpMap->mpMapper->mp2dMap->mUserMenus.insert(uniqueName, menuList);
-            }
-        }
+    if (!host.mpMap) {
+        return warnArgumentValue(L, __func__, "no map present or loaded");
     }
-    return 0;
+    host.mpMap->mUserMenus.insert(uniqueName, menuList);
+    lua_pushboolean(L, true);
+    return 1;
 }
 
 // Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#addRoom
@@ -581,6 +577,7 @@ int TLuaInterpreter::addRoom(lua_State* L)
         if (lua_gettop(L) > 1) {
             areaID = getVerifiedInt(L, __func__, 2, "areaID");
         }
+        const int requestedAreaID = areaID;
         // defer area calculations as all new rooms are initialised at 0,0,0 anyway
         if (!host.mpMap->setRoomArea(id, areaID, true)) {
             // The above will fail if the areaID does not exist (given that
@@ -594,7 +591,7 @@ int TLuaInterpreter::addRoom(lua_State* L)
 
         if (issueBadAreaWarning) {
             lua_pushnil(L);
-            lua_pushfstring(L, "addRoom: created roomID %d but failed to place it in areaID %d, does that area actually exist? (Room has been placed in areaID -1 instead.)", id, areaID);
+            lua_pushfstring(L, "addRoom: created roomID %d but failed to place it in areaID %d, does that area actually exist? (Room has been placed in areaID -1 instead.)", id, requestedAreaID);
             return 2;
         }
     }
@@ -1745,38 +1742,35 @@ int TLuaInterpreter::getGridMode(lua_State* L)
 int TLuaInterpreter::getMapEvents(lua_State* L)
 {
     const Host& host = getHostFromLua(L);
-    if (host.mpMap) {
-        if (host.mpMap->mpMapper) {
-            if (host.mpMap->mpMapper->mp2dMap) {
-                // create the result table
-                lua_newtable(L);
-                QMapIterator<QString, QStringList> it(host.mpMap->mpMapper->mp2dMap->mUserActions);
-                while (it.hasNext()) {
-                    it.next();
-                    const QStringList eventInfo = it.value();
-                    lua_createtable(L, 0, 4);
-                    lua_pushstring(L, eventInfo.at(0).toUtf8().constData());
-                    lua_setfield(L, -2, "event name");
-                    lua_pushstring(L, eventInfo.at(1).toUtf8().constData());
-                    lua_setfield(L, -2, "parent");
-                    lua_pushstring(L, eventInfo.at(2).toUtf8().constData());
-                    lua_setfield(L, -2, "display name");
-                    lua_createtable(L, eventInfo.length() - 3, 0);
-                    for (int i = 3; i < eventInfo.length(); i++) {
-                        lua_pushinteger(L, i - 2); //lua indexes are 1 based!
-                        lua_pushstring(L, eventInfo.at(i).toUtf8().constData());
-                        lua_settable(L, -3);
-                    }
-                    lua_setfield(L, -2, "arguments");
-
-                    // Add the mapEvent object to the result table
-                    lua_setfield(L, -2, it.key().toUtf8().constData());
-                }
-            }
-            return 1;
-        }
+    if (!host.mpMap) {
+        return warnArgumentValue(L, __func__, "no map present or loaded");
     }
-    return 0;
+
+    // create the result table
+    lua_newtable(L);
+    QMapIterator<QString, QStringList> it(host.mpMap->mUserActions);
+    while (it.hasNext()) {
+        it.next();
+        const QStringList eventInfo = it.value();
+        lua_createtable(L, 0, 4);
+        lua_pushstring(L, eventInfo.at(0).toUtf8().constData());
+        lua_setfield(L, -2, "event name");
+        lua_pushstring(L, eventInfo.at(1).toUtf8().constData());
+        lua_setfield(L, -2, "parent");
+        lua_pushstring(L, eventInfo.at(2).toUtf8().constData());
+        lua_setfield(L, -2, "display name");
+        lua_createtable(L, eventInfo.length() - 3, 0);
+        for (int i = 3; i < eventInfo.length(); i++) {
+            lua_pushinteger(L, i - 2); //lua indexes are 1 based!
+            lua_pushstring(L, eventInfo.at(i).toUtf8().constData());
+            lua_settable(L, -3);
+        }
+        lua_setfield(L, -2, "arguments");
+
+        // Add the mapEvent object to the result table
+        lua_setfield(L, -2, it.key().toUtf8().constData());
+    }
+    return 1;
 }
 
 // Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#getMapLabel
@@ -1863,12 +1857,12 @@ int TLuaInterpreter::getMapMenus(lua_State* L)
         keyByUniqueName = getVerifiedBool(L, __func__, 1, "key by unique name", true);
     }
     const Host& host = getHostFromLua(L);
-    if (!(host.mpMap && host.mpMap->mpMapper && host.mpMap->mpMapper->mp2dMap)) {
-        return warnArgumentValue(L, __func__, "you haven't opened a map yet");
+    if (!host.mpMap) {
+        return warnArgumentValue(L, __func__, "no map present or loaded");
     }
 
     lua_newtable(L);
-    QMapIterator<QString, QStringList> it(host.mpMap->mpMapper->mp2dMap->mUserMenus);
+    QMapIterator<QString, QStringList> it(host.mpMap->mUserMenus);
     while (it.hasNext()) {
         it.next();
         const QStringList& menuInfo = it.value();
@@ -2913,14 +2907,12 @@ int TLuaInterpreter::removeMapEvent(lua_State* L)
 {
     const QString displayName = getVerifiedString(L, __func__, 1, "event name");
     const Host& host = getHostFromLua(L);
-    if (host.mpMap) {
-        if (host.mpMap->mpMapper) {
-            if (host.mpMap->mpMapper->mp2dMap) {
-                host.mpMap->mpMapper->mp2dMap->mUserActions.remove(displayName);
-            }
-        }
+    if (!host.mpMap) {
+        return warnArgumentValue(L, __func__, "no map present or loaded");
     }
-    return 0;
+    host.mpMap->mUserActions.remove(displayName);
+    lua_pushboolean(L, true);
+    return 1;
 }
 
 // Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#removeMapMenu
@@ -2928,46 +2920,44 @@ int TLuaInterpreter::removeMapMenu(lua_State* L)
 {
     const QString uniqueName = getVerifiedString(L, __func__, 1, "Menu name");
     if (uniqueName.isEmpty()) {
-        return 0;
+        return warnArgumentValue(L, __func__, "the menu name cannot be empty");
     }
     const Host& host = getHostFromLua(L);
-    if (host.mpMap) {
-        if (host.mpMap->mpMapper) {
-            if (host.mpMap->mpMapper->mp2dMap) {
-                host.mpMap->mpMapper->mp2dMap->mUserMenus.remove(uniqueName);
-                //remove all entries with this as parent
-                QStringList removeList;
-                removeList.append(uniqueName);
-                bool newElement = true;
-                while (newElement) {
-                    newElement = false;
-                    QMapIterator<QString, QStringList> it(host.mpMap->mpMapper->mp2dMap->mUserMenus);
-                    while (it.hasNext()) {
-                        it.next();
-                        QStringList menuInfo = it.value();
-                        const QString parent = menuInfo[0];
-                        if (removeList.contains(parent)) {
-                            host.mpMap->mpMapper->mp2dMap->mUserMenus.remove(it.key());
-                            if (it.key() != "" && !removeList.contains(it.key())) {
-                                host.mpMap->mpMapper->mp2dMap->mUserMenus.remove(it.key());
-                                removeList.append(it.key());
-                                newElement = true;
-                            }
-                        }
-                    }
-                }
-                QMapIterator<QString, QStringList> it2(host.mpMap->mpMapper->mp2dMap->mUserActions);
-                while (it2.hasNext()) {
-                    it2.next();
-                    const QString actParent = it2.value()[1];
-                    if (removeList.contains(actParent)) {
-                        host.mpMap->mpMapper->mp2dMap->mUserActions.remove(it2.key());
-                    }
+    if (!host.mpMap) {
+        return warnArgumentValue(L, __func__, "no map present or loaded");
+    }
+    host.mpMap->mUserMenus.remove(uniqueName);
+    //remove all entries with this as parent
+    QStringList removeList;
+    removeList.append(uniqueName);
+    bool newElement = true;
+    while (newElement) {
+        newElement = false;
+        QMapIterator<QString, QStringList> it(host.mpMap->mUserMenus);
+        while (it.hasNext()) {
+            it.next();
+            QStringList menuInfo = it.value();
+            const QString parent = menuInfo[0];
+            if (removeList.contains(parent)) {
+                host.mpMap->mUserMenus.remove(it.key());
+                if (it.key() != "" && !removeList.contains(it.key())) {
+                    host.mpMap->mUserMenus.remove(it.key());
+                    removeList.append(it.key());
+                    newElement = true;
                 }
             }
         }
     }
-    return 0;
+    QMapIterator<QString, QStringList> it2(host.mpMap->mUserActions);
+    while (it2.hasNext()) {
+        it2.next();
+        const QString actParent = it2.value()[1];
+        if (removeList.contains(actParent)) {
+            host.mpMap->mUserActions.remove(it2.key());
+        }
+    }
+    lua_pushboolean(L, true);
+    return 1;
 }
 
 // Documentation: https://wiki.mudlet.org/w/Manual:Lua_Functions#removeSpecialExit

src/TMap.h
@@ -261,6 +261,13 @@ public:
     QPointer<dlgMapper> mpMapper;
     QMap<int, int> roomidToIndex;
 
+    // User-registered mapper context menu entries (addMapEvent()/addMapMenu());
+    // session-only state, never saved with the map.
+    // string list: 0 is event name, 1 is menu it is under if it is
+    QMap<QString, QStringList> mUserActions;
+    // unique name, List:parent name ("" if null), display name
+    QMap<QString, QStringList> mUserMenus;
+
     typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, boost::no_property, boost::property<boost::edge_weight_t, cost>> mygraph_t;
     typedef boost::property_map<mygraph_t, boost::edge_weight_t>::type WeightMap;
     typedef mygraph_t::vertex_descriptor vertex;

src/mudlet-lua/tests/Mapper_spec.lua
@@ -1,3 +1,37 @@
+-- This block must stay first in the file: once a later spec calls
+-- openMapWidget(), the widget persists for the rest of the session and the
+-- pre-widget state becomes unreachable.
+describe("Tests map events and menus before the map widget is opened", function()
+  it("should return an empty table when nothing is registered yet", function()
+    assert.are.same({}, getMapEvents())
+    assert.are.same({}, getMapMenus())
+  end)
+
+  it("should register and remove a map event before the widget is opened", function()
+    assert.is_true(addMapEvent("preWidgetEvent", "myEvent", "", "Pre-widget Event"))
+
+    local events = getMapEvents()
+    assert.is_not_nil(events.preWidgetEvent)
+    assert.are.equal("myEvent", events.preWidgetEvent["event name"])
+    assert.are.equal("Pre-widget Event", events.preWidgetEvent["display name"])
+
+    assert.is_true(removeMapEvent("preWidgetEvent"))
+    assert.is_nil(getMapEvents().preWidgetEvent)
+  end)
+
+  it("should register and remove a map menu before the widget is opened", function()
+    assert.is_true(addMapMenu("PreWidgetMenu"))
+    assert.are.equal("top-level", getMapMenus()["PreWidgetMenu"])
+
+    assert.is_true(removeMapMenu("PreWidgetMenu"))
+    assert.is_nil(getMapMenus()["PreWidgetMenu"])
+  end)
+
+  it("should retain a registration for when the widget opens later", function()
+    assert.is_true(addMapEvent("preWidgetKeptEvent", "myEvent", "", "Kept Event"))
+  end)
+end)
+
 describe("Tests custom map event and menu functions", function()
 
   setup(function()
@@ -9,6 +43,16 @@ describe("Tests custom map event and menu functions", function()
     removeMapEvent("testEvent2")
     removeMapMenu("TestMenu")
     removeMapMenu("TestSubMenu")
+  end)
+
+  describe("Tests that pre-widget registrations survive opening the widget", function()
+    it("should still list an event registered before the widget was opened", function()
+      local events = getMapEvents()
+      assert.is_not_nil(events.preWidgetKeptEvent)
+      assert.are.equal("myEvent", events.preWidgetKeptEvent["event name"])
+      assert.are.equal("Kept Event", events.preWidgetKeptEvent["display name"])
+      removeMapEvent("preWidgetKeptEvent")
+    end)
   end)
 
   describe("Tests addMapEvent and getMapEvents", function()
@@ -231,6 +275,27 @@ describe("Tests per-room border functions", function()
       assert.is_true(result)
       assert.is_nil(getRoomBorderThickness(testRoomId))
     end)
+  end)
+
+end)
+
+describe("Tests addRoom", function()
+
+  it("should return true when the room is created", function()
+    local roomID = createRoomID()
+    assert.is_true(addRoom(roomID))
+    deleteRoom(roomID)
+  end)
+
+  it("should report the requested areaID when it does not exist", function()
+    local roomID = createRoomID()
+    local ok, err = addRoom(roomID, 987654)
+    assert.is_nil(ok)
+    assert.is_string(err)
+    assert.is_not_nil(err:find("areaID 987654", 1, true), err)
+    -- the room is still created, parked in the default area (-1)
+    assert.are.equal(-1, getRoomArea(roomID))
+    deleteRoom(roomID)
   end)
 
 end)