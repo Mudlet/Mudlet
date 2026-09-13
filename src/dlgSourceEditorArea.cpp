ccdd846937a0f9f6849f608206ad705d6388802e
Furox-Art <177975472+Furox-Art@users.noreply.github.com>
2026-09-13 15:54:03 +0300

    fix: include edbee TextEditorComponent header for focus proxy assignment

--- src/dlgSourceEditorArea.cpp
+++ src/dlgSourceEditorArea.cpp
@@ -26,6 +26,7 @@
 
 #include "edbee/edbee.h"
 #include "edbee/models/textdocument.h"
+#include "edbee/texteditorcomponent.h"
 #include "edbee/models/texteditorconfig.h"
 #include "edbee/models/textgrammar.h"
 #include "edbee/views/components/texteditorautocompletecomponent.h"
